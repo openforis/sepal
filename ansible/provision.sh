28cc68248a7d2bfc3ea1949d37c6d79b4be6bec1
Daniel Wiell <daniel.wiell@fao.org>
2015-09-17 17:04:28 +0200

    Updated ansible config.

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -9,4 +9,4 @@ ansible-playbook setup.yml \
 ansible-playbook configure-instances.yml \
     -i ec2.py \
     --private-key=~/.ssh/sepal/eu-central-1.pem \
-    --extra-vars "region=eu-central-1 deploy_environment=Staging"
+    --extra-vars "region=eu-central-1 deploy_environment=Staging secret_vars_file=~/.sepal/secret.yml"
