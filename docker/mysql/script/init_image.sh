4b1a49b65c7838bdb28404a455da3d273dd4b003
OttavioMonzione <ottavio.monzione@gmail.com>
2015-10-26 17:13:33 +0100

    Project structure refactoring.

--- docker/mysql/script/init_image.sh
+++ docker/mysql/script/init_image.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install -y supervisor net-tools wget
+apt-get update && apt-get install -y supervisor net-tools wget procps nano
 chmod u+x /init_container.sh
+chmod u+x /entrypoint.sh
+
 
 wget --no-check-certificate -O "flyway.tar.gz" "https://bintray.com/artifact/download/business/maven/flyway-commandline-$FLYWAY_VERSION-linux-x64.tar.gz"
 tar -zxvf "flyway.tar.gz"  -C /opt/
@@ -12,6 +14,7 @@ mv "mysql-connector-java-5.1.36.jar" "/opt/flyway/drivers"
 
 mv /sqlScripts/* /opt/flyway/sql/
 
+mkdir -p /home/mysql
 
 
 
