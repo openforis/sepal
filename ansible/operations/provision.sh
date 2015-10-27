35d25625805f5ff5a57cc709c234cb40989df660
ottavioMonzione <ottavio.monzione@gmail.com>
2015-10-27 15:39:59 +0100

    Jenkins Builds implementation

--- ansible/operations/provision.sh
+++ ansible/operations/provision.sh
@@ -2,14 +2,14 @@
 
 SKIP_CACHE=${1:-false}
 
-ansible-playbook setup.yml \
-    -i ec2.py \
+ansible-playbook ./setup.yml \
+    -i ./ec2.py \
     --private-key=~/.ssh/sepal/eu-central-1.pem \
     --extra-vars "region=eu-central-1"
 
 ./ec2.py --refresh-cache > /dev/null
 
-ansible-playbook operations.yml \
-    -i ec2.py \
+ansible-playbook ./operations.yml \
+    -i ./ec2.py \
     --private-key=~/.ssh/sepal/eu-central-1.pem \
     --extra-vars "region=eu-central-1 secret_vars_file=~/.sepal/secret.yml skip_cache=$SKIP_CACHE"
--- ansible/sepal/provision.sh
+++ ansible/sepal/provision.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-ansible-playbook setup.yml \
-    -i ec2.py \
+ansible-playbook ./setup.yml \
+    -i ./ec2.py \
     --private-key=~/.ssh/sepal/eu-west-1.pem  \
     --extra-vars "region=eu-west-1 availability_zone=eu-west-1a deploy_environment=Development"
 
 ./ec2.py --refresh-cache > /dev/null
 
-ansible-playbook configure-instances.yml \
-    -i ec2.py \
+ansible-playbook ./configure-instances.yml \
+    -i ./ec2.py \
     --private-key=~/.ssh/sepal/eu-west-1.pem \
     --extra-vars "deploy_environment=Development use_custom_host=false secret_vars_file=~/.sepal/secret.yml"
