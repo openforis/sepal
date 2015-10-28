b1644493317f223043882708d943d8086fa67dcd
ottavioMonzione <ottavio.monzione@gmail.com>
2015-10-28 10:03:08 +0100

    Jenkins Builds implementation

--- ansible/operations/provision.sh
+++ ansible/operations/provision.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 SKIP_CACHE=${1:-false}
+CONTEXT_DIR=${2:-"."}
 
-ansible-playbook ./setup.yml \
-    -i ./ec2.py \
+INVENTORY_FILE="$CONTEXT_DIR"/ec2.py
+
+ansible-playbook ${CONTEXT_DIR}/setup.yml \
+    -i ${INVENTORY_FILE} \
     --private-key=~/.ssh/sepal/eu-central-1.pem \
     --extra-vars "region=eu-central-1"
 
 ./ec2.py --refresh-cache > /dev/null
 
-ansible-playbook ./operations.yml \
-    -i ./ec2.py \
+ansible-playbook ${CONTEXT_DIR}/operations.yml \
+    -i ${INVENTORY_FILE} \
     --private-key=~/.ssh/sepal/eu-central-1.pem \
     --extra-vars "region=eu-central-1 secret_vars_file=~/.sepal/secret.yml skip_cache=$SKIP_CACHE"
--- ansible/sepal/provision.sh
+++ ansible/sepal/provision.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-ansible-playbook ./setup.yml \
-    -i ./ec2.py \
+
+CONTEXT_DIR=${1:-"."}
+
+INVENTORY_FILE="$CONTEXT_DIR"/ec2.py
+
+
+ansible-playbook ${CONTEXT_DIR}/setup.yml \
+    -i ${INVENTORY_FILE} \
     --private-key=~/.ssh/sepal/eu-west-1.pem  \
     --extra-vars "region=eu-west-1 availability_zone=eu-west-1a deploy_environment=Development"
 
 ./ec2.py --refresh-cache > /dev/null
 
-ansible-playbook ./configure-instances.yml \
-    -i ./ec2.py \
+ansible-playbook ${CONTEXT_DIR}/configure-instances.yml \
+    -i ${INVENTORY_FILE} \
     --private-key=~/.ssh/sepal/eu-west-1.pem \
     --extra-vars "deploy_environment=Development use_custom_host=false secret_vars_file=~/.sepal/secret.yml"
