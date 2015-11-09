78617988a8586c1ef3447d2de0f20981cf9597da
ottavioMonzione <ottavio.monzione@gmail.com>
2015-11-09 14:36:27 +0100

    Fixing mysql image creation

--- ansible/sepal/provision.sh
+++ ansible/sepal/provision.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-CONTEXT_DIR=${1:-"."}
+
+REGION=$1
+AV_ZONE=$2
+ENV=$3
+CONTEXT_DIR=${4:-"."}
+
 
 INVENTORY_FILE="$CONTEXT_DIR"/ec2.py
 
 
 ansible-playbook ${CONTEXT_DIR}/setup.yml \
     -i ${INVENTORY_FILE} \
-    --private-key=~/.ssh/sepal/eu-west-1.pem  \
-    --extra-vars "region=eu-west-1 availability_zone=eu-west-1a deploy_environment=Development"
+    --private-key=~/.ssh/sepal/${REGION}.pem  \
+    --extra-vars "region=eu-west-1 availability_zone=$AV_ZONE deploy_environment=$ENV"
 
 ./ec2.py --refresh-cache > /dev/null
 
 ansible-playbook ${CONTEXT_DIR}/configure-instances.yml \
     -i ${INVENTORY_FILE} \
-    --private-key=~/.ssh/sepal/eu-west-1.pem \
-    --extra-vars "local_php=false local_sepal=false deploy_environment=Development use_custom_host=false secret_vars_file=~/.sepal/secret.yml"
+    --private-key=~/.ssh/sepal/${REGION}.pem \
+    --extra-vars "local_php=false local_sepal=false deploy_environment=$ENV use_custom_host=false secret_vars_file=~/.sepal/secret.yml"
