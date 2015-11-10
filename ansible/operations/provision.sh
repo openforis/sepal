acf772cd8d4208f4a496e74a0b9de466513521e5
ottavioMonzione <ottavio.monzione@gmail.com>
2015-11-10 11:22:48 +0100

    Changes on Jenkins image

--- ansible/operations/provision.sh
+++ ansible/operations/provision.sh
@@ -2,17 +2,19 @@
 
 SKIP_CACHE=${1:-false}
 CONTEXT_DIR=${2:-"."}
+VERSION=${3:-"latest"}
+INVENTORY_FILE_NAME=${4:-"ec2.py"}
 
-INVENTORY_FILE="$CONTEXT_DIR"/ec2.py
+INVENTORY_FILE_PATH="$CONTEXT_DIR"/"$INVENTORY_FILE_NAME"
 
 ansible-playbook ${CONTEXT_DIR}/setup.yml \
-    -i ${INVENTORY_FILE} \
+    -i ${INVENTORY_FILE_PATH} \
     --private-key=~/.ssh/sepal/eu-central-1.pem \
     --extra-vars "region=eu-central-1"
 
 ./ec2.py --refresh-cache > /dev/null
 
 ansible-playbook ${CONTEXT_DIR}/operations.yml \
-    -i ${INVENTORY_FILE} \
+    -i ${INVENTORY_FILE_PATH} \
     --private-key=~/.ssh/sepal/eu-central-1.pem \
-    --extra-vars "region=eu-central-1 secret_vars_file=~/.sepal/secret.yml skip_cache=$SKIP_CACHE"
+    --extra-vars "region=eu-central-1 version=$VERSION secret_vars_file=~/.sepal/secret.yml skip_cache=$SKIP_CACHE"
