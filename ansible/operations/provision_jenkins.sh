6c957b14add902b447639565c49b63ca0bf0c28e
ottavioMonzione <ottavio.monzione@gmail.com>
2015-11-10 15:44:43 +0100

    Changes on Jenkins image

--- ansible/operations/provision_jenkins.sh
+++ ansible/operations/provision_jenkins.sh
@@ -7,9 +7,10 @@ INVENTORY_FILE_NAME=${4:-"local_inventory"}
 
 INVENTORY_FILE_PATH="$CONTEXT_DIR"/"$INVENTORY_FILE_NAME"
 
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 
 ansible-playbook ${CONTEXT_DIR}/jenkins.yml \
     -i ${INVENTORY_FILE_PATH} \
-    --private-key=~/.ssh/sepal/eu-central-1.pem
+    --private-key=~cd /.ssh/sepal/eu-central-1.pem \
     --extra-vars "version=$VERSION secret_vars_file=~/.sepal/secret.yml skip_cache=$SKIP_CACHE"
