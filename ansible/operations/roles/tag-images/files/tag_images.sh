a025f8b96f3bbe324b69194c56892c6108a0a650
ottavioMonzione <ottavio.monzione@gmail.com>
2015-11-10 17:15:58 +0100

    Changes on Jenkins building pipeline

--- ansible/operations/roles/tag-images/files/tag_images.sh
+++ ansible/operations/roles/tag-images/files/tag_images.sh
@@ -3,13 +3,13 @@
 VERSION=${1:-"latest"}
 SRC_VER=${2:-"latest"}
 
-docker tag localhost/openforis/nginx:${VERSION} localhost/openforis/nginx:${SRC_VER}
-docker tag localhost/openforis/sepal-php:${VERSION} localhost/openforis/sepal-php:${SRC_VER}
-docker tag localhost/openforis/sepal:${VERSION} localhost/openforis/sepal:${SRC_VER}
-docker tag localhost/openforis/mysql:${VERSION} localhost/openforis/mysql:${SRC_VER}
-docker tag localhost/openforis/ssh-gateway:${VERSION} localhost/openforis/ssh-gateway:${SRC_VER}
-docker tag localhost/openforis/sandbox:${VERSION} localhost/openforis/sandbox:${SRC_VER}
-docker tag localhost/openforis/gateone:${VERSION} localhost/openforis/gateone:${SRC_VER}
-docker tag localhost/openforis/geoserver:${VERSION} localhost/openforis/geoserver:${SRC_VER}
-docker tag localhost/openforis/haproxy:${VERSION} localhost/openforis/haproxy:${SRC_VER}
+docker tag localhost/openforis/nginx:${SRC_VER} localhost/openforis/nginx:${VERSION}
+docker tag localhost/openforis/sepal-php:${SRC_VER} localhost/openforis/sepal-php:${VERSION}
+docker tag localhost/openforis/sepal:${SRC_VER} localhost/openforis/sepal:${VERSION}
+docker tag localhost/openforis/mysql:${SRC_VER} localhost/openforis/mysql:${VERSION}
+docker tag localhost/openforis/ssh-gateway:${SRC_VER} localhost/openforis/ssh-gateway:${VERSION}
+docker tag localhost/openforis/sandbox:${SRC_VER} localhost/openforis/sandbox:${VERSION}
+docker tag localhost/openforis/gateone:${SRC_VER} localhost/openforis/gateone:${VERSION}
+docker tag localhost/openforis/geoserver:${SRC_VER} localhost/openforis/geoserver:${VERSION}
+docker tag localhost/openforis/haproxy:${SRC_VER} localhost/openforis/haproxy:${VERSION}
 
