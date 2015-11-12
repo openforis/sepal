bb1cdf2db216a66e7b0fd25264b21c2a6b391144
ottavioMonzione <ottavio.monzione@gmail.com>
2015-11-12 11:43:49 +0100

    ContinuousDelivery

---
+++ ansible/operations/roles/push-images/templates/push_images.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+docker login localhost
+docker push localhost/openforis/nginx:{{ version }}
+docker push localhost/openforis/sepal-php:{{ version }}
+docker push localhost/openforis/sepal:{{ version }}
+docker push localhost/openforis/mysql:{{ version }}
+docker push localhost/openforis/ssh-gateway:{{ version }}
+docker push localhost/openforis/sandbox:{{ version }}
+docker push localhost/openforis/gateone:{{ version }}
+docker push localhost/openforis/geoserver:{{ version }}
+docker push localhost/openforis/haproxy:{{ version }}
+docker logout localhost
--- ansible/sepal/roles/sandbox/templates/pull_sandbox.sh
+++ ansible/sepal/roles/sandbox/templates/pull_sandbox.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 docker login {{ docker_repository_host }}
 docker pull {{ docker_repository_host }}/openforis/sandbox:{{ version }}
 docker logout {{ docker_repository_host }}
