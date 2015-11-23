923da222f98640deccc020bb105db03655e40e17
ottavioMonzione <ottavio.monzione@gmail.com>
2015-11-23 16:10:55 +0100

    Project structure refactoring

--- ansible/operations/bin/provision.sh
+++ ansible/operations/bin/provision.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-CONTEXT_DIR=${1:-".."}
+CONTEXT_DIR=${1:-"../.."}
 VERSION=${2:-"latest"}
 INVENTORY_FILE_NAME=${3:-"inventory/ec2.py"}
 
 
 INVENTORY_FILE_PATH="$CONTEXT_DIR"/"$INVENTORY_FILE_NAME"
 
-export ANSIBLE_CONFIG=../${CONTEXT_DIR}/ansible.cfg
+export ANSIBLE_CONFIG=${CONTEXT_DIR}/ansible.cfg
 
 ansible-playbook ${CONTEXT_DIR}/setup.yml \
     -i ${INVENTORY_FILE_PATH} \
