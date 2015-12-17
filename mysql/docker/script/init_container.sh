1c7b82a74394c667ebb0f1ba355642c5afcac5c0
Daniel Wiell <daniel.wiell@fao.org>
2015-12-17 09:27:16 +0100

    Cleaned up Ansible and Docker config.

--- mysql/docker/script/init_container.sh
+++ mysql/docker/script/init_container.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-mysqlWaiting=true
-netstat -ntlp | grep ":3306"  >/dev/null 2>&1 && mysqlWaiting=false
+mkdir -p /data/db
+rm -rf /var/lib/mysql && ln -sf /data/db /var/lib/mysql
 
-for i in {50..0}; do
-    if netstat -ntlp | grep ":3306"  >/dev/null 2>&1; then
-		break
-	fi
-	echo 'Waiting for mysql...'
-	sleep 1
-done
-if [ "$i" = 0 ]; then
-    echo >&2 'MySQL init process failed.'
-	exit 1
-fi
+mkdir -p /data/logs
+rm -rf /var/log/supervisor && ln -sf /data/logs /var/log/supervisor
 
-/opt/flyway/flyway migrate -baselineVersion=${SCHEMA_BASELINE_VERSION} -baselineOnMigrate=true -url=jdbc:mysql://${INSTANCE_HOSTNAME}:3306/${MYSQL_DATABASE} -user=root -password=${MYSQL_ROOT_PASSWORD}
+cp -R /script/sqlScripts/* /opt/flyway/sql/
 
+/usr/bin/supervisord -c /config/supervisord.conf
--- mysql/docker/script/init_image.sh
+++ mysql/docker/script/init_image.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install -y supervisor net-tools wget procps nano
-chmod u+x /init_container.sh
+apt-get update -y && apt-get install -y \
+    supervisor \
+    net-tools \
+    wget \
+    procps
 
-wget --no-check-certificate -O "flyway.tar.gz" "https://bintray.com/artifact/download/business/maven/flyway-commandline-$FLYWAY_VERSION-linux-x64.tar.gz"
+wget --no-check-certificate \
+    -O "flyway.tar.gz" \
+    "https://bintray.com/artifact/download/business/maven/flyway-commandline-$FLYWAY_VERSION-linux-x64.tar.gz"
 tar -zxvf "flyway.tar.gz"  -C /opt/
 ln -s "/opt/flyway-${FLYWAY_VERSION}" "/opt/flyway"
 
 wget "http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.36/mysql-connector-java-5.1.36.jar"
 mv "mysql-connector-java-5.1.36.jar" "/opt/flyway/drivers"
-
-mv /sqlScripts/* /opt/flyway/sql/
 
 mkdir -p /home/mysql
