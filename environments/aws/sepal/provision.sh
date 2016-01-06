3bef059f57eae5f2e8777479961609176a2db747
ottavioMonzione <ottavio.monzione@gmail.com>
2016-01-06 14:23:24 +0100

    Project structure refactoring

--- environments/aws/sepal/provision.sh
+++ environments/aws/sepal/provision.sh
@@ -6,7 +6,7 @@ AV_ZONE=$2
 ENV=$3
 VERSION=${4:-latest}
 EFS_ID=$5
-CONTEXT_DIR=${6:-"../.."}
+CONTEXT_DIR=${6:-".."}
 
 INVENTORY_FILE=${7:-"ec2.py"}
 PRIVATE_KEY=${8:-"~/.ssh/sepal/$REGION.pem"}
@@ -16,7 +16,7 @@ export ANSIBLE_CONFIG=${CONTEXT_DIR}/ansible.cfg
 
 #to make the provisioning script works locally. Create a symlink from ProjectRoot to /opt/sepal
 
-INVENTORY_FILE_PATH="$CONTEXT_DIR"/sepal/inventory/"$INVENTORY_FILE"
+INVENTORY_FILE_PATH="$CONTEXT_DIR"/inventory/"$INVENTORY_FILE"
 
 echo "using inventory file $INVENTORY_FILE_PATH"
 echo "using private key $PRIVATE_KEY"
@@ -28,7 +28,7 @@ ansible-playbook ${CONTEXT_DIR}/sepal/provision.yml \
 
 ${INVENTORY_FILE_PATH} --refresh-cache > /dev/null
 
-ansible-playbook ${CONTEXT_DIR}/sepal/sepal.yml \
+ansible-playbook ${CONTEXT_DIR}/sepal/deploy.yml \
     -i ${INVENTORY_FILE_PATH} \
     --private-key=${PRIVATE_KEY} \
     --extra-vars " region=$REGION efs_id=$EFS_ID availability_zone=$AV_ZONE local_php=false local_sepal=false deploy_environment=$ENV version=$VERSION use_custom_host=false secret_vars_file=~/.sepal/secret.yml"
