92e3a11411aa114b11f29bc52401e4a3d14abb0b
Daniel Wiell <daniel.wiell@fao.org>
2016-01-07 11:04:27 +0100

    Added maven modules for environment specific sepal modules.

---
+++ environments/aws/sepal/src/main/groovy/org/openforis/sepal/aws/Main.groovy
@@ -0,0 +1,7 @@
+package org.openforis.sepal.aws
+
+class Main {
+    public static void main(String[] args) {
+        println 'AWS'
+    }
+}
---
+++ environments/vagrant/sepal/src/main/groovy/org/openforis/sepal/vagrant/Main.groovy
@@ -0,0 +1,7 @@
+package org.openforis.sepal.vagrant
+
+class Main {
+    public static void main(String[] args) {
+        println 'Vagrant'
+    }
+}
