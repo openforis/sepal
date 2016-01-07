91dbb9bbb4c542c04fd424362334e91262b09df3
ottavioMonzione <ottavio.monzione@gmail.com>
2016-01-07 09:31:33 +0100

    Project structure refactoring

--- environments/common/operations/build-and-push-images.sh
+++ environments/common/operations/build-and-push-images.sh
@@ -2,14 +2,13 @@
 
 VERSION=${1:-"latest"}
 CONTEXT_DIR=$2
-INVENTORY_FILE_NAME=${3:-"local_inventory"}
 
-INVENTORY_FILE_PATH="$CONTEXT_DIR"/environments/aws/operations/"$INVENTORY_FILE_NAME"
+INVENTORY_FILE_PATH="$CONTEXT_DIR"/environments/common/operations/local_inventory
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_CONFIG=${CONTEXT_DIR}/ansible.cfg
 
-ansible-playbook ${CONTEXT_DIR}/environments/aws/operations/build-and-push-images.yml \
+ansible-playbook ${CONTEXT_DIR}/environments/common/operations/build-and-push-images.yml \
     -i ${INVENTORY_FILE_PATH} \
     --private-key=~/.ssh/sepal/eu-central-1.pem \
     --extra-vars "version=$VERSION secret_vars_file=~/.sepal/secret.yml context"
