521dd5d55f40f8c506057ba2920efd2933e7ddd1
ottavioMonzione <ottavio.monzione@gmail.com>
2016-01-12 17:01:43 +0100

    ReIncluded swiftmailer classes

--- environments/aws/sepal/deploy.sh
+++ environments/aws/sepal/deploy.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 REGION=$1
-EFS_ID=$2
-AV_ZONE=$3
-ENV=$4
-VERSION=${5:-latest}
+AV_ZONE=$2
+ENV=$3
+VERSION=${4:-latest}
+EFS_ID=$5
 CONTEXT_DIR=${6:-".."}
 PRIVATE_KEY=${7:-"~/.ssh/sepal/$REGION.pem"}
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_CONFIG=${CONTEXT_DIR}/ansible.cfg
+
+../inventory/ec2.py --refresh-cache > /dev/null
 
 ansible-playbook deploy.yml \
     -i ../inventory/ec2.py \
