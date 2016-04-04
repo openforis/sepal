15fd4927fc13819d3a913369a9e4db486586d107
Daniel Wiell <daniel.wiell@fao.org>
2016-04-04 11:23:09 +0200

    Cleaned up GateOne javascript.

--- modules/sepal-php/www/app/views/terminal/gateoneTerminal.blade.php
+++ modules/sepal-php/www/app/views/terminal/gateoneTerminal.blade.php
@@ -2,22 +2,13 @@
 <html lang="en" class="no-js">
 <!--<![endif]-->
 <head>
-    {{ HTML::style( asset('css/bootstrap.css') ) }}
-    {{ HTML::style( asset('css/common.css') ) }}
     {{ HTML::style( asset('css/gateone.css') ) }}
-    {{ HTML::script('js/jquery.js'); }} {{ HTML::script('js/gateone.js'); }}
-    {{ HTML::script('js/script.js'); }}
-    {{ HTML::script('js/bootstrap.js'); }}
-    {{ HTML::script('js/lodash.js'); }}
-    {{ HTML::script('js/jquery.dateFormat.min.js'); }}
+    {{ HTML::script('js/jquery.js'); }}
+    {{ HTML::script('js/gateone.js'); }}
     <title>SEPAL - Terminal</title>
     <style>
-        A.resetTerminal {
-            color: white;
-        }
-
-        A.resetTerminal:hover {
-            text-decoration: underline;
+        #go_default_noticecontainer {
+            display: none !important;
         }
     </style>
     <script type="text/javascript">
@@ -28,39 +19,36 @@
                         var response = data;
                         var terminalUri = "{{SdmsConfig::value('hostSSH')}}";
                         var authObject = response.authObject;
-                        GateOne.prefs.autoConnectURL = 'ssh://{{Session::get("username")}}@' + terminalUri + '?identities=id_rsa';
-                        GateOne.prefs.disableTermTransitions = true;
-                        GateOne.prefs.showToolbar = false;
-                        GateOne.prefs.showTitle = false;
-                        GateOne.prefs.skipChecks = true;
-                        GateOne.noSavePrefs = {
-                            url: null,
-                            webWorker: null,
-                            fillContainer: null,
-                            style: null,
-                            goDiv: null,
-                            prefix: null,
-                            autoConnectURL: null,
-                            embedded: null,
-                            auth: null,
-                            showTitle: null,
-                            showToolbar: null,
-                            rowAdjust: null,
-                            colAdjust: null
+                        var registeredCloseCallback = false
+
+                        function initTerminal() {
+                            var terminalCount = GateOne.Terminal.terminals.count();
+                            for (var i = 0; i < terminalCount; i++)
+                                GateOne.Terminal.closeTerminal(i);
+                            GateOne.Terminal.newTerminal()
                         }
+
+                        GateOne.Events.on("go:js_loaded", function () {
+                            if (!registeredCloseCallback) {
+                                GateOne.Terminal.closeTermCallbacks.push(function () {
+                                    initTerminal();
+                                })
+                                registeredCloseCallback = true
+                            }
+
+                            initTerminal();
+                        });
                         GateOne.Utils.deleteCookie('gateone_user', '/', ''); // Deletes the 'gateone_user' cookie
-                        var doInit = function () {
-                            GateOne.Base.superSandbox("NewExternalTerm", ["GateOne.Terminal", "GateOne.Terminal.Input"], function (window, undefined) {
-                                "use strict";
-                            });
-                        };
-                        GateOne.init({auth: authObject, embedded: true, url: "{{SdmsConfig::value('gateOneURI')}}", style: {}}, doInit);
-                        GateOne.Events.on("go:js_loaded", function() {
-                            GateOne.Terminal.closeTermCallbacks.push(function (term) {
-                                GateOne.Terminal.newTerminal()
-                            })
-                            if (!GateOne.Terminal.terminals.length)
-                                GateOne.Terminal.newTerminal()
+                        GateOne.init({
+                            audibleBell: false,
+                            auth: authObject,
+                            autoConnectURL: 'ssh://{{Session::get("username")}}@' + terminalUri + '?identities=id_rsa',
+                            disableTermTransitions: true,
+                            embedded: true,
+                            url: "{{SdmsConfig::value('gateOneURI')}}",
+                            showTitle: false,
+                            showToolbar: false,
+                            style: {}
                         });
                     }
                 );
