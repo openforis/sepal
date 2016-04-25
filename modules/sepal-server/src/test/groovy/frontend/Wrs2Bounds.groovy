154e56c6bde06afd58aac2fd64517aff1d736248
Daniel Wiell <daniel.wiell@fao.org>
2016-04-25 14:51:52 +0200

    Updated scene areas API.

--- modules/sepal-server/src/test/groovy/frontend/Wrs2Bounds.groovy
+++ modules/sepal-server/src/test/groovy/frontend/Wrs2Bounds.groovy
@@ -8,18 +8,15 @@ class Wrs2Bounds {
         def kml = new XmlSlurper().parse(input)
         def pathRows = [:]
         kml.Document.Placemark.each { placemark ->
-            def coordinates = placemark.Polygon.outerBoundaryIs.LinearRing.coordinates.text().trim()
+            def polygon = placemark.Polygon.outerBoundaryIs.LinearRing.coordinates.text().trim()
                     .split(' ')
                     .collect { String s ->
-                (s.split(',') as List).subList(0, 2)
-            }.subList(0, 4) as List<List>
+                (s.split(',') as List).subList(0, 2).reverse().collect { it as double }
+            } as List<List>
             def sceneAreaId = placemark.name.text()
             pathRows[sceneAreaId] = [
-                    sceneAreaId         : sceneAreaId,
-                    lowerLeftCoordinate : [coordinates[3][1], coordinates[3][0]],
-                    upperLeftCoordinate : [coordinates[0][1], coordinates[0][0]],
-                    upperRightCoordinate: [coordinates[1][1], coordinates[1][0]],
-                    lowerRightCoordinate: [coordinates[2][1], coordinates[2][0]],
+                    sceneAreaId: sceneAreaId,
+                    polygon: polygon
             ]
         }
         this.pathRows = pathRows
