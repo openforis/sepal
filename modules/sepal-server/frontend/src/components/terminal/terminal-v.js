370fab2d4464903d596a9fdece59b55ee0549abb
Daniel Wiell <daniel.wiell@fao.org>
2016-05-20 13:30:21 +0200

    Made sure terminal scrollback is cleared before initializing the terminal.

--- modules/sepal-server/frontend/src/components/terminal/terminal-v.js
+++ modules/sepal-server/frontend/src/components/terminal/terminal-v.js
@@ -18,34 +18,44 @@ var init = function () {
     var appSection = $( '#app-section' ).find( '.terminal' )
     if ( appSection.children().length <= 0 ) {
         appSection.append( html )
-
+        
         var params = {
             url      : '/api/gateone/auth-object'
             , success: initTerminal
         }
         EventBus.dispatch( Events.AJAX.REQUEST, null, params )
-
+        
     }
 }
 
-var initTerminal = function ( response ) {
-    var terminalUri = "ssh-gateway"
-    var authObject  = response.authObject
-    GateOne.Utils.deleteCookie( 'gateone_user', '/', '' ); // Deletes the 'gateone_user' cookie
-    GateOne.Events.on( "go:js_loaded", function () {
-        if ( GateOne.Terminal.closeTermCallbacks.length == 0 )
-            GateOne.Terminal.closeTermCallbacks.push( function () {
-                GateOne.Terminal.newTerminal()
-            } )
+function initTerminal( response ) {
+    purgeUserPrefs()
+    onTerminalInitialized( function () {
+        GateOne.Storage.clearDatabase('terminal')
+        onTerminalClosed( function () { GateOne.Terminal.newTerminal() } )
         GateOne.Terminal.newTerminal()
     } )
     GateOne.init( {
         url     : 'https://' + window.location.host + '/gateone',
         // url           : 'https://172.28.128.3/gateone',
-        autoConnectURL: 'ssh://' + Sepal.User.username + '@' + terminalUri + '?identities=id_rsa',
-        auth          : authObject,
-        embedded      : true,
+        autoConnectURL: 'ssh://' + Sepal.User.username + '@ssh-gateway?identities=id_rsa',
+        auth          : response.authObject,
+        embedded      : true
     } )
+}
+
+function onTerminalClosed( callback ) {
+    if ( GateOne.Terminal.closeTermCallbacks.length == 0 )
+        GateOne.Terminal.closeTermCallbacks.push( callback )
+}
+
+function onTerminalInitialized( callback ) {
+    GateOne.Events.on( "go:js_loaded", callback )
+}
+
+function purgeUserPrefs() {
+    if ( typeof(Storage) !== "undefined" )
+        window.localStorage.removeItem( 'go_default_prefs' )
 }
 
 module.exports = {
