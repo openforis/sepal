23377a00bdf23ce42bcbbd59d32e9319a4f5e9b5
Daniel Wiell <daniel.wiell@fao.org>
2016-05-25 11:46:41 +0200

    Tested failures scenarios.

--- modules/sepal-server/src/main/groovy/org/openforis/sepal/component/task/event/InstanceLaunchFailed.groovy
+++ modules/sepal-server/src/main/groovy/org/openforis/sepal/component/task/event/InstanceLaunchFailed.groovy
@@ -3,7 +3,7 @@ package org.openforis.sepal.component.task.event
 import groovy.transform.Immutable
 import org.openforis.sepal.event.Event
 
-@Immutable
+@Immutable(knownImmutableClasses = [Exception])
 class InstanceLaunchFailed implements Event {
     String instanceType
     Exception exception
