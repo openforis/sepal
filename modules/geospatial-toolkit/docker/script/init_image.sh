2e309cc1e596de035f7cb98d0a18e3e14b574b2c
Daniel Wiell <daniel.wiell@fao.org>
2016-06-23 15:53:05 +0200

    Set LD_LIBRARY_PATH when initializing geospatial toolkit image.

--- modules/geospatial-toolkit/docker/script/init_image.sh
+++ modules/geospatial-toolkit/docker/script/init_image.sh
@@ -108,6 +108,7 @@ echo "*****************************************"
 echo "*** Installing R and related packages ***"
 echo "*****************************************"
 /opt/miniconda3/bin/conda install -y -c r r
+/opt/miniconda3/bin/conda install krb5
 /opt/miniconda3/bin/conda install -y -c r r-essentials
 /opt/miniconda3/bin/conda install -y -c r r-rcpp
 
@@ -127,6 +128,8 @@ apt-get install -y \
 export JAVA_CPPFLAGS="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"
 export JAVA_LD_LIBRARY_PATH=${JAVA_HOME}/jre/lib/amd64/server:${JAVA_HOME}/jre/lib/amd64
 /opt/miniconda3/bin/R CMD javareconf
+
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/libfakeroot:/usr/local/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/mesa:/usr/lib:x86_64-linux-gnu/mir/clientplatform/mesa:/lib32:/usr/lib32:/opt/miniconda3/lib
 
 /opt/miniconda3/bin/R -e "install.packages('dismo', dependencies=TRUE, repos='http://cran.rstudio.com/')"
 /opt/miniconda3/bin/R -e "install.packages('DT', dependencies=TRUE, repos='http://cran.rstudio.com/')"
