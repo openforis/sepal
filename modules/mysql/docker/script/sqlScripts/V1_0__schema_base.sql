1d985c48cdd389db682d481db6b9d4ea3a372388
Daniel Wiell <daniel.wiell@fao.org>
2016-07-27 16:21:01 +0200

    Removed unused database tables.

--- modules/mysql/docker/script/sqlScripts/V1_0__schema_base.sql
+++ modules/mysql/docker/script/sqlScripts/V1_0__schema_base.sql
@@ -1,30 +1,3 @@
-CREATE TABLE admin_groups (
-  id         INT(10)      NOT NULL,
-  user_id    INT(10)      NOT NULL,
-  group_id   VARCHAR(100) NOT NULL,
-  created_by INT(10)      NOT NULL,
-  created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-);
-
-CREATE INDEX idx_admin_groups_1
-  ON admin_groups (user_id);
-
-CREATE TABLE config_details (
-  name  VARCHAR(50) NOT NULL,
-  value VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE data_set (
-  id                INT(11)     NOT NULL AUTO_INCREMENT,
-  dataset_name      VARCHAR(50) NOT NULL,
-  dataset_value     VARCHAR(50) NOT NULL,
-  dataset_active    TINYINT(1)  NOT NULL,
-  metadata_provider INT(11)     NOT NULL DEFAULT 1,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE groups (
   id         INT(11)     NOT NULL AUTO_INCREMENT,
   group_name VARCHAR(50) NOT NULL,
@@ -42,19 +15,9 @@ CREATE TABLE groups_system (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE image_log (
-  id            INT(11)      NOT NULL AUTO_INCREMENT,
-  name          VARCHAR(256) NOT NULL,
-  downloaded_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  last_accessed DATETIME,
-  accessed_by   VARCHAR(50),
-  deleted       INT(11)      NOT NULL DEFAULT 0,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE restricted_group (
   id   INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100),
+  name VARCHAR(100)     DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
@@ -67,24 +30,15 @@ CREATE TABLE roles (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE temp_download_log (
-  id         BIGINT(20)   NOT NULL AUTO_INCREMENT,
-  username   VARCHAR(200) NOT NULL,
-  filepath   VARCHAR(200) NOT NULL,
-  created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE users (
   id             INT(11)     NOT NULL AUTO_INCREMENT,
   username       VARCHAR(50) NOT NULL,
-  full_name      VARCHAR(100),
-  email          VARCHAR(50),
-  remember_token VARCHAR(256),
+  full_name      VARCHAR(100)         DEFAULT NULL,
+  email          VARCHAR(50)          DEFAULT NULL,
+  remember_token VARCHAR(256)         DEFAULT NULL,
   permissions    TEXT,
   created_at     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at     TIMESTAMP   NULL,
+  updated_at     TIMESTAMP   NULL     DEFAULT NULL,
   user_uid       INTEGER     NOT NULL DEFAULT 0,
   is_system_user TINYINT(4),
   PRIMARY KEY (id)
@@ -111,125 +65,6 @@ CREATE TABLE users_roles (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE usgs_data_repo (
-  id                        INT(50) NOT NULL AUTO_INCREMENT,
-  dataset_id                INT(50) NOT NULL,
-  browseAvailable           VARCHAR(50),
-  browseURL                 VARCHAR(256),
-  sceneID                   VARCHAR(256),
-  sensor                    VARCHAR(50),
-  acquisitionDate           DATE,
-  dateUpdated               DATE,
-  path                      INT(11) NOT NULL,
-  row                       INT(11) NOT NULL,
-  geometry                  GEOMETRY,
-  upperLeftCornerLatitude   DECIMAL(15, 12),
-  upperLeftCornerLongitude  DECIMAL(15, 12),
-  upperRightCornerLatitude  DECIMAL(15, 12),
-  upperRightCornerLongitude DECIMAL(15, 12),
-  lowerLeftCornerLatitude   DECIMAL(15, 12),
-  lowerLeftCornerLongitude  DECIMAL(15, 12),
-  lowerRightCornerLatitude  DECIMAL(15, 12),
-  lowerRightCornerLongitude DECIMAL(15, 12),
-  sceneCenterLatitude       DECIMAL(15, 12),
-  sceneCenterLongitude      DECIMAL(15, 12),
-  cloudCover                INT(11),
-  cloudCoverFull            FLOAT,
-  dayOrNight                VARCHAR(50),
-  sunElevation              VARCHAR(256),
-  sunAzimuth                VARCHAR(256),
-  receivingStation          VARCHAR(256),
-  sceneStartTime            DATETIME,
-  sceneStopTime             DATETIME,
-  imageQuality1             INT(11),
-  DATA_TYPE_L1              VARCHAR(256),
-  cartURL                   VARCHAR(256),
-  GEOMETRIC_RMSE_MODEL_X    VARCHAR(256),
-  GEOMETRIC_RMSE_MODEL_Y    VARCHAR(256),
-  FULL_PARTIAL_SCENE        VARCHAR(256),
-  tiffavailability          VARCHAR(256),
-  tiffverification          VARCHAR(256),
-  PRIMARY KEY (id)
-);
-
-CREATE INDEX idx_usgs_data_repo_1
-  ON usgs_data_repo (dataset_id);
-CREATE INDEX idx_usgs_data_repo_2
-  ON usgs_data_repo (sceneID);
-CREATE INDEX idx_usgs_data_repo_3
-  ON usgs_data_repo (sensor);
-CREATE INDEX idx_usgs_data_repo_4
-  ON usgs_data_repo (path);
-CREATE INDEX idx_usgs_data_repo_5
-  ON usgs_data_repo (row);
-CREATE INDEX idx_usgs_data_repo_6
-  ON usgs_data_repo (cloudCoverFull);
-CREATE INDEX idx_usgs_data_repo_7
-  ON usgs_data_repo (dataset_id, acquisitionDate);
-
-CREATE TABLE wrs_points (
-  id                  INT(11)         NOT NULL AUTO_INCREMENT,
-  path                INT(11)         NOT NULL,
-  row                 INT(11)         NOT NULL,
-  centreLongitude     DECIMAL(15, 12) NOT NULL,
-  centreLatitude      DECIMAL(15, 12) NOT NULL,
-  upperLeftLongitude  DECIMAL(15, 12) NOT NULL,
-  upperLeftLatitude   DECIMAL(15, 12) NOT NULL,
-  lowerLeftLongitude  DECIMAL(15, 12) NOT NULL,
-  lowerLeftLatitude   DECIMAL(15, 12) NOT NULL,
-  lowerRightLongitude DECIMAL(15, 12) NOT NULL,
-  lowerRightLatitude  DECIMAL(15, 12) NOT NULL,
-  upperRightLongitude DECIMAL(15, 12) NOT NULL,
-  upperRightLatitude  DECIMAL(15, 12) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-
-CREATE TABLE download_requests (
-  request_id     INT(11)      NOT NULL AUTO_INCREMENT,
-  request_time   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  username       VARCHAR(255) NOT NULL,
-  group_scenes   INT(1)       NOT NULL DEFAULT 0,
-  request_name   VARCHAR(255),
-  request_status VARCHAR(255) NOT NULL DEFAULT 'REQUESTED',
-  PRIMARY KEY (request_id)
-);
-
-ALTER TABLE download_requests
-  ADD CONSTRAINT uc_user_req_name UNIQUE (username, request_name);
-
-
-CREATE TABLE requested_scenes (
-  id               INT(11)      NOT NULL AUTO_INCREMENT,
-  request_id       INT(11)      NOT NULL,
-  scene_id         VARCHAR(255) NOT NULL,
-  dataset_id       INT(11)      NOT NULL,
-  last_updated     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status           VARCHAR(255) NOT NULL DEFAULT 'REQUESTED',
-  processing_chain VARCHAR(255),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE metadata_providers (
-  id                   INT(11) UNSIGNED NOT NULL,
-  name                 VARCHAR(45)      NOT NULL,
-  active               TINYINT(1)       NOT NULL DEFAULT '1',
-  crawling_entrypoint  VARCHAR(255)     NOT NULL,
-  iterations           INT(10)                   DEFAULT 4,
-  iteration_size       INT(10)                   DEFAULT 18,
-  last_execution_start DATETIME,
-  last_execution_end   DATETIME,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE metadata_crawling_criteria (
-  criteria_id          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  metadata_provider_id INT(10) UNSIGNED NOT NULL,
-  field_name           VARCHAR(255)     NOT NULL,
-  expected_value       VARCHAR(255)     NOT NULL,
-  PRIMARY KEY (criteria_id)
-);
-
 CREATE TABLE sandbox_session (
   id            INT          NOT NULL AUTO_INCREMENT,
   username      VARCHAR(255) NOT NULL,
@@ -243,13 +78,6 @@ CREATE TABLE sandbox_session (
   PRIMARY KEY (id)
 );
 
-
-CREATE TABLE default_user_budget (
-  monthly_instance INT NOT NULL,
-  monthly_storage  INT NOT NULL,
-  storage_quota    INT NOT NULL
-);
-
 CREATE TABLE user_budget (
   username         VARCHAR(255) NOT NULL,
   monthly_instance INT          NOT NULL,
@@ -257,6 +85,14 @@ CREATE TABLE user_budget (
   storage_quota    INT          NOT NULL,
   PRIMARY KEY (username)
 );
+
+CREATE TABLE default_user_budget (
+  monthly_instance INT NOT NULL,
+  monthly_storage  INT NOT NULL,
+  storage_quota    INT NOT NULL
+);
+
+INSERT INTO default_user_budget (monthly_instance, monthly_storage, storage_quota) VALUES (111, 222, 333);
 
 CREATE TABLE user_monthly_storage (
   username     VARCHAR(255) NOT NULL,
@@ -285,8 +121,11 @@ CREATE TABLE scene_meta_data (
   PRIMARY KEY (id)
 );
 
-CREATE INDEX idx_scene_meta_data_1 ON scene_meta_data (scene_area_id, acquisition_date);
-CREATE INDEX idx_scene_meta_data_2 ON scene_meta_data (meta_data_source, sensor_id, update_time);
+CREATE INDEX idx_scene_meta_data_1
+  ON scene_meta_data (scene_area_id, acquisition_date);
+CREATE INDEX idx_scene_meta_data_2
+  ON scene_meta_data (meta_data_source, sensor_id, update_time);
+
 
 CREATE TABLE worker_session (
   id            VARCHAR(255) NOT NULL,
@@ -324,24 +163,6 @@ CREATE VIEW instance_use AS
     update_time
   FROM worker_session;
 
-
-INSERT INTO config_details VALUES ('cron_delay_days', '50');
-
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active) VALUES ('Landsat 8 OLI/TIRS', 'LANDSAT_8', 1);
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active)
-VALUES ('Landsat 7 SLC-off (2003 ->)', 'LANDSAT_ETM_SLC_OFF', 1);
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active)
-VALUES ('Landsat 7 SLC-on (1999-2003)', 'LANDSAT_ETM', 1);
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active) VALUES ('Landsat 4-5 TM', 'LANDSAT_TM', 1);
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active) VALUES ('Landsat 4-5 MSS', 'LANDSAT_MSS', 1);
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active) VALUES ('Landsat 1-3 MSS', 'LANDSAT_MSS1', 0);
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active)
-VALUES ('Landsat 4-8 Combined', 'LANDSAT_COMBINED', 1);
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active)
-VALUES ('Landsat 7/8 Combined', 'LANDSAT_COMBINED78', 1);
-INSERT INTO data_set (dataset_name, dataset_value, dataset_active)
-VALUES ('Planet Labs Scenes', 'PLANET_LAB_SCENES', 1);
-
 INSERT INTO groups_system (id, group_name) VALUES (46, 'admin');
 
 INSERT INTO roles (id, role_name, role_desc) VALUES (1, 'application_admin', 'Application Administrator');
@@ -355,11 +176,3 @@ INSERT INTO users_roles (user_id, role_id, created_by) VALUES (1, 1, 2);
 INSERT INTO users_roles (user_id, role_id, created_by) VALUES (2, 2, 2);
 INSERT INTO user_budget (username, monthly_instance, monthly_storage, storage_quota) VALUES ('admin', 10, 10, 100);
 INSERT INTO default_user_budget (monthly_instance, monthly_storage, storage_quota) VALUES (10, 10, 100);
-
-INSERT INTO metadata_providers
-VALUES (1, 'EarthExplorer', 1, 'http://earthexplorer.usgs.gov/EE/InventoryStream/pathrow', 150, 10, NULL, NULL);
-INSERT INTO metadata_providers VALUES (2, 'PlanetLabs', 0, '', 0, 0, NULL, NULL);
-
-INSERT INTO metadata_crawling_criteria (metadata_provider_id, field_name, expected_value)
-VALUES (1, 'DATA_TYPE_L1', 'L1T');
-
--- modules/sepal-server/src/test/resources/db/schema.sql
+++ modules/sepal-server/src/test/resources/db/schema.sql
@@ -1,61 +1,20 @@
 DROP VIEW IF EXISTS instance_use;
 
-DROP TABLE IF EXISTS admin_groups;
-DROP TABLE IF EXISTS config_details;
-DROP TABLE IF EXISTS data_set;
 DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS groups_system;
-DROP TABLE IF EXISTS image_log;
 DROP TABLE IF EXISTS restricted_group;
 DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS temp_download_log;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS users_groups;
 DROP TABLE IF EXISTS users_roles;
-DROP TABLE IF EXISTS usgs_data_repo;
 DROP TABLE IF EXISTS wrs_points;
-DROP TABLE IF EXISTS download_requests;
-DROP TABLE IF EXISTS requested_scenes;
-DROP TABLE IF EXISTS metadata_providers;
-DROP TABLE IF EXISTS metadata_crawling_criteria;
-DROP TABLE IF EXISTS instance_providers;
-DROP TABLE IF EXISTS datacenters;
-DROP TABLE IF EXISTS instances;
 DROP TABLE IF EXISTS sandbox_session;
 DROP TABLE IF EXISTS user_budget;
 DROP TABLE IF EXISTS default_user_budget;
 DROP TABLE IF EXISTS user_monthly_storage;
 DROP TABLE IF EXISTS scene_meta_data;
-
 DROP TABLE IF EXISTS worker_session;
 DROP TABLE IF EXISTS task;
-
-
-CREATE TABLE admin_groups (
-  id         INT(10)      NOT NULL,
-  user_id    INT(10)      NOT NULL,
-  group_id   VARCHAR(100) NOT NULL,
-  created_by INT(10)      NOT NULL,
-  created_at TIMESTAMP    NOT NULL DEFAULT '0000-01-01 00:00:00',
-  updated_at TIMESTAMP    NOT NULL DEFAULT '0000-01-01 00:00:00',
-  PRIMARY KEY (id)
-);
-
-CREATE INDEX idx_admin_groups_1 ON admin_groups (user_id);
-
-CREATE TABLE config_details (
-  name  VARCHAR(50) NOT NULL,
-  value VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE data_set (
-  id                INT(11)     NOT NULL AUTO_INCREMENT,
-  dataset_name      VARCHAR(50) NOT NULL,
-  dataset_value     VARCHAR(50) NOT NULL,
-  dataset_active    TINYINT(1)  NOT NULL,
-  metadata_provider INT(11)     NOT NULL DEFAULT 1,
-  PRIMARY KEY (id)
-);
 
 CREATE TABLE groups (
   id         INT(11)     NOT NULL AUTO_INCREMENT,
@@ -74,16 +33,6 @@ CREATE TABLE groups_system (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE image_log (
-  id            INT(11)      NOT NULL AUTO_INCREMENT,
-  name          VARCHAR(256) NOT NULL,
-  downloaded_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  last_accessed DATETIME,
-  accessed_by   VARCHAR(50),
-  deleted       INT(11)      NOT NULL DEFAULT 0,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE restricted_group (
   id   INT(11) NOT NULL AUTO_INCREMENT,
   name VARCHAR(100)     DEFAULT NULL,
@@ -96,15 +45,6 @@ CREATE TABLE roles (
   role_desc  TEXT        NOT NULL,
   created_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP   NOT NULL DEFAULT '0000-01-01 00:00:00',
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE temp_download_log (
-  id         BIGINT(20)   NOT NULL AUTO_INCREMENT,
-  username   VARCHAR(200) NOT NULL,
-  filepath   VARCHAR(200) NOT NULL,
-  created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP    NOT NULL DEFAULT '0000-01-01 00:00:00',
   PRIMARY KEY (id)
 );
 
@@ -140,150 +80,6 @@ CREATE TABLE users_roles (
   created_by INT(11)   NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT '0000-01-01 00:00:00',
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE usgs_data_repo (
-  id                        INT(50) NOT NULL AUTO_INCREMENT,
-  dataset_id                INT(50) NOT NULL,
-  browseavailable           VARCHAR(50)      DEFAULT NULL,
-  browseurl                 VARCHAR(256)     DEFAULT NULL,
-  sceneid                   VARCHAR(256)     DEFAULT NULL,
-  sensor                    VARCHAR(50)      DEFAULT NULL,
-  acquisitiondate           DATE             DEFAULT NULL,
-  dateupdated               DATE             DEFAULT NULL,
-  path                      INT(11) NOT NULL,
-  row                       INT(11) NOT NULL,
-  geometry                  GEOMETRY         DEFAULT NULL,
-  upperleftcornerlatitude   DECIMAL(15, 12)  DEFAULT NULL,
-  upperleftcornerlongitude  DECIMAL(15, 12)  DEFAULT NULL,
-  upperrightcornerlatitude  DECIMAL(15, 12)  DEFAULT NULL,
-  upperrightcornerlongitude DECIMAL(15, 12)  DEFAULT NULL,
-  lowerleftcornerlatitude   DECIMAL(15, 12)  DEFAULT NULL,
-  lowerleftcornerlongitude  DECIMAL(15, 12)  DEFAULT NULL,
-  lowerrightcornerlatitude  DECIMAL(15, 12)  DEFAULT NULL,
-  lowerrightcornerlongitude DECIMAL(15, 12)  DEFAULT NULL,
-  scenecenterlatitude       DECIMAL(15, 12)  DEFAULT NULL,
-  scenecenterlongitude      DECIMAL(15, 12)  DEFAULT NULL,
-  cloudcover                INT(11)          DEFAULT NULL,
-  cloudcoverfull            FLOAT            DEFAULT NULL,
-  dayornight                VARCHAR(50)      DEFAULT NULL,
-  sunelevation              VARCHAR(256)     DEFAULT NULL,
-  sunazimuth                VARCHAR(256)     DEFAULT NULL,
-  receivingstation          VARCHAR(256)     DEFAULT NULL,
-  scenestarttime            DATETIME         DEFAULT NULL,
-  scenestoptime             DATETIME         DEFAULT NULL,
-  imagequality1             INT(11)          DEFAULT NULL,
-  data_type_l1              VARCHAR(256)     DEFAULT NULL,
-  carturl                   VARCHAR(256)     DEFAULT NULL,
-  geometric_rmse_model_x    VARCHAR(256)     DEFAULT NULL,
-  geometric_rmse_model_y    VARCHAR(256)     DEFAULT NULL,
-  full_partial_scene        VARCHAR(256)     DEFAULT NULL,
-  tiffavailability          VARCHAR(256)     DEFAULT NULL,
-  tiffverification          VARCHAR(256)     DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE INDEX idx_usgs_data_repo_1 ON usgs_data_repo (dataset_id);
-CREATE INDEX idx_usgs_data_repo_2 ON usgs_data_repo (sceneid);
-CREATE INDEX idx_usgs_data_repo_3 ON usgs_data_repo (sensor);
-CREATE INDEX idx_usgs_data_repo_4 ON usgs_data_repo (path);
-CREATE INDEX idx_usgs_data_repo_5 ON usgs_data_repo (row);
-CREATE INDEX idx_usgs_data_repo_6 ON usgs_data_repo (cloudcoverfull);
-CREATE INDEX idx_usgs_data_repo_7 ON usgs_data_repo (dataset_id, acquisitiondate);
-
-CREATE TABLE wrs_points (
-  id                  INT(11)         NOT NULL AUTO_INCREMENT,
-  path                INT(11)         NOT NULL,
-  row                 INT(11)         NOT NULL,
-  centrelongitude     DECIMAL(15, 12) NOT NULL,
-  centrelatitude      DECIMAL(15, 12) NOT NULL,
-  upperleftlongitude  DECIMAL(15, 12) NOT NULL,
-  upperleftlatitude   DECIMAL(15, 12) NOT NULL,
-  lowerleftlongitude  DECIMAL(15, 12) NOT NULL,
-  lowerleftlatitude   DECIMAL(15, 12) NOT NULL,
-  lowerrightlongitude DECIMAL(15, 12) NOT NULL,
-  lowerrightlatitude  DECIMAL(15, 12) NOT NULL,
-  upperrightlongitude DECIMAL(15, 12) NOT NULL,
-  upperrightlatitude  DECIMAL(15, 12) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-
-CREATE TABLE download_requests (
-  request_id     INT(11)      NOT NULL AUTO_INCREMENT,
-  request_time   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  username       VARCHAR(255) NOT NULL,
-  group_scenes   INT(1)       NOT NULL DEFAULT 0,
-  request_name   VARCHAR(255),
-  request_status VARCHAR(255) NOT NULL DEFAULT 'REQUESTED',
-  PRIMARY KEY (request_id)
-);
-
-ALTER TABLE download_requests
-  ADD CONSTRAINT uc_user_req_name UNIQUE (username, request_name);
-
-
-CREATE TABLE requested_scenes (
-  id               INT(11)      NOT NULL AUTO_INCREMENT,
-  request_id       INT(11)      NOT NULL,
-  scene_id         VARCHAR(255) NOT NULL,
-  dataset_id       INT(11)      NOT NULL,
-  last_updated     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status           VARCHAR(255) NOT NULL DEFAULT 'REQUESTED',
-  processing_chain VARCHAR(255),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE metadata_providers (
-  id                   INT(11) UNSIGNED NOT NULL,
-  name                 VARCHAR(45)      NOT NULL,
-  active               TINYINT(1)       NOT NULL DEFAULT '1',
-  crawling_entrypoint  VARCHAR(255)     NOT NULL,
-  iterations           INT(10)                   DEFAULT 4,
-  iteration_size       INT(10)                   DEFAULT 18,
-  last_execution_start DATETIME                  DEFAULT NULL,
-  last_execution_end   DATETIME                  DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE metadata_crawling_criteria (
-  criteria_id          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  metadata_provider_id INT(10) UNSIGNED NOT NULL,
-  field_name           VARCHAR(255)     NOT NULL,
-  expected_value       VARCHAR(255)     NOT NULL,
-  PRIMARY KEY (criteria_id)
-);
-
-CREATE TABLE instance_providers (
-  id          INT          NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(60)  NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE datacenters (
-  id          INT          NOT NULL AUTO_INCREMENT,
-  name        VARCHAR(60)  NOT NULL,
-  geolocation VARCHAR(60)  NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  provider_id INT          NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE instances (
-  id                 INT          NOT NULL AUTO_INCREMENT,
-  instance_type      INT          NOT NULL,
-  status             VARCHAR(60)  NOT NULL,
-  public_ip          VARCHAR(60)  NULL,
-  private_ip         VARCHAR(60)  NOT NULL,
-  owner              VARCHAR(255) NULL,
-  name               VARCHAR(60)  NOT NULL,
-  launch_time        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  status_update_time DATETIME     NOT NULL,
-  disposable         INT(1)       NOT NULL DEFAULT 1,
-  reserved           INT(1)       NOT NULL DEFAULT 1,
-  data_center_id     INT          NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -326,7 +122,8 @@ CREATE TABLE user_monthly_storage (
   PRIMARY KEY (username, year, month)
 );
 
-CREATE INDEX idx_user_monthly_storage_1 ON user_monthly_storage (username, year, month);
+CREATE INDEX idx_user_monthly_storage_1
+  ON user_monthly_storage (username, year, month);
 
 CREATE TABLE scene_meta_data (
   id               VARCHAR(255) NOT NULL,
@@ -342,8 +139,10 @@ CREATE TABLE scene_meta_data (
   PRIMARY KEY (id)
 );
 
-CREATE INDEX idx_scene_meta_data_1 ON scene_meta_data (scene_area_id, acquisition_date);
-CREATE INDEX idx_scene_meta_data_2 ON scene_meta_data (meta_data_source, sensor_id, update_time);
+CREATE INDEX idx_scene_meta_data_1
+  ON scene_meta_data (scene_area_id, acquisition_date);
+CREATE INDEX idx_scene_meta_data_2
+  ON scene_meta_data (meta_data_source, sensor_id, update_time);
 
 
 CREATE TABLE worker_session (
