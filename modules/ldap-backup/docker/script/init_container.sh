d6bc1f14d5ae9ffff5521581b5bc2a23a4157343
Kevin <globalavocado@gmail.com>
2016-07-27 08:50:44 +0200

    Added LDAP backup module.

---
+++ modules/ldap-backup/docker/script/init_container.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+LATEST_CONFIG=$(ls -t /data/backup | grep config | head -n1)
+LATEST_DATA=$(ls -t /data/backup | grep data | head -n1)
+
+# Recreate Osixia environment
+ln -sf /container/service/slapd-backup/assets/tool/* /sbin/
+mkdir /container/run/
+touch /container/run/environment.sh 
+chmod +x /container/run/environment.sh
+
+if [[ "$RESTORE_BACKUP" == "True" ]]; then
+	rm -rf /data/old-config && mkdir /data/old-config && mv /etc/ldap/slapd.d/* /data/old-config/
+	rm -rf /data/old-data && mkdir /data/old-data && mv /var/lib/ldap/* /data/old-data/
+    /sbin/slapd-restore-config "/data/backup/$LATEST_CONFIG" gzipped
+    /sbin/slapd-restore-data "/data/backup/$LATEST_DATA" gzipped
+fi
+
+cat >/etc/cron.d/slapd-backup <<EOL
+* * * * * root export LDAP_BACKUP_TTL=$LDAP_BACKUP_TTL; /sbin/slapd-backup-config
+* * * * * root export LDAP_BACKUP_TTL=$LDAP_BACKUP_TTL; /sbin/slapd-backup-data
+EOL
+
+touch /data/backup-started
+
+# http://veithen.github.io/2014/11/16/sigterm-propagation.html
+trap 'kill -TERM $PID' TERM INT
+cron -f &
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
+EXIT_STATUS=$?
+
---
+++ modules/ldap-backup/docker/script/init_image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf /container/service/:cron
+
