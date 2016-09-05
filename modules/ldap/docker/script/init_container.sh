0dc216dd2d626c5d3ba103a7e89f515273dbc04d
Daniel Wiell <daniel.wiell@fao.org>
2016-09-05 14:19:57 +0200

    Removed bug-fix fixed in parent image.

--- modules/ldap/docker/script/init_container.sh
+++ modules/ldap/docker/script/init_container.sh
@@ -7,6 +7,5 @@ mkdir -p /data/logs/sssd
 rm -rf /var/log/sssd && ln -sf /data/logs/sssd /var/log/sssd
 
 cp /data/certificates/* /container/service/slapd/assets/certs
-cp /config/container-start.sh /container/service/slapd/
 
 exec /usr/bin/supervisord -c /config/supervisord.conf
