5212815325185fc3513e8d8f7fe9fdd1255d729a
Daniel Wiell <daniel.wiell@fao.org>
2016-09-06 13:19:08 +0200

    Removed redundant log directories.

--- modules/ldap/docker/script/init_container.sh
+++ modules/ldap/docker/script/init_container.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-mkdir -p /data/logs/supervisor
-rm -rf /var/log/supervisor && ln -sf /data/logs/supervisor /var/log/supervisor
-
-mkdir -p /data/logs/sssd
-rm -rf /var/log/sssd && ln -sf /data/logs/sssd /var/log/sssd
-
 cp /data/certificates/* /container/service/slapd/assets/certs
 
 exec /usr/bin/supervisord -c /config/supervisord.conf
