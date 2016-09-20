ec88f0b48b9833a5d1b1e125d5e519d8c106d9fb
Kevin <globalavocado@gmail.com>
2016-09-20 08:47:01 +0200

    upgraded to MySQL 5.7 and added logging to stdout

--- modules/mysql/docker/script/init_container.sh
+++ modules/mysql/docker/script/init_container.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 mkdir -p /data/logs
+
 rm -rf /var/log/supervisor && ln -sf /data/logs /var/log/supervisor
 
 cp -R /script/sqlScripts/* /opt/flyway/sql/
 
+touch /var/log/mysql/error.log
+chmod 666 /var/log/mysql/error.log
+
+touch /var/log/mysql/slow-queries.log
+chmod 666 /var/log/mysql/slow-queries.log
+
+# this starts logging, mysql and the migration
 exec /usr/bin/supervisord -c /config/supervisord.conf
