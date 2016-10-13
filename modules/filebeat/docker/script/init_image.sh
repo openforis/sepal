1ef76920e9af0003c21b2f70533778a2aab3394c
Kevin <globalavocado@gmail.com>
2016-10-13 12:48:50 +0200

    removed Filebeat module.

--- modules/filebeat/docker/script/init_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-apt-get update -y && apt-get install -y apt-transport-https python curl
-curl https://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
-echo "deb https://packages.elastic.co/beats/apt stable main" | tee -a /etc/apt/sources.list.d/beats.list
-
-apt-get update -y && apt-get install -y filebeat
