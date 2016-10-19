52bb4e6b9bafd81c11d8d693ee77d827fc476718
Kevin <globalavocado@gmail.com>
2016-10-19 15:59:24 +0200

    restricted access to Kibana dashboard via nginx & also added this to the operations server

--- modules/nginx/docker/script/init_container.sh
+++ modules/nginx/docker/script/init_container.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+apt-get install openssl
+
 mkdir -p /etc/nginx/sites-enabled/
-rm -rf /etc/nginx/sites-enabled/* && cp /config/sepal /etc/nginx/sites-enabled/
+rm -rf /etc/nginx/sites-enabled/* && cp /config/operations.conf /etc/nginx/sites-enabled/
 
 mkdir -p /etc/ssl
 cp /data/certificates/* /etc/ssl/
 
-exec /usr/bin/supervisord -c /config/supervisord.conf
+echo -n 'admin:' >> /etc/nginx/.htpasswd
+openssl passwd "$PASSWORD_SEPAL_ENV" >> /etc/nginx/.htpasswd
+chown www-data .htpasswd
+chmod 400 .htpasswd
 
+# Unset all env variables ending with _SEPAL_ENV
+unset $(printenv | grep '_SEPAL_ENV' | sed -E "s/([0-9a-zA-Z]+)=.*/\\1/" | tr '\n' ' ')
+
+exec /usr/bin/supervisord -c /config/supervisord.conf
