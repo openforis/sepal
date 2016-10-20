28c5a6909b7991db550d71ce68c595b4e2595733
Daniel Wiell <daniel.wiell@fao.org>
2016-10-20 15:23:42 +0200

    Added logging to storage usage checks.

--- modules/sepal-server/src/main/groovy/org/openforis/sepal/component/budget/command/DetermineUserStorageUsage.groovy
+++ modules/sepal-server/src/main/groovy/org/openforis/sepal/component/budget/command/DetermineUserStorageUsage.groovy
@@ -5,12 +5,14 @@ import org.openforis.sepal.command.CommandHandler
 import org.openforis.sepal.component.budget.internal.StorageUseService
 import org.openforis.sepal.user.UserRepository
 import org.openforis.sepal.util.annotation.Data
+import org.slf4j.LoggerFactory
 
 @Data(callSuper = true)
 class DetermineUserStorageUsage extends AbstractCommand<Void> {
 }
 
 class DetermineUserStorageUsageHandler implements CommandHandler<Void, DetermineUserStorageUsage> {
+    private static final LOG = LoggerFactory.getLogger(this)
     private final StorageUseService storageUseService
     private final UserRepository userRepository
 
@@ -21,9 +23,11 @@ class DetermineUserStorageUsageHandler implements CommandHandler<Void, Determine
 
     Void execute(DetermineUserStorageUsage command) {
         userRepository.eachUsername {
-            storageUseService.updateStorageUseForThisMonth(it)
+            long startTime = System.currentTimeMillis()
+            def storageUse = storageUseService.updateStorageUseForThisMonth(it)
+            LOG.debug([command: command, username: it, storageUse: storageUse,
+                       duration: (System.currentTimeMillis() - startTime) / 1000 + 's'] as String)
         }
-
         return null
     }
 
