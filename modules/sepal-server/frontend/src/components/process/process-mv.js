f297b144fc7992cfabd20a8d1e0ed110972627b5
Mino Togna <Cosimo.Togna@Fao.org>
2016-11-23 12:09:51 +0100

    update apps section

--- modules/sepal-server/frontend/src/components/process/process-mv.js
+++ modules/sepal-server/frontend/src/components/process/process-mv.js
@@ -11,7 +11,18 @@ var View     = require( './process-v' )
 var show = function ( e, type ) {
     if ( type == 'process' ) {
         View.init()
+        loadApp()
     }
+}
+
+var loadApp = function () {
+    var params = {
+        url      : '/apps'
+        , success: function ( response ) {
+            View.setApps( response )
+        }
+    }
+    EventBus.dispatch( Events.AJAX.GET, null, params )
 }
 
 EventBus.addEventListener( Events.SECTION.SHOW, show )
--- modules/sepal-server/frontend/src/components/process/process-v.js
+++ modules/sepal-server/frontend/src/components/process/process-v.js
@@ -3,15 +3,10 @@
  */
 require( './process.scss' )
 
-var UserMV = require( '../user/user-mv' )
-// var EventBus = require( '../event/event-bus' )
-// var Events   = require( '../event/events' )
-var html = null
-
+var html       = null
+var $apps      = null
 var rStudioImg = require( './img/r-studio.png' )
 
-//link to rstudio
-var apps = null
 var init = function () {
     var template = require( './process.html' )
     html         = $( template( {} ) )
@@ -20,43 +15,29 @@ var init = function () {
     if ( appSection.children().length <= 0 ) {
         appSection.append( html )
         
-        apps = html.find( '.apps' )
-        
-        var rStudioBtn = $( '<a class="btn btn-base round app r-studio" target="_blank"></a>' )
-        var href       = '/sandbox/rstudio/'
-        rStudioBtn.attr( 'href', href )
-        rStudioBtn.append( '<img src="' + rStudioImg + '"/>' )
-        apps.append( rStudioBtn )
-        
-        apps.append('' +
-            '<div>' +
-            '    <a class="btn btn-base round app"' +
-            '       target="_blank" ' +
-            '       href="/sandbox/shiny/accuracy-assessment/">' +
-            '        Accuracy Assessment' +
-            '    </a>' +
-            '</div>')
-        
-        apps.append('' +
-            '<div>' +
-            '    <a class="btn btn-base round app"' +
-            '       target="_blank" ' +
-            '       href="/sandbox/shiny/visualize/">' +
-            '        Visualize' +
-            '    </a>' +
-            '</div>')
-        
-        apps.append('' +
-            '<div>' +
-            '    <a class="btn btn-base round app"' +
-            '       target="_blank" ' +
-            '       href="/sandbox/shiny/osk/">' +
-            '        SAR Toolkit' +
-            '    </a>' +
-            '</div>')
+        $apps = html.find( '.apps' )
     }
 }
 
+var setApps = function ( apps ) {
+    $apps.empty()
+    
+    var rStudioBtn = $( '<div><a class="btn btn-base app r-studio" target="_blank" href="/sandbox/rstudio"><img src="' + rStudioImg + '"/></a></div>' )
+    $apps.append( rStudioBtn )
+    
+    $.each( apps, function ( i, app ) {
+        var div = $( '<div/>' )
+        var a   = $( '<a class="btn btn-base app" target="_blank"></a>' )
+        
+        a.html( app.label )
+        a.attr( 'href', app.path )
+        
+        div.append( a )
+        $apps.append( div )
+    } )
+}
+
 module.exports = {
-    init: init
+    init     : init
+    , setApps: setApps
 }
