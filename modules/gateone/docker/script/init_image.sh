a195e831b2f42ade362a76d8ef394829dc5abd6f
Daniel Wiell <daniel.wiell@fao.org>
2016-11-24 15:51:28 +0100

    Removed unused GateOne plugins and applications.

--- modules/gateone/docker/script/init_image.sh
+++ modules/gateone/docker/script/init_image.sh
@@ -2,3 +2,11 @@
 
 apt-get update -y && apt-get install -y \
     gettext
+
+rm -rf /gateone/GateOne/gateone/applications/example
+rm -rf /gateone/GateOne/gateone/applications/terminal/plugins/example
+rm -rf /gateone/GateOne/gateone/applications/terminal/plugins/playback
+
+rm -rf /usr/local/lib/python2.7/dist-packages/gateone-1.2.0-py2.7.egg/gateone/applications/example
+rm -rf /usr/local/lib/python2.7/dist-packages/gateone-1.2.0-py2.7.egg/gateone/applications/terminal/plugins/example
+rm -rf /usr/local/lib/python2.7/dist-packages/gateone-1.2.0-py2.7.egg/gateone/applications/terminal/plugins/playback
