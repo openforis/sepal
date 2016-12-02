ce819bf0dc498b68dd701d77ca8199b335db36b8
Daniel Wiell <daniel.wiell@fao.org>
2016-12-02 15:07:42 +0100

    Prompted for operator email when generating configuration.

--- config-generator/src/create.py
+++ config-generator/src/create.py
@@ -134,6 +134,7 @@ def _config_sepal():
     _random_password('gate_one_secret', 32)
     _random_password('gate_one_public', 32)
     _text('sepal_host', 'Sepal Host')
+    _text('sepal_operator_email', 'Sepal Operator Email')
     _password('sepal_admin_user_password', 'Sepal admin user password')
     _random_password('sepal_machine_admin_password', 32)
     _random_password('ldap_admin_password', 32)
