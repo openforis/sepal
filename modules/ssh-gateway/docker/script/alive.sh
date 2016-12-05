db2947470a80267b39129af8f2238f8c6624e0eb
Daniel Wiell <daniel.wiell@fao.org>
2016-12-05 08:54:01 +0100

    Fixed authentication when ssh-gateway sends session heartbeats.

--- modules/ssh-gateway/docker/script/alive.sh
+++ modules/ssh-gateway/docker/script/alive.sh
@@ -3,6 +3,6 @@
 password=$(cat /etc/sepalAdmin.passwd)
 while true
 do
-    curl -u "sepalAdmin:$password" -X POST -s "http://sepal:1025/api/sandbox/$USER/session/$1"
-    /bin/sleep 10
+    curl -H "sepal-user:{username: \"sepalAdmin\", roles: [\"application_admin\"]}" -X POST -s "http://sepal:1025/api/sandbox/$USER/session/$1"
+    /bin/sleep 30
 done
