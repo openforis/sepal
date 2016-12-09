412bd266b921792c0c55d3a37e5ae527efdb3b2e
Daniel Wiell <daniel.wiell@fao.org>
2016-12-09 16:26:42 +0100

    Renamed bands.

--- modules/sepal-server/frontend/src/components/search-retrieve/views/mosaic/bands.js
+++ modules/sepal-server/frontend/src/components/search-retrieve/views/mosaic/bands.js
@@ -3,12 +3,15 @@
  */
 
 var bands = [
-    { data: 'B3, B2, B1', value: 'Natural (3, 2, 1)' }
-    , { data: 'B4, B3, B2', value: 'Infrared (4, 3, 2)' }
-    , { data: 'B4, B5, B3', value: 'False color (4, 5, 3)' }
-    , { data: 'B7, B4, B3', value: 'False color (7, 4, 3)' }
-    , { data: 'B7, B5, B3', value: 'False color (7, 5, 3)' }
-    , { data: 'B7, B4, B2', value: 'False color (7, 4, 2)' }
+    { data: 'B3, B2, B1', value: 'Natural (Red, Green, Blue)' }
+    , { data: 'B4, B3, B2', value: 'Infrared (NIR, Red, Green)' }
+    , { data: 'B4, B5, B3', value: 'False color(NIR, SWIR 1, Red)' }
+    , { data: 'B7, B4, B3', value: 'False color(SWIR 2, NIR, Red)' }
+    , { data: 'B7, B5, B3', value: 'False color(SWIR 2, SWIR 1, Red)' }
+    , { data: 'B7, B4, B2', value: 'False color(SWIR 2, NIR, Green)' }
+    , { data: 'B4_brdf, B5_brdf, B3_brdf', value: 'False color(NIR, SWIR 1, Red - BRDF corrected)' }
+    , { data: 'B7_brdf, B4_brdf, B3_brdf', value: 'False color(SWIR 2, NIR, Red - BRDF corrected)' }
+    , { data: 'B7_brdf, B5_brdf, B3_brdf', value: 'False color(SWIR 2, SWIR 1, Red - BRDF corrected)' }
     , { data: 'temp', value: 'Temperature' }
     , { data: 'date', value: 'Date of pixel in days' }
     , { data: 'days', value: 'Days from target day' }
