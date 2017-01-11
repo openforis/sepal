8397d1cf21462c62ce69e9c90fafd25447eba812
Daniel Wiell <daniel.wiell@fao.org>
2017-01-11 14:53:33 +0100

    Run geo-web-viz backend in test server.

--- modules/sandbox/docker/geo-web-viz/app.py
+++ modules/sandbox/docker/geo-web-viz/app.py
@@ -1,6 +1,7 @@
 import json
+import logging
 
-from flask import Flask, request, Response
+from flask import Flask, Blueprint, request, Response
 
 import config
 import layers
@@ -8,55 +9,61 @@ import raster
 import render
 from config import to_file
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
+http = Blueprint(__name__, __name__)
 
 
-@app.route('/layers', methods=['GET'])
+@http.route('/layers', methods=['GET'])
 def list_layers():
     return json_response(layers.list_layers())
 
 
-@app.route('/layers/order', methods=['POST'])
+@http.route('/layers/order', methods=['POST'])
 def order_layers():
     layers.reorder(json.loads(request.values['order']))
     return json_response({'status': 'OK'})
 
 
-@app.route('/raster/band/count', methods=['GET'])
+@http.route('/raster/band/count', methods=['GET'])
 def band_count():
     return json_response(
         {'count': raster.band_count(to_file(request.values['path']))}
     )
 
 
-@app.route('/raster/band/<band_index>', methods=['GET'])
+@http.route('/raster/band/<band_index>', methods=['GET'])
 def band_info(band_index):
     return json_response(
         raster.band_info(to_file(request.values['path']), int(band_index))
     )
 
 
-@app.route('/raster/save', methods=['POST'])
+@http.route('/raster/save', methods=['POST'])
 def save_raster():
     layer = json.loads(request.values['layer'])
     bounds = layers.save_raster(layer)
     return json_response({'bounds': bounds})
 
 
-@app.route('/shape/save', methods=['POST'])
+@http.route('/shape/save', methods=['POST'])
 def save_shape():
     layer = json.loads(request.values['layer'])
     bounds = layers.save_shape(layer)
     return json_response({'bounds': bounds})
 
 
-@app.route('/layers/<layer_id>', methods=['DELETE'])
+@http.route('/layers/<layer_id>', methods=['DELETE'])
 def remove_raster(layer_id):
     layers.remove_layer(layer_id)
     return json_response({'status': 'OK'})
 
 
-@app.route('/layer/<layer_id>/<z>/<x>/<y>.<fmt>')
+@http.route('/layers/features/<lat>/<lng>')
+def features(lat, lng):
+    return json_response(layers.features(float(lat), float(lng)))
+
+
+@http.route('/layer/<layer_id>/<z>/<x>/<y>.<fmt>')
 def render_tile(layer_id, z, x, y, fmt):
     return Response(
         render.render_tile(layer_id, int(z), int(x), int(y), str(fmt)),
@@ -69,6 +76,8 @@ def json_response(data):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.WARNING)
+    app.register_blueprint(http)
     app.run(
         host='0.0.0.0',
         port=config.server_port,
