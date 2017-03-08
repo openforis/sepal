ad5ed3646c29bceaf20a496d6f3edcca97b363e5
Daniel Wiell <daniel.wiell@fao.org>
2017-03-08 12:40:31 +0100

    Updated band names.

--- modules/sepal-server/frontend/src/components/search-retrieve/views/mosaic/bands-sentinel2.js
+++ modules/sepal-server/frontend/src/components/search-retrieve/views/mosaic/bands-sentinel2.js
@@ -3,13 +3,12 @@
  */
 
 var bands = [
-    { data: 'B3, B2, B1', value: 'Natural (Red, Green, Blue)' }
-    , { data: 'B4, B3, B2', value: 'Infrared (NIR, Red, Green)' }
-    , { data: 'B4, B5, B3', value: 'False color(NIR, SWIR 1, Red)' }
-    , { data: 'B7, B4, B3', value: 'False color(SWIR 2, NIR, Red)' }
-    , { data: 'B7, B5, B3', value: 'False color(SWIR 2, SWIR 1, Red)' }
-    , { data: 'B7, B4, B2', value: 'False color(SWIR 2, NIR, Green)' }
-    , { data: 'temp', value: 'Temperature' }
+    { data: 'RED, GREEN, BLUE', value: 'Natural (Red, Green, Blue)' }
+    , { data: 'NIR, RED, GREEN', value: 'Infrared (NIR, Red, Green)' }
+    , { data: 'NIR, SWIR1, RED', value: 'False color(NIR, SWIR 1, Red)' }
+    , { data: 'SWIR2, NIR, RED', value: 'False color(SWIR 2, NIR, Red)' }
+    , { data: 'SWIR2, SWIR1, RED', value: 'False color(SWIR 2, SWIR 1, Red)' }
+    , { data: 'SWIR2, NIR, GREEN', value: 'False color(SWIR 2, NIR, Green)' }
     , { data: 'date', value: 'Date of pixel in days' }
     , { data: 'days', value: 'Days from target day' }
 ]
