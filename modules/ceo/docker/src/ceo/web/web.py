b2edb55eae887375b47a8de9735137e49dc484f2
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2017-04-21 15:39:44 +0200

    initial commit for ceo

---
+++ modules/ceo/docker/src/ceo/web/web.py
@@ -0,0 +1,42 @@
+import os, logging, json
+
+from flask import session, request, redirect, url_for, jsonify, render_template, send_file
+from flask_cors import CORS, cross_origin
+
+from .. import app
+
+from ..common.utils import import_sepal_auth, requires_auth, requires_role
+
+logger = logging.getLogger(__name__)
+
+@app.route('/', methods=['GET'])
+@cross_origin(origins=app.config['CO_ORIGINS'])
+@import_sepal_auth
+@requires_auth
+@requires_role('user')
+def index():
+    return render_template('index.html', username=session.get('username'), is_admin=session.get('is_admin'))
+
+@app.route('/project-list', methods=['GET'])
+@cross_origin(origins=app.config['CO_ORIGINS'])
+@import_sepal_auth
+@requires_auth
+@requires_role('user')
+def project_list():
+    return render_template('project-list.html', username=session.get('username'), is_admin=session.get('is_admin'))
+
+@app.route('/project-add', methods=['GET'])
+@cross_origin(origins=app.config['CO_ORIGINS'])
+@import_sepal_auth
+@requires_auth
+@requires_role('user')
+def project_add():
+    return render_template('project-add.html', username=session.get('username'), is_admin=session.get('is_admin'))
+
+@app.route('/collect-form', methods=['GET'])
+@cross_origin(origins=app.config['CO_ORIGINS'])
+@import_sepal_auth
+@requires_auth
+@requires_role('user')
+def collect_form():
+    return render_template('collect-form.html', username=session.get('username'), is_admin=session.get('is_admin'))
