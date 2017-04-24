0771424a969fab49906b34b1fe4ae0590d2f9dd7
Daniel Wiell <daniel.wiell@fao.org>
2017-04-24 11:12:41 +0200

    Removed non-existing directory from backup job.

--- modules/ssh-gateway/src/main/groovy/org/openforis/sepal/sshgateway/BudgetChecker.groovy
+++ modules/ssh-gateway/src/main/groovy/org/openforis/sepal/sshgateway/BudgetChecker.groovy
@@ -2,9 +2,9 @@ package org.openforis.sepal.sshgateway
 
 class BudgetChecker {
      static void assertWithinBudget(info) {
-        check(info.monthlyInstanceSpending, info.monthlyInstanceBudget, "You have spent more than you have been allocated on instances.")
-        check(info.monthlyStorageSpending, info.monthlyStorageBudget, "You have spent more than you have been allocated on storage.")
-        check(info.storageUsed, info.storageQuota, "You have used up more storage than you are allocated.")
+        check(info.monthlyInstanceSpending, info.monthlyInstanceBudget, "You have spent more than you have been allocated on instances. Please contact a system administrator to increase your allocation.")
+        check(info.monthlyStorageSpending, info.monthlyStorageBudget, "You have spent more than you have been allocated on storage. Please contact a system administrator to increase your allocation.")
+        check(info.storageUsed, info.storageQuota, "You have used up more storage than you are allocated. Please contact a system administrator to increase your allocation.")
     }
 
     private static void check(spending, budget, String message) {
