12f58a3d29cb4e32f0c911f142fd415f2491d896
Daniel Wiell <daniel.wiell@fao.org>
2017-04-26 13:20:06 +0200

    Included Google Cloud utils in geospatial-toolkit.

---
+++ modules/geospatial-toolkit/docker/script/init_gcloud.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo
+echo "*************************************"
+echo "*** Installing Google Cloud Tools ***"
+echo "*************************************"
+
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-152.0.0-linux-x86_64.tar.gz
+tar -xzf google-cloud-sdk-152.0.0-linux-x86_64.tar.gz
+mv google-cloud-sdk /usr/local/lib
+/usr/local/lib/google-cloud-sdk/install.sh -q
+ln -s /usr/local/lib/google-cloud-sdk/bin/gcloud /usr/local/bin/
+ln -s /usr/local/lib/google-cloud-sdk/bin/gsutil /usr/local/bin/
+ln -s /usr/local/lib/google-cloud-sdk/bin/bq /usr/local/bin/
+rm google-cloud-sdk-152.0.0-linux-x86_64.tar.gz
