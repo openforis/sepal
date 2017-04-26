12bba36522e27ae20bb7bdaaca48d7baaab5c757
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2017-04-26 16:45:30 +0200

    small fixes

--- modules/ceo/docker/src/ceo/ceo/__init__.py
+++ modules/ceo/docker/src/ceo/ceo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True, static_url_path="/static", static_folder="../static")
 app.config.from_object('config')
@@ -8,4 +8,5 @@ app.secret_key = app.config['SESSION_SECRET_KEY']
 
 from flask_pymongo import PyMongo
 mongo = PyMongo(app)
+
 import api_mongo, web
