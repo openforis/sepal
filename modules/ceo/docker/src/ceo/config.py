42cc742f50fc2887802da55474cbc295cc4b2118
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2017-04-28 14:34:18 +0200

    bugfixes and cleanups

--- modules/ceo/docker/src/ceo/config.py
+++ modules/ceo/docker/src/ceo/config.py
@@ -14,7 +14,7 @@ MONGO_DBNAME = 'ceo'
 
 SESSION_SECRET_KEY = 'notasecret'
 
-CEO_API_URL = 'http://127.0.0.1:' + str(PORT) + '/api'
-GEEG_API_URL = 'http://127.0.0.1:8888'
-
 BASE = '/ceo/'
+
+CEO_API_URL = 'http://127.0.0.1:' + str(PORT) + BASE + 'api'
+GEEG_API_URL = 'http://127.0.0.1:8888'
