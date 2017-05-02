95ac5cd3b262090958fbedf5d5042e5c7c84f44b
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2017-05-02 16:41:26 +0200

    copy gee_gateway module in ceo

--- modules/ceo/docker/script/init_image.sh
+++ modules/ceo/docker/script/init_image.sh
@@ -14,4 +14,6 @@ apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -y\
 
 git clone https://github.com/openforis/gee-gateway.git /src/gee-gateway
 
+cp -r /src/gee-gateway/gee_gateway /src/ceo
+
 npm install --global bower
