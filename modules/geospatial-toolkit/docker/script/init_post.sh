ed7945dfa9a29a875d464991a4622b29d2508277
Daniel Wiell <daniel.wiell@fao.org>
2017-05-17 11:07:06 +0200

    Set language locale environmental vars in /etc/environment to en_US.

--- modules/geospatial-toolkit/docker/script/init_post.sh
+++ modules/geospatial-toolkit/docker/script/init_post.sh
@@ -13,6 +13,17 @@ printf '%s\n' \
     'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' \
     'GDAL_DATA="/usr/share/gdal/2.1"' \
     'SEPAL="true"' \
+    'LC_ALL="en_US.UTF-8"' \
+    'LC_PAPER="en_US.UTF-8"' \
+    'LC_ADDRESS="en_US.UTF-8"' \
+    'LC_MONETARY="en_US.UTF-8"' \
+    'LC_NUMERIC="en_US.UTF-8"' \
+    'LC_TELEPHONE="en_US.UTF-8"' \
+    'LC_IDENTIFICATION="en_US.UTF-8"' \
+    'LC_MEASUREMENT="en_US.UTF-8"' \
+    'LC_TIME="en_US.UTF-8"' \
+    'LC_NAME="en_US.UTF-8"' \
+    'LANG="en_US.UTF-8"' \
     >> /etc/environment
 
 # Remove redundant files
