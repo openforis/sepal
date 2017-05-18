3a811b2d9f1b841651e72a48ed6a061163a4ed9b
Daniel Wiell <daniel.wiell@fao.org>
2017-05-18 13:08:12 +0200

    Updated RStudio server.

--- modules/sandbox/docker/script/init_image.sh
+++ modules/sandbox/docker/script/init_image.sh
@@ -42,14 +42,13 @@ echo
 echo "*********************************"
 echo "*** Installing RStudio Server ***"
 echo "*********************************"
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen \
-	&& locale-gen en_US.utf8 \
-	&& /usr/sbin/update-locale LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-wget https://download2.rstudio.org/rstudio-server-0.99.484-amd64.deb
-gdebi -n rstudio-server-0.99.484-amd64.deb
-rm -f rstudio-*
+rstudio=rstudio-server-0.99.491-amd64.deb
+wget https://download2.rstudio.org/$rstudio
+gdebi -n $rstudio
+printf '%s\n' \
+    "server-app-armor-enabled=0" \
+    >> /etc/rstudio/rserver.conf
+rm -f $rstudio
 
 echo
 echo "*******************************"
