97ec84844722cf760c785d25444b04a37817271b
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2017-06-06 16:55:19 +0200

    gunicors is now the new wsgi http server for test and production (replacing flask)

---
+++ modules/ceo/docker/src/ceo/wsgi.py
@@ -0,0 +1,16 @@
+from ceo import app
+
+def build_app(gmaps_api_key='', digital_globe_api_key='', dgcs_connect_id='', ee_account='', ee_key_path=''):
+
+    app.config['GMAPS_API_KEY'] = gmaps_api_key
+    app.config['DIGITALGLOBE_API_KEY'] = digital_globe_api_key
+    app.config['DGCS_CONNECT_ID'] = dgcs_connect_id
+
+    try:
+        from gee_gateway import gee_gateway, gee_initialize
+        gee_initialize(ee_account=ee_account, ee_key_path=ee_key_path)
+        app.register_blueprint(gee_gateway, url_prefix='/' + app.config['GEEG_API_URL'])
+    except ImportError as e:
+        pass
+
+    return app
