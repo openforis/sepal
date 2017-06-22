d75020cee194e1be78cbb0fa17ede0d13328556a
Daniel Wiell <daniel.wiell@fao.org>
2017-06-22 11:10:58 +0200

    Removed database migration from ConfiguredMain.
    This is handled by the components themselves.

--- modules/sepal-server/src/test/groovy/manual/ConfiguredMain.groovy
+++ modules/sepal-server/src/test/groovy/manual/ConfiguredMain.groovy
@@ -8,19 +8,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class ConfiguredMain {
-    static Logger LOG = LoggerFactory.getLogger(this)
-
     static void main(String[] args) {
-        def config = new SepalConfiguration()
-        LOG.info("Migrating database")
-        def sql = new Sql(config.dataSource)
-        sql.execute('CREATE SCHEMA IF NOT EXISTS "public"')
-        new Flyway(
-                locations: ["filesystem:modules/mysql/docker/script/sqlScripts"],
-                dataSource: config.dataSource,
-                schemas: ['PUBLIC']
-        ).migrate()
-        LOG.info("Starting Sepal")
         Main.main()
     }
 }
