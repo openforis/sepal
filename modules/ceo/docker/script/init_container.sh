dd8e8ca7d2f182ef06ddaae2603548e6fd1f8bec
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2017-06-26 12:18:57 +0200

    planet api key support

--- modules/ceo/docker/script/init_container.sh
+++ modules/ceo/docker/script/init_container.sh
@@ -4,6 +4,7 @@ set -e
 export mapApiKey=$GOOGLE_MAPS_API_KEY_SEPAL_ENV
 export digitalGlobeApiKey=$DIGITAL_GLOBE_API_KEY_SEPAL_ENV
 export digitalGlobeConnectId=$DIGITAL_GLOBE_CONNECT_ID_SEPAL_ENV
+export planetApiKey=$PLANET_API_KEY_SEPAL_ENV
 
 export account=$EE_ACCOUNT_SEPAL_ENV
 export privateKey=${EE_PRIVATE_KEY_SEPAL_ENV//-----LINE BREAK-----/\\n}
