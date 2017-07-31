7956da01002be77f316567a3a08a0eec336223e2
Mino Togna <Cosimo.Togna@Fao.org>
2017-07-31 11:51:48 +0200

    initializing classification form based on initial value

--- modules/gui/frontend/src/components/search/views/classify-form.js
+++ modules/gui/frontend/src/components/search/views/classify-form.js
@@ -3,15 +3,17 @@ var Events        = require('./../../event/events')
 var FormValidator = require('../../form/form-validator')
 var Model         = require('./../model/search-model')
 
-var state = {}
+var state   = {}
+var mosaics = []
 
-var form                   = null
-var formNotify             = null
-var name                   = null
-var inputRecipe            = null
-var fusionTableId          = null
-var fusionTableClassColumn = null
-var algorithm              = null
+var form                    = null
+var formNotify              = null
+var name                    = null
+var inputRecipe             = null
+var inputRecipeAutocomplete = null
+var fusionTableId           = null
+var fusionTableClassColumn  = null
+var rowAlgorithms           = null
 
 var init = function (container) {
   form       = container.find('form')
@@ -19,11 +21,38 @@ var init = function (container) {
   
   name                   = form.find('input[name=name]')
   inputRecipe            = form.find('input[name=input-recipe]')
-  fusionTableId          = form.find('input[name=name]')
-  fusionTableClassColumn = form.find('input[name=name]')
-  algorithm              = form.find('input[name=name]')
+  fusionTableId          = form.find('input[name=fusion-table-id]')
+  fusionTableClassColumn = form.find('input[name=fusion-table-class-column]')
+  rowAlgorithms          = form.find('.row-algorithm-btns')
+  
+  initEvents()
   
   form.submit(submit)
+}
+
+var initEvents = function () {
+  name.change(function (e) {
+    state.name = name.val()
+    EventBus.dispatch(Events.SECTION.SEARCH.STATE.ACTIVE_CHANGE, null, state, {field: 'name'})
+  })
+  fusionTableId.change(function (e) {
+    state.fusionTableId = fusionTableId.val()
+  })
+  fusionTableClassColumn.change(function (e) {
+    state.fusionTableClassColumn = fusionTableClassColumn.val()
+  })
+  
+  var btns = rowAlgorithms.find('button')
+  btns.click(function (e) {
+    var btn = $(e.target)
+    if (!btn.hasClass('active')) {
+      btns.removeClass('active')
+      btn.addClass('active')
+      state.algorithm = btn.val()
+    }
+  })
+  
+  updateInputRecipe()
 }
 
 var submit = function (e) {
@@ -36,14 +65,50 @@ var submit = function (e) {
 // model change methods
 var setState = function (e, newState, params) {
   FormValidator.resetFormErrors(form, formNotify)
-  state = newState
   
-  if (state && state.type == Model.TYPES.CLASSIFY) {
-    console.log(state)
+  if (newState && newState.type == Model.TYPES.CLASSIFY && (!state || newState.id !== state.id)) {
+    
+    name.val(newState.name)
+    
+    var mosaic = mosaics.find(function (o) {
+      return o.id == newState.inputRecipe
+    })
+    if (mosaic)
+      inputRecipe.val(mosaic.name).data('reset-btn').enable()
+    
+    fusionTableId.val(newState.fusionTableId)
+    fusionTableClassColumn.val(newState.fusionTableClassColumn)
+    rowAlgorithms.find('button').removeClass('active')
+    if (newState.algorithm)
+      rowAlgorithms.find('button[value=' + newState.algorithm + ']').addClass('active')
   }
+  
+  state = newState
   
 }
 
+var listMosaicsChanged = function (e, list) {
+  mosaics = list
+  updateInputRecipe()
+}
+
+var updateInputRecipe = function () {
+  var getRecipeOpt = function (mosaic) {
+    return {data: mosaic.id, value: mosaic.name}
+  }
+  
+  if (inputRecipeAutocomplete)
+    inputRecipeAutocomplete.sepalAutocomplete('dispose')
+  
+  inputRecipeAutocomplete = inputRecipe.sepalAutocomplete({
+    lookup  : mosaics.map(getRecipeOpt),
+    onChange: function (selection) {
+      state.inputRecipe = selection ? selection.data : null
+    }
+  })
+}
+
 EventBus.addEventListener(Events.SECTION.SEARCH.STATE.ACTIVE_CHANGED, setState)
+EventBus.addEventListener(Events.SECTION.SEARCH.STATE.LIST_CHANGED, listMosaicsChanged)
 
 module.exports.init = init
