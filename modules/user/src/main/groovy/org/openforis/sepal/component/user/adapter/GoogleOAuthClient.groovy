8e6e1b7f99ce0e4090d0077c01dfe4e53b996a25
Daniel Wiell <daniel.wiell@fao.org>
2017-09-14 11:01:32 +0200

    Requested fusion table access.

--- modules/user/src/main/groovy/org/openforis/sepal/component/user/adapter/GoogleOAuthClient.groovy
+++ modules/user/src/main/groovy/org/openforis/sepal/component/user/adapter/GoogleOAuthClient.groovy
@@ -32,7 +32,8 @@ class RestBackedGoogleOAuthClient implements GoogleOAuthClient {
     public static final SCOPE = '' +
             'https://www.googleapis.com/auth/earthengine ' +
             'https://www.googleapis.com/auth/devstorage.full_control ' +
-            'https://www.googleapis.com/auth/drive'
+            'https://www.googleapis.com/auth/drive ' +
+            'https://www.googleapis.com/auth/fusiontables'
     private final String sepalHost
     private final String clientId
     private final String clientSecret
