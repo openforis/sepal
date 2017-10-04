b4905271dac2ab38f828a7c5cff089d8ef87339f
Daniel Wiell <daniel.wiell@fao.org>
2017-10-04 09:40:50 +0200

    Updated GDAL_DATA path.

--- modules/geospatial-toolkit/docker/script/init_post.sh
+++ modules/geospatial-toolkit/docker/script/init_post.sh
@@ -11,7 +11,7 @@ mkdir /var/run/sshd
 printf '%s\n' \
     'PATH="usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/lib/orfeo/bin"' \
     'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' \
-    'GDAL_DATA="/usr/share/gdal/2.1"' \
+    'GDAL_DATA="/usr/share/gdal/2.2"' \
     'SEPAL="true"' \
     >> /etc/environment
 
