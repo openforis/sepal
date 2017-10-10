915d3f3d0e60569394248592442129c9d4ffd7f9
Daniel Wiell <daniel.wiell@fao.org>
2017-10-10 13:17:43 +0200

    Used ee.layers.ImageOverlay to render GEE tiles.

--- modules/gui/frontend/src/components/scene-area-mosaics/ee-mosaic.js
+++ modules/gui/frontend/src/components/scene-area-mosaics/ee-mosaic.js
@@ -1,26 +1,19 @@
 /**
  * @author Mino Togna
  */
+var ee = require('earthengine-api').ee
+ee.MapTileManager.MAX_RETRIES = 3
 
 var EEMosaic = function ( mapId, token ) {
-    var eeMapOptions = {
-        getTileUrl: function ( tile, zoom ) {
-            var baseUrl = 'https://earthengine.googleapis.com/map'
-            var url     = [ baseUrl, mapId, zoom, tile.x, tile.y ].join( '/' )
-            url += '?token=' + token
-            return url
-        },
-        tileSize  : new google.maps.Size( 256, 256 )
-    }
-    
-    // Create the map type.
-    this.layer = new google.maps.ImageMapType( eeMapOptions )
+  this.layer = new ee.layers.ImageOverlay(
+    new ee.layers.EarthEngineTileSource('https://earthengine.googleapis.com/map', mapId, token)
+  )
 }
 
 var newInstance = function ( mapId, token ) {
-    return new EEMosaic( mapId, token )
+  return new EEMosaic( mapId, token )
 }
 
 module.exports = {
-    newInstance: newInstance
+  newInstance: newInstance
 }
