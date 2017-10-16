70b2ba1848cc9a978f798a48be8563c0f7d0bc86
Mino Togna <Cosimo.Togna@Fao.org>
2017-10-16 12:04:21 +0200

    using deprecated GEE class

--- modules/gui/frontend/src/components/scene-area-mosaics/ee-mosaic.js
+++ modules/gui/frontend/src/components/scene-area-mosaics/ee-mosaic.js
@@ -2,12 +2,13 @@
  * @author Mino Togna
  */
 var ee = require('earthengine-api').ee
-ee.MapTileManager.MAX_RETRIES = 3
+// ee.MapTileManager.MAX_RETRIES = 3
 
 var EEMosaic = function ( mapId, token ) {
-  this.layer = new ee.layers.ImageOverlay(
-    new ee.layers.EarthEngineTileSource('https://earthengine.googleapis.com/map', mapId, token)
-  )
+  // this.layer = new ee.layers.ImageOverlay(
+  //   new ee.layers.EarthEngineTileSource('https://earthengine.googleapis.com/map', mapId, token)
+  // )
+  this.layer = new ee.MapLayerOverlay('https://earthengine.googleapis.com/map', mapId, token, {})
 }
 
 var newInstance = function ( mapId, token ) {
