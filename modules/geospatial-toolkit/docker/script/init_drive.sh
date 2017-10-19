0bda527e2b19ac9074b9217797d8353102e0a811
Daniel Wiell <daniel.wiell@fao.org>
2017-10-19 10:56:26 +0200

    Included command line tool for Google Drive.

---
+++ modules/geospatial-toolkit/docker/script/init_drive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo
+echo "************************"
+echo "*** Installing Drive ***"
+echo "************************"
+
+wget https://github.com/odeke-em/drive/releases/download/v0.3.9/drive_linux
+chmod +x drive_linux
+cp drive_linux /usr/bin/drive
