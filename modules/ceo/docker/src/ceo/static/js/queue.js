a8336e2757475c2f4fe8049d71602b84c091e133
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2017-12-08 15:28:49 +0100

    missing queue.js

---
+++ modules/ceo/docker/src/ceo/static/js/queue.js
@@ -0,0 +1,42 @@
+var Queue = function(worker) {
+
+    var queue = [];
+    var delay = 100;
+    var running = false;
+
+    var enqueue = function(o, params) {
+        var promise = new Promise(function(resolve, reject) {
+            queue.push({
+                o: o,
+                resolve: resolve,
+                reject: reject,
+                params: params
+            });
+        });
+        promise.then(poll, poll);
+        return promise;
+    };
+
+    var poll = function() {
+        running = false;
+        var message = queue.shift();
+        if (message) {
+            running = true;
+            worker(message.o, message.resolve, message.reject, message.params);
+        } else {
+            setTimeout(poll, delay);
+        }
+    };
+
+    var empty = function() {
+        return !running && queue.length == 0;
+    }
+
+    setTimeout(poll, delay);
+
+    return {
+        enqueue: enqueue,
+        empty: empty
+    }
+
+}
