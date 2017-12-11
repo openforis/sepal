558e44113404b9678c98442b2f8fc18bb3976d99
Mino Togna <Cosimo.Togna@Fao.org>
2017-12-11 16:41:53 +0100

    updated sensors and labels

--- modules/gui/frontend/src/components/search/model/sensors.js
+++ modules/gui/frontend/src/components/search/model/sensors.js
@@ -3,60 +3,67 @@
  */
 
 var Sensors = {
+  
+  'LANDSAT': {
+    'LANDSAT_8': {
+      id                : 'LANDSAT_8',
+      name              : 'Landsat 8',
+      shortName         : 'L8',
+      selected          : true,
+      surfaceReflectance: true
+    },
     
-    'LANDSAT': {
-        'LANDSAT_8': {
-            id       : 'LANDSAT_8',
-            name     : 'Landsat 8',
-            shortName: 'L8',
-            selected : true
-        },
-        
-        'LANDSAT_7': {
-            id       : 'LANDSAT_7',
-            name     : 'Landsat 7',
-            shortName: 'L7',
-            selected : true
-        },
-        
-        'LANDSAT_TM': {
-            id       : 'LANDSAT_TM',
-            name     : 'Landsat 4-5',
-            shortName: 'L4-5',
-            selected : true
-        },
-      
-        'LANDSAT_8_T2': {
-            id       : 'LANDSAT_8_T2',
-            name     : 'Landsat 8 Tier 2',
-            shortName: 'L8 T2',
-            selected : false
-        },
-        
-        'LANDSAT_7_T2': {
-            id       : 'LANDSAT_7_T2',
-            name     : 'Landsat 7 Tier 2',
-            shortName: 'L7 T2',
-            selected : false
-        },
-        
-        'LANDSAT_TM_T2': {
-            id       : 'LANDSAT_TM_T2',
-            name     : 'Landsat 4-5 Tier 2',
-            shortName: 'L5 T2',
-            selected : false
-        }
+    'LANDSAT_7': {
+      id                : 'LANDSAT_7',
+      name              : 'Landsat 7',
+      shortName         : 'L7',
+      selected          : true,
+      surfaceReflectance: true
+    },
+    
+    'LANDSAT_TM': {
+      id                : 'LANDSAT_TM',
+      name              : 'Landsat 4-5',
+      shortName         : 'L4-5',
+      selected          : true,
+      surfaceReflectance: false
+    },
+    
+    'LANDSAT_8_T2': {
+      id                : 'LANDSAT_8_T2',
+      name              : 'Landsat 8 Tier 2',
+      shortName         : 'L8 T2',
+      selected          : false,
+      surfaceReflectance: true
+    },
+    
+    'LANDSAT_7_T2': {
+      id                : 'LANDSAT_7_T2',
+      name              : 'Landsat 7 Tier 2',
+      shortName         : 'L7 T2',
+      selected          : false,
+      surfaceReflectance: true
+    },
+    
+    'LANDSAT_TM_T2': {
+      id                : 'LANDSAT_TM_T2',
+      name              : 'Landsat 4-5 Tier 2',
+      shortName         : 'L5 T2',
+      selected          : false,
+      surfaceReflectance: false
     }
-    
-    , 'SENTINEL2': {
-        'SENTINEL2A': {
-            id       : 'SENTINEL2A',
-            name     : 'SENTINEL-2A',
-            shortName: 'S-2A',
-            selected : true
-        }
+  }
+  
+  , 'SENTINEL2': {
+    'SENTINEL2A': {
+      id                : 'SENTINEL2A',
+      name              : 'SENTINEL-2A',
+      shortName         : 'S-2A',
+      selected          : true,
+      surfaceReflectance: false
     }
-    
+  }
+  
 }
 
 module.exports = Sensors
