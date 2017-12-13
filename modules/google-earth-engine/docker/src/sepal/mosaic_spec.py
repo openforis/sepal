a0ca018b8a55273462e8e77c28deee41a9be0b24
Daniel Wiell <daniel.wiell@fao.org>
2017-12-13 14:57:05 +0100

    Reformatted code.

--- modules/google-earth-engine/docker/src/sepal/mosaic_spec.py
+++ modules/google-earth-engine/docker/src/sepal/mosaic_spec.py
@@ -42,13 +42,32 @@ class MosaicSpec(ImageSpec):
 
 
 _viz_by_bands = {
-    'red, green, blue': lambda params: {'bands': 'red, green, blue', 'min': '200, 400, 600', 'max': '2400, 2200, 2400',
-                                        'gamma': 1.2},
-    'nir, red, green': lambda params: {'bands': 'nir, red, green', 'min': '500, 200, 400', 'max': '5000, 2400, 2200'},
-    'nir, swir1, red': lambda params: {'bands': 'nir, swir1, red', 'min': 0, 'max': 5000, 'gamma': 1.5},
-    'swir2, nir, red': lambda params: {'bands': 'swir2, nir, red', 'min': '0, 500, 200', 'max': '1800, 6000, 3500'},
-    'swir2, swir1, red': lambda params: {'bands': 'swir2, swir1, red', 'min': '0, 500, 200', 'max': '1800, 3000, 2400'},
-    'swir2, nir, green': lambda params: {'bands': 'swir2, nir, green', 'min': '0, 500, 400', 'max': '1800, 6000, 3500'},
+    'red, green, blue': lambda params: {
+        'bands': 'red, green, blue',
+        'min': '200, 400, 600',
+        'max': '2400, 2200, 2400',
+        'gamma': 1.2},
+    'nir, red, green': lambda params: {
+        'bands': 'nir, red, green',
+        'min': '500, 200, 400',
+        'max': '5000, 2400, 2200'},
+    'nir, swir1, red': lambda params: {
+        'bands': 'nir, swir1, red',
+        'min': 0,
+        'max': 5000,
+        'gamma': 1.5},
+    'swir2, nir, red': lambda params: {
+        'bands': 'swir2, nir, red',
+        'min': '0, 500, 200',
+        'max': '1800, 6000, 3500'},
+    'swir2, swir1, red': lambda params: {
+        'bands': 'swir2, swir1, red',
+        'min': '0, 500, 200',
+        'max': '1800, 3000, 2400'},
+    'swir2, nir, green': lambda params: {
+        'bands': 'swir2, nir, green',
+        'min': '0, 500, 400',
+        'max': '1800, 6000, 3500'},
     'unixTimeDays': lambda params: {
         'bands': 'unixTimeDays',
         'min': params['from_days_since_epoch'],
