593fe5889a9546661c2d50f272de8d359f69dac5
Daniel Wiell <daniel.wiell@fao.org>
2018-01-15 13:31:51 +0100

    Allow home directory size check in Windows.

--- modules/sepal-server/src/main/groovy/org/openforis/sepal/component/files/query/GbUsed.groovy
+++ modules/sepal-server/src/main/groovy/org/openforis/sepal/component/files/query/GbUsed.groovy
@@ -1,5 +1,6 @@
 package org.openforis.sepal.component.files.query
 
+import org.apache.commons.lang.SystemUtils
 import org.openforis.sepal.query.Query
 import org.openforis.sepal.query.QueryHandler
 import org.openforis.sepal.util.Terminal
@@ -20,11 +21,16 @@ class GbUsedHandler implements QueryHandler<Double, GbUsed> {
     Double execute(GbUsed query) {
         def userDir = new File(homeDir, query.username)
         userDir.mkdirs()
-        def du = Terminal.execute(userDir,
-                '/bin/bash', '-c',
-                'command -v ionice >/dev/null 2>&1 && ionice -c 2 -n 7 nice -n 19 du -sk . || nice -n 19 du -sk .'
-        )
-        def bytes = du.split(/\W/).first() as double
+        double bytes
+        if (SystemUtils.IS_OS_WINDOWS)
+            bytes = userDir.directorySize()
+        else {
+            def du = Terminal.execute(userDir,
+                    '/bin/bash', '-c',
+                    'command -v ionice >/dev/null 2>&1 && ionice -c 2 -n 7 nice -n 19 du -sk . || nice -n 19 du -sk .'
+            )
+            bytes = du.split(/\W/).first() as double
+        }
         return bytes / 1000 / 1000
     }
 }
