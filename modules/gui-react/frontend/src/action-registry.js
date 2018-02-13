7cfa23f7096c04953e27e637781b02452c742f94
Daniel Wiell <daniel.wiell@fao.org>
2018-02-13 08:20:40 +0100

    Renamed reducer-registry to action-registry.

---
+++ modules/gui-react/frontend/src/action-registry.js
@@ -0,0 +1,19 @@
+class ActionRegistry {
+    reducers = {}
+
+    register(actionType, reducer, actionCreator) {
+        this.reducers[actionType] = reducer
+        actionCreator = actionCreator ? actionCreator : () => ({})
+        return (...args) => Object.assign({type: actionType}, actionCreator(...args))
+    }
+
+    rootReducer() {
+        return (state = [], action) => {
+            const reducer = this.reducers[action.type]
+            return reducer ? reducer(state, action) : state
+        }
+    }
+}
+
+const actionRegistry = new ActionRegistry()
+export default actionRegistry
