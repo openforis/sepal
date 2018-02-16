8eb933e113826159f67f531a75b37a1e7b70effd
Daniel Wiell <daniel.wiell@fao.org>
2018-02-16 14:44:44 +0100

    Switched worker instances to Amazon Linux AMI.

--- ansible/pull-worker-images/templates/pull_worker_images.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sg docker -c 'docker login -e "{{ docker_email }}" -p "{{ docker_password }}" -u "{{ docker_username }}" {{ docker_repository_host }}'
-sg docker -c 'docker pull {{ docker_repository_host }}/openforis/sandbox:{{ version }}'
-sg docker -c 'docker pull {{ docker_repository_host }}/openforis/google-earth-engine:{{ version }}'
-sg docker -c 'docker pull {{ docker_repository_host }}/openforis/task-executor:{{ version }}'
-sg docker -c 'docker logout {{ docker_repository_host }}'
