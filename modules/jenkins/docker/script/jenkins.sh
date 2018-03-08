0ee551900a1369c638c4d346f82fd6095ddc59d7
Daniel Wiell <daniel.wiell@fao.org>
2018-03-08 14:48:04 +0100

    Installed Java and Maven using sdkman.

--- modules/jenkins/docker/script/jenkins.sh
+++ modules/jenkins/docker/script/jenkins.sh
@@ -25,7 +25,7 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-   exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
+   exec /usr/local/lib/sdkman/candidates/java/current/bin/java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
