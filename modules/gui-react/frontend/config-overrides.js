0451d1dee807de577231c88d7cc85bf80c015748
Daniel Wiell <daniel.wiell@fao.org>
2018-03-22 12:18:56 +0100

    Added ability to start sandboxes.

--- modules/gui-react/frontend/config-overrides.js
+++ modules/gui-react/frontend/config-overrides.js
@@ -1,91 +1,98 @@
-module.exports = function override(config, env) {
-    // verifyAllKeysTranslated(['en', 'es'])
+module.exports = {
+    devServer: function (configFunction) {
+        return function (proxy, allowedHost) {
+            const config = configFunction(proxy, allowedHost)
+            return config
+        }
+    },
+    webpack: function override(config, env) {
+        // verifyAllKeysTranslated(['en', 'es'])
 
-    configureCssModuleLoader(config.module.rules)
-    return config
+        configureCssModuleLoader(config.module.rules)
+        return config
 
-    function configureImportsLoaders(importsLoaders) {
-        Object.keys(importsLoaders).forEach(path =>
-            config.module.rules = (config.module.rules || []).concat({
-                test: require.resolve(path),
-                enforce: 'pre',
-                use: 'imports-loader?' + importsLoaders[path]
-            })
-        )
-    }
-
-    /**
-     * Find the /\.css$/ matching rule with a css-loader.
-     * Exclude /\.module\.css$/.
-     * Clone it to match /\.module\.css$/, and enable module support.
-     */
-    function configureCssModuleLoader(o) {
-        if (o instanceof Array)
-            return o.find((item, i) => {
-                if (item instanceof Object && 'test' in item && String(item.test) === '/\\.css$/') {
-                    const cssLoader = findCssLoader(item)
-                    if (cssLoader) {
-                        const loaders = o
-                        const globalCssConfig = item
-                        const moduleCssConfig = JSON.parse(JSON.stringify(globalCssConfig))
-
-                        globalCssConfig.exclude = /\.module\.css$/
-                        moduleCssConfig.test = /\.module\.css$/
-                        loaders.splice(i, 0, moduleCssConfig)
-
-                        const cssModuleLoader = findCssLoader(moduleCssConfig)
-                        cssModuleLoader.options.modules = true
-                        cssModuleLoader.options.localIdentName = '[name]__[local]___[hash:base64:5]'
-                        return globalCssConfig
-                    } else
-                        return null
-                } else
-                    configureCssModuleLoader(item)
-            })
-        else if (o instanceof Object)
-            return Object.keys(o).find((key) => configureCssModuleLoader(o[key]))
-        else
-            return null
-    }
-
-    function findCssLoader(o) {
-        if (o instanceof Array) {
-            return o
-                .map((item) => findCssLoader(item))
-                .filter((item) => item)
-                .find((item) => item)
-        } else if (o instanceof Object) {
-            if ('loader' in o && o.loader.indexOf('/css-loader/') > -1)
-                return o
-            else
-                return Object.keys(o).map((key) => findCssLoader(o[key]))
-                    .filter((item) => item)
-                    .find((item) => item)
-        } else
-            return null
-    }
-    
-    function verifyAllKeysTranslated(locales) {
-        const _ = require('lodash')
-        const flat = require('flat')
-
-        function getLocalesInDirectory(directory) {
-            const fs = require('fs')
-            return fs.readdirSync(directory).filter((name) => /^[^\\.]/.test(name))
+        function configureImportsLoaders(importsLoaders) {
+            Object.keys(importsLoaders).forEach(path =>
+                config.module.rules = (config.module.rules || []).concat({
+                    test: require.resolve(path),
+                    enforce: 'pre',
+                    use: 'imports-loader?' + importsLoaders[path]
+                })
+            )
         }
 
-        const incompleteKeys = _.chain(locales || getLocalesInDirectory('src/locale'))
-            .map((locale) => require(`./src/locale/${locale}/translations`))
-            .map((messages) =>
-                _.chain(flat.flatten(messages))
-                    .pickBy(_.identity)
-                    .keys()
-                    .value())
-            .thru((keys) => _.difference(_.union(...keys), _.intersection(...keys)))
-            .value()
+        /**
+         * Find the /\.css$/ matching rule with a css-loader.
+         * Exclude /\.module\.css$/.
+         * Clone it to match /\.module\.css$/, and enable module support.
+         */
+        function configureCssModuleLoader(o) {
+            if (o instanceof Array)
+                return o.find((item, i) => {
+                    if (item instanceof Object && 'test' in item && String(item.test) === '/\\.css$/') {
+                        const cssLoader = findCssLoader(item)
+                        if (cssLoader) {
+                            const loaders = o
+                            const globalCssConfig = item
+                            const moduleCssConfig = JSON.parse(JSON.stringify(globalCssConfig))
 
-        if (incompleteKeys.length)
-            throw new Error(`Missing translations: \n\t${incompleteKeys.join('\n\t')}\n\n`)
+                            globalCssConfig.exclude = /\.module\.css$/
+                            moduleCssConfig.test = /\.module\.css$/
+                            loaders.splice(i, 0, moduleCssConfig)
+
+                            const cssModuleLoader = findCssLoader(moduleCssConfig)
+                            cssModuleLoader.options.modules = true
+                            cssModuleLoader.options.localIdentName = '[name]__[local]___[hash:base64:5]'
+                            return globalCssConfig
+                        } else
+                            return null
+                    } else
+                        configureCssModuleLoader(item)
+                })
+            else if (o instanceof Object)
+                return Object.keys(o).find((key) => configureCssModuleLoader(o[key]))
+            else
+                return null
+        }
+
+        function findCssLoader(o) {
+            if (o instanceof Array) {
+                return o
+                    .map((item) => findCssLoader(item))
+                    .filter((item) => item)
+                    .find((item) => item)
+            } else if (o instanceof Object) {
+                if ('loader' in o && o.loader.indexOf('/css-loader/') > -1)
+                    return o
+                else
+                    return Object.keys(o).map((key) => findCssLoader(o[key]))
+                        .filter((item) => item)
+                        .find((item) => item)
+            } else
+                return null
+        }
+
+        function verifyAllKeysTranslated(locales) {
+            const _ = require('lodash')
+            const flat = require('flat')
+
+            function getLocalesInDirectory(directory) {
+                const fs = require('fs')
+                return fs.readdirSync(directory).filter((name) => /^[^\\.]/.test(name))
+            }
+
+            const incompleteKeys = _.chain(locales || getLocalesInDirectory('src/locale'))
+                .map((locale) => require(`./src/locale/${locale}/translations`))
+                .map((messages) =>
+                    _.chain(flat.flatten(messages))
+                        .pickBy(_.identity)
+                        .keys()
+                        .value())
+                .thru((keys) => _.difference(_.union(...keys), _.intersection(...keys)))
+                .value()
+
+            if (incompleteKeys.length)
+                throw new Error(`Missing translations: \n\t${incompleteKeys.join('\n\t')}\n\n`)
+        }
     }
 }
-
--- modules/gui-react/frontend/src/app/home/body/apps/apps.js
+++ modules/gui-react/frontend/src/app/home/body/apps/apps.js
@@ -6,31 +6,73 @@ import styles from './apps.module.css'
 import {msg} from 'translate'
 import Icon from 'widget/icon'
 import rstudioIcon from './r-studio.png'
+import Rx from 'rxjs'
 
 const mapStateToProps = () => ({
-    apps: select('apps')
+    apps: select('apps.list'),
+    runningApps: select('apps.running')
 })
+
+function loadApps$() {
+    return Http.get$('/apps')
+        .map((e) =>
+            actionBuilder('SET_APPS')
+                .set('apps.list', e.response)
+                .build()
+        )
+}
+
+function requestSandbox$(app) {
+    const url = `/sandbox/start?endpoint=${app.endpoint ? app.endpoint : 'shiny'}`
+    return Http.post$(url)
+        .takeWhile((e) => e.response.status === 'STARTING')
+        .switchMap(() => Rx.Observable.interval(1000)
+            // .map((secondsPassed) => {
+            //     if (secondsPassed > 30) throw new Error('Timed out')
+            //     else return secondsPassed
+            // })
+            .switchMap((secondsPassed) => secondsPassed < 30
+                ? Rx.Observable.of(secondsPassed)
+                : Rx.Observable.throw({message: msg('')}))
+            .exhaustMap(() => Http.get$(url))
+            .takeWhile((e) => e.response.status === 'STARTING')
+        )
+}
 
 class Apps extends React.Component {
     componentWillMount() {
         this.props.asyncActionBuilder('LOAD_APPS',
-            Http.get$('/apps')
-                .map((e) =>
-                    actionBuilder('SET_APPS')
-                        .set('apps', e.response)
-                        .build()
-                )).dispatch()
+            loadApps$())
+            .dispatch()
+    }
+
+    openApp(app) {
+        this.props.asyncActionBuilder('OPEN_APP',
+            requestSandbox$(app))
+            .onComplete(() =>
+                actionBuilder('SHOW_APP')
+                    .push(['apps', 'running'], app)
+                    .build()
+            )
+            .dispatch()
     }
 
     render() {
+        if (this.props.runningApps && this.props.runningApps.length) {
+            const app = this.props.runningApps[0]
+            return (
+                <iframe src={`http://localhost:8001/sandbox/${app.path}`} title={app.label}/>
+            )
+        }
+
         const {apps, action} = this.props
         if (!action('LOAD_APPS').dispatched)
             return <div>Spinner</div>
 
-        const dataVis = {path: '/data-vis', label: msg('apps.dataVis'), icon: 'map-o'}
-        const rStudio = {path: '/rstudio', image: rstudioIcon, alt: 'RStudio'}
+        const dataVis = {path: '/data-vis', label: msg('apps.dataVis'), icon: 'map-o', endpoint: 'geo-web-viz'}
+        const rStudio = {path: '/rstudio', image: rstudioIcon, alt: 'RStudio', endpoint: 'rstudio'}
         const items = [dataVis, rStudio, ...apps].map(
-            (app) => <App key={app.path} app={app}/>
+            (app) => <App key={app.path} app={app} onClick={this.openApp.bind(this)}/>
         )
         return (
             <div className={styles.apps}>
@@ -42,12 +84,12 @@ class Apps extends React.Component {
 
 export default Apps = connect(mapStateToProps)(Apps)
 
-const App = ({app: {path, label, icon, image, alt}}) =>
-    <div className={styles.app}>
-        <Image src={image}/>
-        <Icon name={icon} alt={alt}/>
-        {label}
-    </div>
+const App = ({app, onClick}) =>
+    <button className={styles.app} onClick={() => onClick(app)}>
+        <Image src={app.image}/>
+        <Icon name={app.icon} alt={app.alt}/>
+        {app.label}
+    </button>
 
 const Image = ({src, alt}) => {
     if (!src)
