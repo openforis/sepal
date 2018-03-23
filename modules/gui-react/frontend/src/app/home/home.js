51b5dace06b717841fb5a3e6fb54bfe624c7c34c
Luca Paolini <luca.paolini@fao.org>
2018-03-23 16:32:38 +0100

    Implemented fixed/collapsible panel

--- modules/gui-react/frontend/src/app/home/home.js
+++ modules/gui-react/frontend/src/app/home/home.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import {connect, select} from 'store'
 import PropTypes from 'prop-types'
 import styles from './home.module.css'
 import Sections from './sections/sections'
 import Footer from './footer/footer'
 import Body from './body/body'
 
-const Home = ({user}) =>
-    <div className={styles.home}>
-        <Sections/>
-        <Footer className={styles.footer} user={user}/>
-        <Body/>
-    </div>
-export default Home
+const mapStateToProps = () => ({
+    fixedMenu: select('menu.locked') || false
+})
+
+const Home = ({user, fixedMenu}) => {
+    return (
+        <div className={[styles.home, fixedMenu && styles.fixedMenu].join(' ')}>
+            <Sections/>
+            <Footer className={styles.footer} user={user}/>
+            <Body/>
+        </div>
+    )
+}
+export default connect(mapStateToProps)(Home)
 
 Home.propTypes = {
     user: PropTypes.object.isRequired
--- modules/gui-react/frontend/src/app/home/sections/sections.js
+++ modules/gui-react/frontend/src/app/home/sections/sections.js
@@ -3,31 +3,45 @@ import styles from './sections.module.css'
 // import {IconButton} from 'widget/button'
 import Tooltip from 'widget/tooltip'
 import {Link} from 'route'
-import {connect} from 'store'
+import {connect, select} from 'store'
 import {runningApps} from 'app/home/body/apps/apps'
 import Icon from 'widget/icon'
+import actionBuilder from 'action-builder'
+import { dispatch } from '../../../store';
 
 const mapStateToProps = () => ({
-    runningApps: runningApps()
+    runningApps: runningApps(),
+    locked: select('menu.locked')
 })
 
-let Sections = ({runningApps}) => {
-    const appSections = runningApps.map((app) => <App key={app.path} app={app}/>)
-
-    return <div className={styles.sectionsContainer}>
-        <div className={styles.sections}>
-            {/*<Section name='dashboard' icon='dashboard'/>*/}
-            <Section name='process' icon='globe'/>
-            <Section name='browse' icon='folder-open'/>
-            <Section name='apps' icon='wrench'/>
-            <Section name='terminal' icon='terminal'/>
-            {appSections}
-        </div>
-        {/*<div className={styles.sections}>*/}
-            {/*{appSections}*/}
-        {/*</div>*/}
-    </div>
+class Sections extends React.Component {
+    appSection(app) {
+        return <App key={app.path} app={app}/>
+    }
+    toggle() {
+        actionBuilder('TOGGLE_MENU')
+            .set('menu.locked', !this.props.locked)
+            .dispatch()
+    }
+    render() {
+        return (
+            <div className={styles.sectionsContainer}>
+                <div className={[styles.sections, this.props.locked && styles.locked].join(' ')}>
+                    <button className={styles.lock} onClick={this.toggle.bind(this)}>
+                        <Icon name={this.props.locked ? 'lock' : 'unlock'}/>
+                    </button>
+                    {/*<Section name='dashboard' icon='dashboard'/>*/}
+                    <Section name='process' icon='globe'/>
+                    <Section name='browse' icon='folder-open'/>
+                    <Section name='terminal' icon='terminal'/>
+                    <Section name='apps' icon='wrench'/>
+                    {this.props.runningApps.map(this.appSection)}
+                </div>
+            </div>
+        )
+    }
 }
+
 export default Sections = connect(mapStateToProps)(Sections)
 
 const Section = ({name, icon}) =>
@@ -43,7 +57,7 @@ const App = ({app: {path, label}}) =>
     <Link to={'/app' + path} onMouseDown={(e) => e.preventDefault()}>
         {/*<Tooltip rawMsg={label} right>*/}
         <button className={styles.app}>
-            <Icon name={'wrench'}/>
+            <Icon name={'cubes'}/>
         </button>
         {/*</Tooltip>*/}
     </Link>
