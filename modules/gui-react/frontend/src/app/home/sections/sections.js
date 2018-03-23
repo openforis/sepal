d691f9801206b6ed2b9e7a23dcfa22e5ef615b54
Luca Paolini <luca.paolini@fao.org>
2018-03-23 13:03:34 +0100

    Styled sliding panel

--- modules/gui-react/frontend/src/app/home/sections/sections.js
+++ modules/gui-react/frontend/src/app/home/sections/sections.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './sections.module.css'
-import {IconButton} from 'widget/button'
+// import {IconButton} from 'widget/button'
 import Tooltip from 'widget/tooltip'
 import {Link} from 'route'
 import {connect} from 'store'
 import {runningApps} from 'app/home/body/apps/apps'
+import Icon from 'widget/icon'
 
 const mapStateToProps = () => ({
     runningApps: runningApps()
@@ -15,7 +16,7 @@ let Sections = ({runningApps}) => {
 
     return <div className={styles.sectionsContainer}>
         <div className={styles.sections}>
-            {/*<Section name='dashboard' icon='home'/>*/}
+            {/*<Section name='dashboard' icon='dashboard'/>*/}
             <Section name='process' icon='globe'/>
             <Section name='browse' icon='folder-open'/>
             <Section name='apps' icon='wrench'/>
@@ -32,17 +33,17 @@ export default Sections = connect(mapStateToProps)(Sections)
 const Section = ({name, icon}) =>
     <Link to={'/' + name} onMouseDown={(e) => e.preventDefault()}>
         <Tooltip msg={'home.sections.' + name} right>
-            <IconButton
-                icon={icon}
-                className={`${styles.section} ${styles[name + 'Icon']}`}/>
+            <button className={`${styles[name]}`}>
+                <Icon name={icon}/>
+            </button>
         </Tooltip>
     </Link>
 
 const App = ({app: {path, label}}) =>
     <Link to={'/app' + path} onMouseDown={(e) => e.preventDefault()}>
         {/*<Tooltip rawMsg={label} right>*/}
-            <IconButton
-                icon='wrench'
-                className={`${styles.section}`}/>
+        <button className={styles.app}>
+            <Icon name={'wrench'}/>
+        </button>
         {/*</Tooltip>*/}
     </Link>
