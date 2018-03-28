2f6f98f2d017c435fc3504b54da515bbbc4fe68a
Luca Paolini <luca.paolini@fao.org>
2018-03-28 13:29:17 +0200

    Switched from document load to iframe onload.

--- modules/gui-react/frontend/src/app/home/body/iframe.js
+++ modules/gui-react/frontend/src/app/home/body/iframe.js
@@ -1,55 +1,54 @@
 import React from 'react'
 import {connect} from 'store'
-import PropTypes from 'prop-types'
 import {appReady, appState} from 'apps'
 import {CenteredProgress} from 'widget/progress'
 import {msg} from 'translate'
+import PropTypes from 'prop-types'
 
 const mapStateToProps = (_, currentProps) => ({
     appState: appState(currentProps.app.path),
 })
 
 class IFrame extends React.Component {
+    initializing(label, alt) {
+        return <CenteredProgress title={msg('apps.initializing', {label: label || alt})}/>
+    }
+    loading(label, alt) {
+        return <CenteredProgress title={msg('apps.loading', {label: label || alt})}/>
+    }
     render() {
         const {app: {path, label, alt}, appState} = this.props
-        const loadingProgress = appState === 'READY'
-            ? null
-            : <CenteredProgress title={msg('apps.loading', {label: label || alt})}/>
-        if (appState === 'REQUESTED')
-            return <CenteredProgress title={msg('apps.initializing', {label: label || alt})}/>
-        else
-            return (
+        if (appState === 'REQUESTED') 
+            return this.initializing(label, alt)
+        
+        return appState === 'REQUESTED' 
+            ? this.initializing(label, alt)
+            : (
                 <div>
-                    {loadingProgress}
+                    {appState !== 'READY' && this.loading(label, alt)}
                     <iframe
                         ref={(iframe) => this.iframe = iframe}
                         width='100%'
                         height='100%'
                         frameBorder='0'
-                        src={path} title={label || alt}
+                        src={path} 
+                        title={label || alt}
                         style={{display: appState === 'READY' ? 'block' : 'none'}}
+                        onLoad={() => appReady(this.props.app)}
                     />
                 </div>
             )
     }
-
-    componentDidUpdate() {
-        this.addReadyListener(this.props)
-    }
-
-    addReadyListener({appState, app}) {
-        if (!this.ready && appState === 'INITIALIZED') {
-            this.ready = true
-            this.iframe.contentWindow.document.addEventListener(
-                'load',
-                appReady(app),
-                // () => appReady(app),
-                false)
-        }
-    }
 }
 
 export default IFrame = connect(mapStateToProps)(IFrame)
+
+IFrame.propTypes = {
+    path: PropTypes.string,
+    label: PropTypes.string,
+    alt: PropTypes.string,
+    appState: PropTypes.string
+}
 
 IFrame.contextTypes = {
     active: PropTypes.bool,
