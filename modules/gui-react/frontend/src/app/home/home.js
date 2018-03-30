1d88d5253cb38a5adbadc0e1da85108fff723ea9
Daniel Wiell <daniel.wiell@fao.org>
2018-03-30 10:02:46 +0200

    Added map placeholder.

--- modules/gui-react/frontend/src/app/home/home.js
+++ modules/gui-react/frontend/src/app/home/home.js
@@ -1,10 +1,10 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import {connect} from 'store'
-import PropTypes from 'prop-types'
+import Body from './body/body'
+import Footer from './footer/footer'
 import styles from './home.module.css'
 import NavBar, {isNavBarLocked} from './navBar/navBar'
-import Footer from './footer/footer'
-import Body from './body/body'
 
 const mapStateToProps = () => ({
     navBarLocked: isNavBarLocked()
@@ -12,10 +12,14 @@ const mapStateToProps = () => ({
 
 const Home = ({user, navBarLocked}) => {
     return (
-        <div className={[styles.home, navBarLocked && styles.navBarLocked].join(' ')}>
-            <NavBar className={styles.navBar}/>
-            <Footer className={styles.footer} user={user}/>
-            <Body className={styles.body}/>
+        <div className={styles.homeContainer}>
+            <div className={styles.map}/>
+            <div className={styles.mapOverlay}/>
+            <div className={[styles.home, navBarLocked && styles.navBarLocked].join(' ')}>
+                <NavBar className={styles.navBar}/>
+                <Footer className={styles.footer} user={user}/>
+                <Body className={styles.body}/>
+            </div>
         </div>
     )
 }
