da7d7cde955787cdcbc971cff8d1a9082ce56550
Daniel Wiell <daniel.wiell@fao.org>
2018-04-20 10:57:05 +0200

    Added props to recipe actions, for debugging.

--- modules/gui-react/frontend/src/app/home/body/process/mosaic/mosaicRecipe.js
+++ modules/gui-react/frontend/src/app/home/body/process/mosaic/mosaicRecipe.js
@@ -18,30 +18,30 @@ export const RecipeState = (id) => {
 }
 
 export const RecipeActions = (id) => {
-    const _actionBuilder = (name) => {
-        return actionBuilder(name)
+    const _actionBuilder = (name, props) => {
+        return actionBuilder(name, props)
             .within(recipePath(id))
     }
-    const set = (name, prop, value) => {
-        _actionBuilder(name)
+    const set = (name, prop, value, otherProps) => {
+        _actionBuilder(name, otherProps)
             .set(prop, value)
             .dispatch()
     }
     return {
         setLabelsShown(shown) {
-            set('SET_LABELS_SHOWN', 'labelsShown', shown)
+            set('SET_LABELS_SHOWN', 'labelsShown', shown, {shown})
         },
         setGridShown(shown) {
-            set('SET_GRID_SHOWN', 'gridShown', shown)
+            set('SET_GRID_SHOWN', 'gridShown', shown, {shown})
         },
         selectPanel(panel) {
-            set('SELECT_MOSAIC_PANEL', 'selectedPanel', panel)
+            set('SELECT_MOSAIC_PANEL', 'selectedPanel', panel, {panel})
         },
         setAoi(aoi) {
-            set('SET_AOI', 'aoi', aoi)
+            set('SET_AOI', 'aoi', aoi, {aoi})
         },
         setModal(enabled) {
-            set('SET_MODAL', 'modal', enabled)
+            set('SET_MODAL', 'modal', enabled, {enabled})
         }
     }
 }
