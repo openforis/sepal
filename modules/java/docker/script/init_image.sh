e201897c298eec6d64c2f882b0044cdf386abca6
Daniel Wiell <daniel.wiell@fao.org>
2018-05-02 09:59:16 +0200

    Upgraded to Java 10.

--- modules/java/docker/script/init_image.sh
+++ modules/java/docker/script/init_image.sh
@@ -14,6 +14,6 @@ apt-get -y update && apt-get install -qq -y \
 export SDKMAN_DIR=/usr/local/lib/sdkman
 curl -s get.sdkman.io | bash
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
-yes | sdk install java 8u161-oracle
+yes | sdk install java 10.0.1-oracle
 
 ln -s `which java` /usr/local/bin/java
