e142f699f601125fb95b70cb68fce123fdd7d3df
rfontanarosa <robertofontanarosa@gmail.com>
2018-05-02 15:40:48 +0200

    fix to prevent that the "export csv button" alert that there are values not saved

--- modules/ceo/docker/src/ceo/static/js/queue.js
+++ modules/ceo/docker/src/ceo/static/js/queue.js
@@ -3,8 +3,11 @@ var Queue = function(worker) {
     var queue = [];
     var delay = 100;
     var running = false;
+    var emptyListeners = [];
+    var empty = true;
 
     var enqueue = function(o, params) {
+        setEmpty(false);
         var promise = new Promise(function(resolve, reject) {
             queue.push({
                 o: o,
@@ -20,6 +23,7 @@ var Queue = function(worker) {
     var poll = function() {
         running = false;
         var message = queue.shift();
+        setEmpty(queue.length === 0);
         if (message) {
             running = true;
             worker(message.o, message.resolve, message.reject, message.params);
@@ -28,14 +32,29 @@ var Queue = function(worker) {
         }
     };
 
-    var empty = function() {
+    var isEmpty = function() {
+        return empty;
+    };
+
+    var setEmpty = function(_empty) {
+        if (_empty != empty) {
+            emptyListeners.forEach(function (listener) { 
+                listener(_empty);
+            })
+        }
+        empty = _empty;
+    }
+
+    var addEmptyListener = function(listener) {
+        emptyListeners.push(listener);
     };
 
     setTimeout(poll, delay);
 
     return {
         enqueue: enqueue,
-        empty: empty
+        empty: isEmpty,
+        addEmptyListener: addEmptyListener
     };
 
 };
