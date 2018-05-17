04ed76b134c3104c9d77466126cdf46d6ddfa4dd
Daniel Wiell <daniel.wiell@fao.org>
2018-05-17 15:52:37 +0200

    Created component to render panel form.

---
+++ modules/gui-react/frontend/src/app/home/body/process/mosaic/panels/panelForm.js
@@ -0,0 +1,37 @@
+import ConfirmationButtons from './confirmationButtons'
+import PropTypes from 'prop-types'
+import React from 'react'
+import PanelContent from './panelContent'
+import styles from './panelForm.module.css'
+
+const PanelForm = ({recipeId, form, onApply, icon, onBack, title, className, children}) => {
+    return (
+        <PanelContent
+            icon={icon}
+            onBack={onBack}
+            title={title}
+            className={className}>
+            {children}
+            <div className={styles.footer}>
+                <ConfirmationButtons
+                    recipeId={recipeId}
+                    form={form}
+                    onApply={onApply}
+                />
+            </div>
+        </PanelContent>
+    )
+}
+
+PanelForm.propTypes = {
+    recipeId: PropTypes.string,
+    form: PropTypes.object,
+    onApply: PropTypes.func,
+    icon: PropTypes.string,
+    onBack: PropTypes.func,
+    title: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired,
+    children: PropTypes.any.isRequired
+}
+
+export default PanelForm
