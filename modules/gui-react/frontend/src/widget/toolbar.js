ccaa26ea51e0752e3d7d859bed2912a9d4e34919
Luca Paolini <luca.paolini@fao.org>
2018-06-05 17:37:50 +0200

    Implemented Toolbar component.

---
+++ modules/gui-react/frontend/src/widget/toolbar.js
@@ -0,0 +1,76 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styles from './toolbar.module.css'
+import Tooltip from 'widget/tooltip'
+import Icon from 'widget/icon'
+import {Msg} from 'translate'
+import buttonColors from 'style/button-colors.module.css'
+
+const Context = React.createContext()
+
+export class Toolbar extends React.Component {
+    render() {
+        const {className, horizontal, vertical, top, bottom, left, right} = this.props
+        const classNames = [
+            styles.toolbar, 
+            buttonColors.buttons,
+            horizontal && styles.horizontal,
+            vertical && styles.vertical,
+            top && styles.top,
+            bottom && styles.bottom,
+            left && styles.left,
+            right && styles.right
+        ]
+        return (
+            <div className={className}>
+                <div className={classNames.join(' ')}> 
+                    <Context.Provider value={!!horizontal}>
+                        {this.props.children}
+                    </Context.Provider>
+                </div>
+            </div>
+        )
+    }
+}
+
+Toolbar.propTypes = {
+    className: PropTypes.string,
+    horizontal: PropTypes.any,
+    vertical: PropTypes.any,
+    top: PropTypes.any,
+    bottom: PropTypes.any,
+    left: PropTypes.any,
+    right: PropTypes.any,
+    children: PropTypes.array
+}
+
+export class ToolbarButton extends React.Component {
+    render() {
+        const renderLabel = (msg) => <Msg id={msg}/>
+        const renderIcon = (icon) => <Icon name={icon}/>
+        const {disabled, selected, onClick, icon, label, tooltip} = this.props
+        return (
+            <Context.Consumer>
+                {(horizontal) =>
+                    <Tooltip msg={tooltip} top={horizontal} left={!horizontal} disabled={disabled || selected}>
+                        <button
+                            className={selected ? styles.selected : null} 
+                            onClick={onClick} 
+                            disabled={disabled}>
+                            {icon ? renderIcon(icon) : renderLabel(label)}
+                        </button>
+                    </Tooltip>
+                }
+            </Context.Consumer>
+        )
+    }
+}
+
+ToolbarButton.propTypes = {
+    disabled: PropTypes.any,
+    selected: PropTypes.any,
+    onClick: PropTypes.func,
+    icon: PropTypes.string,
+    label: PropTypes.string,
+    tooltip: PropTypes.string
+}
