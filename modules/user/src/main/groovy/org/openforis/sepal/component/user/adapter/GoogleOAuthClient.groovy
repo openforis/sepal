fc6bc49ce9934bd263455c33a724b1416b9818c6
Daniel Wiell <daniel.wiell@fao.org>
2018-07-18 11:55:28 +0200

    Removed devstore scope from Google OAuth.

--- modules/user/src/main/groovy/org/openforis/sepal/component/user/adapter/GoogleOAuthClient.groovy
+++ modules/user/src/main/groovy/org/openforis/sepal/component/user/adapter/GoogleOAuthClient.groovy
@@ -31,7 +31,6 @@ class GoogleOAuthException extends RuntimeException {
 class RestBackedGoogleOAuthClient implements GoogleOAuthClient {
     public static final SCOPE = '' +
             'https://www.googleapis.com/auth/earthengine ' +
-            'https://www.googleapis.com/auth/devstorage.full_control ' +
             'https://www.googleapis.com/auth/drive ' +
             'https://www.googleapis.com/auth/fusiontables'
     private final String sepalHost
