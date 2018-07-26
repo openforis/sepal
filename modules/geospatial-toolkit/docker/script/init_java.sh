1f9e8bf0415003d7c568aba8ae99b3a04d1caefe
Daniel Wiell <daniel.wiell@fao.org>
2018-07-26 10:51:48 +0200

    Updated Java version.

--- modules/geospatial-toolkit/docker/script/init_java.sh
+++ modules/geospatial-toolkit/docker/script/init_java.sh
@@ -8,7 +8,7 @@ echo "***********************"
 export SDKMAN_DIR=/usr/local/lib/sdkman
 curl -s get.sdkman.io | bash
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
-yes | sdk install java 8.0.171-oracle
+yes | sdk install java 8.0.181-oracle
 sdk install groovy
 
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
