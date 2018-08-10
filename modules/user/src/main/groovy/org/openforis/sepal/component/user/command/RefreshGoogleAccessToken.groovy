a0938d6ba150540d260db5a9fd6d481f582e04be
Daniel Wiell <daniel.wiell@fao.org>
2018-08-10 12:32:46 +0200

    Deleted Google credentials from file system when refreshing tokens and no tokens are associated with user.

--- modules/user/src/main/groovy/org/openforis/sepal/component/user/command/RefreshGoogleAccessToken.groovy
+++ modules/user/src/main/groovy/org/openforis/sepal/component/user/command/RefreshGoogleAccessToken.groovy
@@ -34,8 +34,11 @@ class RefreshGoogleAccessTokenHandler implements CommandHandler<GoogleTokens, Re
 
     GoogleTokens execute(RefreshGoogleAccessToken command) {
         def tokens = command.tokens ?: userRepository.lookupUser(command.username).googleTokens
-        if (!tokens || !tokens.shouldBeRefreshed(System.currentTimeMillis()))
+        if (!tokens || !tokens.shouldBeRefreshed(System.currentTimeMillis())) {
+            if (!tokens)
+                googleAccessTokenFileGateway.delete(command.username)
             return null
+        }
 
         def refreshedToken = null
         try {
