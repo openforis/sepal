438e878dc8f8dc8af2c62bedee680bb4d7a1d6b3
Daniel Wiell <daniel.wiell@fao.org>
2018-09-27 11:50:03 +0200

    Prevented unnecessary rebuilds of gui jar.

--- modules/gui/should-rebuild.sh
+++ modules/gui/should-rebuild.sh
@@ -4,7 +4,9 @@ rebuild=false
 if [ -f ./docker/binary/sepal-gui.jar ]; then
     found=$(find . \
         -not \( -path ./target -prune \) \
-        -not \( -path ./frontend/dist -prune \) \
+        -not \( -path ./frontend/build -prune \) \
+        -not \( -path ./frontend/node -prune \) \
+        -not \( -path ./frontend/node_modules -prune \) \
         -newer ./docker/binary/sepal-gui.jar \
         -print -quit)
     if [ ! -z "$found" ]; then
