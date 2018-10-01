518ce6bf052c01cad9be065bbcf20456043197e2
Daniel Wiell <daniel.wiell@fao.org>
2018-10-01 11:39:44 +0200

    Made sure gcloud is updated on build.

--- modules/geospatial-toolkit/docker/script/init_gcloud.sh
+++ modules/geospatial-toolkit/docker/script/init_gcloud.sh
@@ -9,6 +9,7 @@ echo "*************************************"
 wget -nv https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-152.0.0-linux-x86_64.tar.gz
 tar -xzf google-cloud-sdk-152.0.0-linux-x86_64.tar.gz
 mv google-cloud-sdk /usr/local/lib
+/usr/local/lib/google-cloud-sdk/bin/gcloud components update
 /usr/local/lib/google-cloud-sdk/install.sh -q
 ln -s /usr/local/lib/google-cloud-sdk/bin/gcloud /usr/local/bin/
 ln -s /usr/local/lib/google-cloud-sdk/bin/gsutil /usr/local/bin/
