a1e8f7bffdc236d93b7efecf2df943d950c9f6fb
Daniel Wiell <daniel.wiell@fao.org>
2018-10-16 14:28:28 +0200

    Fixed retrieval of classification with recipe-ref.

--- modules/google-earth-engine/docker/src/sepal/reciperef/__init__.py
+++ modules/google-earth-engine/docker/src/sepal/reciperef/__init__.py
@@ -6,7 +6,7 @@ class RecipeRef(ImageSpec):
         super(RecipeRef, self).__init__()
         self.image_spec = create_image_spec(sepal_api, {'recipe': sepal_api.get_recipe(spec['recipe']['id'])})
         self.aoi = self.image_spec.aoi
-        self.scale = self.image_spec.bands
+        self.scale = self.image_spec.scale
         self.bands = self._ee_image().bandNames().getInfo()
 
     def _ee_image(self):
