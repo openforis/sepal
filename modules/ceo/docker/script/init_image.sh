8a001fef93e2051665cd0f3a08b8c7098ba4769e
Daniel Wiell <daniel.wiell@fao.org>
2018-11-06 13:33:03 +0100

    Updated nodejs installation for CEO.

--- modules/ceo/docker/script/init_image.sh
+++ modules/ceo/docker/script/init_image.sh
@@ -8,8 +8,11 @@ apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -y\
  gettext\
  git\
  mongodb\
- nodejs\
  npm\
  supervisor
 
-npm install --global yarn
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get -y install yarn
