1a78489066dced8400e02b47a237d3acb85ca070
Daniel Wiell <daniel.wiell@fao.org>
2018-11-15 11:52:12 +0100

    Corrected incorrect DB schema.

--- modules/sepal-server/src/main/resources/sql/processing_recipe/V1__schema.sql
+++ modules/sepal-server/src/main/resources/sql/processing_recipe/V1__schema.sql
@@ -1,5 +1,13 @@
-SELECT *
-FROM recipe
-WHERE NOT removed
-ORDER BY creation_time DESC
-         LIMIT 10;
+CREATE SCHEMA IF NOT EXISTS processing_recipe;
+
+CREATE TABLE processing_recipe.recipe (
+                                        id            VARCHAR(255) NOT NULL,
+                                        username      VARCHAR(255) NOT NULL,
+                                        name          VARCHAR(255) NOT NULL,
+                                        type          VARCHAR(63)  NOT NULL,
+                                        contents      LONGTEXT     NOT NULL,
+                                        creation_time TIMESTAMP    NOT NULL,
+                                        update_time   TIMESTAMP    NOT NULL,
+                                        removed       BOOLEAN      NOT NULL DEFAULT FALSE,
+                                        PRIMARY KEY (id)
+);
