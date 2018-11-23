51b6814331ad81f7592f39e3d4a89c5fc3be91b9
Luca Paolini <luca.paolini@fao.org>
2018-11-23 19:02:20 +0100

    Cleanup.

--- modules/gui/frontend/src/app/home/body/process/process.js
+++ modules/gui/frontend/src/app/home/body/process/process.js
@@ -1,29 +1,29 @@
-import Revisions from 'app/home/body/process/revisions'
-import React from 'react'
-import Tabs from 'widget/tabs'
+import {saveRecipe} from './recipe'
 import ChangeDetection from './changeDetection/changeDetection'
 import Classification from './classification/classification'
 import CreateOrLoadRecipe from './createOrLoadRecipe'
 import LandCover from './landCover/landCover'
 import Mosaic from './mosaic/mosaic'
 import ProcessMenu from './processMenu'
-import {saveRecipe} from './recipe'
+import React from 'react'
+import Revisions from 'app/home/body/process/revisions'
+import Tabs from 'widget/tabs'
 import TimeSeries from './timeSeries/timeSeries'
 
 const recipeComponent = (id, type) => {
     switch (type) {
-        case 'MOSAIC':
-            return <Mosaic recipeId={id}/>
-        case 'CLASSIFICATION':
-            return <Classification recipeId={id}/>
-        case 'CHANGE_DETECTION':
-            return <ChangeDetection recipeId={id}/>
-        case 'TIME_SERIES':
-            return <TimeSeries recipeId={id}/>
-        case 'LAND_COVER':
-            return <LandCover recipeId={id}/>
-        default:
-            return <CreateOrLoadRecipe recipeId={id}/>
+    case 'MOSAIC':
+        return <Mosaic recipeId={id}/>
+    case 'CLASSIFICATION':
+        return <Classification recipeId={id}/>
+    case 'CHANGE_DETECTION':
+        return <ChangeDetection recipeId={id}/>
+    case 'TIME_SERIES':
+        return <TimeSeries recipeId={id}/>
+    case 'LAND_COVER':
+        return <LandCover recipeId={id}/>
+    default:
+        return <CreateOrLoadRecipe recipeId={id}/>
     }
 }
 
