f1814475b5340b5f09adbbe3b201d49ce30b0e8e
Luca Paolini <luca.paolini@fao.org>
2018-11-23 12:58:09 +0100

    Restyled main menu.

--- modules/gui/frontend/src/app/home/menu/menu.js
+++ modules/gui/frontend/src/app/home/menu/menu.js
@@ -59,6 +59,7 @@ let SectionLink = ({active, name, icon}) => {
             className={[styles[name], activeClass].join(' ')}
             icon={icon}
             link={link}
+            onClick={(e, he) => console.log(e, he)}
             tooltip={msg(`home.sections.${name}.tooltip`)}
             tooltipPlacement='right'/>
     )
