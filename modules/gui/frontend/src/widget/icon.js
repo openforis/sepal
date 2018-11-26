bcf3d8b0200bd9387e62d7f4ec9c7fef59a247b8
Luca Paolini <luca.paolini@fao.org>
2018-11-26 23:06:31 +0100

    Updated npm packages.

--- modules/gui/frontend/src/widget/icon.js
+++ modules/gui/frontend/src/widget/icon.js
@@ -1,4 +1,4 @@
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import PropTypes from 'prop-types'
 import React from 'react'
 import far from '@fortawesome/fontawesome-free-regular'
