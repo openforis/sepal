5e2752913f501e6071c73caa9d56511de54daf07
Daniel Wiell <daniel.wiell@fao.org>
2018-11-29 11:56:45 +0100

    Used a separate network interface for ssh-gateway.

--- modules/haproxy/docker/script/init_container.sh
+++ modules/haproxy/docker/script/init_container.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 mkdir -p /etc/haproxy
+mkdir -p /etc/sepal
 
 cp /config/haproxy.cfg /etc/haproxy/haproxy.cfg
+cat /etc/letsencrypt/live/$SEPAL_HOST_SEPAL_ENV/privkey.pem > /etc/sepal/sepal.pem
+cat /etc/letsencrypt/live/$SEPAL_HOST_SEPAL_ENV/fullchain.pem >> /etc/sepal/sepal.pem
 
 exec /usr/bin/supervisord -c /config/supervisord.conf
