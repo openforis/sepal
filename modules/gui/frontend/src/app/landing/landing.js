a862df43549ad5d24cecb056ce5f73bb0c53601f
Luca Paolini <luca.paolini@fao.org>
2018-12-13 16:20:58 +0100

    Refactored landing page, fixed mibehaving animation on Safari.

--- modules/gui/frontend/src/app/landing/landing.js
+++ modules/gui/frontend/src/app/landing/landing.js
@@ -19,37 +19,28 @@ const Landing =
             <SlideShow/>
             <div className={styles.landing}>
                 <div className={styles.tagline}>
-                    <div className={styles.content}>
-                        <Caption/>
-                    </div>
+                    <Msg id='landing.caption'/>
                 </div>
                 <div className={styles.title}>
-                    <div className={styles.content}>
-                        <Title/>
-                    </div>
+                    <Msg id='landing.title'/>
+                    <hr className={styles.titleUnderline}/>
                 </div>
                 <div className={styles.language}>
-                    <div className={styles.content}>
-                        <LanguageSelector/>
-                    </div>
+                    <LanguageSelector/>
                 </div>
                 <div className={styles.features}>
-                    <div className={styles.content}>
-                        <Feature name='process' icon='globe'/>
-                        <Feature name='browse' icon='folder-open'/>
-                        <Feature name='apps' icon='wrench'/>
-                        <Feature name='terminal' icon='terminal'/>
-                    </div>
+                    <Feature name='process' icon='globe'/>
+                    <Feature name='browse' icon='folder-open'/>
+                    <Feature name='apps' icon='wrench'/>
+                    <Feature name='terminal' icon='terminal'/>
                 </div>
                 <div className={styles.credentials}>
-                    <div className={styles.content}>
-                        <Credentials location={location}/>
-                    </div>
+                    <Credentials location={location}/>
                 </div>
                 <div className={styles.privacy}>
-                    <div className={styles.content}>
-                        <PrivacyPolicy/>
-                    </div>
+                    <a href={'/privacy-policy'} target="sepal-privacy-policy">
+                        <Msg id='landing.privacyPolicy'/>
+                    </a>
                 </div>
             </div>
         </div>
@@ -59,17 +50,6 @@ export default connect(mapStateToProps)(Landing)
 Landing.propTypes = {
     location: PropTypes.object
 }
-
-const Caption = () =>
-    <p className={styles.caption}>
-        <Msg id='landing.caption'/>
-    </p>
-
-const Title = () =>
-    <div className={styles.titleSection}>
-        <h2 className={styles.title}><Msg id='landing.title'/></h2>
-        <hr className={styles.titleUnderline}/>
-    </div>
 
 const Feature = ({icon, name}) =>
     <div className={styles.feature}>
@@ -86,6 +66,3 @@ Feature.propTypes = {
     icon: PropTypes.string,
     name: PropTypes.string
 }
-
-const PrivacyPolicy = () =>
-    <a href={'/privacy-policy'}><Msg id='landing.privacyPolicy'/></a>
