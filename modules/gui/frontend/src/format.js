2ee6535c6ea80b82c0bc05be21d5c1325bb76684
Luca Paolini <luca.paolini@fao.org>
2018-12-19 14:42:39 +0100

    Improved number formatting function.

--- modules/gui/frontend/src/format.js
+++ modules/gui/frontend/src/format.js
@@ -51,18 +51,24 @@ const fileSize = (size, {scale, precisionDigits} = {}) =>
 
 const number = ({value = 0, scale = '', precisionDigits = 3, prefix = '', unit = ''}) => {
     const join = (...items) => _.compact(items).join(' ')
-    // safe up to yottabytes (10^24)...
-    const magnitudes = ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
-    const scaleIndex = Math.max(magnitudes.indexOf(scale), 0)
-    const scaleMultiplier = Math.pow(10, 3 * scaleIndex)
-    const scaledValue = value * scaleMultiplier
-    const digits = Math.max(Math.trunc(Math.log10(scaledValue)), 0)
-    const magnitude = Math.trunc(digits / 3)
-    const multiplier = Math.pow(10, 3 * magnitude)
-    const decimals = multiplier > 1
-        ? Math.min(precisionDigits - 1, 2) - digits % 3
-        : 0
-    return join(prefix, (scaledValue / multiplier).toFixed(decimals), magnitudes[magnitude] + unit)
+    if (precisionDigits < 3) {
+        throw new Error('Unsupported number of precision digits (less than 3).')
+    }
+    // safe from pico to yotta
+    const magnitudes = ['p', 'n', 'µ', 'm', '', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
+    const scaleMagnitude = magnitudes.indexOf(scale)
+    if (scaleMagnitude === -1) {
+        throw new Error('Unsupported scale.')
+    }
+    const digits = Math.max(Math.trunc(Math.log10(value)), 0)
+    const valueMagnitude = Math.trunc(digits / 3)
+    const magnitude = valueMagnitude + scaleMagnitude
+    if (magnitude < 0 || magnitude > magnitudes.length - 1) {
+        throw new Error('Out of range.')
+    }
+    const multiplier = Math.pow(10, 3 * valueMagnitude)
+    const decimals = multiplier > 1 ? precisionDigits - digits % 3 - 1 : 0
+    return join(prefix, (value / multiplier).toFixed(decimals), magnitudes[magnitude] + unit)
 }
 
 export default {
