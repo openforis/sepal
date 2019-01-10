7256f0cea8655b924a715cbe0e3ace1da5595f6a
Daniel Wiell <daniel.wiell@fao.org>
2019-01-10 15:52:06 +0100

    Updated GDAL_DATA location.

--- modules/geospatial-toolkit/docker/script/init_post.sh
+++ modules/geospatial-toolkit/docker/script/init_post.sh
@@ -12,7 +12,7 @@ printf '%s\n' \
     'PATH="usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/lib/orfeo/bin"' \
     'JAVA_HOME="/usr/local/lib/sdkman/candidates/java/current"' \
     'SDKMAN_DIR="/usr/local/lib/sdkman"' \
-    'GDAL_DATA="/usr/share/gdal/2.2"' \
+    'GDAL_DATA="/usr/share/gdal"' \
     'SHELL="/bin/bash"' \
     'SEPAL="true"' \
     >> /etc/environment
