9faf23dfef9ee2c1c382e4d1ca986d58cab5edce
Daniel Wiell <daniel.wiell@fao.org>
2019-01-15 16:01:04 +0100

    Redesigned user report.

--- modules/gui/frontend/src/app/home/user/userSessions.js
+++ modules/gui/frontend/src/app/home/user/userSessions.js
@@ -1,8 +1,10 @@
-import {connect} from 'store'
-import {msg} from 'translate'
-import React from 'react'
-import UserSession from './userSession'
 import format from 'format'
+import moment from 'moment'
+import React from 'react'
+import {connect} from 'store'
+import lookStyles from 'style/look.module.css'
+import {msg} from 'translate'
+import UserSession from './userSession'
 import styles from './userSessions.module.css'
 
 const mapStateToProps = state => {
@@ -53,47 +55,62 @@ class UserSessions extends React.Component {
         const {selectedSession, clean} = this.state
         const selectable = selectedSession !== session.id && clean
         return (
-            <React.Fragment key={session.id}>
-                <tr className={selectable ? styles.clickable : null}
-                    onClick={() => selectable ? this.editSessionDetails(session.id) : null}>
-                    <td>{session.instanceType.name}</td>
-                    <td>{session.instanceType.description}</td>
-                    <td>{format.fullDateTime(session.creationTime)}</td>
-                    <td className={styles.number}>{format.dollars(session.costSinceCreation)}</td>
-                    <td className={styles.number}>{format.dollarsPerHour(session.instanceType.hourlyCost)}</td>
-                    <td className={styles.number}>{format.hours(session.earliestTimeoutHours)}</td>
-                </tr>
+            <div
+                key={session.id}
+                className={[styles.session, lookStyles.look, lookStyles.transparent].join(' ')}
+                onClick={() => selectable ? this.editSessionDetails(session.id) : null}>
+                <div className={styles.header}>
+                    <span className={'itemType'}>
+                        {moment.utc(session.creationTime).fromNow()}
+                    </span>
+                    <span className={styles.cost}>
+                        {format.dollars(session.costSinceCreation)} ({format.dollarsPerHour(session.instanceType.hourlyCost)})
+                    </span>
+                </div>
+                <div>{session.instanceType.name} ({session.instanceType.description})</div>
                 {selectedSession === session.id
-                    ? <tr>
-                        <td colSpan={6}>
-                            <UserSession
-                                session={session}
-                                onDirty={() => this.setUnclean()}
-                                onClose={() => this.closeSessionDetails()}/>
-                        </td>
-                    </tr>
+                    ? <UserSession
+                        session={session}
+                        onDirty={() => this.setUnclean()}
+                        onClose={() => this.closeSessionDetails()}/>
                     : null}
-            </React.Fragment>
+            </div>
         )
+        // return (
+        //     <React.Fragment key={session.id}>
+        //         <tr className={selectable ? styles.clickable : null}
+        //             onClick={() => selectable ? this.editSessionDetails(session.id) : null}>
+        //             <td>{session.instanceType.name}</td>
+        //             <td>{session.instanceType.description}</td>
+        //             <td>{format.fullDateTime(session.creationTime)}</td>
+        //             <td className={styles.number}>{format.dollars(session.costSinceCreation)}</td>
+        //             <td className={styles.number}>{format.dollarsPerHour(session.instanceType.hourlyCost)}</td>
+        //             <td className={styles.number}>{format.hours(session.earliestTimeoutHours)}</td>
+        //         </tr>
+        //     </React.Fragment>
+        // )
     }
 
     renderSessions(sessions) {
         return (
-            <table className={styles.sessions}>
-                <thead>
-                    <tr>
-                        <th>{msg('user.report.sessions.instanceType')}</th>
-                        <th>{msg('user.report.sessions.instanceDescription')}</th>
-                        <th>{msg('user.report.sessions.time')}</th>
-                        <th className={styles.number}>{msg('user.report.sessions.cost')}</th>
-                        <th className={styles.number}>{msg('user.report.sessions.hourlyCost')}</th>
-                        <th className={styles.number}>{msg('user.report.sessions.earliestTimeoutHours')}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {sessions.map(session => this.renderSession(session))}
-                </tbody>
-            </table>
+            <div>
+                {sessions.map(session => this.renderSession(session))}
+            </div>
+            // <table className={styles.sessions}>
+            //     <thead>
+            //         <tr>
+            //             <th>{msg('user.report.sessions.instanceType')}</th>
+            //             <th>{msg('user.report.sessions.instanceDescription')}</th>
+            //             <th>{msg('user.report.sessions.time')}</th>
+            //             <th className={styles.number}>{msg('user.report.sessions.cost')}</th>
+            //             <th className={styles.number}>{msg('user.report.sessions.hourlyCost')}</th>
+            //             <th className={styles.number}>{msg('user.report.sessions.earliestTimeoutHours')}</th>
+            //         </tr>
+            //     </thead>
+            //     <tbody>
+            //         {sessions.map(session => this.renderSession(session))}
+            //     </tbody>
+            // </table>
         )
     }
 
