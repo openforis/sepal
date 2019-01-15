f092ef2a3f9dd04c4469c51dcb67ef38023cccd0
Luca Paolini <luca.paolini@fao.org>
2019-01-15 10:48:20 +0100

    Improved Scrollable (can now specify direction of scrolling).

--- modules/gui/frontend/src/widget/scrollable.js
+++ modules/gui/frontend/src/widget/scrollable.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './scrollable.module.css'
 
@@ -9,6 +10,11 @@ export const ScrollableContainer = ({className, children}) => {
     )
 }
 
+ScrollableContainer.propTypes = {
+    children: PropTypes.any.isRequired,
+    className: PropTypes.string
+}
+
 export const Unscrollable = ({className, children}) => {
     return (
         <div className={[className, styles.unscrollable].join(' ')}>
@@ -17,10 +23,21 @@ export const Unscrollable = ({className, children}) => {
     )
 }
 
-export const Scrollable = ({className, children}) => {
+Unscrollable.propTypes = {
+    children: PropTypes.any.isRequired,
+    className: PropTypes.string
+}
+
+export const Scrollable = ({className, direction = 'y', children}) => {
     return (
-        <div className={[className, styles.scrollable].join(' ')}>
+        <div className={[className, styles.scrollable, styles[direction]].join(' ')}>
             {children}
         </div>
     )
+}
+
+Scrollable.propTypes = {
+    children: PropTypes.any.isRequired,
+    className: PropTypes.string,
+    direction: PropTypes.oneOf(['x', 'y', 'xy'])
 }
