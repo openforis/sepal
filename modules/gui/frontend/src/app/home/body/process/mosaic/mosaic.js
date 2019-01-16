494dc104652b50bc66842ff797a798a848b6fd4e
Luca Paolini <luca.paolini@fao.org>
2019-01-16 11:22:45 +0100

    Removed test bars.

--- modules/gui/frontend/src/app/home/body/process/mosaic/mosaic.js
+++ modules/gui/frontend/src/app/home/body/process/mosaic/mosaic.js
@@ -1,4 +1,4 @@
-import {BottomBar, Content, SectionLayout} from 'widget/sectionLayout'
+import {Content, SectionLayout} from 'widget/sectionLayout'
 import {RecipeState, recipePath} from './mosaicRecipe'
 import {SceneSelectionType} from 'app/home/body/process/mosaic/mosaicRecipe'
 import {connect, select} from 'store'
@@ -59,7 +59,6 @@ class Mosaic extends React.Component {
                             : null}
                     </div>
                 </Content>
-                <BottomBar>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</BottomBar>
             </SectionLayout>
         )
     }
