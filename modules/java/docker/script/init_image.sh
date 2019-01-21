8717dbfcbabd693b65cf186a174d229b0e12426a
Daniel Wiell <daniel.wiell@fao.org>
2019-01-21 13:39:55 +0100

    Upgraded to Java 11.0.1-open.

--- modules/java/docker/script/init_image.sh
+++ modules/java/docker/script/init_image.sh
@@ -14,6 +14,6 @@ apt-get -y update && apt-get install -qq -y \
 export SDKMAN_DIR=/usr/local/lib/sdkman
 curl -s get.sdkman.io | bash
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
-yes | sdk install java 11.0.1-open
+yes | sdk install java 11.0.2-open
 
 ln -s `which java` /usr/local/bin/java
