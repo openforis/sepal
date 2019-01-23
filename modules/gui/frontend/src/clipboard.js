0df70fdbfbf1b673cd7104583fb6d3d8dec9a10b
Daniel Wiell <daniel.wiell@fao.org>
2019-01-23 11:08:26 +0100

    Added copy task to clipboard button, and used standard styling on tasks.

---
+++ modules/gui/frontend/src/clipboard.js
@@ -0,0 +1,10 @@
+export default {
+    copy: (text) => {
+        const element = document.createElement('textarea')
+        element.value = text
+        document.body.appendChild(element)
+        element.select()
+        document.execCommand('copy')
+        document.body.removeChild(element)
+    }
+}
