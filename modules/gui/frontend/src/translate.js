c1217a0bd048653a44731e215586e21e2c1321f0
Daniel Wiell <daniel.wiell@fao.org>
2019-01-29 13:58:21 +0100

    Used English translations as fallback.

--- modules/gui/frontend/src/translate.js
+++ modules/gui/frontend/src/translate.js
@@ -1,11 +1,12 @@
-import {addLocaleData, IntlProvider, injectIntl} from 'react-intl'
-import PropTypes from 'prop-types'
-import React from 'react'
 import flat from 'flat'
 import moment from 'moment'
+import PropTypes from 'prop-types'
+import React from 'react'
+import {addLocaleData, injectIntl, IntlProvider} from 'react-intl'
 
-let intl
+let intl, intlEn
 export const initIntl = intlInstance => intl = intlInstance
+export const initIntlEn = intlInstance => intlEn = intlInstance
 
 export const Msg = ({id, ...values}) => (
     <span>{msg(id, values)}</span>
@@ -20,10 +21,17 @@ const flattenDeep = arr => Array.isArray(arr)
     : [arr]
 
 export const msg = (id, values = {}, defaultMessage) => {
-    const idString = flattenDeep(id).join('.')
+    const idString = String(flattenDeep(id).join('.'))
     return intl.formatMessage({
-        id: String(idString),
-        defaultMessage: defaultMessage || idString
+        id: idString,
+        defaultMessage: defaultMessage || (
+            getLanguage() === 'en'
+                ? idString
+                : intlEn.formatMessage({
+                    id: idString,
+                    defaultMessage: idString
+                }, values)
+        )
     }, values)
 }
 
@@ -55,13 +63,17 @@ export default class TranslationProvider extends React.Component {
         const languageState = (language) => {
             const localeData = require(`react-intl/locale-data/${language}`)
             addLocaleData(localeData)
-            const messages = flat.flatten( // react-intl requires a flat object
+            const loadTranslations = language => flat.flatten( // react-intl requires a flat object
                 require(`locale/${language}/translations.json`)
             )
+            const messages = loadTranslations(language)
+            const messagesEn = language === 'en'
+                ? messages
+                : loadTranslations('en')
             require('moment/min/locales.min') // Load all moment locales
 
             moment.locale(language)
-            return {...state, messages, language}
+            return {...state, messages, messagesEn, language}
         }
         let language = getLanguage()
         if (language === state.language) {
@@ -77,13 +89,16 @@ export default class TranslationProvider extends React.Component {
 
     render() {
         const {children} = this.props
-        const {language, messages} = this.state
-        console.log({language, messages})
+        const {language, messages, messagesEn} = this.state
         return (
-            <IntlProvider locale={language} messages={messages}>
-                <IntlInit>
-                    {children}
-                </IntlInit>
+            <IntlProvider locale={'en'} messages={messagesEn}>
+                <IntlInitEn>
+                    <IntlProvider locale={language} messages={messages}>
+                        <IntlInit>
+                            {children}
+                        </IntlInit>
+                    </IntlProvider>
+                </IntlInitEn>
             </IntlProvider>
         )
     }
@@ -93,7 +108,6 @@ class IntlInitializer extends React.Component {
     constructor(props) {
         super(props)
         initIntl(this.props.intl)
-        // TODO: resolve references here
     }
 
     render() {
@@ -105,11 +119,23 @@ class IntlInitializer extends React.Component {
     }
 }
 
+class IntlInitializerEn extends React.Component {
+    constructor(props) {
+        super(props)
+        initIntlEn(this.props.intl)
+    }
+
+    render() {
+        return this.props.children
+    }
+}
+
 IntlInitializer.propTypes = {
     children: PropTypes.any,
     intl: PropTypes.object
 }
 
 const IntlInit = injectIntl(IntlInitializer)
+const IntlInitEn = injectIntl(IntlInitializerEn)
 
 // Read language from local storage, fallback to default
