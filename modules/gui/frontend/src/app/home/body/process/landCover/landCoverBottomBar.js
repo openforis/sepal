57542132f45930d75246f31759d347c47c0e77d1
Daniel Wiell <daniel.wiell@fao.org>
2019-01-29 09:30:23 +0100

    Added land-cover bottom bar.

---
+++ modules/gui/frontend/src/app/home/body/process/landCover/landCoverBottomBar.js
@@ -0,0 +1,119 @@
+import PropTypes from 'prop-types'
+import React, {Component} from 'react'
+import {Button, ButtonGroup} from 'widget/button'
+import ComboBox from 'widget/comboBox'
+import {Field, form} from 'widget/form'
+import Icon from 'widget/icon'
+import {BottomBar} from 'widget/sectionLayout'
+import styles from './landCoverBottomBar.module.css'
+
+const fields = {
+    primitive: new Field()
+}
+
+class LandCoverBottomBar extends Component {
+    render() {
+        const hasNext = this.hasNext()
+        return <BottomBar padding className={styles.container}>
+            <div className={styles.left}>
+                {this.renderBackButton()}
+            </div>
+            <div className={[
+                styles.middle,
+                hasNext ? styles.hasNext : null
+            ].join(' ')}>
+                {this.renderPrimitiveSelector()}
+                {hasNext ? this.renderNextPrimitiveButton() : null}
+            </div>
+            <ButtonGroup
+                wrap={false}>
+                {this.renderCreatePrimitiveButton()}
+                {this.renderNextButton()}
+            </ButtonGroup>
+        </BottomBar>
+    }
+
+    renderBackButton() {
+        return (
+            <Button
+                additionalClassName={styles.back}
+                look={'transparent'}
+                onClick={() => console.log('Going back to composites')}>
+                <Icon name='backward'/>
+                <span>Composite</span>
+            </Button>
+        )
+    }
+
+    renderPrimitiveSelector() {
+        const {inputs: {primitive}} = this.props
+        const primitives = [
+            {value: 'forest', label: 'Forest',},
+            {value: 'plantation', label: 'Plantation',},
+            {value: 'shrub', label: 'Shrub',},
+            {value: 'grass', label: 'Grass',},
+            {value: 'crop', label: 'Crop',},
+            {value: 'paramo', label: 'Paramo',},
+            {value: 'water', label: 'Water',},
+            {value: 'urban', label: 'Urban',},
+            {value: 'barren', label: 'Barren',}
+        ]
+        return (
+            <div className={styles.primitiveSelector}>
+                <ComboBox
+                    input={primitive}
+                    options={primitives}
+                    menuPlacement='top'/>
+            </div>
+        )
+    }
+
+    renderNextPrimitiveButton() {
+        return (
+            <Button
+                icon='angle-double-right'
+                tooltip={'Select next primitive'}
+                chromeless
+                onClick={() => console.log('Next primitive')}
+                additionalClassName={styles.nextPrimitive}/>
+        )
+    }
+
+    renderCreatePrimitiveButton() {
+        return (
+            <Button
+                look={'transparent'}
+                onClick={() => console.log('Starting to create primitive')}
+                label='Create primitive'/>
+        )
+    }
+
+    renderNextButton() {
+        return (
+            <Button
+                disabled={!this.allPrimitivesCreated()}
+                additionalClassName={styles.next}
+                look={'transparent'}
+                onClick={() => console.log('Navigate to assemblage')}>
+                <span>Assemblage</span>
+                <Icon name='forward'/>
+            </Button>
+        )
+    }
+
+    hasNext() {
+        return true
+        // return false
+    }
+
+    allPrimitivesCreated() {
+        return true
+        // return false
+    }
+}
+
+LandCoverBottomBar.propTypes = {
+    recipeId: PropTypes.string.isRequired
+}
+
+export default form({fields})(LandCoverBottomBar)
