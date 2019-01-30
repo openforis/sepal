8bc7edf2fd3f538fcb77fb398d62626c3aa8c004
Daniel Wiell <daniel.wiell@fao.org>
2019-01-30 11:34:16 +0100

    Converted inline style attributes to camelCase.

--- modules/gui/frontend/src/app/home/body/process/createRecipeRLCMS.js
+++ modules/gui/frontend/src/app/home/body/process/createRecipeRLCMS.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class CreateRecipeRLCMS extends React.Component {
     render() {
         return (
-            <div style={{'margin': '1rem 2rem', 'display': 'grid', 'grid-gap': '1rem'}}>
+            <div style={{margin: '1rem 2rem', display: 'grid', gridGap: '1rem', color: '#ccc'}}>
                 <div>
                     Monitoring land cover and land use change is important for land resource planning and for ecosystem services
                     including biodiversity conservation and resilience to climate change. However, data updates are infrequent and
@@ -12,25 +12,25 @@ export default class CreateRecipeRLCMS extends React.Component {
                     (i.e., there are two few land cover classes or the classes are not the most appropriate for the region)
                     needed for many regional applications and decision-making contexts.
                 </div>
-                <div style={{'text-align': 'center'}}>
+                <div style={{textAlign: 'center'}}>
                     THE REGIONAL LAND COVER MONITORING SYSTEM IS AN INITIATIVE OF
                 </div>
-                <div style={{'text-align': 'center'}}>
-                    <img style={{'max-width': '80%', 'max-height': '8vh'}} alt='SERVIR logo'
+                <div style={{textAlign: 'center'}}>
+                    <img style={{maxWidth: '80%', maxHeight: '8vh'}} alt='SERVIR logo'
                         src='http://servir-rlcms.appspot.com/static/img/servir-hires.png'/>
                 </div>
-                <div style={{'text-align': 'center'}}>
+                <div style={{textAlign: 'center'}}>
                     a partnership between
                 </div>
-                <div style={{'text-align': 'center'}}>
+                <div style={{textAlign: 'center'}}>
                     <div>
-                        <img style={{'max-width': '30%', 'max-height': '8vh'}} alt='USAID logo'
+                        <img style={{maxWidth: '30%', maxHeight: '8vh'}} alt='USAID logo'
                             src='http://servir-rlcms.appspot.com/static/img/USAID_Logo_Color.png'/>
-                        <img style={{'max-width': '30%', 'max-height': '8vh'}} alt='NASA logo'
+                        <img style={{maxWidth: '30%', maxHeight: '8vh'}} alt='NASA logo'
                             src='http://servir-rlcms.appspot.com/static/img/NASA_Logo_Color.png'/>
                     </div>
                 </div>
-                <div style={{'text-align': 'right'}}>
+                <div style={{textAlign: 'right'}}>
                     <a href='http://servir-rlcms.appspot.com/' rel='noopener noreferrer' target='_blank'>http://servir-rlcms.appspot.com/</a>
                 </div>
             </div>
