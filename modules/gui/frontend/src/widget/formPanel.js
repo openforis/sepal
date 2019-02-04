d23055dd3a8f71f8add0ad846685cb139c54a524
Luca Paolini <luca.paolini@fao.org>
2019-02-04 21:32:32 +0100

    Merged formPanel and formPanelButtons.

--- modules/gui/frontend/src/widget/formPanel.js
+++ modules/gui/frontend/src/widget/formPanel.js
@@ -1,6 +1,7 @@
 import {Form} from 'widget/form'
 import {Panel} from 'widget/panel'
 import {PanelButtonContext} from './toolbar'
+import {PanelButtons} from 'widget/panel'
 import {PanelWizardContext} from './panelWizard'
 import {connect, select} from 'store'
 import PropTypes from 'prop-types'
@@ -14,6 +15,8 @@ const mapStateToProps = (state, ownProps) => {
         selectedPanel: select([statePath, 'selectedPanel'])
     }
 }
+
+const PanelContext = React.createContext()
 
 class FormPanel extends React.Component {
     componentDidMount() {
@@ -145,4 +148,86 @@ FormPanel.propTypes = {
     onCancel: PropTypes.func,
 }
 
-export const PanelContext = React.createContext()
+export class FormPanelButtons extends React.Component {
+    renderBackButton(onClick) {
+        return PanelButtons.renderButton({template: 'back', onClick})
+    }
+
+    renderNextButton({invalid}, onClick) {
+        return PanelButtons.renderButton({template: 'next', disabled: invalid, onClick})
+    }
+
+    renderDoneButton({invalid}, onClick) {
+        return PanelButtons.renderButton({template: 'done', disabled: invalid, onClick})
+    }
+
+    renderCancelButton({isActionForm, dirty}, onClick) {
+        const {label} = this.props
+        const shown = isActionForm || dirty
+        return PanelButtons.renderButton({template: 'cancel', label, shown, onClick})
+    }
+
+    renderCloseButton(onClick) {
+        const {label} = this.props
+        return PanelButtons.renderButton({template: 'close', label, onClick})
+    }
+
+    renderApplyButton({isActionForm, invalid}, onClick) {
+        const {label} = this.props
+        const type = isActionForm ? 'button' : 'submit'
+        const disabled = !isActionForm && invalid
+        return PanelButtons.renderButton({template: 'apply', type, label, disabled, onClick})
+    }
+
+    renderWizardButtons({invalid, first, last, onBack, onNext, onDone}) {
+        return (
+            <PanelButtons.Main>
+                {!first ? this.renderBackButton(onBack) : null}
+                {!last ? this.renderNextButton({invalid}, onNext) : this.renderDoneButton({invalid}, onDone)}
+            </PanelButtons.Main>
+        )
+    }
+
+    renderFormButtons({isActionForm, dirty, invalid, onOk, onCancel}) {
+        return (
+            <PanelButtons.Main>
+                {this.renderCancelButton({isActionForm, dirty}, onCancel)}
+                {this.renderApplyButton({isActionForm, invalid}, onOk)}
+            </PanelButtons.Main>
+        )
+    }
+
+    renderMainButtons({isActionForm, wizard, first, last, dirty, invalid, onOk, onCancel, onBack, onNext, onDone}) {
+        return wizard
+            ? this.renderWizardButtons({first, last, invalid, onBack, onNext, onDone})
+            : this.renderFormButtons({isActionForm, dirty, invalid, onOk, onCancel})
+    }
+
+    renderExtraButtons() {
+        const {children} = this.props
+        return children ? (
+            <PanelButtons.Extra>
+                {children}
+            </PanelButtons.Extra>
+        ) : null
+    }
+
+    render() {
+        return (
+            <PanelContext.Consumer>
+                {props => (
+                    <PanelButtons>
+                        {this.renderMainButtons(props)}
+                        {this.renderExtraButtons()}
+                    </PanelButtons>
+                )}
+            </PanelContext.Consumer>
+        )
+    }
+}
+
+FormPanelButtons.propTypes = {
+    applyLabel: PropTypes.string,
+    cancelLabel: PropTypes.string,
+    children: PropTypes.any
+}
