51b2d1c8b20358557ee47cc4ad09dd7f4a5b11b8
Daniel Wiell <daniel.wiell@fao.org>
2019-03-26 11:17:55 +0100

    Added bootstrap-reboot derived css reset script.

--- modules/gui/frontend/src/app/app.js
+++ modules/gui/frontend/src/app/app.js
@@ -9,11 +9,12 @@ import ReactResizeDetector from 'react-resize-detector'
 import User from 'widget/user'
 import actionBuilder from 'action-builder'
 
-import css1 from './app.css'
-import css2 from '../style/look.css'
-import css3 from '../style/look.module.css'
+import css1 from './reset.css'
+import css2 from './app.css'
+import css3 from '../style/look.css'
+import css4 from '../style/look.module.css'
 
-const _css = [css1, css2, css3] // eslint-disable-line
+const _css = [css1, css2, css3, css4] // eslint-disable-line
 
 const mapStateToProps = state => ({
     initialized: selectFrom(state, 'user.initialized'),
