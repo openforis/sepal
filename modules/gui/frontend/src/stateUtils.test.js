afaa4a742487b666207d3dafb815c033b523b180
Luca Paolini <luca.paolini@fao.org>
2019-04-01 17:22:11 +0200

    Reenabled tests.

--- modules/gui/frontend/src/stateUtils.test.js
+++ modules/gui/frontend/src/stateUtils.test.js
@@ -15,30 +15,30 @@ const test = name => {
     })
 }
 
-// test('toPathList(${params}) === ${result}')
-//     .assert(({params, result}) => expect(toPathList(params)).toEqual(result))
-//     .where(
-//         {params: null, result: null},
-//         {params: undefined, result: null},
-//         {params: 123, result: ['123']},
-//         {params: {a: 1}, result: {a: 1}},
-//         {params: ['a'], result: ['a']},
-//         {params: '', result: ['']},
-//         {params: 'test', result: ['test']},
-//         {params: 'one.two', result: ['one', 'two']},
-//         {params: 'one.two.three', result: ['one', 'two', 'three']},
-//         {params: ['one.two.three', 'four.five'], result: ['one', 'two', 'three', 'four', 'five']},
-//         {params: ['one.two.three', ['four.five', ['six']]], result: ['one', 'two', 'three', 'four', 'five', 'six']},
-//         {params: ['a', {b: 1}, 'c.d'], result: ['a', {b: 1}, 'c', 'd']},
-//         {params: ['a', {b: 1}, 'c.d', {e: true}], result: ['a', {b: 1}, 'c', 'd', {e: true}]},
-//         {params: dotSafe(''), result: ['']},
-//         {params: dotSafe('one'), result: ['one']},
-//         {params: dotSafe('one.two'), result: ['one.two']},
-//         {params: [dotSafe('one.two')], result: ['one.two']},
-//         {params: ['a', dotSafe('b.c'), 'd'], result: ['a', 'b.c', 'd']},
-//         {params: dotSafe(['one.two', 'three.four.five']), result: ['one.two', 'three.four.five']},
-//         {params: dotSafe(['one.two', ['three.four.five', ['six.seven']]]), result: ['one.two', 'three.four.five', 'six.seven']},
-//     )
+test('toPathList(${params}) === ${result}')
+    .assert(({params, result}) => expect(toPathList(params)).toEqual(result))
+    .where(
+        {params: null, result: null},
+        {params: undefined, result: null},
+        {params: 123, result: ['123']},
+        {params: {a: 1}, result: {a: 1}},
+        {params: ['a'], result: ['a']},
+        {params: '', result: ['']},
+        {params: 'test', result: ['test']},
+        {params: 'one.two', result: ['one', 'two']},
+        {params: 'one.two.three', result: ['one', 'two', 'three']},
+        {params: ['one.two.three', 'four.five'], result: ['one', 'two', 'three', 'four', 'five']},
+        {params: ['one.two.three', ['four.five', ['six']]], result: ['one', 'two', 'three', 'four', 'five', 'six']},
+        {params: ['a', {b: 1}, 'c.d'], result: ['a', {b: 1}, 'c', 'd']},
+        {params: ['a', {b: 1}, 'c.d', {e: true}], result: ['a', {b: 1}, 'c', 'd', {e: true}]},
+        {params: dotSafe(''), result: ['']},
+        {params: dotSafe('one'), result: ['one']},
+        {params: dotSafe('one.two'), result: ['one.two']},
+        {params: [dotSafe('one.two')], result: ['one.two']},
+        {params: ['a', dotSafe('b.c'), 'd'], result: ['a', 'b.c', 'd']},
+        {params: dotSafe(['one.two', 'three.four.five']), result: ['one.two', 'three.four.five']},
+        {params: dotSafe(['one.two', ['three.four.five', ['six.seven']]]), result: ['one.two', 'three.four.five', 'six.seven']},
+    )
 
 const object = {
     a: 1,
