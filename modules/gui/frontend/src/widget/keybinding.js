7461ea064a2c91db15895a7393d89ebb72735c8b
Luca Paolini <luca.paolini@fao.org>
2019-04-09 17:51:12 +0200

    Reimplemented Keybinding.

--- modules/gui/frontend/src/widget/keybinding.js
+++ modules/gui/frontend/src/widget/keybinding.js
@@ -1,62 +1,59 @@
+import {filter, map} from 'rxjs/operators'
 import {fromEvent} from 'rxjs'
 import {v4 as uuid} from 'uuid'
 import PropTypes from 'prop-types'
 import React from 'react'
 import _ from 'lodash'
 
+const instances = []
+
+const keypress$ = fromEvent(document, 'keydown').pipe(
+    map(event => {
+        const [instance] = instances
+        return {event, instance}
+    })
+)
+
 export default class Keybinding extends React.Component {
     id = uuid()
     subscriptions = []
-    static instances = []
 
     render() {
         const {children} = this.props
         return children
     }
 
-    handleEscape() {
-        const blurrableElements = ['INPUT', 'SELECT']
-        if (blurrableElements.includes(document.activeElement.tagName)) {
-            document.activeElement.blur()
-        } else {
-            const {onEscape} = this.props
-            onEscape && onEscape()
-        }
-    }
-
-    handleEnter() {
-        const {onEnter} = this.props
-        onEnter && onEnter()
-    }
-
-    keyHandler(key) {
-        switch (key) {
-        case 'Escape':
-            return this.handleEscape()
-        case 'Enter':
-            return this.handleEnter()
-        default:
+    keyHandler(event) {
+        const {keymap} = this.props
+        const {key} = event
+        const handler = keymap[key]
+        if (handler) {
+            handler(event)
+        } else if (keymap.default) {
+            keymap.default(event)
         }
     }
 
     pushInstance() {
-        Keybinding.instances.push(this.id)
+        instances.unshift(this.id)
     }
 
     popInstance() {
-        Keybinding.instances = _.pull(Keybinding.instances, this.id)
-    }
-
-    isActiveInstance() {
-        return _.last(Keybinding.instances) === this.id
+        const index = instances.indexOf(this.id)
+        if (index !== -1) {
+            instances.splice(index, 1)
+        }
     }
 
     componentDidMount() {
         this.pushInstance()
-        const keypress$ = fromEvent(document, 'keydown')
+        const relevantKeypresses$ = keypress$.pipe(
+            filter(({instance}) => instance === this.id),
+            map(({event}) => event)
+        )
         this.subscriptions.push(
-            keypress$.subscribe(
-                event => this.isActiveInstance() && this.keyHandler(event.key)
+            relevantKeypresses$.subscribe(
+                event => this.keyHandler(event)
             )
         )
     }
@@ -68,6 +65,77 @@ export default class Keybinding extends React.Component {
 }
 
 Keybinding.propTypes = {
-    onEnter: PropTypes.func,
-    onEscape: PropTypes.func
+    map: PropTypes.object
 }
+
+// const keypress$ = fromEvent(document, 'keydown')
+
+// export default class Keybinding extends React.Component {
+//     id = uuid()
+//     subscriptions = []
+//     static instances = []
+
+//     render() {
+//         const {children} = this.props
+//         return children
+//     }
+
+//     handleEscape() {
+//         const blurrableElements = ['INPUT', 'SELECT']
+//         if (blurrableElements.includes(document.activeElement.tagName)) {
+//             document.activeElement.blur()
+//         } else {
+//             const {onEscape} = this.props
+//             onEscape && onEscape()
+//         }
+//     }
+
+//     handleEnter() {
+//         const {onEnter} = this.props
+//         onEnter && onEnter()
+//     }
+
+//     keyHandler(key) {
+//         switch (key) {
+//         case 'Escape':
+//             return this.handleEscape()
+//         case 'Enter':
+//             return this.handleEnter()
+//         default:
+//         }
+//     }
+
+//     pushInstance() {
+//         Keybinding.instances.push(this.id)
+//     }
+
+//     popInstance() {
+//         Keybinding.instances = _.pull(Keybinding.instances, this.id)
+//     }
+
+//     isActiveInstance() {
+//         return _.last(Keybinding.instances) === this.id
+//     }
+
+//     componentDidMount() {
+//         this.pushInstance()
+//         const relevantKeypresses$ = keypress$.pipe(
+//             filter(() => _.last(Keybinding.instances) === this.id)
+//         )
+//         this.subscriptions.push(
+//             relevantKeypresses$.subscribe(
+//                 event => this.keyHandler(event.key)
+//             )
+//         )
+//     }
+
+//     componentWillUnmount() {
+//         this.popInstance()
+//         this.subscriptions.forEach(subscription => subscription.unsubscribe())
+//     }
+// }
+
+// Keybinding.propTypes = {
+//     onEnter: PropTypes.func,
+//     onEscape: PropTypes.func
+// }
