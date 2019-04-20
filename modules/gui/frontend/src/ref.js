2ed772edb2d467d370efb8df82aaf92f1e8dbd51
Luca Paolini <luca.paolini@fao.org>
2019-04-21 00:35:20 +0200

    Implemented withForwardedRef HOC.

---
+++ modules/gui/frontend/src/ref.js
@@ -0,0 +1,13 @@
+import React, {forwardRef} from 'react'
+
+const withForwardedRef = Component => {
+    const handle = (props, ref) =>
+        <Component {...props} forwardedRef={ref}/>
+
+    const name = Component.displayName || Component.name
+    handle.displayName = `withForwardedRef(${name})`
+
+    return forwardRef(handle)
+}
+
+export default withForwardedRef
