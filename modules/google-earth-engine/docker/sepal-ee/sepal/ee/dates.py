7f76746b768f65c023d7edd58b4757a1c818e030
Daniel Wiell <daniel.wiell@fao.org>
2019-04-29 13:50:13 +0200

    Added functions for adding and subtracting months to dates.

--- modules/google-earth-engine/docker/sepal-ee/sepal/ee/dates.py
+++ modules/google-earth-engine/docker/sepal-ee/sepal/ee/dates.py
@@ -1,7 +1,8 @@
+import calendar
+import numbers
 from datetime import datetime, timedelta
 
 import six
-import numbers
 from dateutil.parser import parse
 
 
@@ -16,5 +17,22 @@ def to_date(d):
 def subtract_days(d, days):
     return to_date(d) - timedelta(days=days)
 
+
 def add_days(d, days):
     return to_date(d) + timedelta(days=days)
+
+
+def add_months(d, months):
+    month = d.month - 1 + months
+    year = d.year + month // 12
+    month = month % 12 + 1
+    day = min(d.day, calendar.monthrange(year, month)[1])
+    return datetime.date(year, month, day)
+
+
+def subtract_months(d, months):
+    month = d.month - 1 + months
+    year = d.year + month // 12
+    month = month % 12 + 1
+    day = min(d.day, calendar.monthrange(year, month)[1])
+    return datetime.datetime(year, month, day)
