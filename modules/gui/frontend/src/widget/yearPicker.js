c3e9992ef3a5d4463d4d774eab0ae8208928233c
Luca Paolini <luca.paolini@fao.org>
2019-05-03 14:25:16 +0200

    Reimplemented YearPicker using Combo.

--- modules/gui/frontend/src/widget/yearPicker.js
+++ modules/gui/frontend/src/widget/yearPicker.js
@@ -1,128 +1,44 @@
-import {Button} from 'widget/button'
-import {Input} from 'widget/form'
-import {fromEvent} from 'rxjs'
-import FloatingBox from 'widget/floatingBox'
-import Label from 'widget/label'
-import List from 'widget/list'
+import Combo from 'widget/combo'
 import PropTypes from 'prop-types'
 import React from 'react'
 import _ from 'lodash'
 import styles from './yearPicker.module.css'
 
-class YearPicker extends React.Component {
+export default class YearPicker extends React.Component {
     subscriptions = []
     input = React.createRef()
     list = React.createRef()
     state = {edit: false}
 
-    render() {
-        const {className} = this.props
-        const {edit} = this.state
-        return (
-            <div className={[styles.container, className].join(' ')}>
-                {this.renderLabel()}
-                <div
-                    className={styles.input}
-                    ref={this.input}>
-                    {this.renderInput()}
-                    {this.renderTrigger()}
-                </div>
-                {edit ? this.renderOptions() : null}
-            </div>
+    getOptions() {
+        const {startYear, endYear} = this.props
+        return _.concat(
+            _.range(startYear - 5, startYear).map(year => ({label: year})),
+            _.range(startYear, endYear + 1).map(year => ({label: year, value: year})),
+            _.range(endYear + 1, endYear + 6).map(year => ({label: year}))
         )
     }
 
-    renderLabel() {
-        const {label, tooltip, tooltipPlacement = 'top'} = this.props
-        return label ? (
-            <Label
-                msg={label}
+    render() {
+        const {input, label, placement, tooltip, tooltipPlacement, autoFocus, onChange} = this.props
+        return (
+            <Combo
+                className={styles.yearPicker}
+                input={input}
+                label={label}
+                options={this.getOptions()}
+                placement={placement}
                 tooltip={tooltip}
                 tooltipPlacement={tooltipPlacement}
-            />
-        ) : null
-    }
-
-    renderInput() {
-        const {input, autoFocus, onChange} = this.props
-        return (
-            <Input
-                input={input}
-                maxLength={4}
                 autoFocus={autoFocus}
-                onClick={() => this.editYear(true)}
-                onFocus={() => this.editYear(true)}
                 onChange={onChange}
             />
         )
-    }
-
-    renderTrigger() {
-        const {edit} = this.state
-        return (
-            <Button additionalClassName={styles.panelTrigger}
-                chromeless
-                icon='calendar-alt'
-                size='small'
-                onClick={() => {
-                    if (!edit) {
-                        this.input.current.focus()
-                    }
-                    this.editYear(!edit)
-                }}/>
-        )
-    }
-
-    renderOptions() {
-        const {input, startYear, endYear, portal, placement = 'below'} = this.props
-        return (
-            <FloatingBox
-                ref={this.list}
-                element={this.input.current}
-                placement={placement}>
-                <YearPickerControl
-                    startYear={startYear}
-                    endYear={endYear}
-                    input={input}
-                    close={() => this.close()}
-                    portal={portal}
-                />
-            </FloatingBox>
-        )
-    }
-
-    close() {
-        this.editYear(false)
-    }
-
-    editYear(edit) {
-        this.setState({edit})
-        if (!edit) {
-            const {onChange, input} = this.props
-            onChange && onChange(input.value)
-        }
-    }
-
-    handleBlurEvents() {
-        const click$ = fromEvent(document, 'click')
-        const isInputClick = e => this.input.current && this.input.current.contains(e.target)
-        this.subscriptions.push(
-            click$.subscribe(e => !isInputClick(e) && this.close())
-        )
-    }
-
-    componentDidMount() {
-        this.handleBlurEvents()
-    }
-
-    componentWillUnmount() {
-        this.subscriptions.forEach(subscription => subscription.unsubscribe())
     }
 }
 
 YearPicker.propTypes = {
     autoFocus: PropTypes.any,
-    className: PropTypes.string,
     endYear: PropTypes.any,
     input: PropTypes.object,
     placement: PropTypes.string,
@@ -131,42 +47,3 @@ YearPicker.propTypes = {
     year: PropTypes.object,
     onChange: PropTypes.func
 }
-
-export class YearPickerControl extends React.Component {
-    selectYear(year) {
-        const {input, close} = this.props
-        input.set(year)
-        close()
-    }
-
-    render() {
-        const {startYear, endYear, input, close} = this.props
-        const years = _.range(Number(startYear), Number(endYear) + 1)
-        const options = years.map(year =>
-            ({label: year, value: year})
-        )
-        const selectedOption = options.find(option => option.value === input.value)
-        return (
-            <List
-                className={styles.list}
-                options={options}
-                selectedOption={selectedOption}
-                onSelect={option => this.selectYear(option.value)}
-                onCancel={close}
-                overScroll
-            />
-        )
-    }
-}
-
-YearPickerControl.propTypes = {
-    close: PropTypes.func,
-    date: PropTypes.object,
-    endYear: PropTypes.any,
-    input: PropTypes.object,
-    portal: PropTypes.object,
-    resolution: PropTypes.string,
-    startYear: PropTypes.any
-}
-
-export default YearPicker
