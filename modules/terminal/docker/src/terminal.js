48910e1a59dd78c81a5cdc71913991de605d3891
Luca Paolini <luca.paolini@fao.org>
2019-05-08 16:06:02 +0200

    Fixed ssh session not closing.

--- modules/terminal/docker/src/terminal.js
+++ modules/terminal/docker/src/terminal.js
@@ -2,6 +2,8 @@ const express = require('express')
 const expressWs = require('express-ws')
 const pty = require('node-pty')
 
+const {exec} = require('child_process')
+
 const {interval, Subject} = require('rxjs')
 const {map, filter, bufferTime} = require('rxjs/operators')
 
@@ -13,9 +15,16 @@ const connect = (req, res) => {
     const cols = parseInt(req.query.cols)
     const rows = parseInt(req.query.rows)
     const username = JSON.parse(req.headers['sepal-user']).username
-    const keyFile = `/sepalUsers/${username}/.ssh/id_rsa`
+    const usersHome = process.env.USERS_HOME || '/sepalUsers'
+    const keyFile = `${usersHome}/${username}/.ssh/id_rsa`
     const sshGateway = process.env.SSH_GATEWAY_HOST
-    const term = pty.spawn('sudo', ['ssh', '-q', '-o', 'StrictHostKeyChecking=no', '-i', keyFile, `${username}@${sshGateway}`], {
+
+    const key = `/tmp/${username}.key`
+    exec(`sudo cp ${keyFile} ${key}`)
+    exec(`sudo chown node:node ${key}`)
+    const term = pty.spawn('ssh', ['-q', '-o', 'StrictHostKeyChecking=no', '-i', key, `${username}@${sshGateway}`], {
+        uid: process.getuid(),
+        gid: process.getgid(),
         name: 'xterm-color',
         cols: cols || 80,
         rows: rows || 24,
@@ -24,7 +33,10 @@ const connect = (req, res) => {
     })
 
     console.log('Created terminal with PID: ' + term.pid)
-    terminals[term.pid] = term
+    terminals[term.pid] = {
+        term,
+        key
+    }
     logs[term.pid] = ''
     term.on('data', data => logs[term.pid] += data)
     res.send(term.pid.toString())
@@ -35,7 +47,7 @@ const resize = (req, res) => {
     const pid = parseInt(req.params.pid)
     const cols = parseInt(req.query.cols)
     const rows = parseInt(req.query.rows)
-    const term = terminals[pid]
+    const term = terminals[pid].term
 
     term.resize(cols, rows)
     console.log('Resized terminal ' + pid + ' to ' + cols + ' cols and ' + rows + ' rows.')
@@ -43,7 +55,7 @@ const resize = (req, res) => {
 }
 
 const terminal = (ws, req) => {
-    var term = terminals[parseInt(req.params.pid)]
+    var term = terminals[parseInt(req.params.pid)].term
     console.log('Connected to terminal ' + term.pid)
 
     const webSocketSend = data => {
@@ -78,6 +90,8 @@ const terminal = (ws, req) => {
 
     ws.on('close', () => {
         term.kill()
+        exec(`rm -f ${terminals[term.pid].key}`)
+        
         subscriptions.forEach(subscription => subscription.unsubscribe())
         console.log('Closed terminal ' + term.pid)
         delete terminals[term.pid]
