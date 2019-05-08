0f656d3582fad487a32ddbb1edb7998db1f20e5c
Daniel Wiell <daniel.wiell@fao.org>
2019-05-08 12:35:16 +0200

    Removed old radar harmonics implementation.

--- modules/google-earth-engine/docker/sepal-ee/sepal/ee/__init__.py
+++ modules/google-earth-engine/docker/sepal-ee/sepal/ee/__init__.py
@@ -1,1 +1,1 @@
-from sepal.ee.radar import radar_collection, radar_harmonics, radar_mosaic, radar_time_scan, radar_viz
+from sepal.ee.radar import radar_collection, radar_mosaic, radar_time_scan, radar_viz
