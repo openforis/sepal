4fc6a5c4a71d354942d11cded2b166685550398e
Daniel Wiell <daniel.wiell@fao.org>
2019-05-21 10:04:09 +0200

    Added blur detector component.

---
+++ modules/gui/frontend/src/widget/blurDetector.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import {fromEvent, merge} from 'rxjs'
+
+
+export default class BlurDetector extends React.Component {
+    subscriptions = []
+    element = React.createRef()
+
+    render() {
+        const {className, children} = this.props
+        return (
+            <div ref={this.element} className={className}>
+                {children}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.subscriptions.push(
+            merge(
+                fromEvent(document, 'click'),
+                fromEvent(document, 'focus'),
+            ).subscribe(
+                e => this.onEvent(e)
+            )
+        )
+    }
+
+    onEvent(e) {
+        const {onBlur} = this.props
+        const inside = this.element.current.contains(e.target)
+        if (!inside)
+            onBlur()
+    }
+
+    componentWillUnmount() {
+        this.subscriptions.forEach(subscription => subscription.unsubscribe())
+    }
+}
+
+BlurDetector.propTypes = {
+    onBlur: PropTypes.func.isRequired,
+    children: PropTypes.any.isRequired,
+    className: PropTypes.string
+}
