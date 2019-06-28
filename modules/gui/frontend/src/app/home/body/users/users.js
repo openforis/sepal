b467cd68838d312c1f5b8071f096c3c3321da875
Luca Paolini <luca.paolini@fao.org>
2019-06-28 15:24:33 +0200

    Disabled transitions in user list.

--- modules/gui/frontend/src/app/home/body/users/users.js
+++ modules/gui/frontend/src/app/home/body/users/users.js
@@ -495,6 +495,7 @@ class User extends React.Component {
                     lookStyles.look,
                     lookStyles.transparent,
                     lookStyles.chromeless,
+                    lookStyles.noTransitions,
                     styles.grid,
                     styles.user,
                     status ? styles.clickable : null
