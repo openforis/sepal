f5f7e224a16c59fdc9d7b599726033a9df17c7bf
Daniel Wiell <daniel.wiell@fao.org>
2019-07-01 10:04:57 +0200

    Updated node version to 10.x.

--- modules/geospatial-toolkit/docker/script/init_node.sh
+++ modules/geospatial-toolkit/docker/script/init_node.sh
@@ -6,7 +6,7 @@ echo "**************************"
 echo "*** Installing Node.js ***"
 echo "**************************"
 
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
