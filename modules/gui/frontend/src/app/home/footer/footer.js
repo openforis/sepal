e959fc44f9236e5430d414c79972c880d6da086e
Daniel Wiell <daniel.wiell@fao.org>
2019-07-01 14:54:19 +0200

    Added link to Git commit and Jenkins build number.

--- modules/gui/frontend/src/app/home/footer/footer.js
+++ modules/gui/frontend/src/app/home/footer/footer.js
@@ -1,12 +1,12 @@
+import React from 'react'
+import {msg} from 'translate'
 import {Button} from 'widget/button'
 import {ButtonGroup} from 'widget/buttonGroup'
 import Tooltip from 'widget/tooltip'
+import {logout} from 'widget/user'
 import {UsageButton} from '../user/usage'
 import {UserDetailsButton} from '../user/userDetails'
 import {UserMessagesButton} from '../user/userMessages'
-import {logout} from 'widget/user'
-import {msg} from 'translate'
-import React from 'react'
 import styles from './footer.module.css'
 
 const Footer = ({className}) => {
@@ -44,12 +44,22 @@ const Logout = () =>
 
 const Title = () => {
     const wikiURL = 'https://github.com/openforis/sepal/wiki'
+    const gitCommit = process.env.REACT_APP_GIT_COMMIT || '?'
+    const buildNumber = process.env.REACT_APP_BUILD_NUMBER || '?'
+    const tooltip =
+        <React.Fragment>
+            <div>
+                Build number:&nbsp;
+                <a href={`http://ops.sepal.io:8080/job/Sepal/${buildNumber}/`} target='jenkins'>{buildNumber}</a>
+            </div>
+            <div>
+                <a href={`https://github.com/openforis/sepal/tree/${gitCommit}`} target='github'>
+                    {gitCommit}
+                </a>
+            </div>
+        </React.Fragment>
     return (
-        <Tooltip
-            msg={`
-                    Git commit: ${process.env.REACT_APP_GIT_COMMIT || '?'},
-                    Build number: ${process.env.REACT_APP_BUILD_NUMBER || '?'}`}
-            top>
+        <Tooltip msg={tooltip} top>
             <a href={wikiURL} className={styles.title} target={'sepal-wiki'}>
                 Sepal
             </a>
