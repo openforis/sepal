1ea8c2f243589b4cb8a52875fad500ab456061ee
Daniel Wiell <daniel.wiell@fao.org>
2019-07-02 07:58:02 +0200

    Installed earthengine-api JavaScript API in geospatial-toolkit.

--- modules/geospatial-toolkit/docker/script/init_node.sh
+++ modules/geospatial-toolkit/docker/script/init_node.sh
@@ -11,3 +11,4 @@ apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update && apt-get -y install yarn
+npm install -g "git+https://github.com/openforis/earthengine-api.git#v0.1.171"
