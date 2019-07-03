fb5755d8fe71c955571a56429bdfdbce2fa0d237
Luca Paolini <luca.paolini@fao.org>
2019-07-03 13:46:22 +0200

    Fixed scene selection layout.

--- modules/gui/frontend/src/app/home/body/process/mosaic/sceneSelection.js
+++ modules/gui/frontend/src/app/home/body/process/mosaic/sceneSelection.js
@@ -142,7 +142,9 @@ class SceneSelection extends React.Component {
                     <Label msg={msg('process.mosaic.panel.sceneSelection.availableScenes')}/>
                 </Unscrollable>
                 <Scrollable className={styles.grid}>
+                    {/* <div> */}
                     {scenes.map(scene => this.renderScene(scene, false))}
+                    {/* </div> */}
                 </Scrollable>
             </ScrollableContainer>
         )
@@ -155,7 +157,9 @@ class SceneSelection extends React.Component {
                     <Label msg={msg('process.mosaic.panel.sceneSelection.selectedScenes')}/>
                 </Unscrollable>
                 <Scrollable className={styles.grid}>
+                    {/* <div> */}
                     {scenes.map(scene => this.renderScene(scene, true))}
+                    {/* </div> */}
                 </Scrollable>
             </ScrollableContainer>
         )
