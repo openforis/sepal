ac82fbd6bcf0389858ff7da127bae41e7731b89c
Daniel Wiell <daniel.wiell@fao.org>
2019-07-03 09:06:36 +0200

    Updated RStudio download path.

--- modules/sandbox/docker/script/init_rstudio.sh
+++ modules/sandbox/docker/script/init_rstudio.sh
@@ -6,7 +6,7 @@ echo "*********************************"
 echo "*** Installing RStudio Server ***"
 echo "*********************************"
 rstudio=rstudio-server-1.2.1335-amd64.deb
-wget -nv https://download2.rstudio.org/$rstudio
+wget -nv https://download2.rstudio.org/server/bionic/amd64/$rstudio
 gdebi -n $rstudio
 printf '%s\n' \
     "server-app-armor-enabled=0" \
