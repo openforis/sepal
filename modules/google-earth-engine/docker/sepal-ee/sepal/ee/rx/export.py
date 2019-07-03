760472bac2a9b95aad83a041ecc9ceabf250623c
Daniel Wiell <daniel.wiell@fao.org>
2019-07-03 08:40:29 +0200

    Fixed sandbox build.

--- modules/google-earth-engine/docker/sepal-ee/sepal/ee/rx/export.py
+++ modules/google-earth-engine/docker/sepal-ee/sepal/ee/rx/export.py
@@ -1,57 +1,35 @@
-import logging
-import random
-
 import ee
-import rx
 from ee.batch import Task
-from rx import interval, from_callable, of
-from rx.operators import flat_map, catch, delay, do_action, take_while, distinct_until_changed, concat
-from rx.scheduler import ThreadPoolScheduler, TimeoutScheduler
-from sepal.throttle import Throttler
+from rx import interval, of, throw
+from rx.operators import distinct_until_changed, flat_map, take_while
+from rx.scheduler import TimeoutScheduler
 
-ee_scheduler = ThreadPoolScheduler(50)
+from sepal.rx.retry import retry_with_backoff
+from sepal.rx.workqueue import WorkQueue
 
-ee_export_throttler = Throttler(2)
-ee_operation_throttler = Throttler(2)
-
-
-def retry_with_backoff(retries, backoff=lambda times: min(pow(2, times * random.uniform(0.1, 0.2)), 30)):
-    def do_retry(source, tries=1):
-        if tries < retries:
-            return of(None).pipe(
-                delay(backoff(tries)),
-                flat_map(source),
-                catch(handler=lambda e, src: do_retry(src, tries + 1))
-            )
-        else:
-            return source
-
-    return rx.pipe(
-        catch(handler=lambda e, o: do_retry(o))
-    )
+ee_exports = WorkQueue(2, 'ee_exports')
+ee_operations = WorkQueue(20, 'ee_operations')
 
 
-def ee_observable(action, retries=3):
+def ee_observable(action, retries=3, description=None):
     def execute():
         # credentials = load_credentials(
         #     '/Users/wiell/sepal-config/sepal-server/home/admin/.config/earthengine/credentials'
         # )
         # ee.InitializeThread(credentials)
+
+        # if random.random() < 0.5:
+        #     raise Exception('Randomly failed')
+        # else:
+        #     return action()
+        #
         return action()
 
-    # return ee_operation_throttler.throttle(from_callable(execute, ee_scheduler), 'some_group').pipe(
-    #     retry_with_backoff(retries)
-    # )
-    # return ee_operation_throttler.throttle(from_callable(execute), 'some_group').pipe(
-    #     retry_with_backoff(retries)
-    # )
-    # return from_callable(execute, ee_scheduler).pipe(
-    #     retry_with_backoff(retries)
-    # )
-    return of(True).pipe(
-        do_action(lambda _: print('before ee_observable')),
-        flat_map(lambda _: from_callable(execute, TimeoutScheduler())),
-        do_action(lambda _: print('after ee_observable')),
+    return ee_operations.enqueue(
+        callable=execute,
+        retries=retries,
+        group='not-grouped',
+        description=description
     )
 
 
@@ -60,48 +38,67 @@ def delete_asset(asset_id):
         if ee.data.getInfo(asset_id):
             ee.data.deleteAsset(asset_id)
 
-    return ee_observable(delete)
+    return ee_observable(delete, description='delete asset ' + asset_id)
 
 
 def export_image_to_asset(image, asset_id, description):
-    def create_task():
-        return ee.batch.Export.image.toAsset(
-            image=image,
-            description=description,
-            assetId=asset_id,
-            crs='EPSG:4326',
-            scale=30,
+    def export():
+        return delete_asset(asset_id).pipe(
+            flat_map(
+                lambda _: execute_task(
+                    ee.batch.Export.image.toAsset(
+                        image=image,
+                        description=description,
+                        assetId=asset_id,
+                        crs='EPSG:4326',
+                        scale=30,
+                    )
+                )
+            )
         )
 
-    # export = delete_asset(asset_id).pipe(map(lambda _: create_task()), flat_map(lambda task: execute_task(task)))
-    # return of('PENDING').pipe(
-    #     concat(ee_export_throttler.throttle(export, 'some_group'))
-    # )
-
-    return ee_export_throttler.throttle(delete_asset(asset_id), 'some_group')
+    return of('PENDING').pipe(
+        flat_map(
+            lambda _: ee_exports.enqueue(
+                observable=export(),
+                group='some_group',
+                description=asset_id
+            ),
+        ),
+        retry_with_backoff(
+            retries=3,
+            description='export_image_to_asset(asset_id={}, description={}'.format(
+                asset_id, description)
+        )
+    )
 
 
 def execute_task(task):
-    def load_state():
-        status = task.status()
-        # status = None
-        return status['state']
-
     def start():
-        print('start {}'.format(task))
         task.start()
         return task.status()['id']
+
+    def load_status():
+        return task.status()
+
+    def extract_state(status):
+        state = status['state']
+        if state == 'FAILED':
+            return throw(ee.EEException(status.get('error_message')))
+        else:
+            return of(state)
 
     def monitor():
         def is_running(state):
             return state in [Task.State.UNSUBMITTED, Task.State.READY, Task.State.RUNNING]
 
         return interval(2, TimeoutScheduler()).pipe(
-            flat_map(lambda _: ee_observable(load_state)),
+            flat_map(lambda _: ee_observable(load_status, description='monitor task ' + str(task))),
+            flat_map(extract_state),
             distinct_until_changed(),
             take_while(is_running, inclusive=True)
         )
 
-    return ee_observable(start).pipe(
+    return ee_observable(start, description='start task ' + str(task)).pipe(
         flat_map(lambda _: monitor())
     )
