bf9f5ad9a689f71e0e39a54e5d6cb170dc6b6383
Daniel Wiell <daniel.wiell@fao.org>
2019-07-04 16:21:31 +0200

    Raised error when unable to load credentials.

--- modules/google-earth-engine/docker/sepal-ee/sepal/ee/auth/__init__.py
+++ modules/google-earth-engine/docker/sepal-ee/sepal/ee/auth/__init__.py
@@ -1,7 +1,10 @@
 import os
 
-from ee.auth import AccessTokenCredentials
+import ee
 
 
 def load_credentials(credentials_file=os.path.expanduser('~/.config/earthengine/credentials')):
-    return AccessTokenCredentials.create(credentials_file)
+    credentials = ee.oauth.AccessTokenCredentials.create(credentials_file)
+    if not credentials:
+        raise ValueError('Unable to load credentials from {}'.format(credentials_file))
+    return credentials
