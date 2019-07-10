5767fe81fccb8bd06207b319596d51fade0dff9d
Daniel Wiell <daniel.wiell@fao.org>
2019-07-10 16:44:44 +0200

    Added RxPY operators.

--- modules/google-earth-engine/docker/sepal-ee/sepal/rx/__init__.py
+++ modules/google-earth-engine/docker/sepal-ee/sepal/rx/__init__.py
@@ -1,7 +1,14 @@
 from rx import generate
+from rx.subject import Subject
 
 from .file import using_file
+
+_dispose = Subject()
 
 
 def forever():
     return generate(None, lambda _: True, lambda _: None)
+
+
+def dispose():
+    _dispose.on_next(True)
---
+++ modules/google-earth-engine/docker/sepal-ee/sepal/rx/operators.py
@@ -0,0 +1,29 @@
+import rx
+from rx import Callable, Observable
+from rx.disposable import Disposable
+from rx.operators import take_until
+
+from . import _dispose
+
+
+def take_until_disposed():
+    return rx.pipe(
+        take_until(_dispose)
+    )
+
+
+def on_dispose(action: Callable, scheduler=None):
+    def _subscription_listener(source: Observable) -> Observable:
+        def subscribe(observer, scheduler_=None):
+            _scheduler = scheduler or scheduler_
+            subscription = source.subscribe(observer, scheduler=scheduler)
+
+            def dispose():
+                subscription.dispose()
+                action()
+
+            return Disposable(dispose)
+
+        return Observable(subscribe)
+
+    return _subscription_listener
