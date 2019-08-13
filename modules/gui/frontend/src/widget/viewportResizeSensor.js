2c8babcb3048fd4a226be276cd502bb7f6f49c8c
Daniel Wiell <daniel.wiell@fao.org>
2019-08-13 15:35:46 +0200

    Reverted to preview ViewportResizeSensor implementation.

--- modules/gui/frontend/src/widget/viewportResizeSensor.js
+++ modules/gui/frontend/src/widget/viewportResizeSensor.js
@@ -1,14 +1,34 @@
-import {ElementResizeDetector} from 'widget/elementResizeDetector'
+import {Subject} from 'rxjs'
+import {compose} from 'compose'
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators'
 import React from 'react'
+import ReactResizeDetector from 'react-resize-detector'
 import actionBuilder from 'action-builder'
+import withSubscriptions from 'subscription'
 
-export default class ViewportResizeSensor extends React.Component {
+const resize$ = new Subject()
+const UPDATE_DEBOUNCE_MS = 100
+
+class ViewportResizeSensor extends React.Component {
     render() {
         return (
-            <ElementResizeDetector
-                debounce={100}
-                onResize={size => this.updateDimensions(size)}
+            <ReactResizeDetector
+                handleWidth
+                handleHeight
+                onResize={(width, height) => resize$.next({width, height})}
             />
+        )
+    }
+
+    componentDidMount() {
+        const {addSubscription} = this.props
+        addSubscription(
+            resize$.pipe(
+                debounceTime(UPDATE_DEBOUNCE_MS),
+                distinctUntilChanged()
+            ).subscribe(
+                ({width, height}) => this.updateDimensions({width, height})
+            )
         )
     }
 
@@ -18,5 +38,10 @@ export default class ViewportResizeSensor extends React.Component {
             .dispatch()
     }
 }
+
+export default compose(
+    ViewportResizeSensor,
+    withSubscriptions()
+)
 
 ViewportResizeSensor.propTypes = {}
