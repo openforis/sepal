bdaa32954eb29c9b495ecd28b91a8f34d70f2be6
Daniel Wiell <daniel.wiell@fao.org>
2019-11-12 16:27:36 +0100

    Implementing image processor.

--- modules/gee/docker/src/ee/optical/imageProcessor.js
+++ modules/gee/docker/src/ee/optical/imageProcessor.js
@@ -1,3 +1,4 @@
+const ee = require('@google/earthengine')
 const _ = require('lodash')
 
 const imageProcessor = ({spec}) => {
@@ -12,7 +13,8 @@ const imageProcessor = ({spec}) => {
     return image =>
         compose(
             normalize(fromBands, toBands, bandsToConvertToFloat),
-            applyQABands(),
+            applyQABands(toBands),
+            maskClouds(),
             toInt16()
         )(image)
 }
@@ -22,8 +24,53 @@ const normalize = (fromBands, toBands, bandsToConvertToFloat) =>
         .select(fromBands, toBands)
         .updateBands(bandsToConvertToFloat, image => image.divide(10000))
 
-const applyQABands = () =>
-    image => image
+const applyQABands = bands =>
+    image => bands.includes('pixel_qa')
+        ? pixelQA(image)
+        : bands.includes('BQA')
+            ? BQA(image)
+            : noQA(image)
+
+const pixelQA = image => {
+    const attributes = getAttributes(
+        image.select('pixel_qa'),
+        {'badPixels': 15, 'cloud': 16, 'shadow': 256, 'snow': 1024, 'cirrus': 4096}
+    )
+    return image
+        .addBands(attributes.includes('shadow').rename('toMask'))
+        .addBands(attributes.includes('cloud').rename('cloud'))
+        .addBands(attributes.includes('snow').rename('snow'))
+    // .removeBands('pixel_qa')
+}
+
+const BQA = image => {
+    const attributes = getAttributes(
+        image.select('BQA'),
+        {'badPixels': 15, 'cloud': 16, 'shadow': 256, 'snow': 1024, 'cirrus': 4096}
+    )
+    return image
+        .addBands(attributes.includes('badPixels', 'shadow').rename('toMask'))
+        .addBands(attributes.includes('cloud', 'cirrus').rename('cloud'))
+        .addBands(attributes.includes('snow').rename('snow'))
+    // .removeBands('BQA')
+}
+
+const noQA = image =>
+    image
+        .addBands(ee.Image(0).rename('toMask'))
+        .addBands(ee.Image(0).rename('cloud'))
+        .addBands(ee.Image(0).rename('snow'))
+
+const getAttributes = (image, attributeByValue) => ({
+    includes: (...attributes) =>
+        attributes.reduce(
+            (acc, attribute) => acc.or(image.select(0).bitwiseAnd(attributeByValue[attribute]).neq(0)),
+            ee.Image(0)
+        )
+})
+
+const maskClouds = () =>
+    image => image.updateMask(image.select('cloud').not())
 
 const toInt16 = () =>
     image => image
