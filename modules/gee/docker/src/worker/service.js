b6543343b6eb2b439370b2bab8ac9d2d83e0153a
Luca Paolini <luca.paolini@fao.org>
2019-12-12 16:11:13 +0100

    Fixed service.

--- modules/gee/docker/src/worker/service.js
+++ modules/gee/docker/src/worker/service.js
@@ -1,44 +1,35 @@
 const {ReplaySubject, Subject} = require('rxjs')
 const {filter, map, share, first} = require('rxjs/operators')
 const {v4: uuid} = require('uuid')
-const log = require('../log')
 
 const serviceRequest$ = new ReplaySubject()
 const serviceResponse$ = new Subject()
 
-const initMain = (in$, out$) => {
+const initMain = (request$, response$) => {
     const handle$ = ({serviceName, requestId, data}) =>
         require(`@sepal/service/${serviceName}`).handle$(requestId, data)
 
-    out$.subscribe(
-        ({serviceName, requestId, data}) => {
-            log.warn('service request:', {serviceName, requestId, data})
-            handle$({serviceName, requestId, data}).subscribe({
-                next: value => {
-                    in$.next(value)
-                },
-                error: error => in$.error(error),
-                complete: () => in$.complete()
-            })
-        }
+    request$.subscribe(
+        ({serviceName, requestId, data}) =>
+            handle$({serviceName, requestId, data}).subscribe(
+                value => response$.next(value)
+            )
     )
 }
 
-const initWorker = (in$, out$) => {
-    serviceRequest$.subscribe({
-        next: request => in$.next(request)
-    })
-    out$.subscribe({
-        next: response => {
-            log.warn('got service response', response)
-            serviceResponse$.next(response)
-        }
-    })
+const initWorker = (request$, response$) => {
+    serviceRequest$.subscribe(
+        request => request$.next(request)
+    )
+    response$.subscribe(
+        response => serviceResponse$.next(response)
+    )
 }
 
 const request$ = (serviceName, data) => {
     const requestId = uuid()
     serviceRequest$.next({serviceName, requestId, data})
+
     return serviceResponse$.pipe(
         share(),
         filter(({requestId: currentRequestId}) => currentRequestId === requestId),
