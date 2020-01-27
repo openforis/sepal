126f5bf5389dc55b38757da86de99336056d36dc
Luca Paolini <luca.paolini@fao.org>
2020-01-27 11:29:15 +0100

    Added generic Widget 'loading' message.

--- modules/gui/frontend/src/app/home/body/process/mosaic/panels/aoi/countrySection.js
+++ modules/gui/frontend/src/app/home/body/process/mosaic/panels/aoi/countrySection.js
@@ -98,7 +98,7 @@ class CountrySection extends React.Component {
                     placement='below'
                     options={countries || []}
                     placeholder={countryPlaceholder}
-                    busyMessage={loadCountries.active}
+                    busyMessage={loadCountries.active && msg('widget.loading')}
                     disabled={loadCountries.failed}
                     autoFocus
                     onChange={option => {
@@ -113,7 +113,7 @@ class CountrySection extends React.Component {
                     placement='below'
                     options={(countryAreas || [])}
                     placeholder={areaPlaceholder}
-                    busyMessage={loadCountryAreas.active}
+                    busyMessage={loadCountryAreas.active && msg('widget.loading')}
                     disabled={loadCountryAreas.failed || !countryAreas || countryAreas.length === 0}
                     onChange={() => this.aoiChanged$.next()}
                 />
