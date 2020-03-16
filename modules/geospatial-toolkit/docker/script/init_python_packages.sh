ffd8cf932a814eb79f4e96be5605cf493b221caa
Daniel Wiell <daniel.wiell@fao.org>
2020-03-16 11:41:19 +0000

    Made sure python packages are installed with latest version, except for GDAL.

--- modules/geospatial-toolkit/docker/script/init_python_packages.sh
+++ modules/geospatial-toolkit/docker/script/init_python_packages.sh
@@ -5,5 +5,15 @@ echo
 echo "************************************"
 echo "*** Installing misc python tools ***"
 echo "************************************"
-pip3 install -r /config/requirements.txt
+export GDAL_VERSION=`pip3 show GDAL|grep 'Version: ' | cut -c10-`
+
+function template {
+    local template=$1
+    local destination=$2
+    envsubst < $template > $destination
+}
+
+template /config/requirements.txt /tmp/requirements.txt
+
+pip3 install -r /tmp/requirements.txt -U
 pip3 install --upgrade pip
