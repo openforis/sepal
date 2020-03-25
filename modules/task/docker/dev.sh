6fc0b9dc6444ecd0909e3a411141b087a1480f64
Daniel Wiell <daniel.wiell@fao.org>
2020-03-25 11:44:57 +0000

    Corrected GEE key path.

--- modules/task/docker/dev.sh
+++ modules/task/docker/dev.sh
@@ -5,10 +5,10 @@ NODE_TLS_REJECT_UNAUTHORIZED=0 nodemon \
     --watch $LIBS/shared \
     --inspect=0.0.0.0:9229 src/main.js \
     --gee-email google-earth-engine@openforis-sepal.iam.gserviceaccount.com \
-    --gee-key-path $SEPAL_CONFIG/google-earth-engine/key.pem \
+    --gee-key-path $SEPAL_CONFIG/google-earth-engine/gee-service-account.pem \
     --google-project-id 'openforis-sepal' \
     --sepal-host localhost:3000 \
     --sepal-username 'sepalAdmin' \
     --sepal-password 'the admin password' \
-    --home-dir $SEPAL_CONFIG/sepal-server/home/admin \
+    --home-dir /var/sepal/sepal-server/home/admin \
     --username admin
