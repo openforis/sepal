b512c12a30c46c36e563e6f0a034ee8e155a0bc7
Daniel Wiell <daniel.wiell@fao.org>
2020-04-17 12:34:32 +0000

    Fixed issues after upgrading earthengine-api and Python version.

--- modules/ceo/docker/script/init_image.sh
+++ modules/ceo/docker/script/init_image.sh
@@ -2,16 +2,20 @@
 set -e
 
 apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -y\
- python-pip\
+ python3\
+ python3-pip\
  libssl-dev\
  libffi-dev\
  gettext\
  git\
- mongodb\
  supervisor
 
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list  
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
-apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get -y install yarn
+apt-get update && apt-get install -y\
+ mongodb-org\
+ nodejs\
+ yarn
