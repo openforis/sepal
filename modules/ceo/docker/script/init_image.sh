3cc767e5e8c33e5b7bd10522a5d095aa17830750
Daniel Wiell <daniel.wiell@fao.org>
2020-04-18 07:04:46 +0000

    Reverted ceo to old mongo version.

--- modules/ceo/docker/script/init_image.sh
+++ modules/ceo/docker/script/init_image.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 set -e
-export DEBIAN_FRONTEND=noninteractive
-apt-get -y update && apt-get install -y\
- curl\
- python3\
- python3-pip\
+
+apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -y\
+ python-pip\
  libssl-dev\
  libffi-dev\
  gettext\
  git\
- supervisor\
- wget
+ mongodb\
+ supervisor
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list  
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install -y\
- mongodb-org\
- nodejs\
- yarn
+apt-get update && apt-get -y install yarn
