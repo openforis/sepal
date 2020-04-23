ee75cf2abf24a91fc03f874de25137873da18e9c
Luca Paolini <luca.paolini@fao.org>
2020-04-23 16:07:49 +0200

    Used gee test.sh from dev.sh.

--- modules/gee/docker/dev.sh
+++ modules/gee/docker/dev.sh
@@ -4,11 +4,13 @@ LIBS=../../../lib/js
 nodemon \
     --watch src \
     --watch $LIBS/shared \
-    --inspect=0.0.0.0:9228 src/main.js \
-    --gee-email google-earth-engine@openforis-sepal.iam.gserviceaccount.com \
-    --gee-key-path $SEPAL_CONFIG/google-earth-engine/gee-service-account.pem \
-    --sepal-host localhost:3000 \
-    --sepal-username 'sepalAdmin' \
-    --sepal-password 'the admin password' \
-    --home-dir $SEPAL_CONFIG/sepal-server/home/admin \
-    --username admin
+    --inspect=0.0.0.0:9228 \
+    --exec npm run test
+    # src/main.js \
+    # --gee-email google-earth-engine@openforis-sepal.iam.gserviceaccount.com \
+    # --gee-key-path $SEPAL_CONFIG/google-earth-engine/gee-service-account.pem \
+    # --sepal-host localhost:3000 \
+    # --sepal-username 'sepalAdmin' \
+    # --sepal-password 'the admin password' \
+    # --home-dir $SEPAL_CONFIG/sepal-server/home/admin \
+    # --username admin
