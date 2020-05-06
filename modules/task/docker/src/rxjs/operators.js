01292eae2fcc27479b53959f1b73f60485135fea
Daniel Wiell <daniel.wiell@fao.org>
2020-05-06 10:09:55 +0000

    Made sure custom operators completed.

--- modules/task/docker/src/rxjs/operators.js
+++ modules/task/docker/src/rxjs/operators.js
@@ -1,19 +1,26 @@
-const {concat, defer, interval, of, pipe} = require('rxjs')
-const {exhaustMap, filter, last, map, switchMap, windowTime} = require('rxjs/operators')
+const {Subject, concat, defer, interval, of, pipe} = require('rxjs')
+const {exhaustMap, filter, finalize, last, map, switchMap, takeUntil, windowTime} = require('rxjs/operators')
 const progress = require('root/progress')
+const log = require('sepal/log').getLogger('task')
 
 const EMPTY_WINDOW = Symbol('NO_MESSAGE_IN_WINDOW')
 
 module.exports = {
-    lastInWindow: time =>
-        pipe(
+    lastInWindow: time => {
+        var cancel$ = new Subject()
+        return pipe(
+            finalize(() => cancel$.next('finalize')),
             windowTime(time),
             switchMap(window$ => concat(of(EMPTY_WINDOW), window$).pipe(last())),
-            filter(value => value !== EMPTY_WINDOW)
-        ),
+            filter(value => value !== EMPTY_WINDOW),
+            takeUntil(cancel$)
+        )
+    },
 
     repeating: (project, rate) => {
+        var cancel$ = new Subject()
         return pipe(
+            finalize(() => cancel$.next('finalize')),
             switchMap(item =>
                 concat(
                     defer(() => project(item)),
@@ -21,7 +28,8 @@ module.exports = {
                         exhaustMap(() => project(item)),
                     )
                 )
-            )
+            ),
+            takeUntil(cancel$)
         )
     },
 
