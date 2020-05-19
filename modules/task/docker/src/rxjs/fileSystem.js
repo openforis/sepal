6aea3931d054c34b14f56377a7350bf8be8172fc
Daniel Wiell <daniel.wiell@fao.org>
2020-05-19 18:20:45 +0200

    Added RxJS API for basic filesystem operations.

---
+++ modules/task/docker/src/rxjs/fileSystem.js
@@ -0,0 +1,47 @@
+const {from, of} = require('rxjs')
+const {catchError, concatAll, filter, map, switchMap, tap} = require('rxjs/operators')
+const fs = require('fs')
+const Path = require('path')
+
+const ls$ = (path, options = {}) =>
+    from(fs.promises.readdir(path, options))
+
+const mkdir$ = (path, options = {}) =>
+    from(fs.promises.mkdir(path, options)).pipe(
+        map(() => path)
+    )
+
+const exists$ = path =>
+    from(fs.promises.stat(path)).pipe(
+        catchError(() => of(null)),
+        map(stat => !!stat)
+    )
+
+const getPostfixIndex = (name, preferredName) => {
+    const match = name.match(new RegExp(`^${preferredName}_(\\d)$`))
+    return match && match[1]
+        ? parseInt(match[1]) + 1
+        : 1
+}
+
+const mkdirSafe$ = (preferredPath, options = {}) => {
+    const preferredName = Path.basename(preferredPath)
+    const parent = Path.dirname(preferredPath)
+    const mkdirPostfix$ = () => ls$(parent).pipe(
+        map(names => names
+            .filter(name => name.startsWith(preferredName))
+            .map(name => getPostfixIndex(name, preferredName))
+        ),
+        map(postfixIndexes => Math.max(...postfixIndexes)),
+        switchMap(postfixIndex =>
+            mkdir$(`${preferredPath}_${postfixIndex}`, options))
+    )
+    return exists$(preferredPath).pipe(
+        switchMap(exists => exists
+            ? mkdirPostfix$()
+            : mkdir$(preferredPath, options)
+        )
+    )
+}
+
+module.exports = {ls$, mkdir$, mkdirSafe$}
---
+++ modules/task/docker/src/rxjs/fileSystem.test.js
@@ -0,0 +1,89 @@
+const {from} = require('rxjs')
+const {switchMap, tap} = require('rxjs/operators')
+const fs = require('fs')
+const os = require('os')
+const Path = require('path')
+const {ls$, mkdir$, mkdirSafe$} = require('./fileSystem')
+const {stream, emitsOne, emitsNothing, throwsError} = require('./testHelper')
+
+let emptyDirPath
+
+describe('ls$()', () => {
+    stream('emits an empty list when dir is empty',
+        () => ls$(emptyDirPath),
+        emitsOne(files => expect(files.length).toBe(0))
+    )
+
+    const basename = 'some_dir'
+    stream('emits basenames when dir is non-empty',
+        () => mkdir$(Path.join(emptyDirPath, basename)).pipe(
+            switchMap(() => ls$(emptyDirPath))
+        ),
+        emitsOne(files => {
+            expect(files).toEqual([basename])
+        })
+    )
+
+    stream('fails when dir does not exist',
+        () => ls$(emptyDirPath + '-non-existing'),
+        emitsNothing(),
+        throwsError()
+    )
+})
+
+
+describe('mkdir$()', () => {
+    const path = () => Path.join(emptyDirPath, 'some_dir')
+
+    stream('emits path of dir when successfully created',
+        () => mkdir$(path()),
+        emitsOne(dirPath => expect(dirPath).toEqual(path()))
+    )
+
+    stream('throws error when directory already exists',
+        () => mkdir$(path()).pipe(
+            switchMap(() => mkdir$(path())) // Should fail - it already exists
+        ),
+        emitsNothing(),
+        throwsError()
+    )
+})
+
+
+describe('mkdirSafe$()', () => {
+    const preferredPath = () => Path.join(emptyDirPath, 'some_dir')
+
+    stream('emits preferred path when path not already exists',
+        () => mkdirSafe$(preferredPath()),
+        emitsOne(dirPath => expect(dirPath).toEqual(preferredPath()))
+    )
+
+    stream('emits preferredPath_1 when directory already exists',
+        () => mkdir$(preferredPath()).pipe(
+            switchMap(() => mkdirSafe$(preferredPath())) // Should fail - it already exists
+        ),
+        emitsOne(dirPath => expect(dirPath).toEqual(preferredPath() + '_1'))
+    )
+
+    stream('emits preferredPath_2 when directory and _1 already exists',
+        () => mkdir$(preferredPath()).pipe(
+            switchMap(() => mkdir$(preferredPath() + '_1')), // _1 is taken
+            switchMap(() => mkdirSafe$(preferredPath())) // Should fail - it already exists
+        ),
+        emitsOne(dirPath => expect(dirPath).toEqual(preferredPath() + '_2'))
+    )
+})
+
+
+const mkTmpDir$ = () =>
+    from(fs.promises.mkdtemp(Path.join(os.tmpdir(), 'test-')))
+
+
+beforeEach(() =>
+    mkTmpDir$().pipe(
+        tap(path => emptyDirPath = path)
+    ).toPromise()
+)
+
+afterEach(() => {
+})
