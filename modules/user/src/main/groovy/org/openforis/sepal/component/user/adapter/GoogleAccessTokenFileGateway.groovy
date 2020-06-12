7c02d1eb6091f75cd96b570bfaf84dc5cbe5d7fb
Daniel Wiell <daniel.wiell@fao.org>
2020-06-12 16:23:41 +0200

    Prevented ~/.config/earthengine/credentials from being deleted.

--- modules/user/src/main/groovy/org/openforis/sepal/component/user/adapter/GoogleAccessTokenFileGateway.groovy
+++ modules/user/src/main/groovy/org/openforis/sepal/component/user/adapter/GoogleAccessTokenFileGateway.groovy
@@ -2,8 +2,16 @@ package org.openforis.sepal.component.user.adapter
 
 import groovy.json.JsonOutput
 import org.openforis.sepal.user.GoogleTokens
+import org.openforis.sepal.util.Terminal
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.nio.file.Files
+import java.nio.file.LinkOption
+import java.nio.file.Path
 
 class GoogleAccessTokenFileGateway {
+    private Logger LOG = LoggerFactory.getLogger(GoogleAccessTokenFileGateway)
     private final String homeDirectory
 
     GoogleAccessTokenFileGateway(String homeDirectory) {
@@ -11,11 +19,14 @@ class GoogleAccessTokenFileGateway {
     }
 
     void save(String username, GoogleTokens tokens) {
-        if (!tokens) {
+        if (!tokens)org.openforis.sepal.component.user.adapter.GoogleAccessTokenFileGateway {
             delete(username)
             return
         }
         def file = credentialsFile(username)
+        def lockFile = new File('.lock', file.parentFile)
+        if (!lockFile.exists())
+            lockFile.createNewFile()
         if (!file.exists()) {
             file.parentFile.mkdirs()
             file.createNewFile()
@@ -24,6 +35,10 @@ class GoogleAccessTokenFileGateway {
                 access_token            : tokens.accessToken,
                 access_token_expiry_date: tokens.accessTokenExpiryDate
         ]))
+        def gid = Files.getAttribute(Path.of(homeDirectory, username), 'unix:gid', LinkOption.NOFOLLOW_LINKS);
+        Terminal.execute(file.parentFile.parentFile, 'sudo', 'chown', '-R', "root:$gid", '.')
+        Terminal.execute(file.parentFile.parentFile, 'sudo', 'chmod', "1775", '.')
+        Terminal.execute(file.parentFile, 'sudo', 'chmod', "1775", '.')
     }
 
     void delete(String username) {
