462f47e7fd0fd55f5f958681905d0ac150ce3673
Daniel Wiell <daniel.wiell@fao.org>
2020-06-16 10:54:30 +0200

    Removed logging when EE account not whitelisted.

--- modules/user/src/main/groovy/org/openforis/sepal/component/user/adapter/RestGoogleEarthEngineWhitelistChecker.groovy
+++ modules/user/src/main/groovy/org/openforis/sepal/component/user/adapter/RestGoogleEarthEngineWhitelistChecker.groovy
@@ -4,11 +4,8 @@ import groovyx.net.http.RESTClient
 import org.openforis.sepal.component.user.api.GoogleEarthEngineWhitelistChecker
 import org.openforis.sepal.user.GoogleTokens
 import org.openforis.sepal.user.User
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 class RestGoogleEarthEngineWhitelistChecker implements GoogleEarthEngineWhitelistChecker {
-    private static final Logger LOG = LoggerFactory.getLogger(RestGoogleEarthEngineWhitelistChecker)
     private final String googleEarthEngineUri
 
     RestGoogleEarthEngineWhitelistChecker(String googleEarthEngineUri) {
@@ -23,7 +20,6 @@ class RestGoogleEarthEngineWhitelistChecker implements GoogleEarthEngineWhitelis
             )
             return true
         } catch (Exception e) {
-            LOG.debug("Not whitelisted. username: $username, googleEarthEngineUri: $googleEarthEngineUri", e)
             return false
         }
     }
