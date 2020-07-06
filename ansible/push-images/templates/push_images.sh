54a628fe41d22fdcef45e12d92108afda8591aaa
Daniel Wiell <daniel.wiell@fao.org>
2020-07-06 18:48:54 +0200

    Removed peatlands module.

--- ansible/push-images/templates/push_images.sh
+++ ansible/push-images/templates/push_images.sh
@@ -12,7 +12,6 @@ docker push localhost/openforis/sepal:{{ version }}
 docker push localhost/openforis/gui:{{ version }}
 docker push localhost/openforis/ceo:{{ version }}
 docker push localhost/openforis/ceo-gateway:{{ version }}
-docker push localhost/openforis/peatlands:{{ version }}
 docker push localhost/openforis/mysql:{{ version }}
 docker push localhost/openforis/mysql-backup:{{ version }}
 docker push localhost/openforis/gee:{{ version }}
