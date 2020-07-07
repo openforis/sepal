e33dd9f0afe50de0c3960645721fc9c1d3ae422f
Daniel Wiell <daniel.wiell@fao.org>
2020-07-07 11:14:40 +0200

    Removed incorrect sudo.

--- modules/geospatial-toolkit/docker/script/init_osk.sh
+++ modules/geospatial-toolkit/docker/script/init_osk.sh
@@ -6,5 +6,5 @@ echo "*****************************"
 echo "*** Installing OpenSARKit ***"
 echo "*****************************"
 wget https://raw.githubusercontent.com/openforis/opensarkit/master/bins/Install_OST/installer_ubuntu1604.sh
-yes yes | sudo bash installer_ubuntu1604.sh
+yes yes | bash installer_ubuntu1604.sh
 rm installer_ubuntu1604.sh
