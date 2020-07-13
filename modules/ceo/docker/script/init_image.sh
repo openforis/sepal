750f83512594a0c28366a17857e5a14c839d2e01
Daniel Wiell <daniel.wiell@fao.org>
2020-07-13 10:30:55 +0200

    Explicitly installed mongodb 2.6.

--- modules/ceo/docker/script/init_image.sh
+++ modules/ceo/docker/script/init_image.sh
@@ -7,11 +7,20 @@ apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -y\
  libffi-dev\
  gettext\
  git\
- mongodb\
- supervisor
+ supervisor\
+ wget
 
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update && apt-get -y install yarn
+
+cd /usr/local/lib
+export MONGODB=mongodb-linux-x86_64-2.6.10
+wget https://fastdl.mongodb.org/linux/$MONGODB.tgz
+tar -xvf $MONGODB.tgz
+rm $MONGODB.tgz
+ln -sf mongodb $MONGODB
+ln -sf mongodb/bin/* /usr/local/bin
+cd -
