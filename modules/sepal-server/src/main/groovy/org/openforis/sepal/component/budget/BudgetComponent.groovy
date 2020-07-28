ea2179bb1b94c6504207373b2b3b12366b1be5c1
Daniel Wiell <daniel.wiell@fao.org>
2020-07-28 17:23:23 +0200

    Made sure to convert storage use to GB.

--- modules/sepal-server/src/main/groovy/org/openforis/sepal/component/budget/BudgetComponent.groovy
+++ modules/sepal-server/src/main/groovy/org/openforis/sepal/component/budget/BudgetComponent.groovy
@@ -91,7 +91,7 @@ class BudgetComponent extends DataSourceBackedComponent implements EndpointRegis
             submit(new UpdateUserSpendingReport(userToUpdate: message.username as String))
         }
         subscribe(COMPONENT_NAME, userStorageTopic) { message, type ->
-            submit(new UpdateUserStorageUsage(userToUpdate: message.username as String, gbUsed: message.size/1000/1000))
+            submit(new UpdateUserStorageUsage(userToUpdate: message.username as String, gbUsed: message.size/1000/1000/1000))
         }
     }
 
