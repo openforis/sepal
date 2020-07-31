cc3ada83bd758d5f81a96c2260d3aa2079c95202
Luca Paolini <luca.paolini@fao.org>
2020-07-31 17:53:44 +0200

    Implemented debouncing of file deletions.

--- modules/user-storage/docker/src/messageHandler.js
+++ modules/user-storage/docker/src/messageHandler.js
@@ -2,9 +2,25 @@ const _ = require('lodash')
 const log = require('sepal/log').getLogger('messageQueue')
 const {scheduleRescan} = require('./scan')
 const {setSessionActive, setSessionInactive} = require('./persistence')
+const {Subject} = require('rxjs')
+const {debounceTime, groupBy, mergeMap, switchMap} = require('rxjs/operators')
 
 const logError = (key, msg) =>
     log.error('Incoming message doesn\'t match expected shape', {key, msg})
+
+const filesDeleted$ = new Subject()
+
+filesDeleted$.pipe(
+    groupBy(username => username),
+    mergeMap(group$ =>
+        group$.pipe(
+            debounceTime(1000),
+            switchMap(username =>
+                scheduleRescan({username, type: 'fileDeleted'})
+            )
+        )
+    )
+).subscribe()
 
 const handlers = {
     'workerSession.WorkerSessionActivated': async (key, msg) => {
@@ -28,7 +44,7 @@ const handlers = {
     'files.FilesDeleted': async (key, msg) => {
         const {username} = msg
         if (username) {
-            await scheduleRescan({username, type: 'fileDeleted'})
+            filesDeleted$.next(username)
         } else {
             logError(key, msg)
         }
