667656e222c10af558048cbe7e502227146f3528
Luca Paolini <luca.paolini@fao.org>
2020-07-31 16:07:13 +0200

    Added user-storage validation of critical arguments.

--- modules/user-storage/docker/src/config.js
+++ modules/user-storage/docker/src/config.js
@@ -2,6 +2,11 @@ const program = require('commander')
 const log = require('sepal/log').getLogger('config')
 const _ = require('lodash')
 
+const fatalError = error => {
+    log.fatal(error)
+    process.exit(1)
+}
+
 program.exitOverride()
 
 try {
@@ -11,16 +16,15 @@ try {
         .requiredOption('--home-dir <value>', 'Base directory of user homes')
         .option('--min-delay-seconds <number>', 'Minimum delay in seconds before rescheduling', parseInt)
         .option('--max-delay-seconds <number>', 'Maximum delay in seconds before rescheduling', parseInt)
-        .option('--delay-increase-factor <number>', 'Auto-rescheduling delay increase factor', parseInt)
+        .option('--delay-increase-factor <number>', 'Auto-rescheduling delay increase factor', parseFloat)
         .option('--concurrency <number>', 'Concurrent rescan jobs', parseInt)
         .option('--max-retries <number>', 'Maximum number of retries when job has failed', parseInt)
         .option('--initial-retry-delay-seconds <number>', 'Initial delay in seconds between retries (exponential backoff)', parseInt)
         .parse(process.argv)
 } catch (error) {
-    log.fatal(error)
-    process.exit(1)
+    fatalError(error)
 }
-    
+
 const {
     amqpUri,
     redisUri,
@@ -34,6 +38,18 @@ const {
 } = program
 
 log.info('Configuration loaded')
+
+if (minDelaySeconds < 5) {
+    fatalError(`Argument --min-delay-seconds (${minDelaySeconds}) cannot be less than 5`)
+}
+
+if (delayIncreaseFactor <= 1) {
+    fatalError(`Argument --delay-increase-factor (${delayIncreaseFactor}) cannot be less or equal to 1`)
+}
+
+if (maxDelaySeconds <= minDelaySeconds) {
+    fatalError(`Argument --max-delay-seconds (${maxDelaySeconds}) cannot be less or equal to --min-delay-seconds (${minDelaySeconds})`)
+}
 
 module.exports = {
     amqpUri,
