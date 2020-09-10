1c8516d8d27b1b25cb53a265e1b2cbfaa454144d
Daniel Wiell <daniel.wiell@fao.org>
2020-09-10 15:31:13 +0200

    Corrected snap installation script name.

--- modules/geospatial-toolkit/docker/script/init_esa_snap_toolbox.sh
+++ modules/geospatial-toolkit/docker/script/init_esa_snap_toolbox.sh
@@ -5,7 +5,7 @@ echo
 echo "***********************************"
 echo "*** Installing ESA SNAP Toolbox ***"
 echo "***********************************"
-INSTALL_SCRIPT=esa-snap_all_unix_7_0
+INSTALL_SCRIPT=esa-snap_all_unix_7_0.sh
 wget -nv http://step.esa.int/downloads/7.0/installers/$INSTALL_SCRIPT
 sh $INSTALL_SCRIPT -q -overwrite
 rm -f $INSTALL_SCRIPT
