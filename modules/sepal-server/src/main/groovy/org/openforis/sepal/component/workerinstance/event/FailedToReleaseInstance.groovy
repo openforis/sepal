e751103e0c77fd35ef822176b3b7f184f13de55b
Daniel Wiell <daniel.wiell@fao.org>
2020-09-10 15:40:10 +0200

    Removed all mutable objects from events.

--- modules/sepal-server/src/main/groovy/org/openforis/sepal/component/workerinstance/event/FailedToReleaseInstance.groovy
+++ modules/sepal-server/src/main/groovy/org/openforis/sepal/component/workerinstance/event/FailedToReleaseInstance.groovy
@@ -3,7 +3,7 @@ package org.openforis.sepal.component.workerinstance.event
 import groovy.transform.Immutable
 import org.openforis.sepal.event.Event
 
-@Immutable(knownImmutables = ['exception'])
+@Immutable
 class FailedToReleaseInstance implements Event {
     String instanceId
     String error
--- modules/sepal-server/src/main/groovy/org/openforis/sepal/component/workerinstance/event/FailedToRequestInstance.groovy
+++ modules/sepal-server/src/main/groovy/org/openforis/sepal/component/workerinstance/event/FailedToRequestInstance.groovy
@@ -3,7 +3,7 @@ package org.openforis.sepal.component.workerinstance.event
 import groovy.transform.Immutable
 import org.openforis.sepal.event.Event
 
-@Immutable(knownImmutables = ['exception'])
+@Immutable
 class FailedToRequestInstance implements Event {
     String workerType
     String instanceType
