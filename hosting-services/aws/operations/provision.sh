4c0bbb3127fd0db658cfd08762febb7eb230975b
Daniel Wiell <daniel.wiell@fao.org>
2020-09-23 14:24:05 +0200

    Updated Jenkins deployment to work with newer versions.

--- hosting-services/aws/operations/provision.sh
+++ hosting-services/aws/operations/provision.sh
@@ -5,6 +5,7 @@ VERSION=$1
 REGION=$2
 CONFIG_HOME=$3
 PRIVATE_KEY=$CONFIG_HOME/certificates/aws.pem
+LOCAL_IP_ADDRESS=`curl -s api.ipify.org`
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 echo "Provisioning and deploying Ops on AWS [\
@@ -22,7 +23,7 @@ source ../export-aws-keys.sh $CONFIG_HOME/secret.yml
 ansible-playbook provision.yml \
     -i ${INVENTORY_FILE_PATH} \
     --private-key=$PRIVATE_KEY \
-    --extra-vars "region=$REGION secret_vars_file=$CONFIG_HOME/secret.yml config_home=$CONFIG_HOME"
+    --extra-vars "region=$REGION secret_vars_file=$CONFIG_HOME/secret.yml config_home=$CONFIG_HOME local_ip_address=$LOCAL_IP_ADDRESS"
 
 #${INVENTORY_FILE_PATH} --refresh-cache > /dev/null
 
