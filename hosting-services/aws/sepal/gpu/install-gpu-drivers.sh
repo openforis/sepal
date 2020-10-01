ab02f5c8781ceca6d142856cbaaff93d8ac6c00e
Daniel Wiell <daniel.wiell@fao.org>
2020-10-01 12:29:51 +0200

    Removed cuda installation from geospatial-toolkit.

--- hosting-services/aws/sepal/gpu/install-gpu-drivers.sh
+++ hosting-services/aws/sepal/gpu/install-gpu-drivers.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 yum install -y gcc kernel-devel-$(uname -r)
-aws s3 cp s3://ec2-linux-nvidia-drivers/g4/grid-10.2/NVIDIA-Linux-x86_64-440.87-grid-aws.run .
-#aws s3 cp --recursive s3://ec2-linux-nvidia-drivers/latest/ .
+aws s3 cp --recursive s3://ec2-linux-nvidia-drivers/latest/ .
 chmod +x NVIDIA-Linux-x86_64*.run
 /bin/sh ./NVIDIA-Linux-x86_64*.run -s
 chmod +x /usr/local/bin/init-gpu-drivers.sh
--- modules/geospatial-toolkit/docker/script/init_gpu.sh
+++ modules/geospatial-toolkit/docker/script/init_gpu.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
-UPDATE_DATE='2020-09-30'
 
 apt-get update -y
 apt-get install -y nvidia-cuda-toolkit
-wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-apt-get update -y
-apt-get install -y cuda
