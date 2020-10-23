c27138b6ffa91082da3ad5acf4deeeb35065351b
Daniel Wiell <daniel.wiell@fao.org>
2020-10-23 17:41:42 +0200

    Hard-coded available Planet layers.

--- modules/gui/frontend/src/app/home/map/layersMenu.js
+++ modules/gui/frontend/src/app/home/map/layersMenu.js
@@ -16,37 +16,54 @@ import {getNorwayPlanetApiKey} from './map'
 import actionBuilder from '../../../action-builder'
 
 const fields = {
-    year: new Form.Field(),
-    month: new Form.Field()
+    // year: new Form.Field(),
+    // month: new Form.Field(),
+    dateRange: new Form.Field(),
+    proc: new Form.Field()
 }
 
 const mapStateToProps = (state, ownProps) => ({
     labelsShown: select([ownProps.statePath, 'labelsShown']),
     baseLayer: select([ownProps.statePath, 'baseLayer']),
     overlayIndex: select([ownProps.statePath, 'overlayIndex']),
-    year: select([ownProps.statePath, 'options.year']),
-    month: select([ownProps.statePath, 'options.month'])
+    dateRange: select([ownProps.statePath, 'options.dateRange']),
+    proc: select([ownProps.statePath, 'options.proc'])
+    // year: select([ownProps.statePath, 'options.year']),
+    // month: select([ownProps.statePath, 'options.month'])
 })
 
 class _LayersMenu extends React.Component {
 
     getPlanetDateOptions() {
-        const {inputs: {year}} = this.props
-        const startDate = moment('2018-08-01')
-        const endDate = moment().subtract(1, 'months')
+        // const {inputs: {year}} = this.props
+        // const startDate = moment('2018-08-01')
+        // const endDate = moment().subtract(1, 'months')
+        //
+        // const yearOptions = sequence(startDate.year(), moment().year())
+        //     .map(year => ({value: `${year}`, label: `${year}`}))
+        //
+        // const allMonthOptions = moment.monthsShort()
+        //     .map((label, i) => ({value: `${i + 1}`.padStart(2, '0'), label}))
+        //
+        // const monthOptions = year.value === `${startDate.year()}`
+        //     ? allMonthOptions.slice(startDate.month() + 1)
+        //     : year.value === `${endDate.year()}`
+        //         ? allMonthOptions.slice(0, endDate.month() + 1)
+        //         : allMonthOptions
+        // return {yearOptions, monthOptions}
 
-        const yearOptions = sequence(startDate.year(), moment().year())
-            .map(year => ({value: `${year}`, label: `${year}`}))
-
-        const allMonthOptions = moment.monthsShort()
-            .map((label, i) => ({value: `${i + 1}`.padStart(2, '0'), label}))
-
-        const monthOptions = year.value === `${startDate.year()}`
-            ? allMonthOptions.slice(startDate.month() + 1)
-            : year.value === `${endDate.year()}`
-                ? allMonthOptions.slice(0, endDate.month() + 1)
-                : allMonthOptions
-        return {yearOptions, monthOptions}
+        const procOptions = [
+            {value: 'rgb', label: 'RGB'},
+            {value: 'cir', label: 'CIR'},
+        ]
+        const dateRangeOptions = [
+            {value: '2018-12_2019-05', label: 'Dec 2018 (6 months)'},
+            {value: '2019-06_2019-11', label: 'Jun 2019 (6 months)'},
+            {value: '2019-12_2020-05', label: 'Dec 2019 (6 months)'},
+            {value: '2020-06_2020-08', label: 'Jun 2019 (3 months)'},
+            {value: '2020-09', label: 'Sep 2019 (1 month)'},
+        ]
+        return {procOptions, dateRangeOptions}
     }
 
     toggleOverlay = ({selectedOverlayIndex, mapContext, statePath}) => {
@@ -70,14 +87,17 @@ class _LayersMenu extends React.Component {
             .dispatch()
     }
 
-    changeBaseLayer(type, year, month) {
+    // changeBaseLayer(type, year, month, proc) {
+    changeBaseLayer(type, dateRange, proc) {
+        console.log('changeBaseLayer', dateRange.value, proc.value)
         const {statePath, mapContext} = this.props
         const planetApiKey = getNorwayPlanetApiKey()
         changeBaseLayer({
             type,
             mapContext,
             statePath,
-            options: {year: year.value, month: month.value, planetApiKey}
+            options: {dateRange: dateRange.value, proc: proc.value, planetApiKey}
+            // options: {year: year.value, month: month.value, proc: proc.value, planetApiKey}
         })
     }
 
@@ -97,7 +117,8 @@ class _LayersMenu extends React.Component {
 
     render() {
         const {
-            inputs: {year, month},
+            inputs: {dateRange, proc},
+            // inputs: {year, month, proc},
             activatable: {deactivate},
             labelsShown, baseLayer, labelLayerIndex, statePath, mapContext
         } = this.props
@@ -109,10 +130,13 @@ class _LayersMenu extends React.Component {
                 id={layer.layerIndex}
                 label={layer.label}
                 description={layer.description}/>)
-        const {yearOptions, monthOptions} = this.getPlanetDateOptions()
+        // const {yearOptions, monthOptions, procOptions} = this.getPlanetDateOptions()
+        const {dateRangeOptions, procOptions} = this.getPlanetDateOptions()
 
-        const yearOption = yearOptions.find(({value}) => year.value === value)
-        const monthOption = monthOptions.find(({value}) => month.value === value)
+        const procOption = procOptions.find(({value}) => proc.value === value)
+        const dateRangeOption = dateRangeOptions.find(({value}) => dateRange.value === value)
+        // const yearOption = yearOptions.find(({value}) => year.value === value)
+        // const monthOption = monthOptions.find(({value}) => month.value === value)
         return (
             <Menu
                 position='top-right'
@@ -121,7 +145,8 @@ class _LayersMenu extends React.Component {
                 <Menu.Select
                     id={'BASE_LAYER'}
                     label={msg('process.mosaic.mapToolbar.layersMenu.baseLayer')}
-                    onSelect={type => this.changeBaseLayer(type, year, month)}
+                    onSelect={type => this.changeBaseLayer(type, dateRange, proc)}
+                    // onSelect={type => this.changeBaseLayer(type, year, month, proc)}
                     selected={baseLayer || 'SEPAL'}>
                     <Menu.Option
                         id={'SEPAL'}
@@ -140,21 +165,40 @@ class _LayersMenu extends React.Component {
                                 <ButtonSelect
                                     placement='below'
                                     alignment='left'
-                                    input={year}
+                                    input={proc}
                                     tooltipPlacement='bottom'
-                                    options={yearOptions}
-                                    label={yearOption && yearOption.label}
-                                    onSelect={year => this.changeBaseLayer('PLANET', year, this.props.inputs.month)}
+                                    options={procOptions}
+                                    label={procOption && procOption.label}
+                                    onSelect={proc => this.changeBaseLayer('PLANET', this.props.inputs.dateRange, proc)}
+                                    // onSelect={proc => this.changeBaseLayer('PLANET', this.props.inputs.year, this.props.inputs.month, proc)}
                                 />
                                 <ButtonSelect
                                     placement='below'
                                     alignment='left'
-                                    input={month}
+                                    input={dateRange}
                                     tooltipPlacement='bottom'
-                                    options={monthOptions}
-                                    label={monthOption && monthOption.label}
-                                    onSelect={month => this.changeBaseLayer('PLANET', this.props.inputs.year, month)}
+                                    options={dateRangeOptions}
+                                    label={dateRangeOption && dateRangeOption.label}
+                                    onSelect={dateRange => this.changeBaseLayer('PLANET', dateRange, this.props.inputs.proc)}
                                 />
+                                {/*<ButtonSelect*/}
+                                {/*    placement='below'*/}
+                                {/*    alignment='left'*/}
+                                {/*    input={year}*/}
+                                {/*    tooltipPlacement='bottom'*/}
+                                {/*    options={yearOptions}*/}
+                                {/*    label={yearOption && yearOption.label}*/}
+                                {/*    onSelect={year => this.changeBaseLayer('PLANET', year, this.props.inputs.month, this.props.inputs.proc)}*/}
+                                {/*/>*/}
+                                {/*<ButtonSelect*/}
+                                {/*    placement='below'*/}
+                                {/*    alignment='left'*/}
+                                {/*    input={month}*/}
+                                {/*    tooltipPlacement='bottom'*/}
+                                {/*    options={monthOptions}*/}
+                                {/*    label={monthOption && monthOption.label}*/}
+                                {/*    onSelect={month => this.changeBaseLayer('PLANET', this.props.inputs.year, month, this.props.inputs.proc)}*/}
+                                {/*/>*/}
                             </div>
                         }/>
                 </Menu.Select>
@@ -183,19 +227,25 @@ class _LayersMenu extends React.Component {
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        const {inputs: {year, month}} = this.props
-        if (!year.value)
-            year.set(this.props.year || moment().format('YYYY'))
-        if (!month.value)
-            month.set(this.props.month || moment().subtract(1, 'months').format('MM'))
-        else {
-            const {monthOptions} = this.getPlanetDateOptions()
-            if (!monthOptions.find(({value}) => month.value === value)) {
-                const monthOption = monthOptions[0]
-                month.set(monthOption.value)
-                this.changeBaseLayer('PLANET', year, monthOption)
-            }
-        }
+        // const {inputs: {year, month, proc}} = this.props
+        // if (!year.value)
+            // year.set(this.props.year || moment().format('YYYY'))
+        // if (!month.value)
+            // month.set(this.props.month || moment().subtract(1, 'months').format('MM'))
+        // else {
+            // const {monthOptions} = this.getPlanetDateOptions()
+            // if (!monthOptions.find(({value}) => month.value === value)) {
+            //     const monthOption = monthOptions[0]
+            //     month.set(monthOption.value)
+            //     this.changeBaseLayer('PLANET', year, monthOption, )
+            // }
+        // }
+        const {inputs: {dateRange, proc}} = this.props
+        console.log(dateRange.value, proc.value)
+        if (!proc.value)
+            proc.set('rgb')
+        if (!dateRange.value)
+            dateRange.set('2020-09')
     }
 }
 
