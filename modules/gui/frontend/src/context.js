aebba5069aef8231021e2305e428fe654dbed660
Luca Paolini <luca.paolini@fao.org>
2020-11-19 00:55:51 +0100

    Implemented withContext assign or merge.

--- modules/gui/frontend/src/context.js
+++ modules/gui/frontend/src/context.js
@@ -4,20 +4,35 @@ export const withContext = (Context, prop, required = false) =>
     () => // wrapped with apparently useless function for consistency with other "with*" wrappers
         WrappedComponent =>
             class HigherOrderComponent extends React.Component {
-                wrap(context) {
-                    if (required && !context) {
-                        throw Error(`Component has no ${prop}: ${WrappedComponent}`)
-                    }
-                    return React.createElement(WrappedComponent, {
-                        ...this.props,
-                        [prop]: context
-                    })
-                }
                 render() {
                     return (
                         <Context.Consumer>
                             {context => this.wrap(context)}
                         </Context.Consumer>
                     )
+                }
+
+                wrap(context) {
+                    if (required && !context) {
+                        throw Error(`Component has no ${prop}: ${WrappedComponent}`)
+                    }
+                    const props = prop
+                        ? this.assign(context)
+                        : this.merge(context)
+                    return React.createElement(WrappedComponent, props)
+                }
+
+                assign(context) {
+                    return {
+                        ...this.props,
+                        [prop]: context
+                    }
+                }
+
+                merge(context) {
+                    return {
+                        ...this.props,
+                        ...context
+                    }
                 }
             }
