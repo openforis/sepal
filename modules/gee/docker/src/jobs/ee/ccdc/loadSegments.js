4b6cbbbddc1953cac19e24cedec9356005cbf9c5
Daniel Wiell <daniel.wiell@fao.org>
2020-12-02 22:31:24 +0100

    Split loadTimeSeries job into two - one for segments and one for observations.

--- modules/gee/docker/src/jobs/ee/ccdc/loadSegments.js
+++ modules/gee/docker/src/jobs/ee/ccdc/loadSegments.js
@@ -1,22 +1,47 @@
 const {job} = require('root/jobs/job')
 
-const worker$ = ({asset, latLng}) => {
+const worker$ = ({asset, recipe, latLng, bands}) => {
     const {toGeometry} = require('sepal/ee/aoi')
+    const {getCollection$} = require('sepal/ee/timeSeries/collection')
+    const {getSegments$} = require('sepal/ee/timeSeries/ccdc')
+    const {of} = require('rx')
+    const {switchMap} = require('rx/operators')
     const ee = require('ee')
+
     const aoi = {type: 'POINT', ...latLng}
     const geometry = toGeometry(aoi)
 
-    const segmentsForPixel$ = segments =>
-        ee.getInfo$(
-            segments.reduceRegion({
-                reducer: ee.Reducer.first(),
-                geometry,
-                scale: 1
-            }),
-            `Get CCDC segments for pixel (${latLng})`
+    const segmentsForPixel$ = segments$ =>
+        segments$.pipe(
+            switchMap(segments =>
+                ee.getInfo$(
+                    segments.reduceRegion({
+                        reducer: ee.Reducer.first(),
+                        geometry,
+                        scale: 10
+                    }),
+                    `Get CCDC segments for pixel (${latLng})`
+                )
+            )
         )
 
-    return segmentsForPixel$(ee.Image(asset))
+    const assetSegments$ = () =>
+        of(new ee.Image(asset))
+
+    const recipeSegments$ = () => {
+        const aoi = {type: 'POINT', ...latLng}
+        const collectionBands = [...new Set([...bands, ...recipe.breakpointBands])]
+        return getCollection$({...recipe, bands: collectionBands, aoi}).pipe(
+            switchMap(collection => {
+                return getSegments$({...recipe, collection, aoi, bands: collectionBands})
+            })
+        )
+    }
+
+    const segments$ = asset
+        ? assetSegments$()
+        : recipeSegments$()
+    return segmentsForPixel$(segments$)
 }
 
 module.exports = job({
