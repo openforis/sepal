6779c613cb3395408241df467597e1c691b48445
Luca Paolini <luca.paolini@fao.org>
2020-12-04 15:30:03 +0100

    Fixed "ghost tab" bug.

--- modules/gui/frontend/src/app/home/body/process/recipe/mosaic/mosaicPreview.js
+++ modules/gui/frontend/src/app/home/body/process/recipe/mosaic/mosaicPreview.js
@@ -1,18 +1,24 @@
-import {recipeActionBuilder} from '../../recipe'
+import {recipeActionBuilder, recipePath} from '../../recipe'
+import {select} from 'store'
 
 export const MosaicPreview = id => {
     const actionBuilder = recipeActionBuilder(id)
+    const tabPath = recipePath(id)
 
     return {
         hide() {
-            actionBuilder('HIDE_PREVIEW')
-                .set('ui.hidePreview', true)
-                .dispatch()
+            if (select(tabPath)) {
+                actionBuilder('HIDE_PREVIEW')
+                    .set('ui.hidePreview', true)
+                    .dispatch()
+            }
         },
         show() {
-            actionBuilder('HIDE_PREVIEW')
-                .set('ui.hidePreview', false)
-                .dispatch()
+            if (select(tabPath)) {
+                actionBuilder('SHOW_PREVIEW')
+                    .set('ui.hidePreview', false)
+                    .dispatch()
+            }
         }
     }
 }
