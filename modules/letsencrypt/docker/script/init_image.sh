c0f36e7411b3367d80072992727e344f04f5d0ac
Daniel Wiell <daniel.wiell@fao.org>
2021-01-20 12:53:34 +0100

    Updated letsencrypt cert issue code and deployed sys-monitor module.

--- modules/letsencrypt/docker/script/init_image.sh
+++ modules/letsencrypt/docker/script/init_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# https://certbot.eff.org/#ubuntutrusty-nginx
 apt-get update -y && apt-get install -y\
+ curl\
  wget\
  supervisor\
  python\
@@ -13,12 +13,5 @@ apt-get update -y && apt-get install -y\
  libffi-dev\
  ca-certificates
 
-cd /root
-wget -nv https://dl.eff.org/certbot-auto
-chmod a+x certbot-auto
-
-# Renew the certificate twice a day. It will not have any effect unless it's about to expire
-# but will catch cases where certificate been revoked for some reason.
-printf '%s\n' \
-    '38  0,12 * * * root /root/certbot-auto renew --quiet --no-self-upgrade' \
-    >> /etc/crontab
+apt-get install -y
+curl https://get.acme.sh | sh
