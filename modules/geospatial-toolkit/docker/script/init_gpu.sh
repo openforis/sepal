1cf589c9c13ae28f67958fa05e23dc3175f702c6
Daniel Wiell <daniel.wiell@fao.org>
2021-02-08 13:54:50 +0100

    Moved pyopencl installation into init_gpu.sh and made it work with OpenCL 1.1.

--- modules/geospatial-toolkit/docker/script/init_gpu.sh
+++ modules/geospatial-toolkit/docker/script/init_gpu.sh
@@ -3,3 +3,10 @@ set -e
 
 apt-get update -y
 apt-get install -y nvidia-cuda-toolkit
+
+cd /usr/local/src/
+git clone https://github.com/pyopencl/pyopencl
+cd pyopencl
+python3 configure.py
+echo 'CL_PRETEND_VERSION = "1.1"' >> siteconf.py
+pip3 install .
