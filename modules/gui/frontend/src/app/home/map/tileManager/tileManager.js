312f40c546d00900fa93cddee34dce4f28215899
Luca Paolini <luca.paolini@fao.org>
2021-02-08 10:29:30 +0100

    Implemented TileManager groups.

--- modules/gui/frontend/src/app/home/map/tileManager/tileManager.js
+++ modules/gui/frontend/src/app/home/map/tileManager/tileManager.js
@@ -2,95 +2,112 @@ import {ReplaySubject, Subject} from 'rxjs'
 import {finalize, first} from 'rxjs/operators'
 import {getRequestExecutor} from './requestExecutor'
 import {getRequestQueue} from './requestQueue'
-import {requestTag, tileProviderTag} from './tag'
+import {tileProviderTag} from './tag'
 import {v4 as uuid} from 'uuid'
 
-const CONCURRENCY = 4
+const tileProviderGroups = {}
 
-const tileProvidersInfo = {}
-const request$ = new Subject()
-const hidden$ = new Subject()
-
-const requestQueue = getRequestQueue()
-const requestExecutor = getRequestExecutor(CONCURRENCY)
-
-const getTileProviderInfo = id => {
-    const tileProviderInfo = tileProvidersInfo[id]
-    if (tileProviderInfo) {
-        return tileProviderInfo
-    }
-    throw new Error(`Unknown ${tileProviderTag(id)}`)
-}
-
-const addTileProvider = (tileProviderId, tileProvider) => {
-    tileProvidersInfo[tileProviderId] = {
-        tileProvider,
-        hidden: false,
-        activeRequests: 0,
-    }
-    console.log(`Added ${tileProviderTag(tileProviderId)}`)
-}
-
-const removeTileProvider = tileProviderId => {
-    delete tileProvidersInfo[tileProviderId]
-    console.log(`Removed ${tileProviderTag(tileProviderId)}`)
-}
-
-request$.subscribe(
-    ({tileProviderId, request, response$, cancel$}) => {
-        const requestId = uuid()
-        if (requestExecutor.available()) {
-            const tileProvider = getTileProviderInfo(tileProviderId).tileProvider
-            requestExecutor.execute({tileProvider, tileProviderId, requestId, request, response$, cancel$})
-        } else {
-            requestQueue.enqueue({tileProviderId, requestId, request, response$, cancel$})
+const createTileManagerGroup = concurrency => {
+    const tileProvidersInfo = {}
+    const request$ = new Subject()
+    const hidden$ = new Subject()
+    
+    const requestQueue = getRequestQueue()
+    const requestExecutor = getRequestExecutor(concurrency)
+    
+    const getTileProviderInfo = id => {
+        const tileProviderInfo = tileProvidersInfo[id]
+        if (tileProviderInfo) {
+            return tileProviderInfo
         }
+        throw new Error(`Unknown ${tileProviderTag(id)}`)
     }
-)
-
-requestQueue.enqueued$.subscribe(
-    requestId => {
-        console.log(`Request enqueued: ${requestId}`)
-        requestQueue.prioritize()
-    }
-)
-
-requestExecutor.started$.subscribe(
-    tileProviderId => {
-        const tileProvider = getTileProviderInfo(tileProviderId)
-        tileProvider.activeRequests += 1
-        console.log(`** Started: ${tileProviderTag(tileProviderId)}, active ${tileProvider.activeRequests}`)
-    }
-)
-
-requestExecutor.finished$.subscribe(
-    tileProviderId => {
-        const tileProvider = getTileProviderInfo(tileProviderId)
-        tileProvider.activeRequests -= 1
-        console.log(`** Finished: ${tileProviderTag(tileProviderId)}, active ${tileProvider.activeRequests}`)
-        if (requestQueue.pending()) {
-            const {tileProviderId, requestId, request, response$, cancel$} = requestQueue.dequeue()
-            const tileProvider = getTileProviderInfo(tileProviderId).tileProvider
-            requestExecutor.execute({tileProvider, tileProviderId, requestId, request, response$, cancel$})
+    
+    const addTileProvider = (tileProviderId, tileProvider) => {
+        tileProvidersInfo[tileProviderId] = {
+            tileProvider,
+            hidden: false,
+            activeRequests: 0,
         }
+        console.log(`Added ${tileProviderTag(tileProviderId)}`)
     }
-)
+    
+    const removeTileProvider = tileProviderId => {
+        delete tileProvidersInfo[tileProviderId]
+        console.log(`Removed ${tileProviderTag(tileProviderId)}`)
+    }
+    
+    request$.subscribe(
+        ({tileProviderId, request, response$, cancel$}) => {
+            const requestId = uuid()
+            if (requestExecutor.available()) {
+                const tileProvider = getTileProviderInfo(tileProviderId).tileProvider
+                requestExecutor.execute({tileProvider, tileProviderId, requestId, request, response$, cancel$})
+            } else {
+                requestQueue.enqueue({tileProviderId, requestId, request, response$, cancel$})
+            }
+        }
+    )
+    
+    requestQueue.enqueued$.subscribe(
+        requestId => {
+            console.log(`Request enqueued: ${requestId}`)
+            requestQueue.prioritize()
+        }
+    )
+    
+    requestExecutor.started$.subscribe(
+        tileProviderId => {
+            const tileProvider = getTileProviderInfo(tileProviderId)
+            tileProvider.activeRequests += 1
+            console.log(`** Started: ${tileProviderTag(tileProviderId)}, active ${tileProvider.activeRequests}`)
+        }
+    )
+    
+    requestExecutor.finished$.subscribe(
+        tileProviderId => {
+            const tileProvider = getTileProviderInfo(tileProviderId)
+            tileProvider.activeRequests -= 1
+            console.log(`** Finished: ${tileProviderTag(tileProviderId)}, active ${tileProvider.activeRequests}`)
+            if (requestQueue.pending()) {
+                const {tileProviderId, requestId, request, response$, cancel$} = requestQueue.dequeue()
+                const tileProvider = getTileProviderInfo(tileProviderId).tileProvider
+                requestExecutor.execute({tileProvider, tileProviderId, requestId, request, response$, cancel$})
+            }
+        }
+    )
+    
+    // hidden$.subscribe(
+    //     ({tileProviderId, hidden}) => {
+    //         console.log(`Changing visibility of ${tileProviderId} to ${hidden ? 'hidden' : 'visible'}`)
+    //         getTileProviderInfo(tileProviderId).hidden = hidden
+    //     }
+    // )
 
-// hidden$.subscribe(
-//     ({tileProviderId, hidden}) => {
-//         console.log(`Changing visibility of ${tileProviderId} to ${hidden ? 'hidden' : 'visible'}`)
-//         getTileProviderInfo(tileProviderId).hidden = hidden
-//     }
-// )
+    return {getTileProviderInfo, addTileProvider, removeTileProvider, request$, hidden$}
+}
+
+const getTileManagerGroup = tileProvider => {
+    const type = tileProvider.getType()
+    const concurrency = tileProvider.getConcurrency()
+    if (!tileProviderGroups[type]) {
+        tileProviderGroups[type] = createTileManagerGroup(concurrency)
+    }
+    return tileProviderGroups[type]
+}
 
 export const getTileManager = tileProvider => {
     const tileProviderId = uuid()
-    // TODO: handle type, maybe providing a completely independent instance of tileManager
-    // const typeProviderType = tileProvider.getType()
+    const {getTileProviderInfo, addTileProvider, removeTileProvider, request$, hidden$} = getTileManagerGroup(tileProvider)
+
     addTileProvider(tileProviderId, tileProvider)
 
     const getType = () => {
         return getTileProviderInfo(tileProviderId).tileProvider.getType()
+    }
+
+    const getConcurrency = () => {
+        return getTileProviderInfo(tileProviderId).tileProvider.getConcurrency()
     }
 
     const loadTile$ = request => {
@@ -118,6 +135,7 @@ export const getTileManager = tileProvider => {
     
     return {
         getType,
+        getConcurrency,
         loadTile$,
         releaseTile,
         hide,
