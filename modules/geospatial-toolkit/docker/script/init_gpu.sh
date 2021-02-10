6c09a7686adeaa2054f3e42b46c3378a61b9fae1
Daniel Wiell <daniel.wiell@fao.org>
2021-02-10 14:20:01 +0100

    Specified exact cuda version to install.

--- modules/geospatial-toolkit/docker/script/init_gpu.sh
+++ modules/geospatial-toolkit/docker/script/init_gpu.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+echo
+echo "***************************"
+echo "*** Installing GPU libs ***"
+echo "***************************"
+
 apt-get update -y
 apt-get install -y nvidia-cuda-toolkit
 
@@ -8,12 +13,7 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/
 mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-apt-get update
-apt-get -y install cuda
+apt-get update -y
+apt-get -y install cuda-11-2
 
-cd /usr/local/src/
-git clone https://github.com/pyopencl/pyopencl
-cd pyopencl
-python3 configure.py
-echo 'CL_PRETEND_VERSION = "2.2"' >> siteconf.py
-pip3 install .
+pip3 install pyopencl
