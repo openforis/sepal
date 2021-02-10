c5d2397673ba1b2eeba5db7cec0b0a87c3ed8b59
Daniel Wiell <daniel.wiell@fao.org>
2021-02-10 14:49:03 +0100

    Excluded nvidia-cuda-toolkit.

--- modules/geospatial-toolkit/docker/script/init_gpu.sh
+++ modules/geospatial-toolkit/docker/script/init_gpu.sh
@@ -6,9 +6,6 @@ echo "***************************"
 echo "*** Installing GPU libs ***"
 echo "***************************"
 
-apt-get update -y
-apt-get install -y nvidia-cuda-toolkit
-
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
