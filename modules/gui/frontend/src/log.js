0165463dd7895bb56813bb88ddaafae6f7ceb0ab
Luca Paolini <luca.paolini@fao.org>
2021-02-22 17:41:54 +0100

    Cleanup.

--- modules/gui/frontend/src/log.js
+++ modules/gui/frontend/src/log.js
@@ -19,18 +19,18 @@ const getLevel = (levelName = '') =>
 const isEnabled = (level, minLevel) =>
     level <= minLevel
 
-const foo = (func, levelName, loggerName, args) =>
+const log = (func, levelName, loggerName, args) =>
     func(`[${new Date().toISOString()}]`, `[${levelName}]`, `${loggerName} -`, ...args)
 
 export const getLogger = name => {
     const levelName = config[name]
     const level = getLevel(levelName)
     return {
-        trace: (...args) => isEnabled(level, levels.TRACE) && foo(console.log, 'TRACE', name, args),
-        debug: (...args) => isEnabled(level, levels.DEBUG) && foo(console.log, 'DEBUG', name, args),
-        info: (...args) => isEnabled(level, levels.INFO) && foo(console.log, 'INFO', name, args),
-        warn: (...args) => isEnabled(level, levels.WARN) && foo(console.warn, 'WARN', name, args),
-        error: (...args) => isEnabled(level, levels.ERROR) && foo(console.error, 'ERROR', name, args),
-        fatal: (...args) => isEnabled(level, levels.FATAL) && foo(console.error, 'UNKNOWN', name, args)
+        trace: (...args) => isEnabled(level, levels.TRACE) && log(console.log, 'TRACE', name, args),
+        debug: (...args) => isEnabled(level, levels.DEBUG) && log(console.log, 'DEBUG', name, args),
+        info: (...args) => isEnabled(level, levels.INFO) && log(console.log, 'INFO', name, args),
+        warn: (...args) => isEnabled(level, levels.WARN) && log(console.warn, 'WARN', name, args),
+        error: (...args) => isEnabled(level, levels.ERROR) && log(console.error, 'ERROR', name, args),
+        fatal: (...args) => isEnabled(level, levels.FATAL) && log(console.error, 'UNKNOWN', name, args)
     }
 }
