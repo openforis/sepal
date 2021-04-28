ab63a5f7bd055708ff12e3e9975cd9c41d73cab6
Daniel Wiell <daniel.wiell@fao.org>
2021-04-28 13:29:55 +0200

    Added back previewing based on band combination, for backward compatibility.

--- modules/gee/docker/src/jobs/ee/image/preview.js
+++ modules/gee/docker/src/jobs/ee/image/preview.js
@@ -4,87 +4,90 @@ const worker$ = ({recipe, bands, visParams}) => {
     const ImageFactory = require('sepal/ee/imageFactory')
     const ee = require('ee')
     const {switchMap} = require('rx/operators')
-    const log = require('sepal/log').getLogger('ee')
-    log.fatal({visParams})
+    if (visParams) {
+        const {getImage$} = ImageFactory(recipe, visParams.bands)
+        const getMap$ = (image, {type, bands, min, max, inverted, gamma, palette}) => {
+            const range = () => ({
+                min: bands.map((_, i) => inverted[i] ? max[i] : min[i]),
+                max: bands.map((_, i) => inverted[i] ? min[i] : max[i]),
+            })
 
-    const {getImage$} = ImageFactory(recipe, bands)
+            const toHsv = image => {
+                const stretchImage = (image, min, max) => {
+                    return image.expression('toMin + (i - fromMin) * (toMax - toMin) / (fromMax - fromMin)', {
+                        'fromMin': min,
+                        'fromMax': max,
+                        'toMin': 0,
+                        'toMax': 1,
+                        'i': image
+                    })
+                }
+                return ee.Image(
+                    bands.map((band, i) => {
+                        const {min, max} = range()
+                        return stretchImage(image.select(band), min[i], max[i])
+                            .rename(band)
+                    })
+                ).hsvToRgb().float()
+            }
 
-    const getMap$ = (image, {type, bands, min, max, inverted, gamma, palette}) => {
-        const range = () => ({
-            min: bands.map((_, i) => inverted[i] ? max[i] : min[i]),
-            max: bands.map((_, i) => inverted[i] ? min[i] : max[i]),
-        })
+            // TODO: Handle categorical images
+            //   Should not support inverted
+            //   Use sldStyle instead of palette
+            switch (type) {
+            case 'continuous':
+                return ee.getMap$(image.select(bands), {bands, ...range(), gamma, palette})
+            case 'rgb':
+                return ee.getMap$(image.select(bands), {bands, ...range(), gamma, palette})
+            case 'hsv':
+                return ee.getMap$(toHsv(image.select(bands)))
+            default:
+                throw Error(`Unsupported type: ${type}`)
+            }
+        }
 
-        const toHsv = image => {
-            const stretchImage = (image, min, max) => {
+        return getImage$().pipe(
+            switchMap(image =>
+                getMap$(image, visParams)
+            )
+        )
+    } else {
+        // For backwards compatibility
+        const hsvToRgb = (image, visParams) => {
+            const stretchImage = (image, fromRange, toRange) => {
                 return image.expression('toMin + (i - fromMin) * (toMax - toMin) / (fromMax - fromMin)', {
-                    'fromMin': min,
-                    'fromMax': max,
-                    'toMin': 0,
-                    'toMax': 1,
+                    'fromMin': fromRange[0],
+                    'fromMax': fromRange[1],
+                    'toMin': toRange[0],
+                    'toMax': toRange[1],
                     'i': image
                 })
             }
-            return ee.Image(
-                bands.map((band, i) => {
-                    const {min, max} = range()
-                    return stretchImage(image.select(band), min[i], max[i])
-                        .rename(band)
-                })
-            ).hsvToRgb().float()
+            const stretch = visParams.stretch || []
+            const bands = visParams.bands
+            stretch.forEach((toRange, i) => {
+                const fromRange = [visParams['min'][i], visParams['max'][i]]
+                if (!toRange)
+                    toRange = [0, 1]
+                const band = bands[i]
+                const stretched = stretchImage(image.select(band), fromRange, toRange)
+                    .rename(band)
+                image = image.addBands(stretched, null, true)
+            })
+            return image.select(bands).hsvToRgb().float()
         }
 
-        // TODO: Handle categorical images
-        //   Should not support inverted
-        //   Use sldStyle instead of palette
-        switch(type) {
-        case 'continuous': return ee.getMap$(image.select(bands), {bands, ...range(), gamma, palette})
-        case 'rgb': return ee.getMap$(image.select(bands), {bands, ...range(), gamma, palette})
-        case 'hsv': return ee.getMap$(toHsv(image.select(bands)))
-        default: throw Error(`Unsupported type: ${type}`)
-        }
-    }
-
-    return getImage$().pipe(
-        switchMap(image =>
-            getMap$(image, visParams)
+        const {getImage$, getVisParams$} = ImageFactory(recipe, bands)
+        return getImage$().pipe(
+            switchMap(image => getVisParams$(image).pipe(
+                switchMap(visParams =>
+                    visParams.hsv
+                        ? ee.getMap$(hsvToRgb(image, visParams))
+                        : ee.getMap$(image, visParams))
+            ))
         )
-    )
-
-    // const {getImage$, getVisParams$} = ImageFactory(recipe, bands, visParams)
-    // return getImage$().pipe(
-    //     switchMap(image => getVisParams$(image).pipe(
-    //         switchMap(visParams =>
-    //             visParams.hsv
-    //                 ? ee.getMap$(hsvToRgb(image, visParams))
-    //                 : ee.getMap$(image, visParams))
-    //     ))
-    // )
+    }
 }
-
-// const hsvToRgb = (image, visParams) => {
-//     const stretchImage = (image, fromRange, toRange) => {
-//         return image.expression('toMin + (i - fromMin) * (toMax - toMin) / (fromMax - fromMin)', {
-//             'fromMin': fromRange[0],
-//             'fromMax': fromRange[1],
-//             'toMin': toRange[0],
-//             'toMax': toRange[1],
-//             'i': image
-//         })
-//     }
-//     const stretch = visParams.stretch || []
-//     const bands = visParams.bands
-//     stretch.forEach((toRange, i) => {
-//         const fromRange = [visParams['min'][i], visParams['max'][i]]
-//         if (!toRange)
-//             toRange = [0, 1]
-//         const band = bands[i]
-//         const stretched = stretchImage(image.select(band), fromRange, toRange)
-//             .rename(band)
-//         image = image.addBands(stretched, null, true)
-//     })
-//     return image.select(bands).hsvToRgb().float()
-// }
 
 module.exports = job({
     jobName: 'Preview',
