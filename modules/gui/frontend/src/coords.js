8184dd5e8ea148c6660526db0081613fc6447411
Luca Paolini <luca.paolini@fao.org>
2021-05-19 14:07:10 +0200

    Fixed coordinates parsing for simultaneous sign and suffix.

--- modules/gui/frontend/src/coords.js
+++ modules/gui/frontend/src/coords.js
@@ -1,14 +1,14 @@
 import _ from 'lodash'
 
-const unsignedDecimal = '(?:\\d*\\.)?\\d+'
-const decimal = `[+-]?${unsignedDecimal}`
-const coordinate = `(${decimal})\\s*([NSEW])?`
+const decimal = '(?:\\d*\\.)?\\d+'
+const sign = '[+-]?'
+const coordinate = `(${sign})?(${decimal})\\s*([NSEW])?`
 const spacer = '\\s*[\\s,]\\s*'
 const coordinates = `${coordinate}${spacer}${coordinate}`
 const regexp = new RegExp(`^${coordinates}(.*)$`)
 
-const isUnsignedLatitudeCompatible = ({value, dir}) =>
-    value >= 0 && value <= 90 && (dir === 'N' || dir === 'S')
+const isUnsignedLatitudeCompatible = ({sign, value, dir}) =>
+    !sign && value >= 0 && value <= 90 && (dir === 'N' || dir === 'S')
 
 const isSignedLatitudeCompatible = ({value, dir}) =>
     value >= -90 && value <= 90 && !dir
@@ -16,8 +16,8 @@ const isSignedLatitudeCompatible = ({value, dir}) =>
 const isLatitudeCompatible = coord =>
     isUnsignedLatitudeCompatible(coord) || isSignedLatitudeCompatible(coord)
 
-const isUnsignedLongitudeCompatible = ({value, dir}) =>
-    value >= 0 && value <= 180 && (dir === 'E' || dir === 'W')
+const isUnsignedLongitudeCompatible = ({sign, value, dir}) =>
+    !sign && value >= 0 && value <= 180 && (dir === 'E' || dir === 'W')
 
 const isSignedLongitudeCompatible = ({value, dir}) =>
     value >= -180 && value <= 180 && !dir
@@ -25,23 +25,26 @@ const isSignedLongitudeCompatible = ({value, dir}) =>
 const isLongitudeCompatible = coord =>
     isUnsignedLongitudeCompatible(coord) || isSignedLongitudeCompatible(coord)
 
-const toLatitude = ({value, dir}) =>
-    parseFloat(value) * (dir === 'S' ? -1 : 1)
+const toLatitude = ({sign, value, dir}) =>
+    (sign == '-' ? -1 : 1) * parseFloat(value) * (dir === 'S' ? -1 : 1)
 
-const toLongitude = ({value, dir}) =>
-    parseFloat(value) * (dir === 'W' ? -1 : 1)
+const toLongitude = ({sign, value, dir}) =>
+    (sign == '-' ? -1 : 1) * parseFloat(value) * (dir === 'W' ? -1 : 1)
 
 export const parseCoordinates = string => {
     const parts = string.trim().toUpperCase().match(regexp)
+    // console.log('*************', parts)
 
     if (parts) {
         const coord1 = {
-            value: parts[1],
-            dir: parts[2]
+            sign: parts[1],
+            value: parts[2],
+            dir: parts[3]
         }
         const coord2 = {
-            value: parts[3],
-            dir: parts[4]
+            sign: parts[4],
+            value: parts[5],
+            dir: parts[6]
         }
 
         const results = []
