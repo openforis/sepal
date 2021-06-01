354771e01705eba3a51de1ef7d7d2ccff070d5e1
Daniel Wiell <daniel.wiell@fao.org>
2021-06-01 12:38:30 +0200

    Migrated CCDC.

--- modules/gui/frontend/src/app/home/body/process/recipe/ccdc/ccdc.js
+++ modules/gui/frontend/src/app/home/body/process/recipe/ccdc/ccdc.js
@@ -1,24 +1,15 @@
 import {Aoi} from '../aoi'
-import {Map} from '../../../../map/map'
-import {MapScale} from 'app/home/map/mapScale'
+import {Map} from 'app/home/map/map'
 import {RecipeActions, defaultModel} from './ccdcRecipe'
-import {SceneAreas} from '../opticalMosaic/sceneAreas'
 import {compose} from 'compose'
 import {initializeLayers} from '../recipeImageLayerSource'
 import {msg} from 'translate'
 import {recipe} from 'app/home/body/process/recipeContext'
 import {recipeAccess} from '../../recipeAccess'
 import {selectFrom} from 'stateUtils'
-import {setAoiLayer} from 'app/home/map/aoiLayer'
-import AutoSelectScenes from '../opticalMosaic/autoSelectScenes'
 import CCDCToolbar from './panels/ccdcToolbar'
-import MapToolbar from 'app/home/map/mapToolbar'
-import MosaicToolbar from '../opticalMosaic/panels/opticalMosaicToolbar'
 import Notifications from 'widget/notifications'
 import React from 'react'
-import SceneDeselection from '../opticalMosaic/sceneDeselection'
-import SceneSelection from '../opticalMosaic/sceneSelection'
-import styles from './ccdc.module.css'
 
 const mapRecipeToProps = recipe => ({
     initialized: selectFrom(recipe, 'ui.initialized'),
@@ -33,7 +24,7 @@ class _CCDC extends React.Component {
         super(props)
         const {layers, recipeId} = props
         this.recipeActions = RecipeActions(recipeId)
-        initializeLayers(recipeId, layers)
+        initializeLayers({recipeId, layers, skipThis: true})
     }
 
     render() {
@@ -47,31 +38,30 @@ class _CCDC extends React.Component {
     }
 
     componentDidMount() {
-        const {componentWillUnmount$} = this.props
-        // this.initClassification()
+        this.initClassification()
     }
 
-    // componentDidUpdate() {
-    //     this.initClassification()
-    // }
+    componentDidUpdate() {
+        this.initClassification()
+    }
 
-    // initClassification() {
-    //     const {stream, classificationLegend, classificationRecipeId, loadRecipe$} = this.props
-    //     if (classificationRecipeId && !classificationLegend && !stream('LOAD_CLASSIFICATION_RECIPE').active) {
-    //         stream('LOAD_CLASSIFICATION_RECIPE',
-    //             loadRecipe$(classificationRecipeId),
-    //             classification => {
-    //                 this.recipeActions.setClassification({
-    //                     classificationLegend: classification.model.legend,
-    //                     classifierType: classification.model.classifier.type
-    //                 })
-    //             },
-    //             error => Notifications.error({message: msg('process.ccdc.panel.sources.classificationLoadError', {error}), error})
-    //         )
-    //     } else if (!classificationRecipeId && classificationLegend && !stream('LOAD_CLASSIFICATION_RECIPE').active) {
-    //         this.recipeActions.setClassification({classificationLegend: null, classifierType: null})
-    //     }
-    // }
+    initClassification() {
+        const {stream, classificationLegend, classificationRecipeId, loadRecipe$} = this.props
+        if (classificationRecipeId && !classificationLegend && !stream('LOAD_CLASSIFICATION_RECIPE').active) {
+            stream('LOAD_CLASSIFICATION_RECIPE',
+                loadRecipe$(classificationRecipeId),
+                classification => {
+                    this.recipeActions.setClassification({
+                        classificationLegend: classification.model.legend,
+                        classifierType: classification.model.classifier.type
+                    })
+                },
+                error => Notifications.error({message: msg('process.ccdc.panel.sources.classificationLoadError', {error}), error})
+            )
+        } else if (!classificationRecipeId && classificationLegend && !stream('LOAD_CLASSIFICATION_RECIPE').active) {
+            this.recipeActions.setClassification({classificationLegend: null, classifierType: null})
+        }
+    }
 }
 
 const CCDC = compose(
