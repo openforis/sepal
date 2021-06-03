f8e6e46d4421e08ade70255cbb3a57c2bedc1733
Luca Paolini <luca.paolini@fao.org>
2021-06-03 17:09:32 +0200

    Added 2px padding around the map.

--- modules/gui/frontend/src/app/home/map/sepalMap.js
+++ modules/gui/frontend/src/app/home/map/sepalMap.js
@@ -247,7 +247,7 @@ export class SepalMap {
     fitBounds(bounds) {
         const {google} = this
         // const PADDING = 50 // compensate for attribution masking
-        const PADDING = 0 // compensate for attribution masking
+        const PADDING = 2 // compensate for attribution masking
         const {googleMap} = this
         const nextBounds = bounds instanceof google.maps.LatLngBounds
             ? bounds
