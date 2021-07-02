76b1a4a291db51518d68161522afb05c1a6c022b
Daniel Wiell <daniel.wiell@fao.org>
2021-07-02 12:34:24 +0200

    Updated session cookie settings.

--- modules/gateway/docker/src/main.js
+++ modules/gateway/docker/src/main.js
@@ -1,22 +1,28 @@
-// require('sepal/log').configureServer(require('./log.json'))
-// const {port, secure} = require('./config')
-// const express = require('express')
-// const session = require('express-session')
-// const {logout} = require('./logout')
-// const {proxyEndpoints} = require('./proxy')
-//
-// const app = express()
-//
-// app.use(session({
-//     secret: Math.random().toString(),
-//     name: 'SEPAL-SESSIONID',
-//     resave: false,
-//     saveUninitialized: false,
-//     httpOnly: false,
-//     cookie: {secure: false}
-//     // cookie: {secure}
-// }))
-// app.use('/api/user/logout', logout)
-// proxyEndpoints(app)
-// app.listen(port)
-//
+require('sepal/log').configureServer(require('./log.json'))
+const {port, secure} = require('./config')
+const express = require('express')
+const session = require('express-session')
+const {logout} = require('./logout')
+const {proxyEndpoints} = require('./proxy')
+
+const app = express()
+
+app.use(session({
+    secret: Math.random().toString(),
+    name: 'SEPAL-SESSIONID',
+    httpOnly: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 30,
+        sameSite: true,
+        secure: false
+    },
+    proxy: true,
+    resave: false,
+    rolling: true,
+    saveUninitialized: false,
+    unset: 'destroy'
+}))
+app.use('/api/user/logout', logout)
+proxyEndpoints(app)
+app.listen(port)
