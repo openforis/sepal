4ba591efafb026e7fd5a0d4638b9fd4ec87ebd27
Daniel Wiell <daniel.wiell@fao.org>
2021-07-05 16:19:39 +0200

    Made sure sepal-user header get set for websockets.

--- modules/gateway/docker/src/main.js
+++ modules/gateway/docker/src/main.js
@@ -9,7 +9,7 @@ const {parse} = require('url')
 const log = require('sepal/log').getLogger('gateway')
 
 const app = express()
-app.use(session({
+const sessionParser = session({
     secret: Math.random().toString(),
     name: 'SEPAL-SESSIONID',
     cookie: {
@@ -23,17 +23,29 @@ app.use(session({
     rolling: true,
     saveUninitialized: false,
     unset: 'destroy'
-}))
+})
+app.use(sessionParser)
 app.use('/api/user/logout', logout)
 const proxies = proxyEndpoints(app)
 const server = app.listen(port)
-server.on('upgrade', (res, socket, head) => {
-    const requestPath = parse(res.url).pathname
-    const {proxy, target} = proxies.find(({path}) => !path || requestPath === path || isMatch(requestPath, `${path}/**`)) || {}
-    if (proxy) {
-        log.debug(`Requesting WebSocket upgrade for "${requestPath}" to target "${target}"`)
-        proxy.upgrade(res, socket, head)
-    } else {
-        log.warn(`No proxy found for WebSocket upgrade "${requestPath}"`)
-    }
+
+server.on('upgrade', (req, socket, head) => {
+    sessionParser(req, {}, () => { // Make sure we have access to session for the websocket
+        const requestPath = parse(req.url).pathname
+        const user = req.session.user
+        const username = user ? user.username : 'not-authenticated'
+        if (user) {
+            log.trace(`[${username}] [${requestPath}] Setting sepal-user header`)
+            req.headers['sepal-user'] = JSON.stringify(user)
+        } else {
+            log.warn(`[${username}] Websocket upgrade without a user`)
+        }
+        const {proxy, target} = proxies.find(({path}) => !path || requestPath === path || isMatch(requestPath, `${path}/**`)) || {}
+        if (proxy) {
+            log.debug(`[${username}] Requesting WebSocket upgrade for "${requestPath}" to target "${target}"`)
+            proxy.upgrade(req, socket, head)
+        } else {
+            log.warn(`[${username}] No proxy found for WebSocket upgrade "${requestPath}"`)
+        }
+    })
 })
