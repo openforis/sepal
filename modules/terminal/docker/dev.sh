8626641452517de419fe440a010213292bd31f87
Luca Paolini <luca.paolini@fao.org>
2021-07-06 13:23:04 +0200

    Implemented terminal server error handling.

--- modules/terminal/docker/dev.sh
+++ modules/terminal/docker/dev.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export SSH_GATEWAY_HOST=127.0.0.1
 LIBS=../../../lib/js
 NODE_TLS_REJECT_UNAUTHORIZED=0 nodemon \
     --watch src \
@@ -9,11 +10,4 @@ NODE_TLS_REJECT_UNAUTHORIZED=0 nodemon \
     --ip '127.0.0.1' \
     --port 8000 \
     --home-dir '/var/sepal/sepal-server/home' \
-    --ssh-script-path 'ssh_gateway.sh'
-
-# module.exports = {
-#     PORT: process.env.PORT || 3000,
-#     HOST: process.env.IP || '127.0.0.1',
-#     USERS_HOME: process.env.USERS_HOME || '/sepalUsers',
-#     SSH_SCRIPT_PATH: process.env.SSH_SCRIPT_PATH ||'ssh_gateway.sh'
-# }
+    --ssh-script-path "$PWD/script/ssh_gateway.sh"
--- modules/terminal/docker/src/terminal.js
+++ modules/terminal/docker/src/terminal.js
@@ -1,77 +1,83 @@
 const log = require('sepal/log').getLogger('terminal')
 const {exec} = require('child_process')
 const {interval, merge, Subject} = require('rxjs')
-const {map, filter, bufferTime} = require('rxjs/operators')
+const {map, mapTo, filter, bufferTime} = require('rxjs/operators')
 
 const Session = require('./session')
 
 const start = (websocket, req) => {
-    const session = Session.get(req)
-    const terminal = session.terminal
-    const subscriptions = []
-
-    log.info(`Started session: ${session.id}, terminal PID: ${terminal.pid}`)
-
-    websocket.on('close', () => {
-        terminal.kill()
-        exec(`rm -f ${session.key}`)
-        subscriptions.forEach(subscription =>
-            subscription.unsubscribe()
-        )
-        log.info(`Closed session: ${session.id}, terminal PID: ${terminal.pid}`)
-        Session.remove(session.id)
-    })
-
-    // upstream (websocket to terminal)
-
-    websocket.on('message', msg =>
-        terminal.write(msg)
-    )
-
-    // downstream (terminal to websocket)
+    try {
+        const session = Session.get(req)
+        const terminal = session.terminal
+        const subscriptions = []
     
-    const websocket$ = (() => {
-        const data$ = new Subject()
-
-        const heartbeat$ = interval(3000).pipe(
-            map(() => '')
-        )
+        log.info(`Started session: ${session.id}, terminal PID: ${terminal.pid}`)
     
-        const bufferedData$ = data$.pipe(
-            bufferTime(10),
-            map(buffered => buffered.join('')),
-            filter(data => data)
-        )
-    
-        subscriptions.push(
-            merge(heartbeat$, bufferedData$).subscribe(
-                data => {
-                    try {
-                        websocket.send(data)
-                    } catch (e) {
-                        log.error('Could not send data to websocket', e)
-                    }
-                }
+        websocket.on('close', () => {
+            terminal.kill()
+            exec(`rm -f ${session.tempKeyFile}`)
+            subscriptions.forEach(subscription =>
+                subscription.unsubscribe()
             )
+            log.info(`Closed session: ${session.id}, terminal PID: ${terminal.pid}`)
+            Session.remove(session.id)
+        })
+    
+        // upstream (websocket to terminal)
+    
+        websocket.on('message', msg =>
+            terminal.write(msg)
         )
     
-        return data$
-    })()
-
-    websocket$.next(session.logs)
-
-    terminal.on('data', data =>
-        websocket$.next(data)
-    )
+        // downstream (terminal to websocket)
+        
+        const websocket$ = (() => {
+            const data$ = new Subject()
+    
+            const heartbeat$ = interval(3000).pipe(
+                mapTo('')
+            )
+            
+            const bufferedData$ = data$.pipe(
+                bufferTime(10),
+                map(buffered => buffered.join('')),
+                filter(data => data)
+            )
+        
+            subscriptions.push(
+                merge(heartbeat$, bufferedData$).subscribe(
+                    data => {
+                        try {
+                            websocket.send(data)
+                        } catch (e) {
+                            log.error('Cannot write to websocket', e)
+                        }
+                    }
+                )
+            )
+        
+            return data$
+        })()
+    
+        terminal.on('data', data =>
+            websocket$.next(data)
+        )
+    } catch(error) {
+        log.error('Cannot start session', error)
+    }
 }
 
 const resize = (req, res) => {
-    const session = Session.get(req)
-    const cols = parseInt(req.query.cols)
-    const rows = parseInt(req.query.rows)
-    session.terminal.resize(cols, rows)
-    log.info(`Resized session: ${session.id}, cols: ${cols}, rows: ${rows}`)
-    res.end()
+    try {
+        const session = Session.get(req)
+        const cols = parseInt(req.query.cols)
+        const rows = parseInt(req.query.rows)
+        session.terminal.resize(cols, rows)
+        log.info(`Resized session: ${session.id}, cols: ${cols}, rows: ${rows}`)
+        res.end()
+    } catch (error) {
+        log.error('Cannot resize session', error)
+    }
 }
 
 module.exports = {start, resize}
