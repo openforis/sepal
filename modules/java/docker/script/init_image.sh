e5ec9cf7addeb3a8969220c6cb742d8e0ef111ba
Daniel Wiell <daniel.wiell@fao.org>
2021-07-09 08:56:39 +0200

    Re-enabled TLSv1 and TLSv1.1 to get javax.mail to work.

--- modules/java/docker/script/init_image.sh
+++ modules/java/docker/script/init_image.sh
@@ -17,3 +17,6 @@ source "$SDKMAN_DIR/bin/sdkman-init.sh"
 yes | sdk install java 11.0.11.hs-adpt
 
 ln -s `which java` /usr/local/bin/java
+JAVA_HOME=$(sdk home java current)
+# Re-enable TLSv1 and TLSv1.1 for java mail
+sed -i 's/jdk.tls.disabledAlgorithms=.*/jdk.tls.disabledAlgorithms=SSLv3, RC4, DES, MD5withRSA, /' $JAVA_HOME/conf/security/java.security
