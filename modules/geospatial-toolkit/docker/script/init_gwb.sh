e94ac2f4ca13e6937ecec86752ec11d674ace1ca
Daniel Wiell <daniel.wiell@fao.org>
2021-07-13 15:28:42 +0200

    Updated GWB version.

--- modules/geospatial-toolkit/docker/script/init_gwb.sh
+++ modules/geospatial-toolkit/docker/script/init_gwb.sh
@@ -5,6 +5,6 @@ echo
 echo "**********************"
 echo "*** Installing GWB ***"
 echo "**********************"
-INSTALLER=gwb_1.8.2-1_amd64.deb
+INSTALLER=gwb_1.8.3-1_amd64.deb
 wget https://ies-ows.jrc.ec.europa.eu/gtb/GWB/$INSTALLER
 apt-get install -y ./$INSTALLER
