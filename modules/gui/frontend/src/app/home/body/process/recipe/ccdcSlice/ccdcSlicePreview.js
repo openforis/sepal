50aa7b198c740e10291cb16868f41ce86e74b1ba
Luca Paolini <luca.paolini@fao.org>
2021-07-14 14:29:40 +0200

    Fixed EarthEngineLayer.

--- modules/gui/frontend/src/app/home/body/process/recipe/ccdcSlice/ccdcSlicePreview.js
+++ modules/gui/frontend/src/app/home/body/process/recipe/ccdcSlice/ccdcSlicePreview.js
@@ -5,12 +5,11 @@ import {compose} from 'compose'
 import {msg} from 'translate'
 import {selectFrom} from 'stateUtils'
 import {withRecipe} from 'app/home/body/process/recipeContext'
-import EarthEngineLayer from 'app/home/map/layer/earthEngineLayer'
+import EarthEnginePreviewLayer from 'app/home/map/layer/earthEnginePreviewLayer'
 import MapStatus from 'widget/mapStatus'
 import Notifications from 'widget/notifications'
 import React from 'react'
 import _ from 'lodash'
-import api from 'api'
 import withSubscriptions from 'subscription'
 
 const LABEL = 'ccdcSlice'
@@ -125,7 +124,7 @@ class CCDCSlicePreview extends React.Component {
         }
         const {map, componentWillUnmount$} = this.props
         const {initializing, error} = this.state
-        const layer = new EarthEngineLayer({
+        const layer = new EarthEnginePreviewLayer({
             map,
             layerIndex: 2,
             // toggleable: true,
