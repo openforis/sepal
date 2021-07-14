1db264205acbf5d4636e9a66d845a634b4561232
Luca Paolini <luca.paolini@fao.org>
2021-07-15 00:32:52 +0200

    Implemented withLeaveAlert HOC.

---
+++ modules/gui/frontend/src/widget/leaveAlert.js
@@ -0,0 +1,43 @@
+import {compose} from 'compose'
+import {connect} from 'store'
+import React from 'react'
+import _ from 'lodash'
+
+export const withLeaveAlert = mapStateToLeaveAlert =>
+    WrappedComponent => {
+        class HigherOrderComponent extends React.Component {
+            constructor() {
+                super()
+                this.onClose = this.onClose.bind(this)
+            }
+        
+            render() {
+                return React.createElement(WrappedComponent, this.props)
+            }
+
+            componentDidMount() {
+                window.addEventListener('beforeunload', this.onClose)
+            }
+        
+            componentWillUnmount() {
+                window.removeEventListener('beforeunload', this.onClose)
+            }
+        
+            onClose(e) {
+                const {leaveAlert} = this.props
+                if (leaveAlert) {
+                    e.returnValue = ''
+                    return ''
+                }
+            }
+        }
+
+        const mapStateToProps = state => ({
+            leaveAlert: mapStateToLeaveAlert(state)
+        })
+
+        return compose(
+            HigherOrderComponent,
+            connect(mapStateToProps)
+        )
+    }
