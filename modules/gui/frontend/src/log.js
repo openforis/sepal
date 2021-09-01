cfa29ef13f08054f4bad66dfaf9391423eb2c6e2
Luca Paolini <luca.paolini@fao.org>
2021-09-01 12:35:23 +0200

    Removed object stringification from log methods.

--- modules/gui/frontend/src/log.js
+++ modules/gui/frontend/src/log.js
@@ -22,15 +22,10 @@ const isEnabled = (level, minLevel) =>
 const log = (func, levelName, loggerName, args = []) =>
     func(`[${new Date().toISOString()}]`, `[${levelName}]`, `${loggerName} -`, ...args)
 
-const getArg = (arg, level) => {
-    if (_.isFunction(arg)) {
-        return getArg(arg(level))
-    }
-    if (_.isObjectLike(arg) && !(arg instanceof Error)) {
-        return JSON.stringify(arg)
-    }
-    return arg
-}
+const getArg = (arg, level) =>
+    _.isFunction(arg)
+        ? getArg(arg(level))
+        : arg
     
 const getArgs = (args, level) =>
     _.map(args, (arg, index) => getArg(arg, level, index === 0))
@@ -39,14 +34,14 @@ export const getLogger = name => {
     const levelName = config[name]
     const level = getLevel(levelName)
     return {
+        isTrace: () => isEnabled(level, levels.TRACE),
+        isDebug: () => isEnabled(level, levels.DEBUG),
+        isInfo: () => isEnabled(level, levels.INFO),
         trace: (...args) => isEnabled(level, levels.TRACE) && log(console.log, 'TRACE', name, getArgs(args, level)),
         debug: (...args) => isEnabled(level, levels.DEBUG) && log(console.log, 'DEBUG', name, getArgs(args, level)),
         info: (...args) => isEnabled(level, levels.INFO) && log(console.log, 'INFO', name, getArgs(args, level)),
         warn: (...args) => isEnabled(level, levels.WARN) && log(console.warn, 'WARN', name, getArgs(args, level)),
         error: (...args) => isEnabled(level, levels.ERROR) && log(console.error, 'ERROR', name, getArgs(args, level)),
-        fatal: (...args) => isEnabled(level, levels.FATAL) && log(console.error, 'UNKNOWN', name, getArgs(args, level)),
-        isTrace: () => isEnabled(level, levels.TRACE),
-        isDebug: () => isEnabled(level, levels.DEBUG),
-        isInfoE: () => isEnabled(level, levels.INFO)
+        fatal: (...args) => isEnabled(level, levels.FATAL) && log(console.error, 'UNKNOWN', name, getArgs(args, level))
     }
 }
