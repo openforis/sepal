7a666dd86864861820327c07ca73bc9ef8373798
Luca Paolini <luca.paolini@fao.org>
2021-09-03 15:04:11 +0200

    Refactoring.

--- modules/gui/frontend/src/widget/tabs/tabContent.js
+++ modules/gui/frontend/src/widget/tabs/tabContent.js
@@ -1,28 +1,16 @@
-import {Enabled, connect} from 'store'
+import {Enabled} from 'store'
 import {PortalContainer, PortalContext} from 'widget/portal'
-import {Subject} from 'rxjs'
 import {TabContext} from './tabContext'
-import {compose} from 'compose'
-import {finalize} from 'rxjs/operators'
-import {v4 as uuid} from 'uuid'
 import PropTypes from 'prop-types'
 import React from 'react'
 import _ from 'lodash'
 import styles from './tabContent.module.css'
-import withSubscriptions from 'subscription'
 
-class _TabContent extends React.PureComponent {
-    constructor() {
-        super()
-        this.getBusy$ = this.getBusy$.bind(this)
-    }
-
+export class TabContent extends React.PureComponent {
     render() {
-        const {id, type, selected, children} = this.props
+        const {id, busy$, type, selected, children} = this.props
         const portalContainerId = `portal_tab_${id}`
-        const tabContext = {
-            getBusy$: this.getBusy$
-        }
+        const tabContext = {id, busy$}
         return (
             <PortalContext id={portalContainerId}>
                 <div className={[
@@ -39,30 +27,7 @@ class _TabContent extends React.PureComponent {
             </PortalContext>
         )
     }
-
-    getBusy$() {
-        const {id, busy$, addSubscription} = this.props
-        const label = uuid()
-        const busyTab$ = new Subject()
-        const setBusy = busy => busy$.next({id, label, busy})
-
-        addSubscription(
-            busyTab$.pipe(
-                finalize(() => setBusy(false))
-            ).subscribe({
-                next: busy => setBusy(busy)
-            })
-        )
-
-        return busyTab$
-    }
 }
-
-export const TabContent = compose(
-    _TabContent,
-    connect(),
-    withSubscriptions()
-)
 
 TabContent.propTypes = {
     busy$: PropTypes.any,
--- modules/gui/frontend/src/widget/tabs/tabContext.js
+++ modules/gui/frontend/src/widget/tabs/tabContext.js
@@ -1,28 +1,46 @@
+import {Subject} from 'rxjs'
+import {compose} from 'compose'
+import {finalize} from 'rxjs/operators'
+import {v4 as uuid} from 'uuid'
 import {withContext} from 'context'
 import React from 'react'
+import withSubscriptions from 'subscription'
 
 export const TabContext = React.createContext()
 
 export const withTabContext = () =>
-    WrappedComponent =>
-        withContext(TabContext, 'tabContext')()(
-            class _HigherOrderComponent extends React.Component {
-                constructor(props) {
-                    super(props)
-                    const {tabContext: {getBusy$}} = props
-                    this.busy$ = getBusy$()
-                }
-
-                render() {
-                    const props = {
-                        ...this.props,
-                        busy$: this.busy$
-                    }
-                    return React.createElement(WrappedComponent, props)
-                }
-
-                componentWillUnmount() {
-                    this.busy$.complete()
-                }
+    WrappedComponent => compose(
+        class _HigherOrderComponent extends React.Component {
+            constructor(props) {
+                super(props)
+                this.busy$ = this.createBusy$()
             }
-        )
+
+            render() {
+                const props = {
+                    ...this.props,
+                    busy$: this.busy$
+                }
+                return React.createElement(WrappedComponent, props)
+            }
+
+            createBusy$() {
+                const {tabContext: {id, busy$}, addSubscription} = this.props
+                const label = uuid()
+                const busyTab$ = new Subject()
+                const setBusy = busy => busy$.next({id, label, busy})
+        
+                addSubscription(
+                    busyTab$.pipe(
+                        finalize(() => setBusy(false))
+                    ).subscribe({
+                        next: busy => setBusy(busy)
+                    })
+                )
+        
+                return busyTab$
+            }
+        },
+        withContext(TabContext, 'tabContext')(),
+        withSubscriptions()
+    )
