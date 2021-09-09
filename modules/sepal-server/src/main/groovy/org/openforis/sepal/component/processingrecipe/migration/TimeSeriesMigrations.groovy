b5544663d63bd6b9a0823a80ab0ab6f60cf5186e
Daniel Wiell <daniel.wiell@fao.org>
2021-09-09 08:09:17 +0200

    Fixed time-series migration.

--- modules/sepal-server/src/main/groovy/org/openforis/sepal/component/processingrecipe/migration/TimeSeriesMigrations.groovy
+++ modules/sepal-server/src/main/groovy/org/openforis/sepal/component/processingrecipe/migration/TimeSeriesMigrations.groovy
@@ -4,7 +4,7 @@ class TimeSeriesMigrations extends AbstractMigrations {
     TimeSeriesMigrations() {
         super('TIME_SERIES')
         addMigration(5, {Map r ->
-            r.model.options.cloudDetection = r.model.options.cloudMasking == 'AGGRESSIVE'
+            r.model.options.cloudDetection = (r.model.options && r.model.options.cloudMasking == 'AGGRESSIVE')
                     ? ['QA', 'CLOUD_MASK']
                     : ['QA']
             return r
