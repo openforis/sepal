ed043933d038a701ef8e77168479c529bb79ad13
Daniel Wiell <daniel.wiell@fao.org>
2021-09-10 07:39:03 +0200

    Updated Python BFAST version.

--- modules/geospatial-toolkit/docker/script/init_python_packages.sh
+++ modules/geospatial-toolkit/docker/script/init_python_packages.sh
@@ -20,4 +20,4 @@ pip3 install --upgrade pip
 pip3 install 'setuptools<58' # https://stackoverflow.com/questions/69100275/error-while-downloading-the-requirements-using-pip-install-setup-command-use-2
 pip3 install --ignore-installed scikit-learn
 pip3 install -r /tmp/requirements.txt
-pip3 install git+git://github.com/diku-dk/bfast.git@0.6
+pip3 install git+git://github.com/diku-dk/bfast.git@0.7
