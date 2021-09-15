4825f59f1411141141132337f46d649d0c4aea1b
Daniel Wiell <daniel.wiell@fao.org>
2021-09-15 09:01:01 +0200

    Upgraded Orfeo ToolBox to 7.4.

--- modules/geospatial-toolkit/docker/script/init_orfeo.sh
+++ modules/geospatial-toolkit/docker/script/init_orfeo.sh
@@ -6,7 +6,7 @@ echo "********************************"
 echo "*** Installing Orfeo ToolBox ***"
 echo "********************************"
 
-otb=OTB-6.6.1-Linux64
+otb=OTB-7.4.0-Linux64
 wget -nv https://www.orfeo-toolbox.org/packages/archives/OTB/$otb.run
 chmod +x $otb.run
 mv $otb.run /usr/local/lib
