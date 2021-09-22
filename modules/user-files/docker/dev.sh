af89fb12a697479022c3f0085310950df056edba
Luca Paolini <luca.paolini@fao.org>
2021-09-22 10:11:05 +0200

    Fixed user-files dev port.

--- modules/user-files/docker/dev.sh
+++ modules/user-files/docker/dev.sh
@@ -4,6 +4,6 @@ LIBS=../../../lib/js
 NODE_TLS_REJECT_UNAUTHORIZED=0 nodemon \
     --watch src \
     --watch $LIBS/shared \
-    --inspect=0.0.0.0:9236 \
+    --inspect=0.0.0.0:9238 \
     src/main.js \
     --home-dir '/var/sepal/sepal-server/home'
