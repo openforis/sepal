fae5f9e58cd2e5bd59de9311b157c62fb0143422
Daniel Wiell <daniel.wiell@fao.org>
2021-10-05 11:56:02 +0200

    Made sure ca-certificates are up to date.

--- modules/geospatial-toolkit/docker/script/init_node.sh
+++ modules/geospatial-toolkit/docker/script/init_node.sh
@@ -6,8 +6,8 @@ echo "**************************"
 echo "*** Installing Node.js ***"
 echo "**************************"
 
-curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
-bash nodesource_setup.sh
+apt-get -y update || apt-get install -y ca-certificates && apt-get upgrade -y ca-certificates
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-get update -y && apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
