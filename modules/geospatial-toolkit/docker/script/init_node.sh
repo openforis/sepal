be6bba87d5f25b1973f78868931090f8632f5ecf
Daniel Wiell <daniel.wiell@fao.org>
2021-10-05 11:57:11 +0200

    Updated curl flag when adding node source.

--- modules/geospatial-toolkit/docker/script/init_node.sh
+++ modules/geospatial-toolkit/docker/script/init_node.sh
@@ -7,7 +7,7 @@ echo "*** Installing Node.js ***"
 echo "**************************"
 
 apt-get -y update || apt-get install -y ca-certificates && apt-get upgrade -y ca-certificates
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt-get update -y && apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
