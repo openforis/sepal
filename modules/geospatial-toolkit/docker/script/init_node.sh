be7029e39d276e6bad15784037d9fc8f9e60cfee
Daniel Wiell <daniel.wiell@fao.org>
2021-10-05 11:40:33 +0200

    Updated sandbox to node 16.

--- modules/geospatial-toolkit/docker/script/init_node.sh
+++ modules/geospatial-toolkit/docker/script/init_node.sh
@@ -6,7 +6,7 @@ echo "**************************"
 echo "*** Installing Node.js ***"
 echo "**************************"
 
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-get update -y && apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
