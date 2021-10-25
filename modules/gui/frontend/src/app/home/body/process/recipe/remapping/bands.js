867eb08759d7b98954594f292250d6246bf5f593
Daniel Wiell <daniel.wiell@fao.org>
2021-10-25 20:27:16 +0200

    Made sure no bands are returned when there are no legend entries.

--- modules/gui/frontend/src/app/home/body/process/recipe/remapping/bands.js
+++ modules/gui/frontend/src/app/home/body/process/recipe/remapping/bands.js
@@ -3,9 +3,12 @@ import {selectFrom} from 'stateUtils'
 import _ from 'lodash'
 
 export const getAvailableBands = recipe => {
-    const entries = selectFrom(recipe, 'model.legend.entries') || []
-    const min = entries.length ? entries[0].value : 0
-    const max = entries.length ? _.last(entries).value : 0
+    const entries = selectFrom(recipe, 'model.legend.entries')
+    if (!entries || !entries.length) {
+        return {}
+    }
+    const min = entries[0].value
+    const max = _.last(entries).value
     return {class: {
         dataType: {precision: 'int', min, max},
         label: msg('process.classification.bands.class')
