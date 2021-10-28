68aec254fdb3a6498620c1127bca45d2de3679ed
Luca Paolini <luca.paolini@fao.org>
2021-10-28 17:28:20 +0200

    Implemented Message iconSize prop.

--- modules/gui/frontend/src/app/home/user/userSessions.js
+++ modules/gui/frontend/src/app/home/user/userSessions.js
@@ -35,9 +35,7 @@ class UserSessions extends React.Component {
 
     renderNoSessions() {
         return (
-            <Message type='info'>
-                {msg('user.report.sessions.noSessions')}
-            </Message>
+            <Message type='info' text={msg('user.report.sessions.noSessions')}/>
         )
     }
 
