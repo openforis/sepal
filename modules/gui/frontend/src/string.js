53c648f73a3ab260d0cc8eaf47c56f0f96e45146
Daniel Wiell <daniel.wiell@fao.org>
2021-10-28 18:37:32 +0200

    Made simplifyString() with non-string a no-op.

--- modules/gui/frontend/src/string.js
+++ modules/gui/frontend/src/string.js
@@ -1,3 +1,5 @@
-export const simplifyString = s => s
+import _ from 'lodash'
+
+export const simplifyString = s => _.isString(s)
     ? s.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-    : ''
+    : s
