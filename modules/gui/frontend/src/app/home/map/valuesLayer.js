d5040371f0246c2d28c97bbf2338f9773254fdcb
Luca Paolini <luca.paolini@fao.org>
2021-11-12 14:48:43 +0100

    Cleanup.

--- modules/gui/frontend/src/app/home/map/valuesLayer.js
+++ modules/gui/frontend/src/app/home/map/valuesLayer.js
@@ -41,7 +41,7 @@ class _ValuesLayer extends React.Component {
         return bands ?
             (
                 <div className={styles.container}>
-                    <Layout type='horizontal-nowrap' spacing='none' className={styles.bands}>
+                    <Layout type='horizontal-nowrap' spacing='none'>
                         {[0, 1, 2].map(i => this.renderBand({
                             key: i,
                             band: bands[i],
