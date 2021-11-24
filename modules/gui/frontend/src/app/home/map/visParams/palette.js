24c5a7719ef9826058bb5d2450578110e1a0b4b6
Luca Paolini <luca.paolini@fao.org>
2021-11-24 16:28:27 +0100

    Cleanup.

--- modules/gui/frontend/src/app/home/map/visParams/palette.js
+++ modules/gui/frontend/src/app/home/map/visParams/palette.js
@@ -32,7 +32,6 @@ export class Palette extends React.Component {
                     labelButtons={[
                         this.renderInputModeButton()
                     ]}
-                    layout={'vertical'}
                     spacing='compact'
                 >
                     {this.renderPalette()}
