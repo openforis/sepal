0f63eb3f888af68e73a5e5c90237ecce0269f47c
Luca Paolini <luca.paolini@fao.org>
2021-11-24 16:14:22 +0100

    Implemented Widget mouse events and ref.

--- modules/gui/frontend/src/widget/widget.js
+++ modules/gui/frontend/src/widget/widget.js
@@ -1,35 +1,41 @@
 import {Layout} from 'widget/layout'
+import {compose} from 'compose'
 import Label from 'widget/label'
 import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './widget.module.css'
+import withForwardedRef from 'ref'
 
-export class Widget extends React.Component {
+export class _Widget extends React.Component {
     render() {
-        const {layout, spacing, framed, scrollable, border, disabled, className, onClick} = this.props
+        const {forwardedRef, layout, spacing, alignment, framed, border, disabled, className, onMouseOver, onMouseOut, onClick} = this.props
         const widgetState = this.getWidgetState()
         return (
             <div
+                ref={forwardedRef}
                 className={[
                     styles.container,
                     styles[widgetState],
                     onClick && !disabled ? styles.clickable : null,
                     disabled ? styles.disabled : null,
-                    scrollable ? styles.scrollable : null,
+                    ['vertical-scrollable', 'vertical-fill'].includes(layout) ? styles.scrollable : null,
                     className
                 ].join(' ')}
                 onClick={e => onClick && onClick(e)}>
                 {this.renderLabel()}
                 <Layout
                     type={layout}
+                    alignment={alignment}
                     spacing={spacing}
                     framed={framed}
-                    scrollable={scrollable}
                     className={[
                         styles.widget,
                         disabled ? styles.normal : styles[widgetState],
                         border ? styles.border : null
-                    ].join(' ')}>
+                    ].join(' ')}
+                    onMouseOver={onMouseOver}
+                    onMouseOut={onMouseOut}
+                >
                     {this.renderContent()}
                 </Layout>
             </div>
@@ -49,13 +55,13 @@ export class Widget extends React.Component {
     }
 
     renderLabel() {
-        const {label, labelButtons, tooltip, tooltipPlacement, tooltipTrigger, alignment, disabled, errorMessage} = this.props
+        const {label, labelButtons, alignment, tooltip, tooltipPlacement, tooltipTrigger, disabled, errorMessage} = this.props
         return label
             ? (
                 <Label
-                    alignment={alignment}
                     msg={label}
                     buttons={labelButtons}
+                    alignment={alignment}
                     tooltip={tooltip}
                     tooltipPlacement={tooltipPlacement}
                     tooltipTrigger={tooltipTrigger}
@@ -72,6 +78,11 @@ export class Widget extends React.Component {
     }
 }
 
+export const Widget = compose(
+    _Widget,
+    withForwardedRef()
+)
+
 Widget.propTypes = {
     children: PropTypes.any.isRequired,
     alignment: PropTypes.any,
@@ -84,15 +95,16 @@ Widget.propTypes = {
     label: PropTypes.any,
     labelButtons: PropTypes.any,
     layout: PropTypes.any,
-    scrollable: PropTypes.any,
     spacing: PropTypes.any,
     tooltip: PropTypes.any,
     tooltipPlacement: PropTypes.any,
     tooltipTrigger: PropTypes.any,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    onMouseOut: PropTypes.func,
+    onMouseOver: PropTypes.func
 }
 
 Widget.defaultProps = {
     layout: 'vertical',
-    spacing: 'none'
+    spacing: 'none' // TODO: why spacing none by default?
 }
