314377c5856cab2b2f3620202bd4d07c96a141ec
Luca Paolini <luca.paolini@fao.org>
2022-01-13 00:33:06 +0100

    Refactored r-proxy Dockerfiles.
    Extracted localhost/openforis/r image.
    Removed r-proxy-test.

--- modules/r-proxy/Dockerfile.r
+++ modules/r-proxy/Dockerfile.r
@@ -1,9 +1,8 @@
-# FROM node:14-stretch
 FROM ubuntu:focal
 
 ENV DEBIAN_FRONTEND noninteractive
 
-# Install apt packages
+# Install base apt packages
 RUN apt-get -y update && apt-get install -y \
     curl \
     git \
@@ -11,7 +10,18 @@ RUN apt-get -y update && apt-get install -y \
     sudo \
     software-properties-common \
     zip \
-    unzip \
+    unzip
+
+# Add repository for misc GIS utilities
+RUN apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
+
+# Add repositories for R
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+RUN add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+RUN add-apt-repository -y ppa:c2d4u.team/c2d4u4.0+
+
+# Install extra apt packages
+RUN apt-get update && apt-get install -y \
     gcc \
     gdal-bin \
     libcgal-dev \
@@ -33,14 +43,6 @@ RUN apt-get -y update && apt-get install -y \
     r-base-dev \
     r-cran-rmpi
 
-# Add repository for misc GIS utilities
-RUN apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
-
-# Add repositories for R
-RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-RUN add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-RUN add-apt-repository -y ppa:c2d4u.team/c2d4u4.0+
-
 # Install Java: configure SDKMAN! directory
 ENV SDKMAN_DIR=/usr/local/lib/sdkman
 ENV JAVA_VERSION 11.0.11.hs-adpt
@@ -56,29 +58,6 @@ RUN bash -c 'source ${SDKMAN_DIR}/bin/sdkman-init.sh && sdk install java ${JAVA_
 
 # Install Java: Symlink Java to /usr/local/bin to make it available to any user
 RUN bash -c 'source ${SDKMAN_DIR}/bin/sdkman-init.sh && update-alternatives --install /usr/local/bin/java java $(sdk home java ${JAVA_VERSION})/bin/java 1'
-
-# Install extra apt packages
-# RUN apt-get update && apt-get install -y \
-#     gcc \
-#     gdal-bin \
-#     libcgal-dev \
-#     libcurl4-openssl-dev \
-#     libfribidi-dev \
-#     libgdal-dev \
-#     libgeos++-dev \
-#     libglu1-mesa-dev \
-#     libharfbuzz-dev \
-#     libmagick++-dev \
-#     libnetcdf-dev \
-#     libopenmpi-dev \
-#     libpq-dev \
-#     libssl-dev \
-#     libudunits2-dev \
-#     libv8-dev \
-#     libxml2-dev \
-#     r-base \
-#     r-base-dev \
-#     r-cran-rmpi
 
 # Configure R Java home
 RUN bash -c 'source ${SDKMAN_DIR}/bin/sdkman-init.sh && JAVA_HOME=$(sdk home java) R CMD javareconf'
