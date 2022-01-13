2ab1eab303d5901e2cee268247c7266c30b9c21c
Luca Paolini <luca.paolini@fao.org>
2022-01-13 17:21:22 +0100

    Improved package bundling script.

--- modules/r-proxy/src/R/bundle_package.sh
+++ modules/r-proxy/src/R/bundle_package.sh
@@ -11,9 +11,10 @@ PACKAGE_FILENAME="${PACKAGE}_${VERSION}.tar.gz"
 TMP_PACKAGE_FILENAME="${PACKAGE_FILENAME}.tmp"
 
 if [[ ! -f "${PACKAGE_PATH}" ]]; then
-    mkdir -p ${BIN} \
-        && rm -f ${BIN}/${TMP_PACKAGE_FILENAME} \
-        && tar czf ${BIN}/${TMP_PACKAGE_FILENAME} -C ${LIB} ${PACKAGE} \
+    mkdir -p ${BIN}
+    retVal = $?; [ $retVal -ne 0 ] && exit $retVal
+    rm -f ${BIN}/${TMP_PACKAGE_FILENAME}
+    tar czf ${BIN}/${TMP_PACKAGE_FILENAME} -C ${LIB} ${PACKAGE} \
         && mv ${BIN}/${TMP_PACKAGE_FILENAME} ${BIN}/${PACKAGE_FILENAME} \
         && rm -f ${SRC}/${PACKAGE_FILENAME}
     exit $?
