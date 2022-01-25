93be77cb0e03f04b41ddc17b33da12e1b06b9adb
Daniel Wiell <daniel.wiell@fao.org>
2022-01-25 20:25:14 +0100

    Pushed all modules to docker registry.

--- modules/jenkins/script/build-and-push-images.sh
+++ modules/jenkins/script/build-and-push-images.sh
@@ -18,6 +18,7 @@ function build {
 
 function push {
   local MODULE=$1
+  echo "******* Pushing ${MODULE} *******"
   docker push localhost/openforis/${MODULE}:${VERSION}
 }
 
@@ -49,5 +50,30 @@ build app-manager
 build sepal-server
 
 echo "${DOCKER_REGISTRY_PASSWORD}" | docker login localhost -u "${DOCKER_REGISTRY_USERNAME}" --password-stdin
+
+push sys-monitor
+push email
+push backup
+push rabbitmq
+push ldap
+push ldap-backup
+push user
+push user-storage
+push user-storage-backup
+push app-manager
+push sepal
+push user-files
+push gui
+push ceo-gateway
+push mysql
+push mysql-backup
+push gee
+push gateway
+push ssh-gateway
+push sandbox
+push task
+push terminal
+push letsencrypt
+push haproxy
 
 docker logout localhost
