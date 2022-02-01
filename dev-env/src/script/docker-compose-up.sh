ec1d8c026e8c8e5eb8aa013d20e5b621325e88c8
Luca Paolini <luca.paolini@fao.org>
2022-02-01 20:59:45 +0100

    Cleanup.

--- dev-env/src/script/docker-compose-up.sh
+++ dev-env/src/script/docker-compose-up.sh
@@ -4,7 +4,6 @@ MODULE=$1
 SEPAL_SRC=$2
 ENV_FILE=$3
 
-# (cd "${SEPAL_SRC}/modules/${MODULE}"; pwd; docker compose --env-file="${ENV_FILE}" up --wait --detach --remove-orphans)
 (cd "${SEPAL_SRC}/modules/${MODULE}"; docker compose --env-file="${ENV_FILE}" up --detach)
 
 exit $?
