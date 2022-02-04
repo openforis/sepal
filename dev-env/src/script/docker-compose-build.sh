75cc8442d490e1bbf9801fb4bfb153a70fc98487
Daniel Wiell <daniel.wiell@fao.org>
2022-02-04 16:12:59 +0100

    Provided gui BUILD_NUMBER and GIT_COMMIT both as build arg in the docker compose file and copied into ENV in the Dockerfile.

--- dev-env/src/script/docker-compose-build.sh
+++ dev-env/src/script/docker-compose-build.sh
@@ -7,11 +7,9 @@ BUILD_OPTIONS=$4
 
 ( \
     cd "${SEPAL_SRC}/modules/${MODULE}"; \
-    docker compose \
+    BUILD_NUMBER="latest" GIT_COMMIT="$(git rev-parse HEAD)" docker compose \
         --env-file="${ENV_FILE}" build \
-        ${BUILD_OPTIONS} \
-        --build-arg BUILD_NUMBER="latest" \
-        --build-arg GIT_COMMIT="$(git rev-parse HEAD)" \
+        ${BUILD_OPTIONS}
 )
 
 exit $?
