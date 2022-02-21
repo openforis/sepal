48d0f1b8b01198d46d4ddec2840168e926cd7b16
Luca Paolini <luca.paolini@fao.org>
2022-02-21 20:06:16 +0100

    Removed unnecessary subshells in dev-env scripts.

--- dev-env/src/script/docker-compose-build.sh
+++ dev-env/src/script/docker-compose-build.sh
@@ -5,10 +5,9 @@ SEPAL_SRC=$2
 ENV_FILE=$3
 OPTIONS=$4
 
-( \
-    cd "${SEPAL_SRC}/modules/${MODULE}"; \
-    BUILD_NUMBER="latest" GIT_COMMIT="$(git rev-parse HEAD)" docker compose \
-        --env-file="${ENV_FILE}" build ${OPTIONS}
-)
+cd ${SEPAL_SRC}/modules/${MODULE}
+BUILD_NUMBER="latest" \
+GIT_COMMIT="$(git rev-parse HEAD)" \
+docker compose --env-file="${ENV_FILE}" build ${OPTIONS}
 
 exit $?
--- dev-env/src/script/docker-compose-logs.sh
+++ dev-env/src/script/docker-compose-logs.sh
@@ -5,9 +5,7 @@ SEPAL_SRC=$2
 ENV_FILE=$3
 OPTIONS=$4
 
-( \
-    cd "${SEPAL_SRC}/modules/${MODULE}"; \
-    docker compose --env-file="${ENV_FILE}" logs ${OPTIONS} \
-)
+cd ${SEPAL_SRC}/modules/${MODULE}
+docker compose --env-file="${ENV_FILE}" logs ${OPTIONS}
 
 exit $?
