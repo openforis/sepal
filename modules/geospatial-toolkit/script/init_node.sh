3cae9550f5b34519190c5beddd2b9338ae77a2bb
Daniel Wiell <daniel.wiell@fao.org>
2022-03-02 11:02:08 +0000

    Removed apt upgrade.

--- modules/geospatial-toolkit/script/init_node.sh
+++ modules/geospatial-toolkit/script/init_node.sh
@@ -6,7 +6,7 @@ echo "**************************"
 echo "*** Installing Node.js ***"
 echo "**************************"
 
-apt-get -y update || apt-get install -y ca-certificates && apt-get upgrade -y ca-certificates
+apt-get -y update || apt-get install -y ca-certificates
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt-get update -y && apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
