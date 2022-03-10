d029ad3b36d9d789591463ca0b43e37a6a036ea0
Luca Paolini <luca.paolini@fao.org>
2022-03-10 09:51:48 +0100

    Protected against division by zero.

--- modules/gui/src/app/home/user/userResourceUsage.js
+++ modules/gui/src/app/home/user/userResourceUsage.js
@@ -4,23 +4,22 @@ import React from 'react'
 import Tooltip from 'widget/tooltip'
 import styles from './userResourceUsage.module.css'
 
-export const UserResourceUsage = ({currentValue, budgetValue, formattedValue}) => {
-    const usage = Math.min(currentValue / budgetValue, 1)
-    return (
-        <Tooltip
-            // className={[overbudget ? styles.overBudget : null].join(' ')}
-            msg={
-                <Gauge
-                    value={currentValue}
-                    maxValue={budgetValue}
-                    infoTop={({percentage}) => msg('user.report.usage', {percentage})}
-                />
-            }
-            delay={250}
-            placement='topRight'>
-            <div className={styles.usage} style={{'--usage': `${usage}`}}>
-                {formattedValue}
-            </div>
-        </Tooltip>
-    )
-}
+export const UserResourceUsage = ({currentValue = 0, budgetValue = 0, formattedValue}) =>
+    budgetValue > 0
+        ? (
+            <Tooltip
+                msg={
+                    <Gauge
+                        value={currentValue}
+                        maxValue={budgetValue}
+                        infoTop={({percentage}) => msg('user.report.usage', {percentage})}
+                    />
+                }
+                delay={250}
+                placement='topRight'>
+                <div className={styles.usage} style={{'--usage': `${Math.min(currentValue / budgetValue, 1)}`}}>
+                    {formattedValue}
+                </div>
+            </Tooltip>
+        )
+        : null
