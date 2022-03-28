973fa5ada53685abd1e4ea562b3d32e4ee36c19a
12rambau <pierrick.rambaud49@gmail.com>
2022-03-28 16:45:11 +0200

    install all the required files at once

--- modules/app-manager/install-requirements.sh
+++ modules/app-manager/install-requirements.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 venv_path=$1
 requirements=$2
-grep -v "^#" "$requirements" | xargs -n 1 -L 1 "$venv_path"/bin/pip3 install -U --cache-dir /root/.cache/pip
+"$venv_path"/bin/pip3 install -r "$requirements" -U --cache-dir /root/.cache/pip
 touch $venv_path/../.installed
