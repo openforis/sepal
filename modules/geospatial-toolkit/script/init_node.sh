c7e9fdd6ca4eab65460a8ee9da0b13d55cbb7bc1
Daniel Wiell <daniel.wiell@fao.org>
2022-04-05 08:55:51 +0000

    Upgrade sandbox node version to node to 17.

--- modules/geospatial-toolkit/script/init_node.sh
+++ modules/geospatial-toolkit/script/init_node.sh
@@ -7,7 +7,7 @@ echo "*** Installing Node.js ***"
 echo "**************************"
 
 apt-get -y update || apt-get install -y ca-certificates
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_17.x | bash -
 apt-get update -y && apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
