8f644828ef6792d94eff5bbee0e600b80b666e9d
Luca Paolini <luca.paolini@fao.org>
2022-05-09 16:55:07 +0200

    Removed @google/earthengine ver. from GS toolkit.

--- modules/geospatial-toolkit/script/init_node.sh
+++ modules/geospatial-toolkit/script/init_node.sh
@@ -12,4 +12,4 @@ apt-get update -y && apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update && apt-get -y install yarn
-npm install -g @google/earthengine@0.1.282
+npm install -g @google/earthengine
