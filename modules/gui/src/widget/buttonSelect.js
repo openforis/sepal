03f622558fcce16ec9079ba72720675d664c710b
Luca Paolini <luca.paolini@fao.org>
2022-05-20 17:12:25 +0200

    Reimplemented FloatingBox.

--- modules/gui/src/widget/buttonSelect.js
+++ modules/gui/src/widget/buttonSelect.js
@@ -130,8 +130,8 @@ class ButtonSelect extends React.Component {
         return (
             <FloatingBox
                 element={this.input.current}
-                verticalPlacement={placement}
-                alignment='left'
+                vPlacement={placement}
+                hPlacement='over-right'
                 onBlur={this.handleBlur}>
                 <ScrollableList
                     ref={this.list}
