aa6b3fd6567df277c70cef48debc0e0010b35332
Luca Paolini <luca.paolini@fao.org>
2022-05-20 15:23:18 +0200

    Implemented InlineConfirmationButton.
    It replaces TooltipConfirmationButton.

---
+++ modules/gui/src/widget/inlineConfirmationButton.js
@@ -0,0 +1,150 @@
+import {Button} from 'widget/button'
+import {ButtonGroup} from './buttonGroup'
+import {msg} from 'translate'
+import FloatingBox from './floatingBox'
+import Keybinding from 'widget/keybinding'
+import PropTypes from 'prop-types'
+import React from 'react'
+import styles from './inlineConfirmationButton.module.css'
+
+export class InlineConfirmationButton extends React.Component {
+    ref = React.createRef()
+
+    state = {
+        showConfirmation: false
+    }
+
+    constructor() {
+        super()
+        this.onClick = this.onClick.bind(this)
+        this.onClickHold = this.onClickHold.bind(this)
+        this.showConfirmation = this.showConfirmation.bind(this)
+        this.hideConfirmation = this.hideConfirmation.bind(this)
+        this.toggleConfirmation = this.toggleConfirmation.bind(this)
+    }
+
+    renderConfirmationButton() {
+        const {confirmationLabel, onConfirm} = this.props
+        const {showConfirmation} = this.state
+        return showConfirmation ? (
+            <FloatingBox
+                element={this.ref.current}
+                vPlacement='center'
+                hPlacement='over-left'
+                onBlur={this.toggleConfirmation}>
+                <div className={styles.wrapper}>
+                    <Keybinding keymap={{'Escape': this.hideConfirmation}}>
+                        <ButtonGroup>
+                            <Button
+                                shape='pill'
+                                look='cancel'
+                                // icon='trash'
+                                icon='exclamation-triangle'
+                                label={confirmationLabel || msg('button.confirm')}
+                                onClick={onConfirm}/>
+                            {this.renderExitButton()}
+                        </ButtonGroup>
+                    </Keybinding>
+                </div>
+            </FloatingBox>
+        ) : null
+    }
+
+    showConfirmation() {
+        this.setState({showConfirmation: true})
+    }
+
+    hideConfirmation() {
+        this.setState({showConfirmation: false})
+    }
+
+    toggleConfirmation() {
+        this.setState(({showConfirmation}) => ({showConfirmation: !showConfirmation}))
+    }
+
+    onClick() {
+        const {skipConfirmation, onConfirm} = this.props
+        skipConfirmation ? onConfirm() : this.toggleConfirmation()
+    }
+
+    onClickHold() {
+        const {onConfirm} = this.props
+        onConfirm()
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.renderButton()}
+                {this.renderConfirmationButton()}
+            </React.Fragment>
+        )
+    }
+
+    renderButton() {
+        const {busy, chromeless, disabled, icon, iconType, label, shape, size, tooltip, tooltipPlacement, width} = this.props
+        const {showConfirmation} = this.state
+        return (
+            <Button
+                ref={this.ref}
+                busy={busy}
+                chromeless={chromeless}
+                icon={icon}
+                iconType={iconType}
+                label={label}
+                shape={shape}
+                size={size}
+                width={width}
+                disabled={disabled}
+                tooltip={tooltip}
+                tooltipDisabled={showConfirmation}
+                tooltipPlacement={tooltipPlacement}
+                onClick={this.onClick}
+                onClickHold={this.onClickHold}
+            />
+        )
+    }
+
+    renderExitButton() {
+        const {chromeless, shape, size, width} = this.props
+        return (
+            <Button
+                chromeless={chromeless}
+                icon='xmark'
+                shape={shape}
+                size={size}
+                width={width}
+                onClick={this.hideConfirmation}
+            />
+        )
+    }
+
+    componentDidUpdate({disabled: wasDisabled}) {
+        const {disabled} = this.props
+        if (!wasDisabled && disabled) {
+            this.hideConfirmation()
+        }
+    }
+}
+
+InlineConfirmationButton.defaultProps = {
+    tooltipPlacement: 'top',
+    confirmationIcon: 'exclamation-triangle'
+}
+
+InlineConfirmationButton.propTypes = {
+    onConfirm: PropTypes.func.isRequired,
+    busy: PropTypes.any,
+    chromeless: PropTypes.any,
+    confirmationLabel: PropTypes.string,
+    disabled: PropTypes.any,
+    icon: PropTypes.any,
+    iconType: PropTypes.any,
+    label: PropTypes.any,
+    shape: PropTypes.any,
+    size: PropTypes.any,
+    skipConfirmation: PropTypes.any,
+    tooltip: PropTypes.any,
+    tooltipPlacement: PropTypes.any,
+    width: PropTypes.any
+}
--- modules/gui/src/widget/removeButton.js
+++ modules/gui/src/widget/removeButton.js
@@ -1,18 +1,19 @@
+import {InlineConfirmationButton} from './inlineConfirmationButton'
 import {ModalConfirmationButton} from './modalConfirmationButton'
-import {TooltipConfirmationButton} from './tooltipConfirmationButton'
+import {msg} from 'translate'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 export default class RemoveButton extends React.Component {
     render() {
-        const {message, children} = this.props
-        return message || children
+        const {message} = this.props
+        return message
             ? this.renderModalConfirmationButton()
             : this.renderTooltipConfirmationButton()
     }
 
     renderModalConfirmationButton() {
-        const {chromeless, icon, label, tooltip, tooltipPlacement, title, message, shape, size, onRemove, disabled, unsafe, children} = this.props
+        const {chromeless, icon, label, tooltip, tooltipPlacement, title, message, shape, size, onRemove, disabled, unsafe} = this.props
         return (
             <ModalConfirmationButton
                 chromeless={chromeless}
@@ -27,21 +28,20 @@ export default class RemoveButton extends React.Component {
                 title={title}
                 message={message}
                 onConfirm={onRemove}
-            >
-                {children}
-            </ModalConfirmationButton>
+            />
         )
     }
 
     renderTooltipConfirmationButton() {
-        const {chromeless, icon, label, tooltip, tooltipPlacement, shape, size, onRemove, disabled, unsafe} = this.props
+        const {chromeless, icon, label, confirmationLabel, tooltip, tooltipPlacement, shape, size, onRemove, disabled, unsafe} = this.props
         return (
-            <TooltipConfirmationButton
+            <InlineConfirmationButton
                 chromeless={chromeless}
                 shape={shape}
                 size={size}
                 icon={icon}
                 label={label}
+                confirmationLabel={confirmationLabel}
                 tooltip={tooltip}
                 tooltipPlacement={tooltipPlacement}
                 disabled={disabled}
@@ -54,8 +54,8 @@ export default class RemoveButton extends React.Component {
 
 RemoveButton.propTypes = {
     onRemove: PropTypes.func.isRequired,
-    children: PropTypes.any,
     chromeless: PropTypes.any,
+    confirmationLabel: PropTypes.any,
     disabled: PropTypes.any,
     label: PropTypes.any,
     message: PropTypes.any,
@@ -69,5 +69,6 @@ RemoveButton.propTypes = {
 
 RemoveButton.defaultProps = {
     unsafe: false,
-    icon: 'trash'
+    icon: 'trash',
+    confirmationLabel: msg('button.remove')
 }
