5592b7282fa092118badd2d63442c439258b4a8a
Daniel Wiell <daniel.wiell@fao.org>
2022-05-30 11:58:23 +0000

    Installed latest version of dggrid.

---
+++ modules/sandbox/script/init_dggrid.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo
+echo "*************************"
+echo "*** Installing DGGRID ***"
+echo "*************************"
+
+# https://github.com/sahrk/DGGRID/blob/master/INSTALL.md
+cd /usr/local/lib
+git clone https://github.com/sahrk/DGGRID.git
+cd DGGRID
+mkdir build 
+cd build
+cmake -DWITH_GDAL=OFF -DCMAKE_BUILD_TYPE=Release ..
+make -j 4 
+ln -s /usr/local/lib/DGGRID/build/src/apps/dggrid/dggrid /usr/local/bin/dggrid
+ln -s /usr/local/lib/DGGRID/build/src/apps/appex/appex /usr/local/bin/appex
