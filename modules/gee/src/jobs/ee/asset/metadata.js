4285e96a3f589358bc07db1a5e7d93205eb9d157
Daniel Wiell <daniel.wiell@fao.org>
2022-05-31 12:31:15 +0000

    Included bands in image collection metadata.

--- modules/gee/src/jobs/ee/asset/metadata.js
+++ modules/gee/src/jobs/ee/asset/metadata.js
@@ -1,11 +1,25 @@
 const {job} = require('gee/jobs/job')
 
 const worker$ = ({asset, expectedType}) => {
-    const {throwError, of, catchError, switchMap} = require('rxjs')
+    const {of, catchError, map, switchMap, throwError} = require('rxjs')
     const {ClientException, NotFoundException} = require('sepal/exception')
     const ee = require('sepal/ee')
     const _ = require('lodash')
 
+    const addFirstImageBands$ = asset => {
+        const collection = ee.ImageCollection(asset.id)
+        const bands = ee.Algorithms.If(
+            collection.size(),
+            ee.Dictionary(ee.Algorithms.Describe(
+                collection.first()
+            )).get('bands'),
+            []
+        )
+        return ee.getInfo$(bands, 'Get collection bands').pipe(
+            map(bands => ({...asset, bands}))
+        )
+    }
+    
     const handleError$ = error =>
         throwError(
             () => error instanceof ClientException
@@ -21,17 +35,21 @@ const worker$ = ({asset, expectedType}) => {
         )
 
     return ee.getAsset$(asset, 0).pipe(
-        switchMap(asset =>
-            !expectedType || asset.type === expectedType || (_.isArray(expectedType) && expectedType.includes(asset.type))
-                ? of(asset)
-                : throwError(() => new ClientException(`Asset is of type ${asset.type} while ${expectedType} is expected.`, {
+        switchMap(asset => {
+            if (!expectedType || asset.type === expectedType || (_.isArray(expectedType) && expectedType.includes(asset.type))) {
+                return asset.type === 'ImageCollection'
+                    ? addFirstImageBands$(asset)
+                    : of(asset)
+            } else {
+                return throwError(() => new ClientException(`Asset is of type ${asset.type} while ${expectedType} is expected.`, {
                     userMessage: {
                         message: `Not an ${expectedType}`,
                         key: 'gee.asset.error.wrongType',
                         args: {asset, expectedType, actualType: asset.type}
                     }
                 }))
-        ),
+            }
+        }),
         catchError(handleError$)
     )
 }
