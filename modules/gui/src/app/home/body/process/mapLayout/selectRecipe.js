284630b603fd0925b34d5f08ae41978e61b7870c
Daniel Wiell <daniel.wiell@fao.org>
2022-06-02 07:29:15 +0000

    Switched to RecipeInput in map layout.

--- modules/gui/src/app/home/body/process/mapLayout/selectRecipe.js
+++ modules/gui/src/app/home/body/process/mapLayout/selectRecipe.js
@@ -1,54 +1,76 @@
+import {Form, form} from 'widget/form/form'
 import {Panel} from 'widget/panel/panel'
-import {RecipeList} from '../recipeList/recipeList'
+import {RecipeInput} from 'widget/recipeInput'
 import {activatable} from 'widget/activation/activatable'
 import {compose} from 'compose'
-import {connect, select} from 'store'
 import {msg} from 'translate'
 import {v4 as uuid} from 'uuid'
 import {withRecipe} from 'app/home/body/process/recipeContext'
 import React from 'react'
 import styles from './selectRecipe.module.css'
 
-const mapStateToProps = () => {
-    return {
-        recipes: select('process.recipes')
-    }
+const fields = {
+    recipe: new Form.Field().notBlank()
 }
 
 class _SelectRecipe extends React.Component {
+    state = {
+        recipe: null
+    }
+
+    constructor(props) {
+        super(props)
+        this.add = this.add.bind(this)
+    }
+
     render() {
+        const {recipe} = this.state
         const {activatable: {deactivate}} = this.props
         return (
-            <RecipeList>
-                <Panel type='modal' className={styles.panel}>
-                    <Panel.Header title={msg('map.layout.addImageLayerSource.types.Recipe.description')}/>
-                    <Panel.Content scrollable={false}>
-                        <RecipeList.Data onClick={recipeId => this.selectRecipe(recipeId)}/>
-                    </Panel.Content>
-                    <Panel.Buttons>
-                        <Panel.Buttons.Main>
-                            <Panel.Buttons.Close
-                                keybinding='Escape'
-                                onClick={deactivate}
-                            />
-                        </Panel.Buttons.Main>
-                        <Panel.Buttons.Extra>
-                            <RecipeList.Pagination/>
-                        </Panel.Buttons.Extra>
-                    </Panel.Buttons>
-                </Panel>
-            </RecipeList>
+            <Panel type='modal' className={styles.panel}>
+                <Panel.Header title={msg('map.layout.addImageLayerSource.types.Recipe.description')}/>
+                <Panel.Content scrollable={false}>
+                    {this.renderContent()}
+                </Panel.Content>
+                <Panel.Buttons>
+                    <Panel.Buttons.Main>
+                        <Panel.Buttons.Cancel
+                            keybinding='Escape'
+                            onClick={deactivate}
+                        />
+                        <Panel.Buttons.Add
+                            keybinding='Enter'
+                            onClick={this.add}
+                            disabled={!recipe}
+                        />
+                    </Panel.Buttons.Main>
+                </Panel.Buttons>
+            </Panel>
         )
     }
 
-    selectRecipe(recipeId) {
+    renderContent() {
+        const {inputs: {recipe}} = this.props
+        return (
+            <RecipeInput
+                input={recipe}
+                filter={type => !type.noImageOutput}
+                autoFocus
+                onLoading={() => this.setState({recipe: null})}
+                onLoaded={({recipe}) => this.setState({recipe})}
+            />
+        )
+    }
+
+    add() {
+        const {recipe} = this.state
         const {recipeActionBuilder, activatable: {deactivate}} = this.props
         recipeActionBuilder('ADD_RECIPE_IMAGE_LAYER_SOURCE')
             .push('layers.additionalImageLayerSources', {
                 id: uuid(),
                 type: 'Recipe',
                 sourceConfig: {
-                    recipeId
+                    recipeId: recipe.id
                 }
             })
             .dispatch()
@@ -62,7 +84,7 @@ const policy = () => ({
 
 export const SelectRecipe = compose(
     _SelectRecipe,
+    form({fields}),
     withRecipe(),
-    activatable({id: 'selectRecipe', policy, alwaysAllow: true}),
-    connect(mapStateToProps)
+    activatable({id: 'selectRecipe', policy, alwaysAllow: true})
 )
