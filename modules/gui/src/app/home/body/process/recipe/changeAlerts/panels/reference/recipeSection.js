de584df061a5088ac9e7a048fe63d1e7e79b22a5
Daniel Wiell <daniel.wiell@fao.org>
2022-06-15 18:36:43 +0000

    Switched to RecipeInput.

--- modules/gui/src/app/home/body/process/recipe/changeAlerts/panels/reference/recipeSection.js
+++ modules/gui/src/app/home/body/process/recipe/changeAlerts/panels/reference/recipeSection.js
@@ -1,17 +1,9 @@
-import {Form} from 'widget/form/form'
+import {RecipeInput} from 'widget/recipeInput'
 import {Subject} from 'rxjs'
 import {compose} from 'compose'
-import {connect, select} from 'store'
-import {msg} from 'translate'
 import {recipeAccess} from 'app/home/body/process/recipeAccess'
 import PropTypes from 'prop-types'
 import React from 'react'
-
-const mapStateToProps = () => {
-    return {
-        recipes: select('process.recipes')
-    }
-}
 
 class RecipeSection extends React.Component {
     constructor(props) {
@@ -20,19 +12,11 @@ class RecipeSection extends React.Component {
     }
 
     render() {
-        const {recipes, inputs: {recipe}} = this.props
-        const options = recipes
-            .filter(({type}) => type === 'CCDC')
-            .map(recipe => ({
-                value: recipe.id,
-                label: recipe.name
-            }))
+        const {inputs: {recipe}} = this.props
         return (
-            <Form.Combo
-                label={msg('process.classification.panel.inputImagery.form.recipe.label')}
+            <RecipeInput
+                filter={type => type.id === 'CCDC'}
                 input={recipe}
-                placeholder={msg('process.classification.panel.inputImagery.form.recipe.placeholder')}
-                options={options}
                 autoFocus
                 errorMessage
             />
@@ -46,6 +30,5 @@ RecipeSection.propTypes = {
 
 export default compose(
     RecipeSection,
-    connect(mapStateToProps),
     recipeAccess()
 )
