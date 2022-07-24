7c8ed8fe35c2bf5f68edccd23e701f02e1b1a036
Rambaud Pierrick <12rambau@users.noreply.github.com>
2022-07-24 18:25:49 +0200

    drop version pin for ipyleaflet

--- modules/sandbox/script/init_jupyter.sh
+++ modules/sandbox/script/init_jupyter.sh
@@ -18,7 +18,7 @@ R -e "IRkernel::installspec(user = FALSE)"
 /usr/bin/python3 -m pip install jupyterlab-language-pack-fr-FR
 /usr/bin/python3 -m pip install jupyterlab-language-pack-es-ES
 /usr/bin/python3 -m pip install folium
-/usr/bin/python3 -m pip install ipyleaflet==0.13.3
+/usr/bin/python3 -m pip install ipyleaflet
 /usr/bin/python3 -m pip install ipyvuetify
 git clone https://github.com/ipython-contrib/jupyter_contrib_nbextensions.git
 /usr/bin/python3 -m pip install jupyter_latex_envs # Required for jupyter_contrib_nbextensions
@@ -36,5 +36,5 @@ npm install -g --unsafe-perm ijavascript
 npm install -g js-beautify
 /usr/bin/ijsinstall --install=global
 
-/usr/bin/python3 /usr/local/bin/jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-leaflet@v0.13.3
+/usr/bin/python3 /usr/local/bin/jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-leaflet
 /usr/bin/python3 /usr/local/bin/jupyter lab build
