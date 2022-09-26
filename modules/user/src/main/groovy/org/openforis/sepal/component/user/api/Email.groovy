ec9de75ddfa7d3a021c5d31507d9104b8bf7325d
Luca Paolini <luca.paolini@fao.org>
2022-09-26 15:42:58 +0200

    Implemented server-side email vaildation.

--- modules/user/src/main/groovy/org/openforis/sepal/component/user/api/Email.groovy
+++ modules/user/src/main/groovy/org/openforis/sepal/component/user/api/Email.groovy
@@ -3,7 +3,9 @@ package org.openforis.sepal.component.user.api
 import java.util.regex.Pattern
 
 class Email {
-    private final static Pattern EMAIL_PATTERN = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+    private final static Pattern EMAIL_PATTERN = Pattern.compile(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
 
     static boolean isValid(String email) {
         def matcher = EMAIL_PATTERN.matcher(email)
