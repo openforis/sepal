080d645e7193493fdfc4ceb4d58a90493e6757dd
Daniel Wiell <daniel.wiell@fao.org>
2022-12-07 16:58:33 +0000

    Implemented StaticMap random postitioning.

--- modules/gui/src/app/home/map/staticMap.js
+++ modules/gui/src/app/home/map/staticMap.js
@@ -3,6 +3,9 @@ import {withMapsContext} from './maps'
 import React from 'react'
 import styles from './staticMap.module.css'
 
+const MIN_ZOOM = 0
+const MAX_ZOOM = 5
+
 class _StaticMap extends React.Component {
     map = React.createRef()
 
@@ -12,9 +15,18 @@ class _StaticMap extends React.Component {
         )
     }
 
+    randomize(map) {
+        map.setCenter({
+            lng: 360 * Math.random() - 180,
+            lat: 90 * Math.random() - 45
+        })
+        map.setZoom(Math.round((MAX_ZOOM - MIN_ZOOM) * Math.random() + MIN_ZOOM))
+    }
+
     componentDidMount() {
         const {mapsContext: {createGoogleMap}} = this.props
-        createGoogleMap(this.map.current)
+        const map = createGoogleMap(this.map.current)
+        setTimeout(() => this.randomize(map), 1000)
     }
 }
 
