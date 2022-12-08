514d957caccc423e5700d598bad9ce71f08d759a
Daniel Wiell <daniel.wiell@fao.org>
2022-12-08 16:24:29 +0000

    Implemented SortButton defaultSortingDirection.

--- modules/gui/src/widget/sortButtons.js
+++ modules/gui/src/widget/sortButtons.js
@@ -3,48 +3,57 @@ import {ButtonGroup} from './buttonGroup'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-class SortButton extends React.Component {
+export class SortButton extends React.Component {
     constructor() {
         super()
         this.toggleSortOrder = this.toggleSortOrder.bind(this)
     }
 
     render() {
-        const {label, sorted, sortOrder} = this.props
+        const {label, shape, additionalClassName, sorted, sortingDirection} = this.props
         return (
             <Button
                 chromeless
                 look='transparent'
-                shape='pill'
+                shape={shape}
+                additionalClassName={additionalClassName}
                 label={label}
                 labelStyle={sorted ? 'smallcaps-highlight' : 'smallcaps'}
-                icon={this.getHandleIcon(sortOrder)}
+                icon={this.getHandleIcon(sortingDirection)}
                 iconPlacement='right'
                 onClick={this.toggleSortOrder}/>
         )
     }
 
-    getHandleIcon(sortOrder) {
+    getHandleIcon(sortingDirection) {
         const {sorted} = this.props
         const orderMap = {
             '-1': 'sort-up',
             '0': 'sort',
             '1': 'sort-down'
         }
-        return orderMap[sorted ? sortOrder : 0]
+        return orderMap[sorted ? sortingDirection : 0]
     }
 
     toggleSortOrder() {
-        const {sorted, sortOrder, onChange} = this.props
-        onChange(sorted ? -sortOrder : 1)
+        const {sorted, sortingDirection, defaultSortingDirection, onChange} = this.props
+        onChange(sorted ? -sortingDirection : defaultSortingDirection)
     }
 }
 
 SortButton.propTypes = {
     onChange: PropTypes.any.isRequired,
+    additionalClassName: PropTypes.any,
+    defaultSortingDirection: PropTypes.oneOf([-1, 1]),
     label: PropTypes.any,
+    shape: PropTypes.any,
     sorted: PropTypes.any,
-    sortOrder: PropTypes.oneOf([-1, 0, 1])
+    sortingDirection: PropTypes.oneOf([-1, 1])
+}
+
+SortButton.defaultProps = {
+    shape: 'pill',
+    defaultSortingDirection: 1
 }
 
 export class SortButtons extends React.Component {
@@ -72,9 +81,10 @@ export class SortButtons extends React.Component {
         return (
             <SortButton
                 key={column}
+                shape='pill'
                 label={labels[column]}
                 sorted={sortingOrder === column}
-                sortOrder={sortingDirection}
+                sortingDirection={sortingDirection}
                 onChange={sortingDirection => onChange(column, sortingDirection)}
             />
         )
