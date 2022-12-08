5301d36ba2765c3709e8d1cec0582c642dbcaa7b
Daniel Wiell <daniel.wiell@fao.org>
2022-12-08 16:27:36 +0000

    Cleanup.

--- modules/gui/src/widget/sortButtons.js
+++ modules/gui/src/widget/sortButtons.js
@@ -1,60 +1,7 @@
-import {Button} from 'widget/button'
 import {ButtonGroup} from './buttonGroup'
+import {SortButton} from './sortButton'
 import PropTypes from 'prop-types'
 import React from 'react'
-
-export class SortButton extends React.Component {
-    constructor() {
-        super()
-        this.toggleSortOrder = this.toggleSortOrder.bind(this)
-    }
-
-    render() {
-        const {label, shape, additionalClassName, sorted, sortingDirection} = this.props
-        return (
-            <Button
-                chromeless
-                look='transparent'
-                shape={shape}
-                additionalClassName={additionalClassName}
-                label={label}
-                labelStyle={sorted ? 'smallcaps-highlight' : 'smallcaps'}
-                icon={this.getHandleIcon(sortingDirection)}
-                iconPlacement='right'
-                onClick={this.toggleSortOrder}/>
-        )
-    }
-
-    getHandleIcon(sortingDirection) {
-        const {sorted} = this.props
-        const orderMap = {
-            '-1': 'sort-up',
-            '0': 'sort',
-            '1': 'sort-down'
-        }
-        return orderMap[sorted ? sortingDirection : 0]
-    }
-
-    toggleSortOrder() {
-        const {sorted, sortingDirection, defaultSortingDirection, onChange} = this.props
-        onChange(sorted ? -sortingDirection : defaultSortingDirection)
-    }
-}
-
-SortButton.propTypes = {
-    onChange: PropTypes.any.isRequired,
-    additionalClassName: PropTypes.any,
-    defaultSortingDirection: PropTypes.oneOf([-1, 1]),
-    label: PropTypes.any,
-    shape: PropTypes.any,
-    sorted: PropTypes.any,
-    sortingDirection: PropTypes.oneOf([-1, 1])
-}
-
-SortButton.defaultProps = {
-    shape: 'pill',
-    defaultSortingDirection: 1
-}
 
 export class SortButtons extends React.Component {
     constructor() {
