c0c9515329ebf0f4bbc577376e8a911aa221421f
Daniel Wiell <daniel.wiell@fao.org>
2023-01-12 07:49:49 +0000

    Forced GWB update.

--- modules/geospatial-toolkit/script/init_gwb.sh
+++ modules/geospatial-toolkit/script/init_gwb.sh
@@ -5,6 +5,7 @@ echo
 echo "**********************"
 echo "*** Installing GWB ***"
 echo "**********************"
+# Last update 2021-01-12
 INSTALLER=gwb_amd64.deb
 wget https://ies-ows.jrc.ec.europa.eu/gtb/GWB/$INSTALLER
 apt-get install -y ./$INSTALLER
