c62dee88abdec7d5c0bce1731c6f466d2a40f816
Luca Paolini <luca.paolini@fao.org>
2023-02-13 15:32:27 +0000

    Cleanup.

--- modules/gui/src/app/home/body/process/recipeAccess.js
+++ modules/gui/src/app/home/body/process/recipeAccess.js
@@ -3,11 +3,11 @@ import {connect} from 'store'
 import {initializeRecipe} from './recipe'
 import {map, of, switchMap, tap} from 'rxjs'
 import {selectFrom} from 'stateUtils'
+import {v4 as uuid} from 'uuid'
 import React from 'react'
 import _ from 'lodash'
 import actionBuilder from 'action-builder'
 import api from 'api'
-import guid from 'guid'
 
 let componentIdsByRecipeId = {}
 
@@ -24,7 +24,7 @@ export const recipeAccess = () =>
         class RecipeAccessHOC extends React.Component {
             constructor(props) {
                 super(props)
-                this.componentId = guid()
+                this.componentId = uuid()
             }
 
             render() {
