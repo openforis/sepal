556d30b258fe27a2a47e55f7beace12c8432be4a
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-03-17 09:21:03 +0100

    install the specific libs before the requirements

--- modules/app-manager/update-app.sh
+++ modules/app-manager/update-app.sh
@@ -65,11 +65,11 @@ function update_venv {
         python3 -m venv $venv_path
         "$venv_path"/bin/python3 -m pip install --cache-dir /root/.cache/pip ipykernel wheel >> "$venv_log_file"
         "$venv_path"/bin/python3 -m pip install --cache-dir /root/.cache/pip --upgrade pip >> "$venv_log_file"
-        "$venv_path"/bin/python3 -m pip install -r "$app_path"/requirements.txt -U --no-cache-dir >> "$venv_log_file"
         "$venv_path"/bin/python3 -m pip install numpy -U --no-cache-dir >> "$venv_log_file"
         "$venv_path"/bin/python3 -m pip install gdal==3.4.3 -U --no-cache-dir >> "$venv_log_file"
         "$venv_path"/bin/python3 -m pip install pyproj==3.4.1 -U --no-cache-dir >> "$venv_log_file"
         "$venv_path"/bin/python3 -m pip install "git+https://github.com/openforis/earthengine-api.git@v0.1.343#egg=earthengine-api&subdirectory=python" -U --no-cache-dir >> "$venv_log_file"
+        "$venv_path"/bin/python3 -m pip install -r "$app_path"/requirements.txt -U --no-cache-dir >> "$venv_log_file"
         if [[ -d $current_venv_path ]] 
         then
             echo "Moving away current venv: $current_venv_path" >> "$venv_log_file"
