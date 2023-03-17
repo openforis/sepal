ed0203c4da52d610a7567fee8146449b79a48f43
Luca Paolini <luca.paolini@fao.org>
2023-03-17 14:51:23 +0000

    Improved Prometheus monitoring.

--- modules/gateway/src/main.js
+++ modules/gateway/src/main.js
@@ -7,10 +7,14 @@ const Redis = require('ioredis')
 const Session = require('express-session')
 const {v4: uuid} = require('uuid')
 const url = require('url')
+const apiMetrics = require('prometheus-api-metrics')
+const RedisSessionStore = require('connect-redis').default
 
 const log = require('#sepal/log').getLogger('gateway')
+
 const {initMessageQueue} = require('#sepal/messageQueue')
 
+const {Auth} = require('./auth')
 const {Proxy} = require('./proxy')
 const {SessionManager} = require('./session')
 const {setRequestUser, getSessionUsername} = require('./user')
@@ -19,10 +23,11 @@ const {usernameTag, urlTag} = require('./tag')
 
 const redis = new Redis(redisUri)
 const userStore = UserStore(redis)
-const RedisSessionStore = require('connect-redis').default
 const sessionStore = new RedisSessionStore({client: redis})
+
 const {messageHandler, logout} = SessionManager(sessionStore, userStore)
-const {proxyEndpoints} = Proxy(userStore)
+const {authMiddleware} = Auth(userStore)
+const {proxyEndpoints} = Proxy(userStore, authMiddleware)
 
 const getSecret = async () => {
     const secret = await redis.get('secret')
@@ -67,6 +72,8 @@ const main = async () => {
     app.use(userStore.userMiddleware)
 
     app.use('/api/user/logout', logout)
+    app.use('/api/gateway/metrics', authMiddleware, apiMetrics({metricsPath: '/api/gateway/metrics'}))
+
     const proxies = proxyEndpoints(app)
     const server = app.listen(port)
 
