1bdddb4714ed9e89bf79a0b718e6a03d6752a9b7
Daniel Wiell <daniel.wiell@fao.org>
2023-03-20 17:01:47 +0000

    Fixed duplicate recipe bug.

--- modules/gui/src/app/home/body/process/processMenu.js
+++ modules/gui/src/app/home/body/process/processMenu.js
@@ -67,7 +67,7 @@ class ProcessMenu extends React.Component {
     }
 
     duplicateRecipe() {
-        const {recipe} = this.propTypes
+        const {recipe} = this.props
         duplicateRecipe(recipe)
     }
 
