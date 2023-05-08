19f7bbcdebc3d8b01a32a6b2bc8f8a462ce5f9e8
Luca Paolini <luca.paolini@fao.org>
2023-05-08 09:57:19 +0200

    Fixed CursorValue disappearing when on position 0.

--- modules/gui/src/app/home/map/paletteLayer.js
+++ modules/gui/src/app/home/map/paletteLayer.js
@@ -147,7 +147,7 @@ class _CursorValue extends React.Component {
         const {position} = this.state
         const clampedValue = _.clamp(value, min, max)
         const formattedValue = formatValue({dataType, value: clampedValue, magnitude})
-        return position ? (
+        return position !== null ? (
             <div
                 className={[styles.cursorValue, styles[dataType || 'default']].join(' ')}
                 style={{'--left': `${position}px`}}>
