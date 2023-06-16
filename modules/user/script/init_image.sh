ec250f8efc7226aaddb1b11059651b6a54504abc
Daniel Wiell <daniel.wiell@fao.org>
2023-06-16 08:25:43 +0000

    Removed sssd from user module.

--- modules/user/script/init_image.sh
+++ modules/user/script/init_image.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
+# apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -qq -y \
+#     curl \
+#     jq \
+#     ldapscripts \
+#     libpam-sss \
+#     libnss-sss \
+#     libnss-ldap \
+#     openssh-client \
+#     sssd \
+#     sssd-tools
+
+# echo "initgroups: files sss" >> /etc/nsswitch.conf
+
 apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get install -qq -y \
     curl \
     jq \
     ldapscripts \
-    libpam-sss \
-    libnss-sss \
-    libnss-ldap \
     openssh-client \
-    sssd \
-    sssd-tools
-
-echo "initgroups: files sss" >> /etc/nsswitch.conf
+    
