49f6965f7b2dcceda1149fcd4605652c29cb9bb9
Luca Paolini <luca.paolini@fao.org>
2023-08-28 08:40:10 +0000

    Implemented missing pass-through FormCombo props.

--- modules/gui/src/widget/form/combo.js
+++ modules/gui/src/widget/form/combo.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 export const FormCombo = ({
-    input, options, additionalButtons, alignment, allowClear, autoFocus, autoOpen, busyMessage, className, disabled,
-    errorMessage, inputClassName, keyboard, label, matchGroups, optionsClassName, optionTooltipPlacement, placeholder, placement,
+    input, options, additionalButtons, alignment, allowClear, autoFocus, autoOpen, border, busyMessage, className, disabled,
+    errorMessage, inputClassName, keyboard, label, labelButtons, matchGroups, optionsClassName, optionTooltipPlacement, placeholder, placement,
     readOnly, stayOpenOnSelect, tooltip, tooltipPlacement, onCancel, onChange, onFilterChange
 }) => {
     const onChangeHandler = option => {
@@ -23,6 +23,7 @@ export const FormCombo = ({
             allowClear={allowClear}
             autoFocus={autoFocus}
             autoOpen={autoOpen}
+            border={border}
             busyMessage={busyMessage}
             className={className}
             disabled={disabled}
@@ -30,6 +31,7 @@ export const FormCombo = ({
             inputClassName={inputClassName}
             keyboard={keyboard}
             label={label}
+            labelButtons={labelButtons}
             matchGroups={matchGroups}
             optionsClassName={optionsClassName}
             optionTooltipPlacement={optionTooltipPlacement}
@@ -54,13 +56,15 @@ FormCombo.propTypes = {
     allowClear: PropTypes.any,
     autoFocus: PropTypes.any,
     autoOpen: PropTypes.any,
+    border: PropTypes.any,
     busyMessage: PropTypes.any,
     className: PropTypes.string,
     disabled: PropTypes.any,
     errorMessage: PropTypes.any,
     inputClassName: PropTypes.string,
     keyboard: PropTypes.any,
-    label: PropTypes.string,
+    label: PropTypes.any,
+    labelButtons: PropTypes.any,
     matchGroups: PropTypes.any,
     optionsClassName: PropTypes.string,
     optionTooltipPlacement: PropTypes.string,
