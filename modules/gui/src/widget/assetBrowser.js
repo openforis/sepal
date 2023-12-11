e3c6333f0d6e31d0904398fcb2bc870cd155fdca
Luca Paolini <luca.paolini@fao.org>
2023-12-11 17:07:01 +0000

    Refactoring.
    Merged AssetLocation into AssetBrowser.

--- modules/gui/src/widget/assetBrowser.js
+++ modules/gui/src/widget/assetBrowser.js
@@ -1,21 +1,44 @@
+import {AssetItem} from './assetItem'
+import {Button} from './button'
 import {Form, withForm} from 'widget/form/form'
+import {Input} from './input'
 import {Panel} from './panel/panel'
+import {ScrollableList} from './list'
+import {SearchBox} from './searchBox'
+import {Tree} from 'tree'
+import {Widget} from './widget'
 import {compose} from 'compose'
+import {isEqual} from 'hash'
 import {msg} from 'translate'
 import {withActivatable} from './activation/activatable'
 import {withAssets} from './assets'
 import PropTypes from 'prop-types'
 import React from 'react'
+import _ from 'lodash'
 import styles from './assetBrowser.module.css'
 
 const fields = {
-    assetLocation: new Form.Field().notBlank()
+    assetId: new Form.Field().notBlank()
 }
+
+const isFolder = type => type === 'Folder'
 
 class _AssetBrowser extends React.Component {
     constructor(props) {
         super(props)
+        this.onInputChange = this.onInputChange.bind(this)
+        this.onFolderSelect = this.onFolderSelect.bind(this)
+        this.onAssetSelect = this.onAssetSelect.bind(this)
+        this.onFilterChange = this.onFilterChange.bind(this)
         this.onApply = this.onApply.bind(this)
+    }
+
+    input = React.createRef()
+
+    state = {
+        filter: '',
+        filteredTree: Tree.createNode(),
+        selectedFolderId: null
     }
 
     render() {
@@ -29,44 +52,212 @@ class _AssetBrowser extends React.Component {
                 modal>
                 <Panel.Header title={msg('asset.browser.title')}/>
                 <Panel.Content scrollable={false}>
-                    {this.renderForm()}
+                    <Widget
+                        label={msg('asset.browser.asset')}
+                        layout='vertical-fill'
+                        spacing='compact'>
+                        {this.renderInput()}
+                        {this.renderFolderTree()}
+                        {this.renderFilter()}
+                    </Widget>
                 </Panel.Content>
                 <Form.PanelButtons/>
             </Form.Panel>
         )
     }
 
-    renderForm() {
-        const {inputs: {assetLocation}} = this.props
+    renderInput() {
+        const {form, inputs: {assetId}} = this.props
         return (
-            <Form.AssetLocation
-                label={msg('asset.browser.asset')}
-                input={assetLocation}
+            <Input
+                ref={this.input}
+                errorMessage={form.getErrorMessage(assetId)}
+                value={assetId.value}
+                onChange={this.onInputChange}
+            />
+
+        )
+    }
+
+    renderFolderTree() {
+        const {filteredTree, selectedFolderId} = this.state
+        const options = this.getFolderTreeOptions(filteredTree)
+        return (
+            <ScrollableList
+                options={options}
+                selectedValue={selectedFolderId}
+                onSelect={this.onFolderSelect}
+                tooltipPlacement='bottomRight'
+                keyboard={false}
             />
         )
     }
 
-    onApply() {
-        const {inputs: {assetLocation}, onChange, activatable: {deactivate}} = this.props
-        const assetId = assetLocation.value
-        onChange && onChange(assetId)
-        deactivate()
+    renderFilter() {
+        return (
+            <SearchBox
+                placeholder={msg('asset.browser.filter.placeholder')}
+                width='fill'
+                onSearchValue={this.onFilterChange}
+            />
+        )
     }
 
-    componentDidMount() {
-        this.update()
+    renderFolderAssets(node) {
+        const options = this.getFolderAssetsOptions(node)
+        return options.length ? (
+            ({close}) =>
+                <ScrollableList
+                    className={styles.folderAssets}
+                    options={options}
+                    keyboard={false}
+                    onSelect={asset => {
+                        this.onAssetSelect(asset)
+                        close()
+                    }}
+                />
+        ) : null
     }
 
-    componentDidUpdate({activatable: {activatables: {assetBrowser: {activationProps: {assetId: prevAssetId}}}}}) {
-        const {activatable: {activatables: {assetBrowser: {activationProps: {assetId}}}}} = this.props
-        if (assetId !== prevAssetId) {
-            this.update()
+    renderItem({id, type, depth, tooltip}) {
+        const showTailOnly = depth > 0 || !isFolder(type)
+        return (
+            <AssetItem
+                key={id}
+                id={id}
+                type={type}
+                tail={showTailOnly}
+                inlineComponents={[
+                    this.renderFolderAssetsButton(tooltip)
+                ]}
+            />
+        )
+    }
+
+    renderFolderAssetsButton(tooltip) {
+        return tooltip ? (
+            <Button
+                key='link'
+                chromeless
+                air='none'
+                size='small'
+                icon='bars'
+                dimmed
+                tooltip={tooltip}
+                tooltipPlacement='bottomLeft'
+            />
+        ) : null
+    }
+
+    getFolderTreeOptions({items} = {}, depth = 0) {
+        return _(items)
+            .pickBy(node => isFolder(node.props.type))
+            .map((node, id) => [
+                this.getItemOption({id, type: node.props.type, depth, node}),
+                ...this.getFolderTreeOptions(node, depth + 1)
+            ])
+            .flatten()
+            .value()
+    }
+
+    getFolderAssetsOptions({items} = {}) {
+        return _(items)
+            .pickBy(node => !isFolder(node.props.type))
+            .map((node, id) => this.getItemOption({id, type: node.props.type}))
+            .value()
+    }
+
+    getItemOption({id, type, depth, node}) {
+        const tooltip = this.renderFolderAssets(node)
+        const render = () => this.renderItem({id, type, depth, tooltip})
+        return {
+            label: id,
+            value: id,
+            indent: depth,
+            render
         }
     }
 
-    update() {
-        const {activatable: {activatables: {assetBrowser: {activationProps: {assetId}}}}, inputs: {assetLocation}} = this.props
-        assetLocation.set(assetId)
+    getFilter() {
+        const {filter} = this.state
+        return id => id ? id.toLowerCase().includes(filter.toLowerCase()) : true
+    }
+    
+    getClosestFolderId(assetId) {
+        const {assets: {userAssets}} = this.props
+        const folder = userAssets.find(({id, type}) => isFolder(type) && assetId === id)
+        if (folder) {
+            return folder.id
+        } else {
+            const parentId = this.getParentFolderId(assetId)
+            return parentId.length
+                ? this.getClosestFolderId(parentId)
+                : ''
+        }
+    }
+
+    getParentFolderId(assetId) {
+        const index = assetId.lastIndexOf('/')
+        return assetId.substr(0, index)
+    }
+
+    getCurrentAssetName() {
+        const {inputs: {assetId}} = this.props
+        const index = assetId.value.lastIndexOf('/')
+        return assetId.value.substr(index + 1)
+    }
+
+    onInputChange(e) {
+        const value = e.target.value
+        this.updateAsset(value)
+    }
+
+    onFolderSelect({value: folderAssetId}) {
+        this.updateAsset(`${folderAssetId}/${this.getCurrentAssetName()}`)
+        this.focusInput()
+    }
+    
+    onAssetSelect({value: assetId}) {
+        this.updateAsset(assetId)
+        this.focusInput()
+    }
+
+    onFilterChange(filter) {
+        this.setState({filter})
+    }
+
+    onApply() {
+        const {inputs: {assetId}, onChange, activatable: {deactivate}} = this.props
+        onChange && onChange(assetId.value)
+        deactivate()
+    }
+
+    focusInput() {
+        this.input?.current.focus()
+    }
+
+    updateTree(tree) {
+        this.setState({filteredTree: Tree.filter(tree, this.getFilter())})
+    }
+
+    updateAsset(value) {
+        const {inputs: {assetId}} = this.props
+        assetId.set(value)
+        this.setState({selectedFolderId: this.getClosestFolderId(value)})
+    }
+
+    componentDidMount() {
+        const {assets: {tree}, activatable: {activatables: {assetBrowser: {activationProps: {assetId}}}}} = this.props
+        this.updateTree(tree)
+        this.updateAsset(assetId)
+    }
+
+    componentDidUpdate({assets: {tree: prevTree}}, {filter: prevFilter}) {
+        const {assets: {tree}} = this.props
+        const {filter} = this.state
+        if (filter !== prevFilter || !isEqual(prevTree, tree)) {
+            this.updateTree(tree)
+        }
     }
 }
 
@@ -83,5 +274,5 @@ export const AssetBrowser = compose(
 
 AssetBrowser.propTypes = {
     onChange: PropTypes.func.isRequired,
-    assetLocation: PropTypes.string
+    assetId: PropTypes.string
 }
