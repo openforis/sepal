48c2f8e23141ba2e8ebc4d6ab02a7d7c94b4c117
Luca Paolini <luca.paolini@fao.org>
2024-02-17 09:43:34 +0000

    Improved applyDefaults.
    It now discards any nil values before merging to defaults.

--- modules/gui/src/utils.js
+++ modules/gui/src/utils.js
@@ -1,4 +1,8 @@
 import _ from 'lodash'
 
-export const applyDefaults = (defaults, ...src) =>
-    _.merge(_.cloneDeep(defaults), ...src)
+export const applyDefaults = (defaults, ...src) => {
+    const objs = _.map(src,
+        obj => _.pickBy(obj, prop => !_.isNil(prop))
+    )
+    return _.merge(_.cloneDeep(defaults), ...objs)
+}
