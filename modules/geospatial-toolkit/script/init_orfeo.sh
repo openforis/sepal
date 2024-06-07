a07a6ff7be8b5b539732885c4a7d665efacc9987
Daniel Wiell <daniel.wiell@fao.org>
2024-06-07 13:27:13 +0000

    Made OTB available from python.

--- modules/geospatial-toolkit/script/init_orfeo.sh
+++ modules/geospatial-toolkit/script/init_orfeo.sh
@@ -10,4 +10,4 @@ otb=OTB-9.0.0-Linux.tar.gz
 curl https://www.orfeo-toolbox.org/packages/archives/OTB/$otb -o /tmp/$otb
 mkdir -p /usr/local/lib/orfeo
 tar xf /tmp/$otb --one-top-level=/usr/local/lib/orfeo
-(cd /usr/local/lib/orfeo; source ./otbenv.profile)
+(cd /usr/local/lib/orfeo; source ./otbenv.profile; sh recompile_bindings.sh)
