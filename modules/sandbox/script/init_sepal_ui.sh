627698d1a207a1f34249c00b290ee1233af43bab
dfguerrerom <dfgm2006@gmail.com>
2024-08-30 07:54:59 -0700

    update sepal_ui packages

--- modules/sandbox/script/init_sepal_ui.sh
+++ modules/sandbox/script/init_sepal_ui.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-python3 -m pip install sepal-ui
+python3 -m pip install "sepal-ui==2.20.0"
 
 # Update the voila-sepal-ui package
 
-python3 -m pip install voila-sepal-ui
+python3 -m pip install "voila-sepal-ui==0.5.2"
