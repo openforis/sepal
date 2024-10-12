a58e5ed6867b9cc3dfd6fd39c0caf682989dc03d
dfguerrerom <dfgm2006@gmail.com>
2024-10-12 18:57:28 +0200

    jupyter doesnt require to be build

--- modules/sandbox/script/init_jupyter.sh
+++ modules/sandbox/script/init_jupyter.sh
@@ -25,7 +25,6 @@ apt-get install -y libzmq3-dev
 /usr/bin/python3 -m pip install "geemap<=v0.34.2"
 /usr/bin/python3 -m pip install "ipecharts>=1.0.8"
 /usr/bin/python3 -m pip install sidecar
-/usr/bin/python3 -m pip install starlette
 
 git clone https://github.com/ipython-contrib/jupyter_contrib_nbextensions.git
 /usr/bin/python3 -m pip install jupyter_latex_envs # Required for jupyter_contrib_nbextensions
@@ -35,12 +34,4 @@ git clone https://github.com/ipython-contrib/jupyter_contrib_nbextensions.git
 /usr/bin/python3 /usr/local/bin/jupyter nbextensions_configurator enable
 /usr/bin/python3 /usr/local/bin/jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
-/usr/bin/python3 -m pip install voila # https://github.com/trungleduc/ipecharts/issues/5
-/usr/bin/python3 /usr/local/bin/jupyter nbextension install voila --sys-prefix --py
-/usr/bin/python3 /usr/local/bin/jupyter nbextension enable voila --sys-prefix --py
-/usr/bin/python3 /usr/local/bin/jupyter serverextension enable voila --sys-prefix
-
-# [HACK] Manually installing de-indent, otherwise jupyter lab build fails.
-/usr/bin/python3 /usr/local/bin/jupyter lab build || npm install --prefix /usr/local/share/jupyter/lab/staging de-indent
-rm -f /usr/local/share/jupyter/lab/staging/package-lock.json /usr/local/share/jupyter/lab/staging/yarn.lock
-/usr/bin/python3 /usr/local/bin/jupyter lab build
+/usr/bin/python3 -m pip install "git+https://github.com/dfguerrerom/voila.git@custom-labextension"
