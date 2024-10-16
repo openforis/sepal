167284fcec9982514509699020cdbcfd57695d1b
Daniel Wiell <daniel.wiell@fao.org>
2024-10-16 08:42:22 +0000

    Ensured code editor autocompletion handles enter and escape.

--- modules/gui/src/widget/codeEditor/codeEditor.jsx
+++ modules/gui/src/widget/codeEditor/codeEditor.jsx
@@ -1,4 +1,4 @@
-import {autocompletion, completionKeymap} from '@codemirror/autocomplete'
+import {autocompletion, completionKeymap, completionStatus} from '@codemirror/autocomplete'
 import {javascript} from '@codemirror/lang-javascript'
 import {EditorState} from '@codemirror/state'
 import {EditorView, keymap} from '@codemirror/view'
@@ -6,17 +6,68 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import {boysAndGirls as theme} from 'thememirror'
 
+import {Keybinding} from '../keybinding'
+
 export class CodeEditor extends React.Component {
-    editorRef = React.createRef()
+    state = {show: false, completing: false}
+
+    constructor(props) {
+        super(props)
+        this.onKeyPress = this.onKeyPress.bind(this)
+        this.setupEditor = this.setupEditor.bind(this)
+    }
 
     render() {
-        return (
-            <div ref={this.editorRef}/>
-        )
+        const {show} = this.state
+        // [HACK] Defer rendering one cycle, to ensure keybindings have priority
+        return show
+            ? (
+                <Keybinding keymap={{
+                    Enter: this.onKeyPress,
+                    Escape: this.onKeyPress
+                }}>
+                    <div ref={this.setupEditor}/>
+                </Keybinding>
+            )
+            : null
     }
 
     componentDidMount() {
+        this.setState({show: true})
+    }
+
+    componentWillUnmount() {
+        this.view.destroy()
+    }
+
+    onKeyPress() {
+        const {completing} = this.state
+        // Disable all keybinding when we're completing
+        // If we're not completing, hand keybinding over to another component
+        return completing ? null : false
+    }
+
+    setupEditor(editorElement) {
         const {autoComplete, input} = this.props
+
+        const updateListener = EditorView.updateListener.of(
+            update => {
+                const {onChange} = this.props
+                // [HACK] Give time for key handlers to react on previous completing state
+                // before updating it
+                setImmediate(() =>
+                    this.setState(({completing: prevCompleting}) => {
+                        const completing = !!completionStatus(this.view.state)
+                        return completing === prevCompleting ? null : {completing}
+                    })
+                )
+                const value = update.state.doc.text.join('\n')
+                if (input.value !== value) {
+                    input.set(value)
+                    onChange && onChange(value)
+                }
+            }
+        )
 
         const state = EditorState.create({
             doc: input.value,
@@ -26,22 +77,20 @@ export class CodeEditor extends React.Component {
                 javascript(),
                 autocompletion({
                     override: [autoComplete]
-                })
+                }),
+                updateListener
             ]
         })
         
         this.view = new EditorView({
-            parent: this.editorRef.current,
+            parent: editorElement,
             state: state
         })
-    }
-
-    componentWillUnmount() {
-        this.view.destroy()
     }
 }
 
 CodeEditor.propTypes = {
     autoComplete: PropTypes.func.isRequired,
-    input: PropTypes.object.isRequired
+    input: PropTypes.object.isRequired,
+    onChange: PropTypes.func
 }
