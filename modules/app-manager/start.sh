b9f3c28c0c9b656c7e8735bbafe437412122eadf
Daniel Wiell <daniel.wiell@fao.org>
2024-10-24 12:22:26 +0000

    Ensured voila labextensions is available in app-manager.

--- modules/app-manager/start.sh
+++ modules/app-manager/start.sh
@@ -4,7 +4,8 @@ sudo mkdir -p /usr/local/share/jupyter/kernels
 sudo mkdir -p /usr/local/share/jupyter/log
 
 sudo cp -rn /etc/sepal/app-manager/kernels-templates/* /usr/local/share/jupyter/current-kernels
-
+# [HACK] Ensures /usr/local/share/jupyter/voila is available when docker volume is shadowing
+sudo ln -sf /usr/local/share/voila /usr/local/share/jupyter/voila
 source /home/node/.nvm/nvm.sh 
 
 if [[ "${DEPLOY_ENVIRONMENT}" == "DEV" ]]
