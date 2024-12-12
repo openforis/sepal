abebe05c284dfebd85ecffdd1781864308e12813
Luca Paolini <luca.paolini@fao.org>
2024-12-12 13:13:53 +0000

    Implemented gee API for renaming an asset or folder.

---
+++ modules/gee/src/jobs/ee/asset/rename.js
@@ -0,0 +1,13 @@
+const {job} = require('#gee/jobs/job')
+
+const worker$ = ({sourceId, destinationId}) => {
+    const ee = require('#sepal/ee')
+    const _ = require('lodash')
+    return ee.renameAsset$(sourceId, destinationId)
+}
+
+module.exports = job({
+    jobName: 'EE rename asset',
+    jobPath: __filename,
+    worker$
+})
