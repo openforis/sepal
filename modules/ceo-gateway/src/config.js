dbb41f62b3af00f4f9e8d78a8a5e13ddaa8bec02
dfguerrerom <dfgm2006@gmail.com>
2024-12-19 16:10:24 +0100

    Reverted CEO_URL as environment variable

--- modules/ceo-gateway/src/config.js
+++ modules/ceo-gateway/src/config.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.ceo = {}
 
-config.ceo.url = 'https://app.collect.earth/'
+config.ceo.url = process.env.CEO_URL || 'https://app.collect.earth/'
 config.ceo.username = process.env.CEO_USERNAME || 'admin@openforis.org'
 config.ceo.password = process.env.CEO_PASSWORD || ''
 config.ceo.institutionId = process.env.CEO_INSTITUTION_ID || '1'
