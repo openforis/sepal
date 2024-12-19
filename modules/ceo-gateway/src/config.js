c40039eb258b6405e5184c9e52bdd98ff6d2d1ec
dfguerrerom <dfgm2006@gmail.com>
2024-12-19 16:32:14 +0100

    Removed harcoded fallback

--- modules/ceo-gateway/src/config.js
+++ modules/ceo-gateway/src/config.js
@@ -2,9 +2,9 @@ var config = {}
 
 config.ceo = {}
 
-config.ceo.url = process.env.CEO_URL || 'https://app.collect.earth/'
-config.ceo.username = process.env.CEO_USERNAME || 'admin@openforis.org'
-config.ceo.password = process.env.CEO_PASSWORD || ''
-config.ceo.institutionId = process.env.CEO_INSTITUTION_ID || '1'
+config.ceo.url = process.env.CEO_URL
+config.ceo.username = process.env.CEO_USERNAME
+config.ceo.password = process.env.CEO_PASSWORD
+config.ceo.institutionId = process.env.CEO_INSTITUTION_ID
 
 module.exports = config
