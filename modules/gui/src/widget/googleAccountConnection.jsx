96a5c438ac643eadce211c671daa7cb00820884b
Luca Paolini <luca.paolini@fao.org>
2025-02-11 18:33:51 +0000

    Implemented GoogleAccountConnection component.

---
+++ modules/gui/src/widget/googleAccountConnection.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {filter, switchMap} from 'rxjs'
+
+import {event$} from '~/api/ws'
+import {userDetailsHint} from '~/app/home/user/userDetails'
+import {compose} from '~/compose'
+import {withSubscriptions} from '~/subscription'
+import {msg} from '~/translate'
+import {loadUser$} from '~/user'
+
+import {Notifications} from './notifications'
+
+class _GoogleAccountConnection extends React.Component {
+    render() {
+        return null
+    }
+
+    componentDidMount() {
+        const {addSubscription} = this.props
+        addSubscription(
+            event$.pipe(
+                filter(({disconnectGoogleAccount}) => disconnectGoogleAccount),
+                switchMap(() => loadUser$())
+            ).subscribe(
+                () => this.notify()
+            )
+        )
+    }
+
+    notify() {
+        userDetailsHint(true)
+        Notifications.error({
+            title: msg('user.googleAccount.revoked.title'),
+            message: msg('user.googleAccount.revoked.message'),
+            timeout: 0,
+            onDismiss: () => userDetailsHint(false)
+        })
+    }
+}
+
+export const GoogleAccountConnection = compose(
+    _GoogleAccountConnection,
+    withSubscriptions()
+)
