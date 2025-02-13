10a5d88d98244b72193dfefc5b0039d28093d76b
dfguerrerom <dfgm2006@gmail.com>
2025-02-12 20:00:22 -0500

    added listFiles endpoint to user-files module

--- modules/user-files/src/filesystem.js
+++ modules/user-files/src/filesystem.js
@@ -1,6 +1,6 @@
 const Path = require('path')
 const {createReadStream, realpathSync} = require('fs')
-const {stat} = require('fs/promises')
+const {stat, readdir} = require('fs/promises')
 const log = require('#sepal/log').getLogger('filesystem')
 
 const resolvePath = (baseDir, path) => {
@@ -47,4 +47,51 @@ const download = async (homeDir, ctx) => {
     }
 }
 
-module.exports = {resolvePath, download}
+const listFiles = async (homeDir, ctx) => {
+    const sepalUser = getSepalUser(ctx.request)
+    if (sepalUser) {
+        const username = sepalUser.username
+        const {absolutePath: userHomeDir} = resolvePath(homeDir, username)
+        const path = ctx.query.path || '.'
+        const {absolutePath} = resolvePath(userHomeDir, path)
+        
+        try {
+            const stats = await stat(absolutePath)
+            if (stats.isDirectory()) {
+                const files = await readdir(absolutePath)
+                const fileDetails = await Promise.all(
+                    files.map(async file => {
+                        const filePath = Path.join(absolutePath, file)
+                        const fileStats = await stat(filePath)
+                        return {
+                            name: file,
+                            path: Path.relative(userHomeDir, filePath),
+                            type: fileStats.isDirectory() ? 'directory' : 'file',
+                            size: fileStats.size,
+                            modifiedTime: fileStats.mtime
+                        }
+                    })
+                )
+                
+                log.debug(() => `Listing directory: ${absolutePath}`)
+                ctx.body = {
+                    path: Path.relative(userHomeDir, absolutePath),
+                    files: fileDetails
+                }
+            } else {
+                log.warn(() => `Cannot list non-directory: ${absolutePath}`)
+                ctx.response.status = 404
+            }
+        } catch (error) {
+            log.error(() => `Error listing directory: ${error.message}`)
+            ctx.response.status = 500
+            ctx.body = {error: 'Failed to list directory contents'}
+        }
+    } else {
+        log.warn(() => 'Cannot list files: unauthenticated user')
+        ctx.response.status = 401
+    }
+}
+
+module.exports = {resolvePath, download, listFiles}
+
