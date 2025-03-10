66f304c81d1c0c9c9639c88c589c849b2c4eeca5
Luca Paolini <luca.paolini@fao.org>
2025-03-10 14:07:50 +0000

    Fixed db migration.

--- modules/sepal-server/src/main/resources/sql/sdms/V13_0__add_indexes.sql
+++ modules/sepal-server/src/main/resources/sql/sdms/V13_0__add_indexes.sql
@@ -1,3 +1,10 @@
+-- drop unneeded indexes
+
+ALTER TABLE `sdms`.`user_monthly_storage`
+DROP INDEX `idx_user_monthly_storage_1`;
+
+-- create new indexes
+
 ALTER TABLE `sdms`.`task`
 ADD INDEX `idx_task_1` (`state`, `update_time`) USING BTREE;
 
@@ -11,7 +18,6 @@ ALTER TABLE `sdms`.`task`
 ADD INDEX `idx_task_4` (`username`, `state`) USING BTREE;
 
 ALTER TABLE `sdms`.`worker_session`
-DROP INDEX `idx_worker_session_1`,
 ADD INDEX `idx_worker_session_1` (`username`, `worker_type`, `state`, `instance_type`) USING BTREE;
 
 ALTER TABLE `sdms`.`worker_session`
@@ -25,9 +31,6 @@ ADD INDEX `idx_worker_session_4` (`instance_id`, `state`) USING BTREE;
 
 ALTER TABLE `sdms`.`worker_session`
 ADD INDEX `idx_worker_session_5` (`username`, `creation_time`, `update_time`) USING BTREE;
-
-ALTER TABLE `sdms`.`user_monthly_storage`
-DROP INDEX `idx_user_monthly_storage_1`;
 
 ALTER TABLE `sdms`.`user_budget`
 ADD INDEX `idx_user_budget_1` (`username`) USING BTREE;
--- modules/user/src/main/resources/sql/sepal_user/V11_0__add_indexes.sql
+++ modules/user/src/main/resources/sql/sepal_user/V11_0__add_indexes.sql
@@ -1,13 +1,21 @@
+-- drop unneeded indexes
+
+ALTER TABLE `sepal_user`.`sepal_user`
+DROP INDEX `username`;
+
+ALTER TABLE `sepal_user`.`sepal_user`
+DROP INDEX `email`;
+
+-- create new indexes
+
 ALTER TABLE `sepal_user`.`sepal_user`
 ADD INDEX `idx_sepal_user_1` (`creation_time`) USING BTREE;
 
 ALTER TABLE `sepal_user`.`sepal_user`
-DROP INDEX `username`,
 ADD UNIQUE `idx_sepal_user_2` (`username`) USING BTREE;
 
 ALTER TABLE `sepal_user`.`sepal_user`
-DROP INDEX `email`,
-ADD UNIQUE `idx_sepal_user_3` (`email`) USING BTREE;
+ADD INDEX `idx_sepal_user_3` (`email`) USING BTREE;
 
 ALTER TABLE `sepal_user`.`sepal_user`
 ADD UNIQUE `idx_sepal_user_4` (`token`) USING BTREE;
