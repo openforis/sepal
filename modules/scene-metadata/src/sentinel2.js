d4a6f82c83ebd9724bbc7fa18e475f5f0cec66f7
Luca Paolini <luca.paolini@fao.org>
2025-10-10 19:11:42 +0000

    Disabled old type Sentinel-2 product URI.

--- modules/scene-metadata/src/sentinel2.js
+++ modules/scene-metadata/src/sentinel2.js
@@ -1,7 +1,7 @@
 const {getDayOfYear} = require('date-fns')
 const log = require('#sepal/log').getLogger('sentinel2')
 
-const PRODUCT_URI_MATCHER = /^S2[ABC]_(?:(?:OPER_PRD_)?MSI(L1C|L2A)_(\d{8}T\d{6})_N\d{4}_R\d{3}_(T\d{2}[A-Z]{3})_(\d{8}T\d{6})(?:\.SAFE)?|OPER_PRD_MSI(L1C|L2A)_PDMC_(\d{8}T\d{6})_R\d{3}_V(\d{8}T\d{6})_(\d{8}T\d{6}))$/
+const PRODUCT_URI_MATCHER = /^S2[ABC]_(?:(?:OPER_PRD_)?MSI(L1C|L2A)_(\d{8}T\d{6})_N\d{4}_R\d{3}_(T\d{2}[A-Z]{3})_(\d{8}T\d{6})(?:\.SAFE)?)$/
 
 const getId = productUri => {
     const match = productUri.match(PRODUCT_URI_MATCHER)
