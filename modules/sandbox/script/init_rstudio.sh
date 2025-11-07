a44b5ec72860ee5347564ca930349a1802e100a0
Daniel Wiell <daniel.wiell@fao.org>
2025-11-07 13:33:08 +0000

    Updated rstudio and shiny servers.

--- modules/sandbox/script/init_rstudio.sh
+++ modules/sandbox/script/init_rstudio.sh
@@ -5,7 +5,7 @@ echo
 echo "*********************************"
 echo "*** Installing RStudio Server ***"
 echo "*********************************"
-rstudio=rstudio-server-2024.12.1-563-amd64.deb
+rstudio=rstudio-server-2025.09.2-418-amd64.deb
 wget -nv https://download2.rstudio.org/server/jammy/amd64/$rstudio
 gdebi -n $rstudio
 printf '%s\n' \
