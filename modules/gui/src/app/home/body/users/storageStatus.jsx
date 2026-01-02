940d3cc0f1fc68aa808ffc2fa6e245813add573b
Luca Paolini <luca.paolini@fao.org>
2026-01-02 14:03:45 +0000

    Improved storage status display.

--- modules/gui/src/app/home/body/users/storageStatus.jsx
+++ modules/gui/src/app/home/body/users/storageStatus.jsx
@@ -2,73 +2,71 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 import {msg} from '~/translate'
-import {Button} from '~/widget/button'
 import {Icon} from '~/widget/icon'
 
 export class StorageStatus extends React.Component {
     static ACTIVE = 'ACTIVE'
-    static INACTIVE = 'INACTIVE'
+    static INACTIVE_LOW = 'INACTIVE_LOW'
+    static INACTIVE_HIGH = 'INACTIVE_HIGH'
+    static INACTIVE_UNKNOWN = 'INACTIVE_UNKNOWN'
     static NOTIFIED = 'NOTIFIED'
-    static ERASED = 'ERASED'
-
-    // static isActive = status =>
-    //     status === StorageStatus.ACTIVE
-
-    // static isInactive = status =>
-    //     status === StorageStatus.INACTIVE
-
-    // static isNotified = status =>
-    //     status === StorageStatus.NOTIFIED
-
-    // static isErased = status =>
-    //     status === StorageStatus.ERASED
+    static PURGED = 'PURGED'
 
     render() {
         const {status} = this.props
         return (
-            <Button
-                chromeless
-                shape='none'
-                air='none'
-                icon={this.getIcon(status)}
-                label={status ? msg(`user.storageStatus.${status}`).toUpperCase() : null}
-            />
+            <div style={{display: 'flex', alignItems: 'center', gap: '0.25rem'}}>
+                {this.getIcon(status)}
+                {status ? msg(`user.storageStatus.${status}`) : null}
+            </div>
         )
     }
 
     getIcon(status) {
         switch(status) {
             case StorageStatus.ACTIVE:
-                return this.getActiveIcon()
-            case StorageStatus.INACTIVE:
-                return this.getInactiveIcon()
+                return this.getOkIcon()
+            case StorageStatus.INACTIVE_LOW:
+                return this.getInactiveLowIcon()
+            case StorageStatus.INACTIVE_HIGH:
+                return this.getInactiveHighIcon()
+            case StorageStatus.INACTIVE_UNKNOWN:
+                return this.getInactiveUnknownIcon()
             case StorageStatus.NOTIFIED:
                 return this.getNotifiedIcon()
-            case StorageStatus.ERASED:
-                return this.getErasedIcon()
+            case StorageStatus.PURGED:
+                return this.getPurgedIcon()
             default:
                 return this.getUnknownIcon()
         }
     }
 
-    getActiveIcon() {
+    getOkIcon() {
         return <Icon name='check' variant='success'/>
     }
 
-    getInactiveIcon() {
-        return <Icon name='clock' type='regular' variant='warning'/>
+    getInactiveLowIcon() {
+        return <Icon name='arrow-down' variant='success'/>
+    }
+
+    getInactiveHighIcon() {
+        return <Icon name='bell' variant='warning'/>
+    }
+
+    getInactiveUnknownIcon() {
+        return <Icon name='hourglass-half' variant='success'/>
     }
 
     getNotifiedIcon() {
-        return <Icon name='clock' type='regular' variant='error'/>
+        return <Icon name='triangle-exclamation' variant='warning'/>
     }
 
-    getErasedIcon() {
-        return <Icon name='trash' variant='info'/>
+    getPurgedIcon() {
+        return <Icon name='trash-can' type='regular' variant='info'/>
     }
 
     getUnknownIcon() {
-        return <Icon name='question' variant='normal'/>
+        return null
     }
 }
 
