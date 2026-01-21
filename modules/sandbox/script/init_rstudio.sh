979fb64ee128bae5ccb1b494ad94fa610c815361
Daniel Wiell <daniel.wiell@fao.org>
2026-01-21 09:44:11 +0000

    Updated RStudio Server version.

--- modules/sandbox/script/init_rstudio.sh
+++ modules/sandbox/script/init_rstudio.sh
@@ -5,7 +5,7 @@ echo
 echo "*********************************"
 echo "*** Installing RStudio Server ***"
 echo "*********************************"
-rstudio=rstudio-server-2025.09.2-418-amd64.deb
+rstudio=rstudio-server-2026.01.0-392-amd64.deb
 wget -nv https://download2.rstudio.org/server/jammy/amd64/$rstudio
 gdebi -n $rstudio
 printf '%s\n' \
