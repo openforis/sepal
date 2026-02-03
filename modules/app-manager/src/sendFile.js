28cc64e2f95c189cf5282fb4817b3a4a7b435b4c
Luca Paolini <luca.paolini@fao.org>
2026-02-03 13:10:18 +0000

    Fixed app-manager if-modified-since logic.

--- modules/app-manager/src/sendFile.js
+++ modules/app-manager/src/sendFile.js
@@ -10,7 +10,7 @@ const sendFile = (ctx, path) =>
             const lastModified = stats.mtime
             ctx.set('Last-Modified', lastModified.toUTCString())
             ctx.set('Cache-Control', 'max-age=10, must-revalidate')
-            if (ifModifiedSince && moment(ifModifiedSince).isBefore(moment(lastModified))) {
+            if (ifModifiedSince && moment(ifModifiedSince).isSame(moment(lastModified))) {
                 ctx.response.status = 304
             } else {
                 ctx.response.set('content-type', mime.lookup(path))
