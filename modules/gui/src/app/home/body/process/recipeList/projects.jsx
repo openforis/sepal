5da842e48fed8b2245a1306a16dff161512d5aa4
Luca Paolini <luca.paolini@fao.org>
2026-03-18 07:45:08 +0000

    Improved project panel.
    Added search and keyboard navigation.

--- modules/gui/src/app/home/body/process/recipeList/projects.jsx
+++ modules/gui/src/app/home/body/process/recipeList/projects.jsx
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+import memoizeOne from 'memoize-one'
 import PropTypes from 'prop-types'
 import React from 'react'
 import {map, switchMap, tap} from 'rxjs'
@@ -7,15 +9,18 @@ import api from '~/apiRegistry'
 import {compose} from '~/compose'
 import {connect} from '~/connect'
 import {select} from '~/store'
+import {simplifyString, splitString} from '~/string'
 import {msg} from '~/translate'
 import {uuid} from '~/uuid'
 import {withActivatable} from '~/widget/activation/activatable'
 import {CrudItem} from '~/widget/crudItem'
+import {FastList} from '~/widget/fastList'
 import {Layout} from '~/widget/layout'
 import {ListItem} from '~/widget/listItem'
 import {NoData} from '~/widget/noData'
 import {Notifications} from '~/widget/notifications'
 import {Panel} from '~/widget/panel/panel'
+import {SearchBox} from '~/widget/searchBox'
 import {Tag} from '~/widget/tag'
 
 import {Project} from './project'
@@ -28,14 +33,26 @@ const mapStateToProps = () => ({
     recipes: select('process.recipes'),
 })
 
+const getHighlightMatcher = memoizeOne(
+    filterValues => filterValues.length
+        ? new RegExp(`(?:${filterValues.join('|')})`, 'i')
+        : ''
+)
+
 class _Projects extends React.Component {
     state = {
-        editProject: null
+        editProject: null,
+        textFilterValues: []
     }
 
     constructor(props) {
         super(props)
         this.close = this.close.bind(this)
+        this.setTextFilter = this.setTextFilter.bind(this)
+        this.renderProject = this.renderProject.bind(this)
+        this.selectProject = this.selectProject.bind(this)
+        this.editProject = this.editProject.bind(this)
+        this.updateProject = this.updateProject.bind(this)
     }
 
     newProject() {
@@ -77,7 +94,7 @@ class _Projects extends React.Component {
         })
     }
 
-    selectProject(projectId) {
+    selectProject({id: projectId}) {
         projectId
             ? actionBuilder('SELECT_PROJECT', {projectId})
                 .set('process.projectId', projectId)
@@ -88,13 +105,29 @@ class _Projects extends React.Component {
         this.close()
     }
 
+    renderSearch() {
+        return (
+            <SearchBox
+                placeholder={msg('process.projects.search.placeholder')}
+                debounce={0}
+                onSearchValue={this.setTextFilter}
+            />
+        )
+    }
+
     renderProjects() {
-        const {projects} = this.props
-        if (projects.length) {
+        const filteredProjects = this.getFilteredProjects()
+        if (filteredProjects.length) {
+            const itemKey = project => `${project.id}|${this.getHighlightMatcher()}`
             return (
-                <Layout type='vertical' spacing='tight'>
-                    {projects.map((project, index) => this.renderProject(project, index))}
-                </Layout>
+                <FastList
+                    items={filteredProjects}
+                    itemKey={itemKey}
+                    itemRenderer={this.renderProject}
+                    spacing='tight'
+                    overflow={50}
+                    onEnter={this.selectProject}
+                />
             )
         } else {
             return (
@@ -109,24 +142,23 @@ class _Projects extends React.Component {
         )
     }
 
-    renderProject(project, index) {
+    renderProject(project, hovered) {
         const {projectId} = this.props
         const projectRecipes = this.getProjectRecipes(project)
         return (
             <ListItem
-                key={index}
-                onClick={() => this.selectProject(project.id)}>
+                key={project.id}
+                hovered={hovered}
+                onClick={() => this.selectProject(project)}>
                 <CrudItem
                     title={project.name}
                     description={msg('process.project.description', {count: projectRecipes.length})}
+                    highlight={this.getHighlightMatcher()}
                     editTooltip={msg('process.project.edit.tooltip')}
                     removeTooltip={msg('process.project.remove.tooltip')}
-                    removeTitle={msg('process.project.remove.title')}
-                    removeMessage={msg('process.project.remove.confirm')}
-                    removeContent={
-                        <RecipeListConfirm recipes={projectRecipes}/>
-                    }
-                    unsafeRemove={!projectRecipes.length}
+                    removeTitle={projectRecipes.length ? msg('process.project.remove.title') : null}
+                    removeMessage={projectRecipes.length ? msg('process.project.remove.confirm') : null}
+                    removeContent={projectRecipes.length ? <RecipeListConfirm recipes={projectRecipes}/> : null}
                     inlineComponents={projectId === project.id && this.renderSelected()}
                     onEdit={() => this.editProject(project)}
                     onRemove={() => this.removeProject(project)}
@@ -136,6 +168,7 @@ class _Projects extends React.Component {
     }
 
     renderProjectsPanel() {
+        const {textFilterValues} = this.state
         const add = () => this.newProject()
         return (
             <Panel
@@ -146,13 +179,16 @@ class _Projects extends React.Component {
                     icon='folder-tree'
                     title={msg('process.projects.title')}
                 />
-                <Panel.Content>
-                    {this.renderProjects()}
+                <Panel.Content scrollable={false}>
+                    <Layout type='vertical' spacing='tight'>
+                        {this.renderSearch()}
+                        {this.renderProjects()}
+                    </Layout>
                 </Panel.Content>
                 <Panel.Buttons>
                     <Panel.Buttons.Main>
                         <Panel.Buttons.Close
-                            keybinding={['Escape', 'Enter']}
+                            keybinding={textFilterValues.length ? null : ['Enter', 'Escape']}
                             disabled={this.isBusy()}
                             onClick={this.close}
                         />
@@ -180,8 +216,8 @@ class _Projects extends React.Component {
             <Project
                 project={project}
                 projectNames={projectNames}
-                onApply={project => this.updateProject(project)}
-                onCancel={() => this.editProject()}
+                onApply={this.updateProject}
+                onCancel={this.editProject}
             />
         )
     }
@@ -201,6 +237,29 @@ class _Projects extends React.Component {
     getProjectRecipes(project) {
         const {recipes} = this.props
         return recipes.filter(({projectId}) => projectId === project.id)
+    }
+
+    setTextFilter(textFilterValue) {
+        const textFilterValues = splitString(simplifyString(textFilterValue))
+        this.setState({textFilterValues})
+    }
+
+    getFilteredProjects() {
+        const {projects} = this.props
+        const {textFilterValues} = this.state
+        const searchMatchers = textFilterValues.map(filter => RegExp(filter, 'i'))
+        return searchMatchers.length
+            ? projects.filter(project =>
+                _.every(searchMatchers, matcher =>
+                    matcher.test(simplifyString(project.name))
+                )
+            )
+            : projects
+    }
+
+    getHighlightMatcher() {
+        const {textFilterValues} = this.state
+        return getHighlightMatcher(textFilterValues)
     }
 
     close() {
