54881c0fbb51dc0939ea440bbac45928e361ce0f
Luca Paolini <luca.paolini@fao.org>
2026-04-28 12:42:24 +0000

    Added GitHub GFM support in markdown.

--- modules/gui/src/widget/markdown.jsx
+++ modules/gui/src/widget/markdown.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 import styles from './markdown.module.css'
 
 export const Markdown = ({source}) =>
     <div className={styles.markdown}>
-        <ReactMarkdown>{source}</ReactMarkdown>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{source}</ReactMarkdown>
     </div>
 
 Markdown.propTypes = {
