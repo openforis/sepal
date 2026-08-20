52c9b1b9a118f02c2e343dba186d213479d748c4
Daniel Wiell <daniel.wiell@fao.org>
2026-08-20 10:48:09 +0000

    Fixed Orfeo ToolBox extraction on overlay filesystems.

--- modules/geospatial-toolkit/script/init_orfeo.sh
+++ modules/geospatial-toolkit/script/init_orfeo.sh
@@ -9,5 +9,5 @@ echo "********************************"
 otb=OTB-9.1.1-Linux.tar.gz
 curl https://www.orfeo-toolbox.org/packages/archives/OTB/$otb -o /tmp/$otb
 mkdir -p /usr/local/lib/orfeo
-tar xf /tmp/$otb --one-top-level=/usr/local/lib/orfeo
+tar xf /tmp/$otb -C /usr/local/lib/orfeo
 (cd /usr/local/lib/orfeo; source otbenv.profile; sh recompile_bindings.sh)
