a76c54cf97b95dfdb4bfe525166d51544c0fb054
Luca Paolini <luca.paolini@fao.org>
2026-09-25 14:21:15 +0000

    refactor(sandbox): drop obsolete user-init waits from server start scripts

--- modules/sandbox/script/start-jupyter.sh
+++ modules/sandbox/script/start-jupyter.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
 sandbox_user=$1
-sandbox_path=$2
-for i in {30..0}; do
-    if [ $(getent passwd $sandbox_user | wc -l) -eq 1 ]; then
-        break
-    fi
-    echo "Waiting for user $sandbox_user to be initialized..."
-    /bin/sleep 1
-done
-if [ "$i" = 0 ]; then
-    echo >&2 "User $sandbox_user not initialized"
-    exit 1
-else
-    echo "User $sandbox_user initialized"
-fi
 
 function exportEnvironment {
     while read line; do
--- modules/sandbox/script/start-shiny-server.sh
+++ modules/sandbox/script/start-shiny-server.sh
@@ -2,19 +2,6 @@
 
 sandbox_user=$1
 sandbox_path=$2
-for i in {30..0}; do
-    if [ $(getent passwd $sandbox_user | wc -l) -eq 1 ]; then
-        break
-    fi
-    echo "Waiting for user $sandbox_user to be initialized..."
-    /bin/sleep 1
-done
-if [ "$i" = 0 ]; then
-    echo >&2 "User $sandbox_user not initialized"
-    exit 1
-else
-    echo "User $sandbox_user initialized"
-fi
 
 sandbox_user_id=`stat -c '%u' /home/$sandbox_user`
 home_group_id=`stat -c '%g' /home/$sandbox_user`
